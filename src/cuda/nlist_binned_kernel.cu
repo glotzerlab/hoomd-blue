28eeacfef23861d87b6708104daa25fd84bcfca5
Joshua A. Anderson <joaander@umich.edu>
2008-04-01 21:46:51 +0000

    Playing around with the "launch timeout" instability in the CUDA neighborlist code. The version being committed *seems* to be stable. Time will tell
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@931 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/cuda/nlist_binned_kernel.cu
+++ src/cuda/nlist_binned_kernel.cu
@@ -65,9 +65,9 @@ THE POSSIBILITY OF SUCH DAMAGE.
 // the shared data will hold the particle positions of all the particles in the neighboring bins as well as the 
 // particle index of the particle (stored as an int to be accessed with __float_as_int)
 // px,py,pz will have to be checked vs all of these
-extern __shared__ float bin_sdata[];
+//extern __shared__ float bin_sdata[];
 
-__device__ int wrap_bin_i(int i, int M)
+/*__device__ int wrap_bin_i(int i, int M)
 	{
 	if (i < 0)
 		i += M;
@@ -85,14 +85,9 @@ __device__ int handle_bin(int a, int b, int c, int my_pidx, float px, float py, 
 	
 	// read in the neighbor position
 	float4 neigh_pos = make_float4(0.0f,0.0f,0.0f,0.0f);
-	if (neigh_pidx != EMPTY_BIN)
-		{
-		neigh_pos = tex1Dfetch(pdata_pos_tex, neigh_pidx);
-		//neigh_pos = pdata.pos[neigh_pidx];
-		}
+	neigh_pos = tex1Dfetch(pdata_pos_tex, neigh_pidx);
 	
 	// put it into shared memory
-	
 	__syncthreads();
 	
 	bin_sdata[threadIdx.x] = neigh_pos.x;
@@ -103,151 +98,35 @@ __device__ int handle_bin(int a, int b, int c, int my_pidx, float px, float py, 
 	__syncthreads();
 	
 	if (my_pidx != EMPTY_BIN)
+	{
+	// now, we are set to loop through the array
+	for (int cur_offset = 0; cur_offset < blockDim.x; cur_offset++)
 		{
-		// now, we are set to loop through the array
-		for (int cur_offset = 0; cur_offset < blockDim.x; cur_offset++)
-			{
-			int cur_neigh = __float_as_int(bin_sdata[cur_offset + 3*blockDim.x]);
-			if (cur_neigh == EMPTY_BIN)
-				return n_neigh;
+		int cur_neigh = __float_as_int(bin_sdata[cur_offset + 3*blockDim.x]);
+		if (cur_neigh == EMPTY_BIN)
+			return n_neigh;
 
-			float dx = px - bin_sdata[cur_offset];
-			float dy = py - bin_sdata[cur_offset + blockDim.x];
-			float dz = pz - bin_sdata[cur_offset + 2*blockDim.x];
-			
-			dx = dx - box.Lx * rintf(dx * box.Lxinv);
-			dy = dy - box.Ly * rintf(dy * box.Lyinv);
-			dz = dz - box.Lz * rintf(dz * box.Lzinv);
-	
-			float dr = dx*dx + dy*dy + dz*dz;
+		float dx = px - bin_sdata[cur_offset];
+		float dy = py - bin_sdata[cur_offset + blockDim.x];
+		float dz = pz - bin_sdata[cur_offset + 2*blockDim.x];
 		
-			int not_excluded = (exclude.x != cur_neigh) & (exclude.y != cur_neigh) & (exclude.z != cur_neigh) & (exclude.w != cur_neigh);
-			if (dr < r_maxsq && (my_pidx != cur_neigh) && not_excluded)
-				{
-				nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh;
-				n_neigh++;
-				}
+		dx = dx - box.Lx * rintf(dx * box.Lxinv);
+		dy = dy - box.Ly * rintf(dy * box.Lyinv);
+		dz = dz - box.Lz * rintf(dz * box.Lzinv);
+
+		float dr = dx*dx + dy*dy + dz*dz;
+	
+		int not_excluded = (exclude.x != cur_neigh) & (exclude.y != cur_neigh) & (exclude.z != cur_neigh) & (exclude.w != cur_neigh);
+		if (dr < r_maxsq && (my_pidx != cur_neigh) && not_excluded)
+			{
+			nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh;
+			n_neigh++;
 			}
 		}
+	}
 	
 	return n_neigh;
 	}
-
-/*__device__ int handle_bin(int a, int b, int c, int my_pidx, float px, float py, float pz, int n_neigh, gpu_pdata_arrays pdata, gpu_bin_array bins, gpu_nlist_array nlist, float r_maxsq, gpu_boxsize box, uint4 exclude)
-	{
-	// now: we finally know the current bin to compare to
-	int neigh_bin = a*bins.Mz*bins.My + b*bins.Mz + c;
-
-	int neigh_pidx = bins.idxlist[neigh_bin*bins.Nmax + threadIdx.x];
-	
-	__syncthreads();
-	// read in the neighbor position
-	float4 neigh_pos = tex1Dfetch(pdata_pos_tex, neigh_pidx);
-	// put it into shared memory
-	
-	bin_sdata[threadIdx.x] = neigh_pos.x;
-	bin_sdata[threadIdx.x + blockDim.x] = neigh_pos.y;
-	bin_sdata[threadIdx.x + 2*blockDim.x] = neigh_pos.z;
-	bin_sdata[threadIdx.x + 3*blockDim.x] = __int_as_float(neigh_pidx);
-	
-	__syncthreads();
-	
-	if (my_pidx != EMPTY_BIN)
-		{
-		// now, we are set to loop through the array
-		for (int cur_offset = 0; cur_offset < blockDim.x; cur_offset+=4)
-			{
-			int cur_neigh = __float_as_int(bin_sdata[cur_offset + 3*blockDim.x]);
-			if (cur_neigh == EMPTY_BIN)
-				return n_neigh;
-
-			float dx = px - bin_sdata[cur_offset];
-			dx = dx - box.Lx * rintf(dx * box.Lxinv);
-		
-			float dy = py - bin_sdata[cur_offset + blockDim.x];
-			dy = dy - box.Ly * rintf(dy * box.Lyinv);
-
-			float dz = pz - bin_sdata[cur_offset + 2*blockDim.x];
-			dz = dz - box.Lz * rintf(dz * box.Lzinv);
-	
-			float dr = dx*dx + dy*dy + dz*dz;
-		
-			int not_excluded = (exclude.x != cur_neigh) & (exclude.y != cur_neigh) & (exclude.z != cur_neigh) & (exclude.w != cur_neigh);
-			if (dr < r_maxsq && (my_pidx != cur_neigh) && not_excluded)
-				{
-				nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh;
-				n_neigh++;
-				}
-			
-			int cur_neigh2 = __float_as_int(bin_sdata[cur_offset+1 + 3*blockDim.x]);
-			if (cur_neigh2 == EMPTY_BIN)
-				return n_neigh;
-
-			float dx2 = px - bin_sdata[cur_offset+1];
-			dx2 = dx2 - box.Lx * rintf(dx2 * box.Lxinv);
-		
-			float dy2 = py - bin_sdata[cur_offset + 1 + blockDim.x];
-			dy2 = dy2 - box.Ly * rintf(dy2 * box.Lyinv);
-
-			float dz2 = pz - bin_sdata[cur_offset + 1 + 2*blockDim.x];
-			dz2 = dz2 - box.Lz * rintf(dz2 * box.Lzinv);
-	
-			float dr2 = dx2*dx2 + dy2*dy2 + dz2*dz2;
-		
-			int not_excluded2 = (exclude.x != cur_neigh2) & (exclude.y != cur_neigh2) & (exclude.z != cur_neigh2) & (exclude.w != cur_neigh2);
-			if (dr2 < r_maxsq && (my_pidx != cur_neigh2) && not_excluded2)
-				{
-				nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh2;
-				n_neigh++;
-				}
-			
-			int cur_neigh3 = __float_as_int(bin_sdata[cur_offset+ 2 + 3*blockDim.x]);
-			if (cur_neigh3 == EMPTY_BIN)
-				return n_neigh;
-
-			float dx3 = px - bin_sdata[cur_offset+2];
-			dx3 = dx3 - box.Lx * rintf(dx3 * box.Lxinv);
-		
-			float dy3 = py - bin_sdata[cur_offset + 2 + blockDim.x];
-			dy3 = dy3 - box.Ly * rintf(dy3 * box.Lyinv);
-
-			float dz3 = pz - bin_sdata[cur_offset + 2 + 2*blockDim.x];
-			dz3 = dz3 - box.Lz * rintf(dz3 * box.Lzinv);
-	
-			float dr3 = dx3*dx3 + dy3*dy3 + dz3*dz3;
-		
-			int not_excluded3 = (exclude.x != cur_neigh3) & (exclude.y != cur_neigh3) & (exclude.z != cur_neigh3) & (exclude.w != cur_neigh3);
-			if (dr3 < r_maxsq && (my_pidx != cur_neigh3) && not_excluded3)
-				{
-				nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh3;
-				n_neigh++;
-				}
-			
-			int cur_neigh4 = __float_as_int(bin_sdata[cur_offset+3 + 3*blockDim.x]);
-			if (cur_neigh4 == EMPTY_BIN)
-				return n_neigh;
-
-			float dx4 = px - bin_sdata[cur_offset+3];
-			dx4 = dx4 - box.Lx * rintf(dx4 * box.Lxinv);
-		
-			float dy4 = py - bin_sdata[cur_offset + 3 + blockDim.x];
-			dy4 = dy4 - box.Ly * rintf(dy4 * box.Lyinv);
-
-			float dz4 = pz - bin_sdata[cur_offset + 3 + 2*blockDim.x];
-			dz4 = dz4 - box.Lz * rintf(dz4 * box.Lzinv);
-	
-			float dr4 = dx4*dx4 + dy4*dy4 + dz4*dz4;
-		
-			int not_excluded4 = (exclude.x != cur_neigh4) & (exclude.y != cur_neigh4) & (exclude.z != cur_neigh4) & (exclude.w != cur_neigh4);
-			if (dr4 < r_maxsq && (my_pidx != cur_neigh4) && not_excluded4)
-				{
-				nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh4;
-				n_neigh++;
-				}
-			}
-		}
-	return n_neigh;
-	}*/
 
 // unrolled version (non full list)
 extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array bins, gpu_nlist_array nlist, float r_maxsq, gpu_boxsize box)
@@ -277,14 +156,14 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 
 	float4 my_pos = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
 	uint4 exclude = make_uint4(EMPTY_BIN, EMPTY_BIN, EMPTY_BIN, EMPTY_BIN);
-	if (my_pidx != EMPTY_BIN)
-		{
+	//if (my_pidx != EMPTY_BIN)
+		//{
 		my_pos = tex1Dfetch(pdata_pos_tex, my_pidx);
 		//my_pos = pdata.pos[my_pidx];
 	
 		exclude = tex1Dfetch(nlist_exclude_tex, my_pidx);
 		//exclude = nlist.exclusions[my_pidx];
-		}
+		//}
 
 	float px = my_pos.x;
 	float py = my_pos.y;
@@ -572,11 +451,11 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 		nlist.list[my_pidx] = n_neigh;
 		nlist.last_updated_pos[my_pidx] = pdata.pos[my_pidx];
 		}
-	}
+	}*/
 
 
 // non-unrolled version 
-/*__global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array bins, gpu_nlist_array nlist, float r_maxsq, gpu_boxsize box)
+extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array bins, gpu_nlist_array nlist, float r_maxsq, gpu_boxsize box)
 	{
 	// each block is going to compute the neighborlist for all of the particles in blockIdx.x
 	int my_bin = blockIdx.x;
@@ -594,11 +473,12 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 	// pull in the particle of this thread that will be compared to everything else
 	int my_pidx = bins.idxlist[my_bin*bins.Nmax + threadIdx.x];
 	
-	//float4 my_pos = tex2D(pdata_pos_tex, my_pidx & TEX_MASK, my_pidx >> LOG2_TEX_WIDTH);
 	float4 my_pos = tex1Dfetch(pdata_pos_tex, my_pidx);
 	float px = my_pos.x;
 	float py = my_pos.y;
 	float pz = my_pos.z;
+	
+	uint4 exclude = tex1Dfetch(nlist_exclude_tex, my_pidx);
 
 	// the shared data will hold the particle positions of all the particles in the neighboring bins as well as the 
 	// particle index of the particle (stored as an int to be accessed with __float_as_int)
@@ -638,7 +518,6 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 				int neigh_pidx = bins.idxlist[neigh_bin*bins.Nmax + threadIdx.x];
 				
 				// read in the neighbor position
-				//float4 neigh_pos = tex2D(pdata_pos_tex, neigh_pidx & TEX_MASK, neigh_pidx >> LOG2_TEX_WIDTH);
 				float4 neigh_pos = tex1Dfetch(pdata_pos_tex, neigh_pidx);
 				// put it into shared memory
 				
@@ -651,9 +530,15 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 			
 				__syncthreads();
 
+				if (my_pidx != EMPTY_BIN)
+					{
 				// now, we are set to loop through the array
 				for (int cur_offset = 0; cur_offset < blockDim.x; cur_offset++)
 					{
+					int cur_neigh = __float_as_int(bin_sdata[cur_offset + 3*blockDim.x]);
+					if (cur_neigh == EMPTY_BIN)
+						break;
+					
 					float dx = px - bin_sdata[cur_offset];
 					dx = dx - box.Lx * rintf(dx * box.Lxinv);
             
@@ -663,23 +548,27 @@ extern "C" __global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array 
 					float dz = pz - bin_sdata[cur_offset + 2*blockDim.x];
 					dz = dz - box.Lz * rintf(dz * box.Lzinv);
 
-					int cur_neigh = __float_as_int(bin_sdata[cur_offset + 3*blockDim.x]);
-					
 					float dr = dx*dx + dy*dy + dz*dz;
 					
-					if ((my_pidx != EMPTY_BIN) && dr < r_maxsq && (cur_neigh != EMPTY_BIN) && (my_pidx != cur_neigh))
+					int not_excluded = (exclude.x != cur_neigh) & (exclude.y != cur_neigh) & (exclude.z != cur_neigh) & (exclude.w != cur_neigh);
+					if (dr < r_maxsq && (my_pidx != cur_neigh) && not_excluded)
 						{
 						nlist.list[my_pidx + (1 + n_neigh)*nlist.pitch] = cur_neigh;
 						n_neigh++;
 						}
 					}
+					}
 				}
 			}
 		}
-	// now, at the very end, we need to write out the number of particles in each list
-	nlist.list[my_pidx] = n_neigh;
-	nlist.last_updated_pos[my_pidx] = pdata.pos[my_pidx];
-	}*/
+		
+	if (my_pidx != EMPTY_BIN)
+		{
+		// now, at the very end, we need to write out the number of particles in each list
+		nlist.list[my_pidx] = n_neigh;
+		nlist.last_updated_pos[my_pidx] = pdata.pos[my_pidx];
+		}
+	}
 
 // non-unrolled version (with idx full) 
 /*__global__ void updateFromBins(gpu_pdata_arrays pdata, gpu_bin_array bins, gpu_nlist_array nlist, float r_maxsq, gpu_boxsize box)
