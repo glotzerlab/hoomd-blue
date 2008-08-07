790d91bb91de4763d902176b9a119dbd17767623
Joshua A. Anderson <joaander@umich.edu>
2008-08-07 17:00:39 +0000

    Adding validation stress tests to run on teslahoomd. ticket #106
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@1138 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/scripts/validation_testing/lj_nvt/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is a SGE job script for running on teslahoomd.physics.iastate.edu
+
+#$ -N nrg_longrun
+#$ -l gpu=1
+#$ -j y
+#$ -cwd
+#$ -S /bin/bash
+
+# determine which GPU to run on
+GPU=`/home/joaander/gputop/gputop.py --reserve`
+# check if no free GPU was found
+if [ "$?" = 1 ]; then
+	echo "Error finding free GPU"
+	echo $GPU
+	exit 100
+fi
+
+echo "Running hoomd on gpu $GPU"
+export PATH=$PATH:/home/joaander/hoomd/bin_rel/python
+hoomd nvt.hoomd --mode=gpu --gpu=$GPU
