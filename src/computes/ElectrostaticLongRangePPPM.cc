ec8318ccd41dcfe7fba341b63a800fbe43c64ccc
Alex Travesset <trvsst@ameslab.gov>
2008-09-25 21:03:08 +0000

    This is work in progress, it compiles but a lot of work remains. ticket #79
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@1272 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/computes/ElectrostaticLongRangePPPM.cc
@@ -0,0 +1,211 @@
+/*
+Highly Optimized Object-Oriented Molecular Dynamics (HOOMD) Open
+Source Software License
+Copyright (c) 2008 Ames Laboratory Iowa State University
+All rights reserved.
+
+Redistribution and use of HOOMD, in source and binary forms, with or
+without modification, are permitted, provided that the following
+conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names HOOMD's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND
+CONTRIBUTORS ``AS IS''  AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS  BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4244 )
+#endif
+
+#ifdef WIN32
+#pragma warning( push)
+#pragma warning( disable : 4018)
+#endif
+
+
+// conditionally compile only if a fast fourier transform is defined
+#ifdef USE_FFT
+
+#include <iostream>
+using namespace std;
+
+#include "ElectrostaticLongRangePPPM.h"
+#include <stdexcept>
+
+
+/*! \file ElectrostaticLongRangePPPM.cc
+	\brief Contains the code for the ElectrostaticLongRangePPPM class
+*/
+
+using namespace std;
+
+/*!     \param pdata Particle Data to compute forces on
+	\param alpha Split parameter of short vs long range electrostatics (see header file for more info) 
+*/
+ElectrostaticLongRangePPPM::ElectrostaticLongRangePPPM(boost::shared_ptr<ParticleData> pdata, unsigned int Mmesh_x,unsigned int Mmesh_y,unsigned int Mmesh_z,unsigned int P_order_a, Scalar alpha,bool third_law_m)
+	:ForceCompute(pdata),N_mesh_x(Mmesh_x),N_mesh_y(Mmesh_y),N_mesh_z(Mmesh_z),P_order(P_order_a),m_alpha(alpha),third_law(third_law_m)
+	{
+	assert(m_pdata);
+	
+	if (alpha < 0.0)
+		{
+		cerr << endl << "***Error! Negative alpha in ElectrostaticShortRange makes no sense" << endl << endl;
+		throw runtime_error("Error initializing ElectrostaticShortRange");
+		}
+
+	//Cast mesh sizes as a scalar
+	S_mesh_x=static_cast<Scalar>(N_mesh_x);
+	S_mesh_y=static_cast<Scalar>(N_mesh_y);
+	S_mesh_z=static_cast<Scalar>(N_mesh_z);
+
+        // get a local copy of the simulation box too
+	const BoxDim& box = m_pdata->getBox();
+	// sanity check
+	assert(box.xhi > box.xlo && box.yhi > box.ylo && box.zhi > box.zlo);	
+	
+	// precalculate box lenghts for use in the periodic imaging
+	Lx = box.xhi - box.xlo;
+	Ly = box.yhi - box.ylo;
+	Lz = box.zhi - box.zlo;
+	
+	//Compute lattice spacings along the different directions
+	h_x=S_mesh_x/Lx;
+	h_y=S_mesh_y/Ly;
+	h_z=S_mesh_z/Lz;
+	
+	//allocate space for the density and the influence function
+	
+	rho_real=new Scalar**[N_mesh_z];
+	rho_kspace=new Scalar**[N_mesh_z];
+	G_Inf=new Scalar**[N_mesh_z];
+
+	for(int i=0;i<N_mesh_z;i++){
+		rho_real[i]=new Scalar*[N_mesh_y];
+		rho_kspace[i]=new Scalar*[N_mesh_y];
+		G_Inf[i]=new Scalar*[N_mesh_y];
+	}
+
+	for(int j=0;j<N_mesh_z;j++){
+	for(int i=0;i<N_mesh_y;i++){
+		rho_real[i][j]=new Scalar[N_mesh_x];
+		rho_kspace[i][j]=new Scalar[N_mesh_x];
+		G_Inf[i][j]=new Scalar[N_mesh_x];
+	}
+	}
+
+
+
+	}
+
+ElectrostaticLongRangePPPM::~ElectrostaticLongRangePPPM()
+	{
+	
+	for(int j=0;j<N_mesh_z;j++){
+	for(int i=0;i<N_mesh_y;i++){
+		delete[] rho_real[i][j];
+		delete[] rho_kspace[i][j];
+		delete[] G_Inf[i][j];
+	}
+	}
+
+	for(int i=0;i<N_mesh_z;i++){
+		delete[] rho_real[i];
+		delete[] rho_kspace[i];
+		delete[] G_Inf[i];
+	}
+		
+	delete[] rho_real;
+	delete[] rho_kspace;
+	delete[] G_Inf;
+	}
+
+void ElectrostaticLongRangePPPM::computeForces(unsigned int timestep)
+	{
+	// start the profile
+	if (m_prof) m_prof->push("ElecLongRange");
+	
+	// access the particle data
+	const ParticleDataArraysConst& arrays = m_pdata->acquireReadOnly(); 
+	// sanity check
+	assert(arrays.x != NULL && arrays.y != NULL && arrays.z != NULL);
+	
+	// tally up the number of forces calculated
+	int64_t n_calc = 0;
+	
+	// need to start from a zero force, potential energy and virial
+	// (MEM TRANSFER 5*N Scalars)
+	memset(m_fx, 0, sizeof(Scalar)*arrays.nparticles);
+	memset(m_fy, 0, sizeof(Scalar)*arrays.nparticles);
+	memset(m_fz, 0, sizeof(Scalar)*arrays.nparticles);
+	memset(m_pe, 0, sizeof(Scalar)*arrays.nparticles);
+	memset(m_virial, 0, sizeof(Scalar)*arrays.nparticles);
+
+	// for each particle
+	for (unsigned int i = 0; i < arrays.nparticles; i++)
+		{
+		// access the particle's position and charge (MEM TRANSFER: 4 Scalars)
+		Scalar xi = arrays.x[i];
+		Scalar yi = arrays.y[i];
+		Scalar zi = arrays.z[i];
+
+		Scalar q_i=arrays.charge[i];
+		
+		// zero force, potential energy, and virial for the current particle
+		Scalar fxi = 0.0;
+		Scalar fyi = 0.0;
+		Scalar fzi = 0.0;
+		Scalar pei = 0.0;
+		Scalar viriali = 0.0;
+		
+		
+		// (FLOPS: 5 / MEM TRANSFER: 10 Scalars)
+		m_fx[i] += fxi;
+		m_fy[i] += fyi;
+		m_fz[i] += fzi;
+		m_pe[i] += pei;
+		m_virial[i] += viriali;
+		}
+		
+	m_pdata->release();
+	
+	#ifdef USE_CUDA
+	// the force data is now only up to date on the cpu
+	m_data_location = cpu;
+	#endif
+	
+	int64_t flops = n_calc * (3+9+5+18+6+8+8);
+	if (third_law)	flops += n_calc*8;
+	int64_t mem_transfer = m_pdata->getN() * (5 + 4 + 10)*sizeof(Scalar) + n_calc * ( (3+6)*sizeof(Scalar) + (1)*sizeof(unsigned int));
+	if (third_law) mem_transfer += n_calc*10*sizeof(Scalar);
+	if (m_prof) m_prof->pop(flops, mem_transfer);
+	}
+
+#endif
+
+#ifdef WIN32
+#pragma warning( pop )
+#endif
---
+++ src/computes/ElectrostaticLongRangePPPM.h
@@ -0,0 +1,113 @@
+/*
+Highly Optimized Object-Oriented Molecular Dynamics (HOOMD) Open
+Source Software License
+Copyright (c) 2008 Ames Laboratory Iowa State University
+All rights reserved.
+
+Redistribution and use of HOOMD, in source and binary forms, with or
+without modification, are permitted, provided that the following
+conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names HOOMD's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND
+CONTRIBUTORS ``AS IS''  AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS  BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// conditionally compile only if fast fourier transforms are defined
+
+#ifdef USE_FFT
+
+#include <boost/shared_ptr.hpp>
+
+#include "ForceCompute.h"
+
+/*! \file ElectrostaticLongRangePPPM.h
+	\brief Declares a class for computing the short range part of the electrostatic force and energy
+*/
+
+#ifndef __ELECTROSTATICLONGRANGEPPPM_H__
+#define __ELECTROSTATICLONGRANGEPPPM_H__
+
+//! Computes the Long-range electrostatic part of the force by using the PPPM approximation
+/*! In order for this class to be useful it needs to be complemented with a class that computes the short-range electrostatic part
+    The total pair force is summed for each particle when compute() is called.
+	This class requires the use of Fast Fourier Transforms, which are defined somewhere else
+    
+	Usage: Construct a ElectrostaticLongRangePPM class, providing it an already constructed ParticleData.
+	The parameter alpha splits the short range and the long range electrostatic part.
+    
+	Details on how the parameter alpha is defined are found in
+	"How to mesh up Ewald sums.I. A theoretical and numerical comparison of various particle mesh routines"
+	M. Deserno and C. Holm
+	J. Chem. Phys. 109, 7678 (1998).
+
+	NOTE: This class does not compute the parameter alpha, it uses the alpha as specified. 
+	
+	Forces can be computed directly by calling compute() and then retrieved with a call to acquire(), but 
+	a more typical usage will be to add the force compute to NVEUpdator or NVTUpdator. 
+	
+	This base class defines the interface for performing the long-range part of the electrostatic force 
+	computations using the PPPM method. It does provide a functional, single threaded method for computing the forces. 
+
+*/
+class ElectrostaticLongRangePPPM : public ForceCompute
+	{
+	public:
+		//! Constructs the compute
+		ElectrostaticLongRangePPPM(boost::shared_ptr<ParticleData> pdata,unsigned int Mmesh_x,unsigned int Mmesh_y,unsigned int Mmesh_z, unsigned int P_order_a, Scalar alpha,bool third_law_m);
+		
+		//! Destructor
+		virtual ~ElectrostaticLongRangePPPM();
+
+
+	protected:
+	    Scalar m_alpha;              //!< split parameter of the short-range vs long-range forces.
+	    unsigned int N_mesh_x;       //!< number of points in the mesh along the x axis
+	    unsigned int N_mesh_y;       //!< number of points in the mesh along the y axis
+	    unsigned int N_mesh_z;       //!< number of points in the mesh along the z axis
+	    unsigned int P_order;        //!< The charge assignment on the mesh is of order P
+	    bool third_law;              //!< Whether to use third law or not
+		Scalar S_mesh_x;             //!< number of points in the mesh along the x axis as a scalar
+	    Scalar S_mesh_y;             //!< number of points in the mesh along the y axis as a scalar
+	    Scalar S_mesh_z;             //!< number of points in the mesh along the z axis as a scalar
+	    Scalar Lx;                   //!< Length size of the system along the x direction
+	    Scalar Ly;                   //!< Length size of the system along the y direction
+	    Scalar Lz;                   //!< Length size of the system along the z direction
+	    Scalar h_x;                  //!< Spacing along the x axis
+	    Scalar h_y;                  //!< Spacing along the y axis
+	    Scalar h_z;                  //!< Spacing along the z axis
+	    Scalar ***rho_real;          //!< density of charge on the mesh
+	    Scalar ***rho_kspace;        //!< density of charge in fourier space
+	    Scalar ***G_Inf;             //!< Precomputed proximity function
+
+       	virtual void computeForces(unsigned int timestep); //! Actually compute the forces
+	};
+	
+
+#endif
+#endif
+
+
