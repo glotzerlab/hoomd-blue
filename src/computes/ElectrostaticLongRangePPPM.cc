d93dc569768d27090e3f8fa8efbac6be078c511a
Alex Travesset <trvsst@ameslab.gov>
2008-11-26 23:23:39 +0000

    fixed a few bugs, distribution of points on the grid works ticket #79
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@1503 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/computes/ElectrostaticLongRangePPPM.cc
+++ src/computes/ElectrostaticLongRangePPPM.cc
@@ -363,27 +363,27 @@ void ElectrostaticLongRangePPPM::make_rho_odd(void)
 		Scalar q_i= arrays.charge[i];
 
 		//compute the nearest point on the grid
-		Scalar x_lat = (xi-box.xhi)/h_x; //MAKE SURE THIS NUMBER IS ALWAYS POSITIVE
-		Scalar x_floor = floor(x_lat);
-		unsigned int ix_lat=static_cast<unsigned int>(x_floor);
-		if((x_lat-x_floor)>0.5) {      //boundary conditions are tricky here
-			ix_lat+=1-((ix_lat+1)/N_mesh_x)*N_mesh_x;
+		Scalar dx = (xi-box.xlo)/h_x; 
+		Scalar x_floor = floor(dx);
+		int ix_floor=static_cast<int>(x_floor);
+		if((dx-x_floor)>0.5) {      //boundary conditions are tricky here
+			ix_floor+=1-((ix_floor+1)/N_mesh_x)*N_mesh_x;
 			x_floor+=1.0;  //note that x_floor has now become x_ceil
 		}
 			
-		Scalar y_lat=(yi-box.yhi)/h_y;  //MAKE SURE THIS NUMBER IS ALWAYS POSITIVE
-		Scalar y_floor= floor(y_lat);
-		unsigned int iy_lat=static_cast<unsigned int>(y_floor);
-		if((y_lat-y_floor)>0.5){          //boundary conditions are tricky here
-			iy_lat+=1-((iy_lat+1)/N_mesh_y)*N_mesh_y;
+		Scalar dy=(yi-box.ylo)/h_y; 
+		Scalar y_floor= floor(dy);
+		int iy_floor=static_cast<int>(y_floor);
+		if((dy-y_floor)>0.5){          //boundary conditions are tricky here
+			iy_floor+=1-((iy_floor+1)/N_mesh_y)*N_mesh_y;
 			y_floor+=1.0; //note that y_floor has now become y_ceil
 		}
 		
-        Scalar z_lat=(zi-box.zhi)/h_z;  //MAKE SURE THIS NUMBER IS ALWAYS POSITIVE
-		Scalar z_floor= floor(z_lat);
-		unsigned int iz_lat=static_cast<unsigned int>(z_floor);
-		if((z_lat-z_floor)>0.5){            //boundary conditions are tricky here
-			iz_lat+=1-((iz_lat+1)/N_mesh_z)*N_mesh_z;
+        Scalar dz=(zi-box.zlo)/h_z;  //MAKE SURE THIS NUMBER IS ALWAYS POSITIVE
+		Scalar z_floor= floor(dz);
+		int iz_floor=static_cast<int>(z_floor);
+		if((dz-z_floor)>0.5){            //boundary conditions are tricky here
+			iz_floor+=1-((iz_floor+1)/N_mesh_z)*N_mesh_z;
 			z_floor+=1.0; //note that z_floor has now become z_ceil
 		}
 
@@ -391,22 +391,23 @@ void ElectrostaticLongRangePPPM::make_rho_odd(void)
 		unsigned int ind_y=0;
 		unsigned int ind_z=0;
 
-		Scalar dx=xi-x_floor;
-		Scalar dy=yi-y_floor;
-		Scalar dz=zi-z_floor;
-        
+		dx=dx-x_floor;
+		dy=dy-y_floor;
+		dz=dz-z_floor;
+
 		//take into account boundary conditions
 		for(int lx=-P_half;lx<=P_half;lx++){
-				ind_x=ix_lat+lx+((N_mesh_x-ix_lat-lx)/N_mesh_x)*N_mesh_x-((ix_lat+lx)/N_mesh_x)*N_mesh_x;
-        for(int ly=-P_half;ly<=P_half;ly++){
-				ind_y=iy_lat+ly+((N_mesh_y-iy_lat-ly)/N_mesh_y)*N_mesh_y-((iy_lat+ly)/N_mesh_y)*N_mesh_y;
+				ind_x=ix_floor+lx+((Nu_mesh_x-ix_floor-lx-1)/Nu_mesh_x)*Nu_mesh_x-((ix_floor+lx)/Nu_mesh_x)*Nu_mesh_x;
+		for(int ly=-P_half;ly<=P_half;ly++){
+				ind_y=iy_floor+ly+((Nu_mesh_y-iy_floor-ly-1)/Nu_mesh_y)*Nu_mesh_y-((iy_floor+ly)/Nu_mesh_y)*Nu_mesh_y;
 		for(int lz=-P_half;lz<=P_half;lz++){
-			    ind_z=iz_lat+lz+((N_mesh_z-iz_lat-lz)/N_mesh_z)*N_mesh_z-((iz_lat+lz)/N_mesh_z)*N_mesh_z;		
+				ind_z=iz_floor+lz+((Nu_mesh_z-iz_floor-lz-1)/Nu_mesh_z)*Nu_mesh_z-((iz_floor+lz)/Nu_mesh_z)*Nu_mesh_z;
 				ind=T.D3To1D(ind_x,ind_y,ind_z);
 				(rho_real[ind]).r+=q_i*Poly(lx+P_half,dx)*Poly(ly+P_half,dy)*Poly(lz+P_half,dz);
 	    }
 		}
 		}
+
 	}
 
 }
