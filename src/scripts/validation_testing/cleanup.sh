893c4ebd7c573321a5d6de9fe3b12eb3e4527adf
Joshua A. Anderson <joaander@umich.edu>
2008-12-09 00:12:58 +0000

    Adding initial validation test files. ticket #158
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/hoomd-0.8@1535 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/scripts/validation_testing/cleanup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+rm */dump.mol2
+rm */dump.dcd
+rm */log.txt
+rm */msd.txt
---
+++ src/scripts/validation_testing/pair_lj/job_multi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script is a SGE job script for running on teslahoomd.physics.iastate.edu
+
+#$ -N pair_lj_multi
+#$ -l gpu=2
+#$ -j y
+#$ -cwd
+#$ -S /bin/bash
+
+source ~/.bashrc
+
+# determine which GPU to run on
+GPU1=`/home/joaander/gputop/gputop.py --reserve`
+# check if no free GPU was found
+if [ "$?" = 1 ]; then
+	echo "Error finding free GPU"
+	echo $GPU
+	exit 100
+fi
+GPU2=`/home/joaander/gputop/gputop.py --reserve`
+# check if no free GPU was found
+if [ "$?" = 1 ]; then
+	echo "Error finding free GPU"
+	echo $GPU
+	exit 100
+fi
+
+echo "Running hoomd on gpu $GPU1,$GPU2"
+hoomd run.hoomd --mode=gpu --gpu=$GPU1,$GPU2
