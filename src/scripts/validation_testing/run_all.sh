d3ff082794f618f6dc506dc363d875171b51a313
Joshua A. Anderson <joaander@umich.edu>
2008-12-16 16:15:28 +0000

    Added echo print statements to the run_all script for validation testing. ticket #158
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/hoomd-0.8@1551 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/scripts/validation_testing/run_all.sh
+++ src/scripts/validation_testing/run_all.sh
@@ -1,26 +1,42 @@
 #! /bin/bash
 
 cd pair_lj
+echo "****************"
+echo "Running pair_lj"
 hoomd run.hoomd "$@"
 
 cd ../npt
+echo "****************"
+echo "Running npt"
 hoomd run.hoomd "$@"
 
 cd ../nve
+echo "****************"
+echo "Running nve"
 hoomd run.hoomd "$@"
 
 cd ../bdnvt
+echo "****************"
+echo "Running bdnvt"
 hoomd run.hoomd "$@"
 
 cd ../rescale_temp
+echo "****************"
+echo "Running rescale_temp"
 hoomd run.hoomd "$@"
 
 cd ../bond_fene
+echo "****************"
+echo "Running bond_fene"
 hoomd run.hoomd "$@"
 
 cd ../bond_harmonic
+echo "****************"
+echo "Running bond_harmonic"
 hoomd run.hoomd "$@"
 
 cd ../wall_lj
+echo "****************"
+echo "Running wall_lj"
 hoomd run.hoomd "$@"
 
