695174a7d84bfc0d57053c27106eb0d8fcb93061
Joshua A. Anderson <joaander@umich.edu>
2009-01-28 16:07:44 +0000

    Porting validiation script runner changes from trunk over to branches/hoomd-0.8 (oops).
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/hoomd-0.8@1612 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/scripts/validation_testing/run_all.sh
+++ src/scripts/validation_testing/run_all.sh
@@ -3,39 +3,39 @@
 cd pair_lj
 echo "****************"
 echo "Running pair_lj"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../npt
 echo "****************"
 echo "Running npt"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../nve
 echo "****************"
 echo "Running nve"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../bdnvt
 echo "****************"
 echo "Running bdnvt"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../rescale_temp
 echo "****************"
 echo "Running rescale_temp"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../bond_fene
 echo "****************"
 echo "Running bond_fene"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../bond_harmonic
 echo "****************"
 echo "Running bond_harmonic"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
 
 cd ../wall_lj
 echo "****************"
 echo "Running wall_lj"
-hoomd run.hoomd "$@" 2>&1
+hoomd run.hoomd --gpu_error_checking "$@" 2>&1
