6248f58560f4bbeeba615f07d706f23f16af6b24
Joshua A. Anderson <joaander@umich.edu>
2009-02-22 02:51:13 +0000

    Getting closer to a fully operational ctest -S script
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/hoomd-0.8@1694 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/scripts/ctest_hoomd.cmake
+++ src/scripts/ctest_hoomd.cmake
@@ -6,13 +6,14 @@
 # 1) checkout a copy of hoomd's source to be tested
 # 2) make a bin/ directory somewhere
 # 3) modify the variables in this script to match the desired test parameters
-# 4) set TEST_GROUP to "Experimental" and run ctest -S ctest_hoomd.cmake to check that the test runs
+# 4) set TEST_GROUP to "Experimental" and run ctest -V -S ctest_hoomd.cmake to check that the test runs
 #     the results of the test should show up at: http://my.cdash.org/index.php?project=HOOMD
-# 5) chate TEST_GROUP back to "Nightly" and set ctest command line above to run every day
+#     (you may want to ignore the bdnvt and npt tests for this as they are quite long)
+# 5) chate TEST_GROUP back to "Nightly" and set "ctest -S ctest_hoomd.cmake"  to run every day
 
 # **MODIFY THIS (location where hoomd src and bin directory are)
 SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/hoomd/src")
-SET (CTEST_BINARY_DIRECTORY "$ENV{HOMD}/hoomd/bin_dbg")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/hoomd/bin_ctest")
 
 # **MODIFY THIS (for testing the test)
 SET (TEST_GROUP "Experimental")
@@ -22,8 +23,11 @@ SET (TEST_GROUP "Experimental")
 SET (SITE_NAME "sitename")
 
 # **MODIFY THIS (name of hoomd branch you are testing)
-SET (HOOMD_BRANCH "trunk")
-#SET (HOOMD_BRANCH "hoomd-0.8")
+#SET (HOOMD_BRANCH "trunk")
+SET (HOOMD_BRANCH "hoomd-0.8")
+
+# **MODIFY THIS (name of the system)
+set (SYSTEM_NAME "Linux")
 
 # **MODIFY THIS (a string identifying the compiler: this cannot be autodetected yet)
 SET (COMPILER_NAME "gcc412")
@@ -37,17 +41,22 @@ SET (SINGLE_PRECISION "ON")
 # **MODIFY THIS (set to OFF to enable shared library builds)
 SET (ENABLE_STATIC "ON")
 
+# **MODIFY THIS (set tests to ignore, see the example for the format) (bdnvt and npt take minutes to run, and an enternity with valgrind enabled)
+SET (IGNORE_TESTS "")
+#SET (IGNORE_TESTS "-E \"bdnvt|npt\"")
 
 # other stuff
 SET (CTEST_SVN_COMMAND "svn")
-SET (CTEST_COMMAND "ctest -D ${TEST_GROUP}")
+SET (CTEST_COMMAND "ctest -D ${TEST_GROUP} ${IGNORE_TESTS}")
 SET (CTEST_CMAKE_COMMAND "cmake")
 SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+#### Don't modify anything below unless you really know what you are doing
 
 # lets create a build name to idenfity all these options in a string. It will look like
 # Linux-gcc412-trunk-single-cuda (for a single precision build with cuda)
 # Linux-gcc412-hoomd-0.8-double (for a double precision build without cuda and in the hoomd-0.8 branch)
-SET (BUILDNAME "${CMAKE_SYSTEM_NAME}-${COMPILER_NAME}-${HOOMD_BRANCH}")
+SET (BUILDNAME "${SYSTEM_NAME}-${COMPILER_NAME}-${HOOMD_BRANCH}")
 
 if (ENABLE_STATIC MATCHES "ON")
 	SET (BUILDNAME "${BUILDNAME}-static")
@@ -74,7 +83,3 @@ ENABLE_CUDA:BOOL=${ENABLE_CUDA}
 SINGLE_PRECISION:BOOL=${SINGLE_PRECISION}
 ENABLE_STATIC:BOOL=${ENABLE_STATIC}
 ")
-
-# setup for parallel builds with make
-SET (CTEST_ENVIRONMENT
-    "MAKEFLAGS=\"-j 4\"")
