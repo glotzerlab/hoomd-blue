ddf4ddd4fd9cbf977239605222cc09c793810537
Joshua A. Anderson <joaander@umich.edu>
2009-02-25 16:26:44 +0000

    Adding NVT delta t numerics testing job
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/hoomd-0.8@1718 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/scripts/numerics_testing/conservation/nvt_deltat/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#$ -N nvt_dt
+#$ -l gpu=1
+#$ -j y
+#$ -cwd
+#$ -S /bin/bash
+#$ -t 1-12
+
+source ~/.bashrc
+
+# determine which GPU to run on
+GPU=`/home/joaander/gputop/gputop.py --reserve`
+# check if no free GPU was found
+if [ "$?" = 1 ]; then
+	echo "Error finding free GPU"
+	echo $GPU
+	exit 100
+fi
+
+echo "Running hoomd on gpu $GPU"
+hoomd run.hoomd --mode=gpu --gpu=$GPU
