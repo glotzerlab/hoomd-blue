6ce1a2dcb4acfb647b09594e09bb25a9aed1bfe9
Joshua A. Anderson <joaander@umich.edu>
2009-09-23 18:43:48 +0000

    Initial testing of TablePotential
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/table-potential@2090 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/unit_tests/table_potential.cc
@@ -0,0 +1,115 @@
+/*!
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008, 2009 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+Redistribution and use of HOOMD-blue, in source and binary forms, with or
+without modification, are permitted, provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of HOOMD-blue's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
+ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// $Id$
+// $URL$
+// Maintainer: joaander
+
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4103 4244 )
+#endif
+
+//! Name the unit test module
+#define BOOST_TEST_MODULE TablePotentialTests
+#include "boost_utf_configure.h"
+
+#include <fstream>
+
+#include "TablePotential.h"
+#include "BinnedNeighborList.h"
+
+using namespace std;
+using namespace boost;
+
+/*! \file table_potential_test.cc
+    \brief Implements unit tests for TablePotential and descendants
+    \ingroup unit_tests
+*/
+
+BOOST_AUTO_TEST_CASE(potential_writer)
+	{
+	#ifdef CUDA
+	g_gpu_error_checking = true;
+	#endif
+	
+	// this 2-particle test is just to get a plot of the potential and force vs r cut
+	shared_ptr<SystemDefinition> sysdef_2(new SystemDefinition(2, BoxDim(1000.0), 1, 0, 0, 0, 0, ExecutionConfiguration()));
+    shared_ptr<ParticleData> pdata_2 = sysdef_2->getParticleData();
+    
+	ParticleDataArrays arrays = pdata_2->acquireReadWrite();
+	arrays.x[0] = arrays.y[0] = arrays.z[0] = 0.0;
+	arrays.x[1] = Scalar(0.9); arrays.y[1] = arrays.z[1] = 0.0;
+	pdata_2->release();
+	shared_ptr<NeighborList> nlist_2(new NeighborList(sysdef_2, Scalar(3.0), Scalar(0.8)));
+	shared_ptr<TablePotential> fc(new TablePotential(sysdef_2, nlist_2, 5));
+
+    // provide a basic potential and "force"
+    vector<Scalar> V, F;
+    V.push_back(10.0);  F.push_back(-10.0/1.0);
+    V.push_back(15.0);  F.push_back(-15.0/2.0);
+    V.push_back(5.0);   F.push_back(-5.0/3.0);
+    V.push_back(8.0);   F.push_back(-8.0/4.0);
+    V.push_back(18.0);  F.push_back(-18.0/5.0);
+    
+	fc->setTable(0, 0, V, F, 1.0, 5.0);
+	
+	ofstream f("table_dat.m");
+	f << "table = [";
+	unsigned int count = 0;	
+	for (float r = 0.1; r <= 5.0; r+= 0.001)
+		{
+		// set the distance
+		ParticleDataArrays arrays = pdata_2->acquireReadWrite();
+		arrays.x[0] = arrays.y[0] = arrays.z[0] = 0.0;
+		arrays.x[1] = Scalar(r); arrays.y[1] = arrays.z[1] = 0.0;
+		pdata_2->release();
+		
+		// compute the forces
+		fc->compute(count);
+		count++;
+	
+		ForceDataArrays force_arrays = fc->acquire();
+		f << r << " " << force_arrays.fx[1] << " " << fc->calcEnergySum() << " ; " << endl;	
+		}
+	f << "];" << endl;
+	f.close();
+	}
