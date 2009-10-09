fe249def8049b5a0fb17dc631e27a0560f845422
Axel Kohlmeyer <akohlmey@gmail.com>
2009-10-09 18:35:23 +0000

    transfer maintainer tag from blevine and dnlebard to akohlmey for all existing and tested features programmed by the ICMS team. dave and ben will take ownership on modules only for as long as they are actively developing them.
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@2163 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/computes_gpu/HarmonicDihedralForceComputeGPU.cc
+++ src/computes_gpu/HarmonicDihedralForceComputeGPU.cc
@@ -41,7 +41,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // $Id$
 // $URL$
-// Maintainer: dnlebard
+// Maintainer: akohlmey
 
 #ifdef WIN32
 #pragma warning( push )
--- src/cuda/CGCMMForceGPU.cu
+++ src/cuda/CGCMMForceGPU.cu
@@ -41,7 +41,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // $Id$
 // $URL$
-// Maintainer: blevine
+// Maintainer: akohlmey
 
 #include "gpu_settings.h"
 #include "CGCMMForceGPU.cuh"
