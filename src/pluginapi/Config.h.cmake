dcf19be0a68f5ac81021db8c86cdb1d9fd475d8e
Joshua A. Anderson <joaander@umich.edu>
2009-10-12 13:58:07 +0000

    Fixed needed options in hoomd_config.h
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/plugin-caps@2169 fa922fa7-2fde-0310-acd8-f43f465a7996

--- src/pluginapi/Config.h.cmake
+++ src/pluginapi/Config.h.cmake
@@ -1,9 +1,8 @@
 #ifndef _HOOMD_CONFIG_H
 #define _HOOMD_CONFIG_H
 
+#cmakedefine CUDA_BUILD_EMULATION
 #cmakedefine ENABLE_CUDA
-#cmakedefine ENABLE_FFT
-#cmakedefine ENABLE_MINIMAL_INSTALL
 #cmakedefine ENABLE_STATIC
 #cmakedefine SINGLE_PRECISION
 #cmakedefine LARGE_EXCLUSION_LIST
