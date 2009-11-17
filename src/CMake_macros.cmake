b7eca9bf76ea190eef7a629ecb479f4fc6b76b6d
Joshua A. Anderson <joaander@umich.edu>
2009-11-17 15:03:24 +0000

    Found a way to fix the incorrect install name of libcudart.dylib. No more manual setting of DYLD_LIBRARY_PATH on mac OS X, yay!
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@2335 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ src/CMake_macros.cmake
@@ -0,0 +1,7 @@
+macro(fix_cudart_rpath target)
+if (ENABLE_CUDA AND APPLE)
+get_target_property(_target_exe ${target} LOCATION)
+add_custom_command(TARGET ${target} POST_BUILD
+                          COMMAND install_name_tool ARGS -change @rpath/libcudart.dylib ${CUDA_CUDART_LIBRARY} ${_target_exe})
+endif (ENABLE_CUDA AND APPLE)
+endmacro(fix_cudart_rpath)
