cf421f6c3af59f44ada2b95472585f3a084f19b2
Joshua A. Anderson <joaander@umich.edu>
2010-01-06 15:11:31 +0000

    api installation now tested and working
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/cleanup-phase2@2563 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ libhoomd/api/hoomd.h
@@ -0,0 +1,138 @@
+#ifndef _HOOMD_H
+#define _HOOMD_H
+
+#include "hoomd_config.h"
+
+// math setup
+#include "HOOMDMath.h"
+
+// data structures
+#include "GPUArray.h"
+#include "ParticleData.h"
+#include "AngleData.h"
+#include "BondData.h"
+#include "DihedralData.h"
+#include "IntegratorData.h"
+#include "ParticleGroup.h"
+#include "WallData.h"
+
+#include "ExecutionConfiguration.h"
+#include "SystemDefinition.h"
+
+// initializers
+#include "Initializers.h"
+#include "RandomGenerator.h"
+#include "HOOMDBinaryInitializer.h"
+#include "HOOMDInitializer.h"
+
+// base classes
+#include "Analyzer.h"
+#include "Compute.h"
+#include "Updater.h"
+
+// analyzers
+#include "DCDDumpWriter.h"
+#include "HOOMDBinaryDumpWriter.h"
+#include "HOOMDDumpWriter.h"
+#include "IMDInterface.h"
+#include "Logger.h"
+#include "MOL2DumpWriter.h"
+#include "MSDAnalyzer.h"
+#include "PDBDumpWriter.h"
+
+// computes
+#include "ForceCompute.h"
+#include "NeighborList.h"
+#include "BinnedNeighborList.h"
+#include "TempCompute.h"
+#include "ConstForceCompute.h"
+
+#ifdef ENABLE_CUDA
+#include "BinnedNeighborListGPU.h"
+#include "NeighborListNsqGPU.h"
+#endif
+
+// pair potentials
+#include "TablePotential.h"
+#include "CGCMMForceCompute.h"
+#include "AllPairPotentials.h"
+
+#ifdef ENABLE_CUDA
+#include "CGCMMForceComputeGPU.h"
+#include "TablePotentialGPU.h"
+#endif
+
+// bond potentials
+#include "FENEBondForceCompute.h"
+#include "HarmonicBondForceCompute.h"
+
+#ifdef ENABLE_CUDA
+#include "HarmonicBondForceComputeGPU.h"
+#include "FENEBondForceComputeGPU.h"
+#endif
+
+// angle potentials
+#include "CGCMMAngleForceCompute.h"
+#include "HarmonicAngleForceCompute.h"
+
+#ifdef ENABLE_CUDA
+#include "CGCMMAngleForceComputeGPU.h"
+#include "HarmonicAngleForceComputeGPU.h"
+#endif
+
+// dihedral/improper potentials
+#include "HarmonicDihedralForceCompute.h"
+#include "HarmonicImproperForceCompute.h"
+
+#ifdef ENABLE_CUDA
+#include "HarmonicDihedralForceComputeGPU.h"
+#include "HarmonicImproperForceComputeGPU.h"
+#endif
+
+// wall potentials
+#include "LJWallForceCompute.h"
+
+// system
+#include "System.h"
+
+// updaters
+#include "BoxResizeUpdater.h"
+#include "Enforce2DUpdater.h"
+#include "SFCPackUpdater.h"
+#include "TempRescaleUpdater.h"
+#include "ZeroMomentumUpdater.h"
+#ifdef ENABLE_CUDA
+#include "Enforce2DUpdaterGPU.h"
+#endif
+
+// integrators
+#include "IntegrationMethodTwoStep.h"
+#include "Integrator.h"
+#include "IntegratorTwoStep.h"
+#include "TwoStepBDNVT.h"
+#include "TwoStepNPT.h"
+#include "TwoStepNVE.h"
+#include "TwoStepNVT.h"
+#include "ClockSource.h"
+
+#ifdef ENABLE_CUDA
+#include "TwoStepBDNVTGPU.h"
+#include "TwoStepNPTGPU.h"
+#include "TwoStepNVEGPU.h"
+#include "TwoStepNVTGPU.h"
+#endif
+
+// utility classes
+#include "FileFormatManager.h"
+#include "FileFormatProxy.h"
+#include "GPUWorker.h"
+#include "Index1D.h"
+#include "IndexTransform.h"
+#include "MolFilePlugin.h"
+#include "Profiler.h"
+#include "SignalHandler.h"
+#include "Variant.h"
+#include "HOOMDVersion.h"
+
+#endif
+
