852d19c75f8dbdd46ee6ff10e332793c26c84d5c
Joshua A. Anderson <joaander@umich.edu>
2010-01-13 16:57:44 +0000

    Updating nve rods test: debugging lack of energy conservation
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/rigid-bodies@2618 fa922fa7-2fde-0310-acd8-f43f465a7996

--- test/rigid/rods/nve_basic.py
+++ test/rigid/rods/nve_basic.py
@@ -6,14 +6,16 @@ lj = pair.lj(r_cut=2**(1.0/6.0));
 lj.pair_coeff.set('A', 'A', sigma=1.0, epsilon=1.0)
 lj.set_params(mode='shift')
 nlist.reset_exclusions(exclusions=['body'])
+sorter.disable()
+nlist.set_params(r_buff=0.0)
 
-integrate.mode_standard(dt=0.005)
+integrate.mode_standard(dt=0.001)
 bdnvt = integrate.bdnvt_rigid(group=group.all(), T=1.2)
 
-dump.dcd(filename='nve_basic.dcd', period=50, overwrite=True)
-analyze.log(filename="nve_basic.log", period=100, overwrite=True, quantities=['potential_energy', 'kinetic_energy'])
+dump.dcd(filename='nve_basic.dcd', period=1, overwrite=True)
+analyze.log(filename="nve_basic.log", period=1, overwrite=True, quantities=['potential_energy', 'kinetic_energy'])
 
-run(5000)
+run(5000, profile=True)
 
 bdnvt.disable()
 nve = integrate.nve_rigid(group=group.all())
