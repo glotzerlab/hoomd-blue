1623433cb8f01e9c48bdf78bdd6a50d450d45113
Joshua A. Anderson <joaander@umich.edu>
2010-01-26 19:09:41 +0000

    openmp benchmarks now dynamically determine the max number of procs to benchmark
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/openmp@2660 fa922fa7-2fde-0310-acd8-f43f465a7996

--- libhoomd/python/hoomd_module.cc
+++ libhoomd/python/hoomd_module.cc
@@ -325,6 +325,24 @@ string get_hoomd_version()
     return ver.str();
     }
 
+//! Layer for omp_set_num_threads
+void set_num_threads(int nthreads)
+    {
+    #ifdef ENABLE_OPENMP
+    omp_set_num_threads(nthreads);
+    #endif
+    }
+
+//! Layer for omp_get_num_procs()
+int get_num_procs()
+    {
+    #ifdef ENABLE_OPENMP
+    return omp_get_num_procs();
+    #else
+    return 1;
+    #endif
+    }
+
 //! Create the python module
 /*! each class setup their own python exports in a function export_ClassName
     create the hoomd python module and define the exports here.
@@ -338,9 +356,8 @@ BOOST_PYTHON_MODULE(hoomd)
     def("set_gpu_error_checking", &set_gpu_error_checking);
     def("get_hoomd_version", &get_hoomd_version);
 
-    #ifdef ENABLE_OPENMP
-    def("set_num_threads", &omp_set_num_threads);
-    #endif
+    def("set_num_threads", &set_num_threads);
+    def("get_num_procs", &get_num_procs);
 
     // data structures
     class_<std::vector<int> >("std_vector_int")
