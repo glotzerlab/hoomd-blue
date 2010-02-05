d0795c0a37a749911a8865b8a3411d209ba67dd9
Joshua A. Anderson <joaander@umich.edu>
2010-02-05 18:49:09 +0000

    Fixed generate_cubes to work with the latest bbb
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/rigid-bodies@2700 fa922fa7-2fde-0310-acd8-f43f465a7996

--- test/rigid/cubes/generate_cubes.py
+++ test/rigid/cubes/generate_cubes.py
@@ -1,13 +1,13 @@
 from bbb import basic_blocks, packmol
 import copy
 
-cube = basic_blocks.Cuboid(4, 4, 4, 2, 2, 2, type='A');
-n_cubes = 30
+cube = basic_blocks.Cuboid(Nw=4, w=2, type='A');
+n_cubes = 300
 
 box = packmol.ConstraintSimulationBoxHOOMD(1, 1, 1)
 box.scaleToVolume(n_cubes * 100)
 
 g = packmol.GeneratorXML(simulation_box=box, seed=1, tolerance=1.2)
-g.addBuildingBlock(cube, 30)
+g.addBuildingBlock(cube, n_cubes)
 
 g.writeOutput(open('cubes.xml', 'w'));
