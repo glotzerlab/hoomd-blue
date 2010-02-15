345b956197bd7cc9f137bd463f209678074a530b
Joshua A. Anderson <joaander@umich.edu>
2010-02-15 19:30:53 +0000

    Adding script for testing XML rigid body restarts.
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/rigid-bodies@2764 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ test/rigid/cubes/run_restart_xml.py
@@ -0,0 +1,16 @@
+from hoomd_script import *
+
+init.read_xml('continue.xml')
+
+lj = pair.lj(r_cut=2**(1.0/6.0));
+lj.pair_coeff.set('A', 'A', sigma=1.0, epsilon=1.0)
+lj.set_params(mode='shift')
+nlist.reset_exclusions(exclusions=['body'])
+
+integrate.mode_standard(dt=0.005)
+integrate.nve_rigid(group=group.all())
+
+dcd = dump.dcd(filename='restart_xml.dcd', period=100)
+log = analyze.log(filename="restart_xml.log", period=1, overwrite=True, quantities=['potential_energy', 'kinetic_energy'])
+
+run(50000)
