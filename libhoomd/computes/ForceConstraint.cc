43e76697ceca3530cf85eefc7c0327d13769ca1d
Joshua A. Anderson <joaander@umich.edu>
2010-05-25 19:04:45 +0000

    ForceConstraint base class
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/constraint@3104 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ libhoomd/computes/ForceConstraint.cc
@@ -0,0 +1,86 @@
+/*
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008, 2009 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+Redistribution and use of HOOMD-blue, in source and binary forms, with or
+without modification, are permitted, provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of HOOMD-blue's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
+ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// $Id$
+// $URL$
+// Maintainer: joaander
+
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4103 4244 )
+#endif
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+#include "ForceConstraint.h"
+
+using namespace std;
+
+/*! \file ForceConstraint.cc
+    \brief Contains code for the ForceConstraint class
+*/
+
+/*! \param sysdef SystemDefinition containing the ParticleData to compute forces on
+*/
+ForceConstraint::ForceConstraint(boost::shared_ptr<SystemDefinition> sysdef)
+        : ForceCompute(sysdef)
+    {
+    }
+
+/*! Does nothing in the base class
+    \param timestep Current timestep
+*/
+void ForceConstraint::computeForces(unsigned int timestep)
+    {
+    }
+
+
+void export_ForceConstraint()
+    {
+    class_< ForceConstraint, boost::shared_ptr<ForceConstraint>, bases<ForceCompute>, boost::noncopyable >
+    ("ForceConstraint", init< boost::shared_ptr<SystemDefinition> >())
+    ;
+    }
+
+#ifdef WIN32
+#pragma warning( pop )
+#endif
