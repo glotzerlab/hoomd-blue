2d4da8df8fe6f350fadd46884502fad2784efcec
Joshua A. Anderson <joaander@umich.edu>
2010-06-16 18:27:13 +0000

    Added constrain.sphere test to the hoomd_script tests
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/constraint@3164 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ test/hoomd_script/test_constraint_sphere.py
@@ -0,0 +1,37 @@
+# -*- coding: iso-8859-1 -*-
+# $Id$
+# $URL$
+# Maintainer: joaander
+
+from hoomd_script import *
+import unittest
+import os
+
+# test the constrain.sphere command
+class constraint_sphere_tests (unittest.TestCase):
+    def setUp(self):
+        print
+        sysdef = init.create_empty(N=2, box=(40,40,40), n_particle_types=1);
+        sysdef.particles[0].position = (5,0,0);
+        sysdef.particles[1].position = (-5,1,1);
+        import __main__;
+        __main__.sorter.set_params(grid=8)
+
+    def test_basic(self):
+        all = group.all()
+        constrain.sphere(group=all, P=(0,0,0), r=5)
+        integrate.mode_standard(dt=0.005);
+        integrate.bdnvt(group=all, T=1.2);
+        run(10);
+    
+    def test_error(self):
+        all = group.all()
+        self.assertRaises(RuntimeError, constrain.sphere, group=all, P=(0,0,0), r=10)
+
+    def tearDown(self):
+        init.reset();
+
+
+if __name__ == '__main__':
+    unittest.main(argv = ['test.py', '-v'])
+
