290c83f3cfc0de39736e621b253063b698112c6a
Joshua A. Anderson <joaander@umich.edu>
2010-08-26 15:10:51 +0000

    New GPU neighbor lists now active in hoomd_script
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/nlist-rewrite@3389 fa922fa7-2fde-0310-acd8-f43f465a7996

--- python-module/hoomd_script/pair.py
+++ python-module/hoomd_script/pair.py
@@ -332,14 +332,12 @@ class nlist:
                 raise RuntimeError("Error creating neighbor list");
         else:
             if mode == "binned":
-                #self.cpp_nlist = hoomd.BinnedNeighborListGPU(globals.system_definition, r_cut, default_r_buff)
-                #self.cpp_nlist.setBlockSize(tune._get_optimal_block_size('nlist'));
                 cl_g = hoomd.CellListGPU(globals.system_definition);
                 globals.system.addCompute(cl_g, "auto_cl")
                 self.cpp_nlist = hoomd.NeighborListGPUBinned(globals.system_definition, r_cut, default_r_buff, cl_g)
                 self.cpp_nlist.setBlockSize(tune._get_optimal_block_size('nlist'));
             elif mode == "nsq":
-                self.cpp_nlist = hoomd.NeighborListNsqGPU(globals.system_definition, r_cut, default_r_buff)
+                self.cpp_nlist = hoomd.NeighborListGPU(globals.system_definition, r_cut, default_r_buff)
             else:
                 print >> sys.stderr, "\n***Error! Invalid neighbor list mode\n";
                 raise RuntimeError("Error creating neighbor list");
