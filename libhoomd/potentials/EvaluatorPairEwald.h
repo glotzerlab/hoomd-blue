c4eab64a5f1cacfadb5660ad6ea556aff73257ae
Steve Barr <sbarr@princeton.edu>
2010-08-31 20:43:24 +0000

    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/electrostatics@3445 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ libhoomd/potentials/EvaluatorPairEwald.h
@@ -0,0 +1,189 @@
+/*
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008, 2009 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+Redistribution and use of HOOMD-blue, in source and binary forms, with or
+without modification, are permitted, provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of HOOMD-blue's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
+ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// $Id: EvaluatorPairEwald.h 2862 2010-03-12 19:16:16Z joaander $
+// $URL: http://codeblue.umich.edu/hoomd-blue/svn/trunk/libhoomd/potentials/EvaluatorPairEwald.h $
+// Maintainer: joaander
+
+#ifndef __PAIR_EVALUATOR_EWALD_H__
+#define __PAIR_EVALUATOR_EWALD_H__
+
+#ifndef NVCC
+#include <string>
+#endif
+
+#include "HOOMDMath.h"
+
+/*! \file EvaluatorPairEwald.h
+    \brief Defines the pair evaluator class for Ewald potentials
+*/
+
+// need to declare these class methods with __device__ qualifiers when building in nvcc
+//! DEVICE is __host__ __device__ when included in nvcc and blank when included into the host compiler
+#ifdef NVCC
+#define DEVICE __device__
+#else
+#define DEVICE
+#endif
+
+// call different optimized exp functions on the host / device
+//! EXP is expf when included in nvcc and exp when included into the host compiler
+#ifdef NVCC
+#define EXP expf
+#else
+#define EXP exp
+#endif
+
+// call different optimized sqrt functions on the host / device
+//! RSQRT is rsqrtf when included in nvcc and 1.0 / sqrt(x) when included into the host compiler
+#ifdef NVCC
+#define RSQRT(x) rsqrtf( (x) )
+#else
+#define RSQRT(x) Scalar(1.0) / sqrt( (x) )
+#endif
+
+//! Class for evaluating the Ewald pair potential
+/*! <b>General Overview</b>
+
+    See EvaluatorPairLJ
+    
+    <b>Ewald specifics</b>
+    
+    EvaluatorPairEwald evaluates the function:
+    
+    \f[ V_{\mathrm{Ewald}}(r) = q_i q_j erfc(\kappa r)/r \f]
+
+    The Ewald potential does not need diameter. Three parameters are specified and stored in a Scalar3. 
+    \a kappa is placed in \a params.x \a grid size is in \a params.y and \a order is place in \a params.z      
+*/
+
+#ifdef NVCC
+#define ERFC erfc
+#else
+#define ERFC erfcf
+#endif
+
+class EvaluatorPairEwald
+    {
+    public:
+        //! Define the parameter type used by this pair potential evaluator
+        typedef Scalar3 param_type;
+
+        //! Constructs the pair potential evaluator
+        /*! \param _rsq Squared distance beteen the particles
+            \param _rcutsq Sqauared distance at which the potential goes to 0
+            \param _params Per type pair parameters of this potential
+        */
+        DEVICE EvaluatorPairEwald(Scalar _rsq, Scalar _rcutsq, const param_type& _params)
+	  : rsq(_rsq), rcutsq(_rcutsq), kappa(_params.x), grid(_params.y), order(_params.z)
+            {
+            }
+        
+        //! Ewald doesn't use diameter
+        DEVICE static bool needsDiameter() { return false; }
+        //! Accept the optional diameter values
+        /*! \param di Diameter of particle i
+            \param dj Diameter of particle j
+        */
+        DEVICE void setDiameter(Scalar di, Scalar dj) { }
+
+        //! Ewald uses charge !!!
+        DEVICE static bool needsCharge() { return true; }
+        //! Accept the optional diameter values
+        /*! \param qi Charge of particle i
+            \param qj Charge of particle j
+        */
+        DEVICE void setCharge(Scalar qi, Scalar qj) {
+	    qiqj = qi * qj;
+	}
+        //! Evaluate the force and energy
+        /*! \param force_divr Output parameter to write the computed force divided by r.
+            \param pair_eng Output parameter to write the computed pair energy
+            \param energy_shift If true, the potential must be shifted so that V(r) is continuous at the cutoff
+            \note There is no need to check if rsq < rcutsq in this method. Cutoff tests are performed 
+                  in PotentialPair.
+            
+            \return True if they are evaluated or false if they are not because we are beyond the cuttoff
+        */
+        DEVICE bool evalForceAndEnergy(Scalar& force_divr, Scalar& pair_eng, bool energy_shift)
+	{
+	  if (rsq < rcutsq && qiqj != 0)
+	    {
+	      Scalar rinv = RSQRT(rsq);
+	      Scalar r = Scalar(1.0) / rinv;
+	      Scalar r2inv = Scalar(1.0) / rsq;
+		   
+	      Scalar erfc_by_r_val = ERFC(kappa * r) * rinv;
+		  	  
+	      force_divr = qiqj * r2inv * (erfc_by_r_val + 2.0*kappa*RSQRT(M_PI) * EXP(-kappa*kappa* rsq));
+	      pair_eng = qiqj * erfc_by_r_val ;
+
+	      return true;
+	    }
+	  else
+	    return false;
+	    
+	      
+	}
+        
+        #ifndef NVCC
+        //! Get the name of this potential
+        /*! \returns The potential name. Must be short and all lowercase, as this is the name energies will be logged as
+            via analyze.log.
+        */
+        static std::string getName()
+            {
+            return std::string("ewald");
+            }
+        #endif
+
+    protected:
+        Scalar rsq;     //!< Stored rsq from the constructor
+        Scalar rcutsq;  //!< Stored rcutsq from the constructor
+	Scalar kappa;   //!< kappa parameter extracted from the params passed to the constructor
+	Scalar grid;    //!< grid parameter extracted from the params passed to the constructor
+	Scalar order;   //!< order parameter extracted from the params passed to the constructor
+	Scalar qiqj;	//!< product of qi and qj
+    };
+
+
+#endif // __PAIR_EVALUATOR_EWALD_H__
+
