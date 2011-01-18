b7b9dacac12ad1efc399ba33d76c98d05210c456
Joshua A. Anderson <joaander@umich.edu>
2011-01-18 14:13:54 +0000

    Added r_buf and series benchmarking helper scripts.
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@3638 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ python-module/hoomd_script/benchmark.py
@@ -0,0 +1,82 @@
+# -*- coding: iso-8859-1 -*-
+#Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+#(HOOMD-blue) Open Source Software License Copyright 2008, 2009 Ames Laboratory
+#Iowa State University and The Regents of the University of Michigan All rights
+#reserved.
+
+#HOOMD-blue may contain modifications ("Contributions") provided, and to which
+#copyright is held, by various Contributors who have granted The Regents of the
+#University of Michigan the right to modify and/or distribute such Contributions.
+
+#Redistribution and use of HOOMD-blue, in source and binary forms, with or
+#without modification, are permitted, provided that the following conditions are
+#met:
+
+#* Redistributions of source code must retain the above copyright notice, this
+#list of conditions, and the following disclaimer.
+
+#* Redistributions in binary form must reproduce the above copyright notice, this
+#list of conditions, and the following disclaimer in the documentation and/or
+#other materials provided with the distribution.
+
+#* Neither the name of the copyright holder nor the names of HOOMD-blue's
+#contributors may be used to endorse or promote products derived from this
+#software without specific prior written permission.
+
+#Disclaimer
+
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
+#ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+#IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+#INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+#OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# $Id:$
+# $URL:$
+# Maintainer: joaander / All Developers are free to add commands for new features
+
+import globals
+import init
+import hoomd_script
+import hoomd
+
+try:
+    import numpy;
+except ImportError:
+    numpy = None;
+
+## Make a medium length runs to benchmark overall simulation performance
+# \param warmup Number of time steps to run() to warm up the benchmark
+# \param repeat Number of times to repeat the benchmark \a steps
+# \param steps Number of time steps to run() at each benchmark point
+#
+# series_run() executes \a warmup time steps. After that, it simply
+# calls run(steps), \a repeat times and returns a list containing the average TPS for each of those runs.
+#
+# If numpy is available, a brief summary of the benchmark results will be printed to the screen 
+def series(warmup=100000, repeat=20, steps=10000):
+    # check if initialization has occurred
+    if not init.is_initialized():
+        print >> sys.stderr, "\n***Error! Cannot tune r_buff before initialization\n";
+
+    tps_list = [];
+    
+    hoomd_script.run(warmup);
+    for i in xrange(0,repeat):
+        hoomd_script.run(steps);
+        tps_list.append(globals.system.getLastTPS());
+    
+    if numpy is not None:
+        print
+        print "**Notice: Series average TPS: %4.2f" % numpy.average(tps_list);
+        print "          Series median TPS : %4.2f" % numpy.median(tps_list);
+        print "          Series TPS std dev: %4.2f" % numpy.std(tps_list);
+    
+    return tps_list;
--- python-module/hoomd_script/tune.py
+++ python-module/hoomd_script/tune.py
@@ -593,3 +593,92 @@ def bond_fene_setup():
     fc.update_coeffs();
     return fc;
 
+import hoomd;
+import hoomd_script;
+import init;
+import globals;
+
+## Make a series of short runs to determine the fastest performing r_buff setting
+# \param warmup Number of time steps to run() to warm up the benchmark
+# \param r_min Smallest value of r_buff to test
+# \param r_max Largest value of r_buff to test
+# \param jumps Number of different r_buff values to test
+# \param steps Number of time steps to run() at each point
+# \param set_max_check_period Set to True to enable automatic setting of the maximum nlist check_period
+#
+# tune.r_buff() executes \a warmup time steps. Then it sets the nlist \a r_buff value to \a r_min and runs for
+# \a steps time steps. The TPS value is recorded, and the benchmark moves on to the next \a r_buff value
+# completing at \a r_max in \a jumps jumps. Status information is printed out to the screen, and the optimal
+# \a r_buff value is left set for further runs() to continue at optimal settings.
+#
+# Each benchmark is repeated 3 times and the median value chosen. Then, \a warmup time steps are run() again
+# at the optimal r_buff in order to determine the maximum value of check_period. In total,
+# (2*warmup + 3*jump*steps) time steps are run().
+#
+# \note By default, the maximum check_period is \b not set in tune.r_buff() for saftey. If you wish to have it set
+# when the call completes, call with the parameter set_max_check_period=True.
+#
+# \returns (optimal_r_buff, maximum check_period)
+#
+def r_buff(warmup=200000, r_min=0.05, r_max=1.0, jumps=20, steps=5000, set_max_check_period=False):
+    # check if initialization has occurred
+    if not init.is_initialized():
+        print >> sys.stderr, "\n***Error! Cannot tune r_buff before initialization\n";
+    
+    # check that there is a nlist
+    if globals.neighbor_list is None:
+        print >> sys.stderr, "\n***Error! Cannot tune r_buff when there is no neighbor list\n";
+
+    # start off at a check_period of 1
+    globals.neighbor_list.set_params(check_period=1);
+
+    # make the warmup run
+    hoomd_script.run(warmup);
+    
+    # initialize scan variables
+    dr = (r_max - r_min) / (jumps - 1);
+    r_buff_list = [];
+    tps_list = [];
+
+    # loop over all desired r_buff points
+    for i in xrange(0,jumps):
+        # set the current r_buff
+        r_buff = r_min + i * dr;
+        globals.neighbor_list.set_params(r_buff=r_buff);
+        
+        # run the benchmark 3 times
+        tps = [];
+        hoomd_script.run(steps);
+        tps.append(globals.system.getLastTPS())
+        hoomd_script.run(steps);
+        tps.append(globals.system.getLastTPS())
+        hoomd_script.run(steps);
+        tps.append(globals.system.getLastTPS())
+        
+        # record the median tps of the 3
+        tps.sort();
+        tps_list.append(tps[1]);
+        r_buff_list.append(r_buff);
+    
+    # find the fastest r_buff
+    fastest = tps_list.index(max(tps_list));
+    fastest_r_buff = r_buff_list[fastest];
+
+    # set the fastest and rerun the warmup steps to identify the max check period
+    globals.neighbor_list.set_params(r_buff=fastest_r_buff);
+    hoomd_script.run(warmup);
+
+    # notify the user of the benchmark results
+    print "Notice: r_buff =", r_buff_list
+    print "Notice: tps =", tps_list
+    print
+    print "**Notice: Optimal r_buff:", fastest_r_buff;
+    print "**Notice: Maximum check_period:", globals.neighbor_list.query_update_period()
+    
+    # set the found max check period
+    if set_max_check_period:
+        globals.neighbor_list.set_params(check_period=globals.neighbor_list.query_update_period());
+    
+    # return the results to the script
+    return (fastest_r_buff, globals.neighbor_list.query_update_period());
+
