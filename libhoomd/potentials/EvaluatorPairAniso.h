f6040f45ebb04bb041ee27dc977ab1d673e9da22
Benjamin Schultz <baschult@umich.edu>
2011-02-24 20:43:08 +0000

    Created a model anisotropic pair potential evaluator class, acutally added it this time...r
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/aniso@3813 fa922fa7-2fde-0310-acd8-f43f465a7996

---
+++ libhoomd/potentials/EvaluatorPairAniso.h
@@ -0,0 +1,158 @@
+/*
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008, 2009 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+Redistribution and use of HOOMD-blue, in source and binary forms, with or
+without modification, are permitted, provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of HOOMD-blue's
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
+ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// $Id$
+// $URL$
+// Maintainer: grva
+
+#ifndef __EvaluatorPairAniso__
+#define __EvaluatorPairAniso__
+
+#ifndef NVCC
+#include <string>
+#endif
+
+#include "HOOMDMath.h"
+
+/*! \file EvaluatorPairAniso.h
+    \brief Defines a mostly abstract base class for anisotropic pair potentials
+*/
+
+// need to declare these class methods with __device__ qualifiers when building in nvcc
+//! DEVICE is __host__ __device__ when included in nvcc and blank when included into the host compiler
+#ifdef NVCC
+#define DEVICE __device__
+#else
+#define DEVICE
+#endif
+
+// call different optimized sqrt functions on the host / device
+//! RSQRT is rsqrtf when included in nvcc and 1.0 / sqrt(x) when included into the host compiler
+#ifdef NVCC
+#define RSQRT(x) rsqrtf( (x) )
+#else
+#define RSQRT(x) Scalar(1.0) / sqrt( (x) )
+#endif
+
+//! Class for evaluating anisotropic pair interations
+/*! <b>General Overview</b>
+
+    Provides a base class for detailed anisotropic pairwise interactions
+*/
+
+struct EvaluatorPairAnisoParams
+    {
+    };
+
+class EvaluatorPairAniso
+    {
+    public:
+        typedef EvaluatorPairAnisoParams param_type
+        //! Constructs the pair potential evaluator
+        /*! \param _dr Displacement vector between particle centres of mass
+            \param _rcutsq Squared distance at which the potential goes to 0
+	    \param _q_i Quaterion of i^th particle
+	    \param _q_j Quaterion of j^th particle
+            \param _params Per type pair parameters of this potential
+        */
+        DEVICE EvaluatorPairAniso(Scalar3 _dr, Scalar4 _quat_i, Scalar4 _quat_j, Scalar _rcutsq, param_type _params)
+            :dr(_dr),quat_i(_quat_i),quat_j(_quat_j),rcutsq(_rcutsq),params(params)
+            {
+            }
+        
+        //! uses diameter
+        DEVICE static bool needsDiameter()
+            {
+            return false;
+            }
+
+        //! Accept the optional diameter values
+        /*! \param di Diameter of particle i
+            \param dj Diameter of particle j
+        */
+        DEVICE void setDiameter(Scalar di, Scalar dj){}
+
+        //! whether pair potential requires charges
+        DEVICE static bool needsCharge( )
+            {
+            return false;
+            }
+
+        //! Accept the optional diameter values
+        //! This function is pure virtual
+        /*! \param qi Charge of particle i
+            \param qj Charge of particle j
+        */
+        DEVICE void setCharge(Scalar qi, Scalar qj){}
+           
+        //! Evaluate the force and energy
+        /*! \param force Output parameter to write the computed force.
+            \param pair_eng Output parameter to write the computed pair energy.
+            \param energy_shift If true, the potential must be shifted so that V(r) is continuous at the cutoff.
+            \param torque_i The torque exterted on the i^th particle.
+            \param torque_j The torque exterted on the j^th particle.
+            \return True if they are evaluated or false if they are not because we are beyond the cutoff.
+        */
+        DEVICE  bool
+		evalPair(Scalar3& force, Scalar& pair_eng, bool energy_shift, Scalar3& torque_i, Scalar3& torque_j) 
+            {
+            return true;
+            }
+        
+        #ifndef NVCC
+        //! Get the name of the potential
+        /*! \returns The potential name. Must be short and all lowercase, as this is the name energies will be logged as
+            via analyze.log.
+        */
+        static std::string getName();
+        #endif
+
+    protected:
+        Scalar3 dr;     //!< Stored vector pointing between particle centres of mass
+        Scalar rcutsq;  //!< Stored rcutsq from the constructor
+        Scalar4 q_i;     //!< Stored quaternion of i^th particle from constuctor
+        Scalar4 q_j;     //!< Stored quaternion of j^th particle from constructor
+        param_type params;
+    };
+
+
+#endif // __EvaluatorPairAniso__
+
