5b86c55b0c0bec1eefa1728d923eef659e434b67
Ross Smith <rjsm@umich.edu>
2011-03-11 15:49:25 +0000

    fixed file modified date issue and included upload capability
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@3843 fa922fa7-2fde-0310-acd8-f43f465a7996

--- packaging/deb_build_nightly.sh
+++ packaging/deb_build_nightly.sh
@@ -3,34 +3,34 @@ cd  ../
 svn up
 
 #check  the previous version built
-atrev=$(cat ../deb_old_version)
-if [ "$atrev" -eq "$(svnversion . )" ];then
+export atrev="$(cat ../deb_old_version)"
+export old_ver="$(svnversion . )"
+echo $atrev
+echo $old_ver
+if [ $atrev =  $old_ver ];then
 	echo "up to date"
 else
-#move trunk to staging so svn gives us a good version number
-	cd ../
-	rm -r staging
-	mkdir -p staging
-	cp -r trunk/* staging/
-	cd staging
-
-#get what architecture we're building on.
-	export deb_arch=$(dpkg-architecture -qDEB_BUILD_GNU_CPU)
-	if [ "$(dpkg-architecture -qDEB_BUILD_ARCH_BITS )" == "64" ] 2>/dev/null ; 
-		then export lib_suffix="64" ;	fi
-#remove old traces of building packages
+	echo "commence building"
 	rm -r debian/*
-	echo $(svnversion .) > ../deb_old_version
-#create build directory to move cuda libs to
-	export deb_build_folder="obj-$(dpkg-architecture -qDEB_BUILD_GNU_CPU)-linux-gnu" 
-	mkdir -p  debian/hoomd-blue/usr/lib${lib_suffix}
-
-#make sure cuda libs are  where we want it
 	cp -r packaging/debian ./
-	mkdir -p debian/hoomd-blue/usr/lib${lib_suffix}
-	cp /usr/local/cuda/lib${lib_suffix}/libcudart.so*  debian/hoomd-blue/usr/lib${lib_suffix}
-	cp /usr/local/cuda/lib${lib_suffix}/libcudafft.so* debian/hoomd-blue/usr/lib${lib_suffix}
+#get what architecture we're building on.
+	if [ `arch`="x86_64" ]
+		then
+#patch debian folder for 64 bit
+		echo "patching 64bit"
+		export lib_suffix="64"
+		sed -i s/i386/amd64/ debian/control
+		sed -i s/i386/amd64/ debian/files
+	else
+#patch debian folder for building on i386
+		echo "patching 32bit"
+		export lib_suffix=""
+		sed -i s/amd64/i386/ debian/control
+		sed -i s/and64/i386/ debian/files
 
+fi
+#set the version we just setup for in deb_old_version so it won't be built again
+	echo $(svnversion .) > ../deb_old_version
 
 #export the variables to set the version from svn
 	export HSVN_VERSION=$(svnversion . )
@@ -38,7 +38,11 @@ else
 	echo $HVERSION
 #set our package version in changelog
 	sed s/HVERSION/${HVERSION}/ debian/changelog -i
-	sed s/#M/#/ debian/changelog -i
 #call package builder
 	dpkg-buildpackage
+#move files to be uploaded
+	cd ..
+	cp deb_old_version deb_version${lib_suffix}
+	scp deb_version${lib_suffix} joaander@foxx.engin.umich.edu:devel/incoming
+	scp hoomd-blue_${HVERSION}*.deb joaander@foxx.engin.umich.edu:devel/incoming
 fi
