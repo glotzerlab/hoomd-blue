169fb80d5386a129c202c6c696e8776464444f85
Eric Irrgang <eirrgang@umich.edu>
2011-03-17 16:47:51 +0000

    make sure RPM build script transfers files and cleans up after itself
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/trunk@3884 fa922fa7-2fde-0310-acd8-f43f465a7996

--- packaging/redhat_build_package.sh
+++ packaging/redhat_build_package.sh
@@ -9,6 +9,10 @@
 #
 PATH=/bin:/usr/bin:$PATH
 SPECFILE="hoomd-0.9.1.spec"
+# number of RPMs to retain
+NRPMS=8
+# what architecture does rpm think we have?
+ARCH=`rpm --eval '%{_arch}'`
 
 echo "$0 running at "`date`
 
@@ -40,6 +44,14 @@ else
 	#set the version we just built in rh_old_revsion so it won't be built again
 	echo $new_rev > $HOME/rh_old_revsion
 	#move files to be uploaded
-	destination = `/bin/cat /etc/redhat-release | /usr/bin/awk '{print $1}'``/bin/uname -p`
-	echo rsync -ue /usr/bin/ssh $HOME/rpmbuild/RPMS/hoomd*.rpm joaander@foxx.engin.umich.edu:$destination
+	destination="devel/incoming"`/bin/cat /etc/redhat-release | /usr/bin/awk '{print $1}' | tr '[:upper:]' '[:lower:]'`
+	rsync -ue /usr/bin/ssh $HOME/rpmbuild/RPMS/$ARCH/hoomd*.rpm joaander@foxx.engin.umich.edu:$destination/
 fi
+
+#clean up
+cd $HOME/rpmbuild/RPMS/$ARCH
+rpmfiles=( `ls -td hoomd*.rpm` )
+numfiles=${#rpmfiles[*]}
+for ((  i=$(( $NRPMS )); $i < $numfiles ; i++ )); do
+	rm ${rpmfiles[$i]};
+done
