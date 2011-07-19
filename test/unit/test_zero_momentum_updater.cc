ed33cf1282cc5160394c9b52a40605cd1b667e98
Joshua A. Anderson <joaander@umich.edu>
2011-07-19 15:38:09 +0000

    Unit test for zero momentum updater verifies that rigid bodies are handled properly.
    git-svn-id: file:///Users/joaander/devel/hoomd-git-convert/repos/branches/rigid-bodies@4075 fa922fa7-2fde-0310-acd8-f43f465a7996

--- test/unit/test_zero_momentum_updater.cc
+++ test/unit/test_zero_momentum_updater.cc
@@ -115,6 +115,89 @@ BOOST_AUTO_TEST_CASE( ZeroMomentumUpdater_basic )
     MY_BOOST_CHECK_SMALL(avg_pz, tol_small);
     }
 
+//! boost test case to verify proper operation of ZeroMomentumUpdater with rigid bodies
+BOOST_AUTO_TEST_CASE( ZeroMomentumUpdater_rigid )
+    {
+    // create a simple particle data to test with
+    shared_ptr<SystemDefinition> sysdef(new SystemDefinition(8, BoxDim(1000.0), 4));
+    shared_ptr<ParticleData> pdata = sysdef->getParticleData();
+    
+    // set the first 4 particles to be free particles (position doesn't matter, but set it anyways)
+    pdata->setPosition(0, make_scalar3(0,0,0));
+    pdata->setPosition(1, make_scalar3(1,1,1));
+    pdata->setPosition(2, make_scalar3(2,2,2));
+    pdata->setPosition(3, make_scalar3(3,3,3));
+    
+    pdata->setBody(0, -1);
+    pdata->setBody(1, -1);
+    pdata->setBody(2, -1);
+    pdata->setBody(3, -1);
+    
+    // pick some random velocities and masses to make life interesting
+    pdata->setMass(0, 1.5);
+    pdata->setMass(1, 8.5);
+    pdata->setMass(2, 2.5);
+    pdata->setMass(3, 0.5);
+    
+    pdata->setVelocity(0, make_scalar3(-1.0, 4.0, 8.0));
+    pdata->setVelocity(1, make_scalar3(-3.2, -1.5, -9.2));
+    pdata->setVelocity(2, make_scalar3(2.1, 1.1, -4.5));
+    pdata->setVelocity(3, make_scalar3(-3.2, 8.7, 4.9));
+    
+    // now, set up the other 4 particles as 2 dimers
+    pdata->setPosition(4, make_scalar3(-1,0,0));
+    pdata->setPosition(5, make_scalar3(-2,0,0));
+    pdata->setPosition(6, make_scalar3(1,1,0));
+    pdata->setPosition(7, make_scalar3(1,2,0));
+    
+    pdata->setBody(4, 0);
+    pdata->setBody(5, 0);
+    pdata->setBody(6, 1);
+    pdata->setBody(7, 1);
+    
+    shared_ptr<RigidData> rdata = sysdef->getRigidData();
+    // Initialize rigid bodies
+    rdata->initializeData();
+    
+    // give them an initial COM velocity and angular momentum
+    rdata->setBodyVel(0, make_scalar3(1.0, 8.0, -3.0));
+    rdata->setBodyVel(1, make_scalar3(2.0, 12.0, -2.0));
+    
+    rdata->setAngMom(0, make_scalar4(0.5, 2.3, 1.4, 0.0));
+    rdata->setAngMom(1, make_scalar4(8.2, 3.1, 6.4, 0.0));
+    
+    rdata->setRV(false);
+    
+    // construct the updater and make sure everything is set properly
+    shared_ptr<ZeroMomentumUpdater> zerop(new ZeroMomentumUpdater(sysdef));
+    
+    // run the updater and check the new temperature
+    zerop->update(0);
+    
+    // temp variables for holding the sums
+    Scalar sum_px = 0.0;
+    Scalar sum_py = 0.0;
+    Scalar sum_pz = 0.0;
+    
+    for (unsigned int i = 0; i < pdata->getN(); i++)
+        {
+        Scalar mass = pdata->getMass(i);
+        Scalar3 vel = pdata->getVelocity(i);
+        sum_px += mass * vel.x;
+        sum_py += mass * vel.y;
+        sum_pz += mass * vel.z;
+        }
+    
+    // calculate the average
+    Scalar avg_px = sum_px / Scalar(pdata->getN());
+    Scalar avg_py = sum_py / Scalar(pdata->getN());
+    Scalar avg_pz = sum_pz / Scalar(pdata->getN());
+    
+    MY_BOOST_CHECK_SMALL(avg_px, tol_small);
+    MY_BOOST_CHECK_SMALL(avg_py, tol_small);
+    MY_BOOST_CHECK_SMALL(avg_pz, tol_small);
+    }
+
 #ifdef WIN32
 #pragma warning( pop )
 #endif
