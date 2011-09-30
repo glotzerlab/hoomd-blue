651e13fec98dc19adf107b95b6facf022e2fd409
Joshua A. Anderson <joaander@umich.edu>
2011-09-30 15:20:41 -0400

    Break up install directories and version information
    The install directories need to be set before add_subdir(CMake), but the version information cannot be set until after add_subdir(CMake).. thus, the need for this change.

--- CMake_install_options.cmake
+++ CMake_install_options.cmake
@@ -34,19 +34,3 @@ else (WIN32)
     set(INC_INSTALL_DIR "include/hoomd")
 endif (WIN32)
 
-
-################################
-## Version information
-set(HOOMD_VERSION_MAJOR "0")
-set(HOOMD_VERSION_MINOR "9")
-set(HOOMD_VERSION_PATCH "2")
-set(HOOMD_VERSION "${HOOMD_VERSION_MAJOR}.${HOOMD_VERSION_MINOR}.${HOOMD_VERSION_PATCH}")
-
-# users may not have git installed, or this may be a tarball build - set a dummy version if that is the case
-include(GetGitRevisionDescription)
-git_describe(HOOMD_GIT_VERSION)
-if (HOOMD_GIT_VERSION)
-    set(HOOMD_VERSION_LONG "${HOOMD_GIT_VERSION}")
-else (HOOMD_GIT_VERSION)
-    set(HOOMD_VERSION_LONG "${HOOMD_VERSION}-unknown")
-endif (HOOMD_GIT_VERSION)
