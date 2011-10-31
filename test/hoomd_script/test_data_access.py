fd76f10dbe1c2abc4566a93f4ac9c23e028a9f5e
Joshua A. Anderson <joaander@umich.edu>
2011-10-31 10:35:42 -0400

    Add unit tests for dynamic bonds
    Added a hoomd_script level unit test for data access, addition, and removal of bonds, angles, dihedrals, and impropers

--- test/hoomd_script/test_data_access.py
+++ test/hoomd_script/test_data_access.py
@@ -86,6 +86,167 @@ class data_access_tests (unittest.TestCase):
         del self.s
         init.reset();
 
+# tests for bond, angle, dihedral, and improper data access
+class data_access_tests (unittest.TestCase):
+    def setUp(self):
+        print
+        self.s = init.create_empty(N=100, box=(10,10,10), n_bond_types=2, n_angle_types=2, n_dihedral_types=2, n_improper_types=2);
+        import __main__;
+        __main__.sorter.set_params(grid=8)
+
+    # tests bonds
+    def test_bonds(self):
+        self.assertEqual(0, len(self.s.bonds));
+        
+        # add some bonds
+        b0 = self.s.bonds.add('bondA', 0, 1);
+        self.assertEqual(1, len(self.s.bonds));
+        b1 = self.s.bonds.add('bondA', 10, 11);
+        self.assertEqual(2, len(self.s.bonds));
+        b2 = self.s.bonds.add('bondB', 50, 20);
+        self.assertEqual(3, len(self.s.bonds));
+        
+        # check that we can get all bond parameters
+        for b in self.s.bonds:
+            b.tag
+            b.typeid
+            b.type
+            b.a
+            b.b
+            
+        # test deletion by tag
+        self.s.bonds.remove(b1);
+        self.assertEqual(2, len(self.s.bonds));
+        
+        # test deletion by index (check bond a value to delete the bond with a=0)
+        if self.s.bonds[0].tag == b0:
+            del self.s.bonds[0];
+        else:
+            del self.s.bonds[1];
+        
+        self.assertEqual(b2, self.s.bonds[0].tag);
+        self.assertEqual(50, self.s.bonds[0].a);
+        self.assertEqual(20, self.s.bonds[0].b);
+        self.assertEqual('bondB', self.s.bonds[0].type);
+
+    # tests angles
+    def test_angles(self):
+        self.assertEqual(0, len(self.s.angles));
+        
+        # add some angles
+        b0 = self.s.angles.add('angleA', 0, 1, 2);
+        self.assertEqual(1, len(self.s.angles));
+        b1 = self.s.angles.add('angleA', 10, 11, 12);
+        self.assertEqual(2, len(self.s.angles));
+        b2 = self.s.angles.add('angleB', 50, 20, 10);
+        self.assertEqual(3, len(self.s.angles));
+        
+        # check that we can get all bond parameters
+        for b in self.s.angles:
+            b.tag
+            b.typeid
+            b.type
+            b.a
+            b.b
+            b.c
+            
+        # test deletion by tag
+        self.s.angles.remove(b1);
+        self.assertEqual(2, len(self.s.angles));
+        
+        # test deletion by index (check bond a value to delete the bond with a=0)
+        if self.s.angles[0].tag == b0:
+            del self.s.angles[0];
+        else:
+            del self.s.angles[1];
+        
+        self.assertEqual(b2, self.s.angles[0].tag);
+        self.assertEqual(50, self.s.angles[0].a);
+        self.assertEqual(20, self.s.angles[0].b);
+        self.assertEqual(10, self.s.angles[0].c);
+        self.assertEqual('angleB', self.s.angles[0].type);
+
+    # tests dihedrals
+    def test_dihedrals(self):
+        self.assertEqual(0, len(self.s.dihedrals));
+        
+        # add some dihedrals
+        b0 = self.s.dihedrals.add('dihedralA', 0, 1, 2, 3);
+        self.assertEqual(1, len(self.s.dihedrals));
+        b1 = self.s.dihedrals.add('dihedralA', 10, 11, 12, 13);
+        self.assertEqual(2, len(self.s.dihedrals));
+        b2 = self.s.dihedrals.add('dihedralB', 50, 20, 10, 1);
+        self.assertEqual(3, len(self.s.dihedrals));
+        
+        # check that we can get all bond parameters
+        for b in self.s.dihedrals:
+            b.tag
+            b.typeid
+            b.type
+            b.a
+            b.b
+            b.c
+            b.d
+            
+        # test deletion by tag
+        self.s.dihedrals.remove(b1);
+        self.assertEqual(2, len(self.s.dihedrals));
+        
+        # test deletion by index (check bond a value to delete the bond with a=0)
+        if self.s.dihedrals[0].tag == b0:
+            del self.s.dihedrals[0];
+        else:
+            del self.s.dihedrals[1];
+        
+        self.assertEqual(b2, self.s.dihedrals[0].tag);
+        self.assertEqual(50, self.s.dihedrals[0].a);
+        self.assertEqual(20, self.s.dihedrals[0].b);
+        self.assertEqual(10, self.s.dihedrals[0].c);
+        self.assertEqual(1, self.s.dihedrals[0].d);
+        self.assertEqual('dihedralB', self.s.dihedrals[0].type);
+
+    # tests impropers
+    def test_impropers(self):
+        self.assertEqual(0, len(self.s.impropers));
+        
+        # add some impropers
+        b0 = self.s.impropers.add('dihedralA', 0, 1, 2, 3);
+        self.assertEqual(1, len(self.s.impropers));
+        b1 = self.s.impropers.add('dihedralA', 10, 11, 12, 13);
+        self.assertEqual(2, len(self.s.impropers));
+        b2 = self.s.impropers.add('dihedralB', 50, 20, 10, 1);
+        self.assertEqual(3, len(self.s.impropers));
+        
+        # check that we can get all bond parameters
+        for b in self.s.impropers:
+            b.tag
+            b.typeid
+            b.type
+            b.a
+            b.b
+            b.c
+            b.d
+            
+        # test deletion by tag
+        self.s.impropers.remove(b1);
+        self.assertEqual(2, len(self.s.impropers));
+        
+        # test deletion by index (check bond a value to delete the bond with a=0)
+        if self.s.impropers[0].tag == b0:
+            del self.s.impropers[0];
+        else:
+            del self.s.impropers[1];
+        
+        self.assertEqual(b2, self.s.impropers[0].tag);
+        self.assertEqual(50, self.s.impropers[0].a);
+        self.assertEqual(20, self.s.impropers[0].b);
+        self.assertEqual(10, self.s.impropers[0].c);
+        self.assertEqual(1, self.s.impropers[0].d);
+        self.assertEqual('dihedralB', self.s.impropers[0].type);
+
+    def tearDown(self):
+        del self.s
+        init.reset();
 
 if __name__ == '__main__':
     unittest.main(argv = ['test.py', '-v'])
