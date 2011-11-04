394f43d3ab6b28d23892e4b206558a43155af5b6
Joshua A. Anderson <joaander@umich.edu>
2011-11-04 13:53:52 -0400

    Update license with attribution clause
    The license has been revised somewhat with the assistance of the The University of Michigan General Counsel to provide more information regarding derivative works and citations.

--- libhoomd/analyzers/Analyzer.cc
+++ libhoomd/analyzers/Analyzer.cc
@@ -8,27 +8,36 @@ HOOMD-blue may contain modifications ("Contributions") provided, and to which
 copyright is held, by various Contributors who have granted The Regents of the
 University of Michigan the right to modify and/or distribute such Contributions.
 
-Redistribution and use of HOOMD-blue, in source and binary forms, with or
-without modification, are permitted, provided that the following conditions are
-met:
+You may redistribute, use, and create derivate works of HOOMD-blue, in source
+and binary forms, provided you abide by the following conditions:
 
 * Redistributions of source code must retain the above copyright notice, this
-list of conditions, and the following disclaimer.
+list of conditions, and the following disclaimer both in the code and
+prominently in any materials provided with the distribution.
 
 * Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions, and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
 
-* Neither the name of the copyright holder nor the names of HOOMD-blue's
-contributors may be used to endorse or promote products derived from this
-software without specific prior written permission.
+* All publications based on HOOMD-blue, including any reports or published
+results obtained, in whole or in part, with HOOMD-blue, will acknowledge its use
+according to the terms posted at the time of submission on:
+http://codeblue.umich.edu/hoomd-blue/citations.html
+
+* Any electronic documents citing HOOMD-Blue will link to the HOOMD-Blue website
+at: http://codeblue.umich.edu/hoomd-blue/.
+
+* Apart from the above required attributions, neither the name of the copyright
+holder nor the names of HOOMD-blue's contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
 
 Disclaimer
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR
-ANY WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
+WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
 
 IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
 INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
