f52b35705fe0dc6198424e409e40212e5064fd82
Joshua A. Anderson <joaander@umich.edu>
2011-11-17 10:37:14 -0500

    Mark ENABLE_APP_BUNDLE_INSTALL advanced
    Users should not enable this option. Hide it to remove the temptation.

--- CMake_install_options.cmake
+++ CMake_install_options.cmake
@@ -3,6 +3,7 @@
 # option to build Mac OS X .app packages
 if (APPLE)
 option(ENABLE_APP_BUNDLE_INSTALL "Enable installation of an app bundle installation" OFF)
+mark_as_advanced(ENABLE_APP_BUNDLE_INSTALL)
 endif(APPLE)
 
 option(ENABLE_EMBED_CUDA "Enable embedding of the CUDA libraries into lib/hoomd" OFF)
