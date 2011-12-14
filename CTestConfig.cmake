734e2e112968fed37d2d1e81cd00aee1efc900e8
Joshua A. Anderson <joaander@umich.edu>
2011-12-14 08:49:08 -0500

    Dynamically pick the ctest drop site based on site_name
    nyx firewalls outgoing connections from compute nodes, so a ssh tunnel is needed to get out. This commit enables that tunnel when SITE_NAME is nyx.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,16 @@ set(CTEST_PROJECT_NAME "HOOMD")
 set(CTEST_NIGHTLY_START_TIME "23:00:00 CST")
 
 set(CTEST_DROP_METHOD "http")
+
+# this convoluted setup seems to be the only way to get the SITE_NAME variable
+set(SITE ${SITE_NAME})
+
+# the automated test on nyx need to be ssh-tunneled out of the compute node
+if (SITE MATCHES "nyx")
+set(CTEST_DROP_SITE "localhost:8080")
+else (SITE MATCHES "nyx")
 set(CTEST_DROP_SITE "cdash.fourpisolutions.com")
+endif(SITE MATCHES "nyx")
+
 set(CTEST_DROP_LOCATION "/submit.php?project=HOOMD")
 set(CTEST_DROP_SITE_CDASH TRUE)
