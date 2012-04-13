2c829666a4723ac73adf78000805614c9f6d6a8c
Carolyn Phillips <phillicl@umich.edu>
2012-04-13 15:55:38 -0500

    added files for table bonds. Compiles

---
+++ libhoomd/computes/BondTablePotential.cc
@@ -0,0 +1,354 @@
+/*
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008-2011 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+You may redistribute, use, and create derivate works of HOOMD-blue, in source
+and binary forms, provided you abide by the following conditions:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer both in the code and
+prominently in any materials provided with the distribution.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* All publications and presentations based on HOOMD-blue, including any reports
+or published results obtained, in whole or in part, with HOOMD-blue, will
+acknowledge its use according to the terms posted at the time of submission on:
+http://codeblue.umich.edu/hoomd-blue/citations.html
+
+* Any electronic documents citing HOOMD-Blue will link to the HOOMD-Blue website:
+http://codeblue.umich.edu/hoomd-blue/
+
+* Apart from the above required attributions, neither the name of the copyright
+holder nor the names of HOOMD-blue's contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
+WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// Maintainer: phillicl
+
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4103 4244 4267 )
+#endif
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+#include "BondTablePotential.h"
+
+#ifdef ENABLE_OPENMP
+#include <omp.h>
+#endif
+
+#include <stdexcept>
+
+/*! \file BondTablePotential.cc
+    \brief Defines the BondTablePotential class
+*/
+
+using namespace std;
+
+/*! \param sysdef System to compute forces on
+    \param table_width Width the tables will be in memory
+    \param log_suffix Name given to this instance of the table potential
+*/
+BondTablePotential::BondTablePotential(boost::shared_ptr<SystemDefinition> sysdef,
+                               unsigned int table_width,
+                               const std::string& log_suffix)
+        : ForceCompute(sysdef), m_table_width(table_width)
+    {
+    assert(m_pdata);
+
+    // access the bond data for later use
+    m_bond_data = m_sysdef->getBondData();
+    
+    if (table_width == 0)
+        {
+        cerr << endl << "***Error! Table width of 0 given to BondTablePotential makes no sense" << endl << endl;
+        throw runtime_error("Error initializing BondTablePotential");
+        }
+        
+    
+    // allocate storage for the tables 
+    GPUArray<float2> tables(m_table_width, exec_conf);
+    m_tables.swap(tables);
+
+    assert(!m_tables.isNull());
+    
+    m_log_name = std::string("pair_table_energy") + log_suffix;
+    }
+
+/*! \param type Type of the bond to set parameters for
+    \param V Table for the potential V
+    \param F Table for the potential F (must be - dV / dr)
+    \param rmin Minimum r in the potential
+    \param rmax Maximum r in the potential
+    \post Values from \a V and \a F are copied into the interal storage for type pair (type)
+    \note There is no need to call this again for type
+    \note See BondTablePotential for a detailed definiton of rmin and rmax
+*/
+void BondTablePotential::setTable(unsigned int type,
+                              const std::vector<float> &V,
+                              const std::vector<float> &F,
+                              Scalar rmin,
+                              Scalar rmax)
+    {
+    
+    // make sure the type is valid
+    if (type >= m_bond_data->getNBondTypes())
+        {
+        cout << endl << "***Error! Invalid bond type specified" << endl << endl;
+        throw runtime_error("Error setting parameters in PotentialBond");
+        }    
+    
+    
+    
+    // access the arrays
+    ArrayHandle<float2> h_tables(m_tables, access_location::host, access_mode::readwrite);
+    ArrayHandle<Scalar4> h_params(m_params, access_location::host, access_mode::readwrite);
+    
+    // range check on the parameters
+    if (rmin < 0 || rmax < 0 || rmax <= rmin)
+        {
+        cerr << endl << "***Error! rmin, rmax (" << rmin << "," << rmax
+             << ") given to BondTablePotential make no sense." << endl << endl;
+        throw runtime_error("Error initializing BondTablePotential");
+        }
+        
+    if (V.size() != m_table_width || F.size() != m_table_width)
+        {
+        cerr << endl << "***Error! table provided to setTable is not of the correct size" << endl << endl;
+        throw runtime_error("Error initializing BondTablePotential");
+        }
+        
+    // fill out the parameters
+    h_params.data[type].x = rmin;
+    h_params.data[type].y = rmax;
+    h_params.data[type].z = (rmax - rmin) / Scalar(m_table_width - 1);
+    
+    // fill out the table
+    for (unsigned int i = 0; i < m_table_width; i++)
+        {
+        h_tables.data[i].x = V[i];
+        h_tables.data[i].y = F[i];
+        }
+    }
+
+/*! BondTablePotential provides
+    - \c pair_table_energy
+*/
+std::vector< std::string > BondTablePotential::getProvidedLogQuantities()
+    {
+    vector<string> list;
+    list.push_back(m_log_name);
+    return list;
+    }
+
+Scalar BondTablePotential::getLogValue(const std::string& quantity, unsigned int timestep)
+    {
+    if (quantity == m_log_name)
+        {
+        compute(timestep);
+        return calcEnergySum();
+        }
+    else
+        {
+        cerr << endl << "***Error! " << quantity << " is not a valid log quantity for BondTablePotential" << endl << endl;
+        throw runtime_error("Error getting log value");
+        }
+    }
+
+/*! \post The table based forces are computed for the given timestep. The neighborlist's
+compute method is called to ensure that it is up to date.
+
+\param timestep specifies the current time step of the simulation
+*/
+void BondTablePotential::computeForces(unsigned int timestep)
+    {
+    
+    // start the profile for this compute
+    if (m_prof) m_prof->push("Bond Table pair");
+    
+    
+    // access the particle data
+    ArrayHandle<Scalar4> h_pos(m_pdata->getPositions(), access_location::host, access_mode::read);
+    ArrayHandle<Scalar4> h_force(m_force,access_location::host, access_mode::overwrite);
+    ArrayHandle<Scalar> h_virial(m_virial,access_location::host, access_mode::overwrite);
+    ArrayHandle<unsigned int> h_rtag(m_pdata->getRTags(), access_location::host, access_mode::read);
+   
+    
+    // there are enough other checks on the input data: but it doesn't hurt to be safe
+    assert(h_force.data);
+    assert(h_virial.data);
+    assert(h_pos.data);
+    
+    // Zero data for force calculation.
+    memset((void*)h_force.data,0,sizeof(Scalar4)*m_force.getNumElements());
+    memset((void*)h_virial.data,0,sizeof(Scalar)*m_virial.getNumElements());
+
+    // get a local copy of the simulation box too
+    const BoxDim& box = m_pdata->getBox();
+    // sanity check
+    assert(box.xhi > box.xlo && box.yhi > box.ylo && box.zhi > box.zlo);
+    
+    // precalculate box lengths for use in the periodic imaging
+    Scalar Lx = box.xhi - box.xlo;
+    Scalar Ly = box.yhi - box.ylo;
+    Scalar Lz = box.zhi - box.zlo;
+    
+    // access the table data
+    ArrayHandle<float2> h_tables(m_tables, access_location::host, access_mode::read);
+    ArrayHandle<Scalar4> h_params(m_params, access_location::host, access_mode::read);
+    
+
+    // for each of the bonds
+    const unsigned int size = (unsigned int)m_bond_data->getNumBonds();
+    for (unsigned int i = 0; i < size; i++)
+        {
+        // lookup the tag of each of the particles participating in the bond
+        const Bond& bond = m_bond_data->getBond(i);
+        assert(bond.a < m_pdata->getN());
+        assert(bond.b < m_pdata->getN());
+            
+        // transform a and b into indicies into the particle data arrays
+        // (MEM TRANSFER: 4 integers)
+        unsigned int idx_a = h_rtag.data[bond.a];
+        unsigned int idx_b = h_rtag.data[bond.b];
+        assert(idx_a < m_pdata->getN());
+        assert(idx_b < m_pdata->getN());
+
+        // calculate d\vec{r}
+        // (MEM TRANSFER: 6 Scalars / FLOPS: 3)
+        Scalar dx = h_pos.data[idx_b].x - h_pos.data[idx_a].x;
+        Scalar dy = h_pos.data[idx_b].y - h_pos.data[idx_a].y;
+        Scalar dz = h_pos.data[idx_b].z - h_pos.data[idx_a].z;
+                     
+            
+            // apply periodic boundary conditions
+            if (dx >= box.xhi)
+                dx -= Lx;
+            else if (dx < box.xlo)
+                dx += Lx;
+                
+            if (dy >= box.yhi)
+                dy -= Ly;
+            else if (dy < box.ylo)
+                dy += Ly;
+                
+            if (dz >= box.zhi)
+                dz -= Lz;
+            else if (dz < box.zlo)
+                dz += Lz;
+                
+            // access needed parameters
+            Scalar4 params = h_params.data[bond.type];
+            Scalar rmin = params.x;
+            Scalar rmax = params.y;
+            Scalar delta_r = params.z;
+            
+            // start computing the force
+            Scalar rsq = dx*dx + dy*dy + dz*dz;
+            Scalar r = sqrt(rsq);
+            
+            // only compute the force if the particles are within the region defined by V
+            
+            if (r < rmax && r >= rmin)
+                {
+                // precomputed term
+                Scalar value_f = (r - rmin) / delta_r;
+                
+                // compute index into the table and read in values
+                
+                /// Here we use the table!!
+                unsigned int value_i = (unsigned int)floor(value_f);
+                float2 VF0 = h_tables.data[value_i];
+                float2 VF1 = h_tables.data[value_i+1];
+                // unpack the data
+                Scalar V0 = VF0.x;
+                Scalar V1 = VF1.x;
+                Scalar F0 = VF0.y;
+                Scalar F1 = VF1.y;
+                
+                // compute the linear interpolation coefficient
+                Scalar f = value_f - float(value_i);
+                
+                // interpolate to get V and F;
+                Scalar V = V0 + f * (V1 - V0);
+                Scalar F = F0 + f * (F1 - F0);
+                
+                // convert to standard variables used by the other pair computes in HOOMD-blue
+                Scalar force_divr = Scalar(0.0);
+                if (r > Scalar(0.0))
+                    force_divr = F / r;
+                Scalar bond_eng = Scalar(0.5) * V;
+                
+                // compute the virial
+                Scalar bond_virial[6];
+                Scalar force_div2r = Scalar(0.5) * force_divr;
+                bond_virial[0] = dx * dx * force_div2r; // xx
+                bond_virial[1] = dx * dy * force_div2r; // xy
+                bond_virial[2] = dx * dz * force_div2r; // xz
+                bond_virial[3] = dy * dy * force_div2r; // yy
+                bond_virial[4] = dy * dz * force_div2r; // yz
+                bond_virial[5] = dz * dz * force_div2r; // zz
+
+                // add the force to the particles
+                // (MEM TRANSFER: 20 Scalars / FLOPS 16)
+                h_force.data[idx_b].x += force_divr * dx;
+                h_force.data[idx_b].y += force_divr * dy;
+                h_force.data[idx_b].z += force_divr * dz;
+                h_force.data[idx_b].w += bond_eng;
+                for (unsigned int i = 0; i < 6; i++)
+                    h_virial.data[i*m_virial_pitch+idx_b]  += bond_virial[i];
+
+                h_force.data[idx_a].x -= force_divr * dx;
+                h_force.data[idx_a].y -= force_divr * dy;
+                h_force.data[idx_a].z -= force_divr * dz;
+                h_force.data[idx_a].w += bond_eng;
+                for (unsigned int i = 0; i < 6; i++)
+                    h_virial.data[i*m_virial_pitch+idx_a]  += bond_virial[i];
+    
+                }
+
+           }    
+    if (m_prof) m_prof->pop();
+    }
+
+//! Exports the BondTablePotential class to python
+void export_BondTablePotential()
+    {
+    class_<BondTablePotential, boost::shared_ptr<BondTablePotential>, bases<ForceCompute>, boost::noncopyable >
+    ("BondTablePotential", init< boost::shared_ptr<SystemDefinition>, unsigned int, const std::string& >())
+    .def("setTable", &BondTablePotential::setTable)
+    ;
+    
+    }
+
+#ifdef WIN32
+#pragma warning( pop )
+#endif
+
---
+++ libhoomd/computes/BondTablePotential.h
@@ -0,0 +1,136 @@
+/*
+Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
+(HOOMD-blue) Open Source Software License Copyright 2008-2011 Ames Laboratory
+Iowa State University and The Regents of the University of Michigan All rights
+reserved.
+
+HOOMD-blue may contain modifications ("Contributions") provided, and to which
+copyright is held, by various Contributors who have granted The Regents of the
+University of Michigan the right to modify and/or distribute such Contributions.
+
+You may redistribute, use, and create derivate works of HOOMD-blue, in source
+and binary forms, provided you abide by the following conditions:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions, and the following disclaimer both in the code and
+prominently in any materials provided with the distribution.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions, and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* All publications and presentations based on HOOMD-blue, including any reports
+or published results obtained, in whole or in part, with HOOMD-blue, will
+acknowledge its use according to the terms posted at the time of submission on:
+http://codeblue.umich.edu/hoomd-blue/citations.html
+
+* Any electronic documents citing HOOMD-Blue will link to the HOOMD-Blue website:
+http://codeblue.umich.edu/hoomd-blue/
+
+* Apart from the above required attributions, neither the name of the copyright
+holder nor the names of HOOMD-blue's contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+Disclaimer
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
+WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// Maintainer: phillicl
+
+#include <boost/shared_ptr.hpp>
+
+#include "ForceCompute.h"
+#include "BondData.h"
+#include "GPUArray.h"
+
+/*! \file BondTablePotential.h
+    \brief Declares the BondTablePotential class
+*/
+
+#ifdef NVCC
+#error This header cannot be compiled by nvcc
+#endif
+
+#ifndef __BONDTABLEPOTENTIAL_H__
+#define __BONDTABLEPOTENTIAL_H__
+
+//! Computes the potential and force on bonds based on values given in a table
+/*! \b Overview
+    Bond potentials and forces are evaluated for all bonded particle pairs in the system.
+    Both the potentials and forces** are provided the tables V(r) and F(r) at discreet \a r values between \a rmin and
+    \a rmax. Evaluations are performed by simple linear interpolation, thus why F(r) must be explicitly specified to
+    avoid large errors resulting from the numerical derivative. Note that F(r) should store - dV/dr.
+
+    \b Table memory layout
+
+    V(r) and F(r) are specified for each bond type. 
+
+    Three parameters need to be stored for each bond potential: rmin, rmax, and dr, the minimum r, maximum r, and spacing
+    between r values in the table respectively. For simple access on the GPU, these will be stored in a float4 where
+    x is rmin, y is rmax, and z is dr. 
+
+    V(0) is the value of V at r=rmin. V(i) is the value of V at r=rmin + dr * i where i is chosen such that r >= rmin
+    and r <= rmax. V(r) for r < rmin and > rmax is 0. The same goes for F. Thus V and F are defined between the region
+    [rmin,rmax], inclusive.
+
+    For ease of storing the data, all tables must be of the same number of points for all bonds.
+
+    \b Interpolation
+    Values are interpolated linearly between two points straddling the given r. For a given r, the first point needed, i
+    can be calculated via i = floorf((r - rmin) / dr). The fraction between ri and ri+1 can be calculated via
+    f = (r - rmin) / dr - float(i). And the linear interpolation can then be performed via V(r) ~= Vi + f * (Vi+1 - Vi)
+    \ingroup computes
+*/
+class BondTablePotential : public ForceCompute
+    {
+    public:
+        //! Constructs the compute
+        BondTablePotential(boost::shared_ptr<SystemDefinition> sysdef,
+                       unsigned int table_width,
+                       const std::string& log_suffix="");
+                       
+        //! Destructor
+        virtual ~BondTablePotential() { }
+        
+        //! Set the table for a given type pair
+        virtual void setTable(unsigned int type,
+                              const std::vector<float> &V,
+                              const std::vector<float> &F,
+                              Scalar rmin,
+                              Scalar rmax);
+                              
+        //! Returns a list of log quantities this compute calculates
+        virtual std::vector< std::string > getProvidedLogQuantities();
+        
+        //! Calculates the requested log value and returns it
+        virtual Scalar getLogValue(const std::string& quantity, unsigned int timestep);
+        
+    protected:
+        boost::shared_ptr<BondData> m_bond_data;    //!< Bond data to use in computing bonds    
+        unsigned int m_table_width;                 //!< Width of the tables in memory
+        GPUArray<float2> m_tables;                  //!< Stored V and F tables
+        GPUArray<Scalar4> m_params;                 //!< Parameters stored for each table        
+        std::string m_log_name;                     //!< Cached log name
+        
+        //! Actually compute the forces
+        virtual void computeForces(unsigned int timestep);
+    };
+
+//! Exports the TablePotential class to python
+void export_BondTablePotential();
+
+#endif
+
