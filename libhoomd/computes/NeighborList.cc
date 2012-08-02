22dccddd27d455a2324918798cc109b3f85beea0
Jens Glaser <jglaser@umn.edu>
2012-08-01 23:54:59 -0400

    Fixed merge conflict

--- libhoomd/computes/NeighborList.cc
+++ libhoomd/computes/NeighborList.cc
@@ -753,9 +753,6 @@ void NeighborList::setLastUpdatedPos()
         h_last_pos.data[i] = make_scalar4(h_pos.data[i].x, h_pos.data[i].y, h_pos.data[i].z, 0.0f);
         }
    
-    // update last box length
-    m_last_L = m_pdata->getGlobalBox().getL();
-
     if (m_prof) m_prof->pop();
     }
 
