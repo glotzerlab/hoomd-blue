544ca87df3d1f82e9f7daf2095659b5563624a7d
Joshua A. Anderson <joaander@umich.edu>
2012-08-15 10:38:46 -0400

    Fix python3 iterators in group
    refs #1034

--- python-module/hoomd_script/group.py
+++ python-module/hoomd_script/group.py
@@ -128,13 +128,16 @@ class group:
             self.index = 0;
         def __iter__(self):
             return self;
-        def next(self):
+        def __next__(self):
             if self.index == len(self.data):
                 raise StopIteration;
             
             result = self.data[self.index];
             self.index += 1;
             return result;
+        
+        # support python2
+        next = __next__;
     
     ## \internal
     # \brief Creates a group
