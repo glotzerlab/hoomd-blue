1331a3adf74cf85379d283c9c48b7c1f19cdb1cd
Joshua A. Anderson <joaander@umich.edu>
2012-11-28 14:00:46 -0500

    Disable bond removal test
    The feature is currently disabled.

--- test/hoomd_script/test_data_access.py
+++ test/hoomd_script/test_data_access.py
@@ -113,10 +113,12 @@ class bond_data_access_tests (unittest.TestCase):
             b.type
             b.a
             b.b
-            
+         
+        ####################################
+        ## The bond deletion feature is currently disabled   
         # test deletion by tag
-        self.s.bonds.remove(b1);
-        self.assertEqual(2, len(self.s.bonds));
+        #self.s.bonds.remove(b1);
+        #self.assertEqual(2, len(self.s.bonds));
         
         # test deletion by index (check bond a value to delete the bond with a=0)
         #if self.s.bonds[0].tag == b0:
