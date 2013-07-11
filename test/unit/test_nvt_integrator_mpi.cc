88a13139a3d59e47f0991cd0bf20d227e26380d6
Jens Glaser <jglaser@umn.edu>
2013-07-11 17:13:53 -0500

    Fixed NVT MPI unit test

--- test/unit/test_nvt_integrator_mpi.cc
+++ test/unit/test_nvt_integrator_mpi.cc
@@ -32,7 +32,7 @@ using namespace boost;
 
 void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf)
 {
-    // initialize two random particle system
+    // initialize random particle system
     Scalar phi_p = 0.2;
     unsigned int N = 20000;
     Scalar L = pow(M_PI/6.0/phi_p*Scalar(N),1.0/3.0);
@@ -53,10 +53,17 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
     boost::shared_ptr<DomainDecomposition> decomposition(new DomainDecomposition(exec_conf,snap->global_box.getL(), 0));
 
     shared_ptr<SystemDefinition> sysdef_1(new SystemDefinition(snap, exec_conf,decomposition));
-    shared_ptr<SystemDefinition> sysdef_2(new SystemDefinition(snap, exec_conf));
+
+    // initialize a second system (single proc) on rank zero
+    shared_ptr<SystemDefinition> sysdef_2;
+    if (exec_conf->getRank() == 0)
+        sysdef_2 = shared_ptr<SystemDefinition>(new SystemDefinition(snap, exec_conf));
 
     shared_ptr<ParticleData> pdata_1 = sysdef_1->getParticleData();
-    shared_ptr<ParticleData> pdata_2 = sysdef_2->getParticleData();
+
+    shared_ptr<ParticleData> pdata_2;
+    if (exec_conf->getRank() == 0)
+        pdata_2 = sysdef_2->getParticleData();
  
     boost::shared_ptr<Communicator> comm;
 #ifdef ENABLE_CUDA
@@ -69,24 +76,23 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
     shared_ptr<ParticleSelector> selector_all_1(new ParticleSelectorTag(sysdef_1, 0, pdata_1->getNGlobal()-1));
     shared_ptr<ParticleGroup> group_all_1(new ParticleGroup(sysdef_1, selector_all_1));
 
-    shared_ptr<ParticleSelector> selector_all_2(new ParticleSelectorTag(sysdef_2, 0, pdata_2->getNGlobal()-1));
-    shared_ptr<ParticleGroup> group_all_2(new ParticleGroup(sysdef_2, selector_all_2));
+    shared_ptr<ParticleSelector> selector_all_2;
+    shared_ptr<ParticleGroup> group_all_2;
+    if (exec_conf->getRank() ==0)
+        {
+        selector_all_2 = shared_ptr<ParticleSelector>(new ParticleSelectorTag(sysdef_2, 0, pdata_2->getNGlobal()-1));
+        group_all_2 = shared_ptr<ParticleGroup>(new ParticleGroup(sysdef_2, selector_all_2));
+        }
 
     Scalar r_cut = Scalar(3.0);
     Scalar r_buff = Scalar(0.8);
     shared_ptr<NeighborList> nlist_1(new NeighborListBinned(sysdef_1, r_cut, r_buff));
-    shared_ptr<NeighborList> nlist_2(new NeighborListBinned(sysdef_2, r_cut, r_buff));
 
     nlist_1->setStorageMode(NeighborList::full);
-    nlist_2->setStorageMode(NeighborList::full);
-
     nlist_1->setCommunicator(comm);
-
     shared_ptr<PotentialPairLJ> fc_1 = shared_ptr<PotentialPairLJ>(new PotentialPairLJ(sysdef_1, nlist_1));
-    shared_ptr<PotentialPairLJ> fc_2 = shared_ptr<PotentialPairLJ>(new PotentialPairLJ(sysdef_2, nlist_2));
 
     fc_1->setRcut(0, 0, r_cut);
-    fc_2->setRcut(0, 0, r_cut);
 
     // setup some values for alpha and sigma
     Scalar epsilon = Scalar(1.0);
@@ -95,19 +101,37 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
     Scalar lj1 = Scalar(4.0) * epsilon * pow(sigma,Scalar(12.0));
     Scalar lj2 = alpha * Scalar(4.0) * epsilon * pow(sigma,Scalar(6.0));
     fc_1->setParams(0,0,make_scalar2(lj1,lj2));
-    fc_2->setParams(0,0,make_scalar2(lj1,lj2));
+
+    shared_ptr<NeighborList> nlist_2;
+    shared_ptr<PotentialPairLJ> fc_2;
+    if (exec_conf->getRank() == 0)
+        {
+        nlist_2 = shared_ptr<NeighborList>(new NeighborListBinned(sysdef_2, r_cut, r_buff));
+        nlist_2->setStorageMode(NeighborList::full);
+        fc_2 = shared_ptr<PotentialPairLJ>(new PotentialPairLJ(sysdef_2, nlist_2));
+        fc_2->setRcut(0, 0, r_cut);
+        fc_2->setParams(0,0,make_scalar2(lj1,lj2));
+        }
 
     Scalar deltaT = Scalar(0.005);
     Scalar Q = Scalar(2.0);
     Scalar T = Scalar(1.5/3.0);
     Scalar tau = sqrt(Q / (Scalar(3.0) * T));
     shared_ptr<VariantConst> T_variant_1(new VariantConst(T));
-    shared_ptr<VariantConst> T_variant_2(new VariantConst(T));
     shared_ptr<IntegratorTwoStep> nvt_1(new IntegratorTwoStep(sysdef_1, deltaT));
-    shared_ptr<IntegratorTwoStep> nvt_2(new IntegratorTwoStep(sysdef_2, deltaT));
     shared_ptr<ComputeThermo> thermo_1 = shared_ptr<ComputeThermo>(new ComputeThermo(sysdef_1,group_all_1));
     thermo_1->setCommunicator(comm);
-    shared_ptr<ComputeThermo> thermo_2 = shared_ptr<ComputeThermo>(new ComputeThermo(sysdef_2,group_all_2));
+
+    shared_ptr<VariantConst> T_variant_2;
+    shared_ptr<IntegratorTwoStep> nvt_2;
+    shared_ptr<ComputeThermo> thermo_2;
+    
+    if (exec_conf->getRank()==0)
+        {
+        T_variant_2 = shared_ptr<VariantConst>(new VariantConst(T));
+        nvt_2  = shared_ptr<IntegratorTwoStep>(new IntegratorTwoStep(sysdef_2, deltaT));
+        thermo_2 = shared_ptr<ComputeThermo>(new ComputeThermo(sysdef_2,group_all_2));
+        }
 
     shared_ptr<TwoStepNVT> two_step_nvt_1;
     shared_ptr<TwoStepNVT> two_step_nvt_2;
@@ -115,27 +139,40 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
     if (exec_conf->isCUDAEnabled())
         {
         two_step_nvt_1 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVTGPU(sysdef_1, group_all_1, thermo_1, tau, T_variant_1));
-        two_step_nvt_2 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVTGPU(sysdef_2, group_all_2, thermo_2, tau, T_variant_2));
+        if (exec_conf->getRank() == 0)
+            two_step_nvt_2 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVTGPU(sysdef_2, group_all_2, thermo_2, tau, T_variant_2));
         }
     else
 #endif
-    two_step_nvt_1 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVT(sysdef_1, group_all_1, thermo_1, tau, T_variant_1));
-    two_step_nvt_2 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVT(sysdef_2, group_all_2, thermo_2, tau, T_variant_2));
+        {
+        two_step_nvt_1 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVT(sysdef_1, group_all_1, thermo_1, tau, T_variant_1));
+        if (exec_conf->getRank() == 0)
+            two_step_nvt_2 = boost::shared_ptr<TwoStepNVT>(new TwoStepNVT(sysdef_2, group_all_2, thermo_2, tau, T_variant_2));
+        }
 
     nvt_1->addIntegrationMethod(two_step_nvt_1);
     nvt_1->addForceCompute(fc_1);
     nvt_1->setCommunicator(comm);
 
-    nvt_2->addIntegrationMethod(two_step_nvt_2);
-    nvt_2->addForceCompute(fc_2);
+    if (exec_conf->getRank() == 0)
+        {
+        nvt_2->addIntegrationMethod(two_step_nvt_2);
+        nvt_2->addForceCompute(fc_2);
+        }
 
     unsigned int ndof = nvt_1->getNDOF(group_all_1);
-    BOOST_CHECK_EQUAL(ndof, nvt_2->getNDOF(group_all_2));
+    if (exec_conf->getRank() == 0)
+        BOOST_CHECK_EQUAL(ndof, nvt_2->getNDOF(group_all_2));
+
     thermo_1->setNDOF(ndof);
-    thermo_2->setNDOF(ndof);
+
+    if (exec_conf->getRank() == 0)
+        thermo_2->setNDOF(ndof);
 
     nvt_1->prepRun(0);
-    nvt_2->prepRun(0);
+    
+    if (exec_conf->getRank() == 0)
+        nvt_2->prepRun(0);
 
     SnapshotParticleData snap_1(N);
     SnapshotParticleData snap_2(N);
@@ -156,10 +193,10 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
             {
             pdata_1->takeSnapshot(snap_1);
             // ... against the serial simulation
-            pdata_2->takeSnapshot(snap_2);
 
             if (exec_conf->getRank() == 0)
                 {
+                pdata_2->takeSnapshot(snap_2);
                 // check position, velocity and acceleration
                 for (unsigned int j = 0; j < N; j++)
                     {
@@ -179,7 +216,8 @@ void test_nvt_integrator_mpi(boost::shared_ptr<ExecutionConfiguration> exec_conf
                 } 
             }
         nvt_1->update(i);
-        nvt_2->update(i);
+        if (exec_conf->getRank() == 0)
+            nvt_2->update(i);
         }
 
 }
