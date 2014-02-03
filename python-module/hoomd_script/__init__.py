4afe8d543c83315c97d7e90727f68ab93099844f
Joshua A. Anderson <joaander@umich.edu>
2014-02-03 08:37:18 -0500

    Use RTLD_GLOBAL in python sitedir builds

--- python-module/hoomd_script/__init__.py
+++ python-module/hoomd_script/__init__.py
@@ -49,9 +49,20 @@
 # -- end license --
 
 # Maintainer: joaander
+import sys;
+import ctypes;
+import os;
+
+# need to import HOOMD with RTLD_GLOBAL in python sitedir builds
+if 'HOOMD_PYTHON_SITEDIR' in os.environ:
+    flags = sys.getdlopenflags();
+    sys.setdlopenflags(flags | ctypes.RTLD_GLOBAL);
 
 import hoomd;
-import sys;
+
+if 'HOOMD_PYTHON_SITEDIR' in os.environ:
+    sys.setdlopenflags(flags);
+
 from hoomd_script import util;
 from hoomd_script import data;
 from hoomd_script import globals;
