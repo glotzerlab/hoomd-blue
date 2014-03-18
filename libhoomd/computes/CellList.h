918ca43e034a9b12e31be0b5487ffe6898ac5a6d
Joshua A. Anderson <joaander@umich.edu>
2014-03-18 09:36:27 -0400

    Remove unneeded force compute
    CellList::compute() properly forces a recompute of the cell list after a box change. So this manual force is not needed. Leaving the manual force in will cause unintended side effects - for example if the cell list compute() method is called twice in a time step after a box change. With m_force_compute set to true, this will result in the cell list being computed twice where it only needs to be computed once.

--- libhoomd/computes/CellList.h
+++ libhoomd/computes/CellList.h
@@ -225,7 +225,6 @@ class CellList : public Compute
         void slotBoxChanged()
             {
             m_box_changed = true;
-            m_force_compute = true;
             }
 
         //! Set the multiple value
