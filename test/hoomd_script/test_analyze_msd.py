ea60220c5b2811fa24f3765e1d086436d112ac9a
Joshua A. Anderson <joaander@umich.edu>
2014-10-06 10:31:26 -0400

    Enable parallel tests.
    Multiple tests were generating and removing test.log. This causes
    race conditions and test failures when running parallel ctests.

--- test/hoomd_script/test_analyze_msd.py
+++ test/hoomd_script/test_analyze_msd.py
@@ -15,28 +15,28 @@ class analyze_msd_tests (unittest.TestCase):
 
     # tests basic creation of the analyzer
     def test(self):
-        analyze.msd(period = 10, filename="test.log", groups=[group.all()]);
+        analyze.msd(period = 10, filename="test_analyze_msd.log", groups=[group.all()]);
         run(100);
 
     # test variable period
     def test_variable(self):
-        analyze.msd(period = lambda n: n*10, filename="test.log", groups=[group.all()]);
+        analyze.msd(period = lambda n: n*10, filename="test_analyze_msd.log", groups=[group.all()]);
         run(100);
 
     # test error if no groups defined
     def test_no_gropus(self):
-        self.assertRaises(RuntimeError, analyze.msd, period=10, filename="test.log", groups=[]);
+        self.assertRaises(RuntimeError, analyze.msd, period=10, filename="test_analyze_msd.log", groups=[]);
 
     # test set_params
     def test_set_params(self):
-        ana = analyze.msd(period = 10, filename="test.log", groups=[group.all()]);
+        ana = analyze.msd(period = 10, filename="test_analyze_msd.log", groups=[group.all()]);
         ana.set_params(delimiter = ' ');
         run(100);
 
     def tearDown(self):
         init.reset();
         if comm.get_rank() == 0:
-            os.remove("test.log");
+            os.remove("test_analyze_msd.log");
 
 if __name__ == '__main__':
     unittest.main(argv = ['test.py', '-v'])
