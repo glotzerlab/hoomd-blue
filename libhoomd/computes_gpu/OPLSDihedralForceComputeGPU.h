37dcd4b72bd0ece0fd2a4f91ea7d1d449b6fd9ed
Khalid Ahmed <nkahm@umich.edu>
2015-04-07 12:47:27 -0400

    Adding mpi.h for MPI_CUDA flag

--- libhoomd/computes_gpu/OPLSDihedralForceComputeGPU.h
+++ libhoomd/computes_gpu/OPLSDihedralForceComputeGPU.h
@@ -47,6 +47,10 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#ifdef ENABLE_MPI
+#include <mpi.h>
+#endif
+
 // Maintainer: ksil
 
 #include "OPLSDihedralForceCompute.h"
