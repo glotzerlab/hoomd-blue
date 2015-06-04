d877528d0871126b6a25287ab4dde321921e298e
Joshua A. Anderson <joaander@umich.edu>
2015-06-04 13:40:18 -0400

    Removed one more ref to windows

--- CMake/hoomd/HOOMDCFlagsSetup.cmake
+++ CMake/hoomd/HOOMDCFlagsSetup.cmake
@@ -41,19 +41,6 @@ if(NOT PASSED_FIRST_CONFIGURE)
         set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-march=${GCC_ARCH} -g -O3 -funroll-loops -DNDEBUG -Wall -Wno-unknown-pragmas" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
 
         endif (HONOR_GENTOO_FLAGS)
-    elseif(MSVC80)
-        # default flags for visual studio 2005
-        if (CMAKE_CL_64)
-            set(SSE_OPT "")
-        else (CMAKE_CL_64)
-            set(SSE_OPT "/arch:SSE2")
-        endif (CMAKE_CL_64)
-
-        set(CMAKE_CXX_FLAGS "/DWIN32 /D_WINDOWS /W3 /Zm1000 /EHs /GR /MP" CACHE STRING "Flags used by all build types." FORCE)
-        set(CMAKE_CXX_FLAGS_RELEASE "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /Ox /Ob2 /D NDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /Zi /Ox /Ob1 /D NDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /O1 /Ob1 /D NDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
-
     elseif(CMAKE_CXX_COMPILER MATCHES "icpc")
         # default flags for intel
         set(CMAKE_CXX_FLAGS_DEBUG "-xHOST -O0 -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
