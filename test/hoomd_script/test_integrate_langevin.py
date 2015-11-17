dfa4918dd2ecadeed496caafba56c2056e6626f2
Joshua A. Anderson <joaander@umich.edu>
2015-11-17 08:13:19 -0500

    Move langevin diffusion tests to python
    Also shorten them to run in 2 seconds instead of 50.
    
    refs #38

--- test/hoomd_script/test_integrate_langevin.py
+++ test/hoomd_script/test_integrate_langevin.py
@@ -4,6 +4,12 @@
 from hoomd_script import *
 import unittest
 import os
+import numpy
+import math
+context.initialize()
+
+def almost_equal(u, v, e=0.001):
+    return math.fabs(u-v)/math.fabs(u) <= e and math.fabs(u-v) / math.fabs(v) <= e;
 
 # unit tests for integrate.langevin
 class integrate_langevin_tests (unittest.TestCase):
@@ -63,6 +69,79 @@ class integrate_langevin_tests (unittest.TestCase):
     def tearDown(self):
         init.reset();
 
+
+# validate langevin diffusion
+class integrate_langevin_diffusion (unittest.TestCase):
+    def setUp(self):
+        print
+        snap = data.make_snapshot(N=1000, box=data.boxdim(L=100000), particle_types=['A'])
+        # this defaults all particles to position 0, which is what we want for this test
+        self.s = init.read_snapshot(snap)
+
+        sorter.set_params(grid=8)
+
+    def test_gamma(self):
+        # Setup an ideal gas with a gamma and T and validate the MSD
+        T=1.8
+        gamma=1;
+        dt=0.01;
+        steps=10000;
+
+        integrate.mode_standard(dt=dt);
+        bd = integrate.langevin(group.all(), T=T, seed=1, dscale=False);
+        bd.set_gamma('A', gamma);
+
+        run(steps);
+
+        snap = self.s.take_snapshot();
+        if comm.get_rank() == 0:
+            msd = numpy.mean(snap.particles.position[:,0] * snap.particles.position[:,0] +
+                             snap.particles.position[:,1] * snap.particles.position[:,1] +
+                             snap.particles.position[:,2] * snap.particles.position[:,2])
+
+            D = msd / (6*dt*steps);
+
+            vsq = numpy.mean(snap.particles.velocity[:,0] * snap.particles.velocity[:,0] +
+                             snap.particles.velocity[:,1] * snap.particles.velocity[:,1] +
+                             snap.particles.velocity[:,2] * snap.particles.velocity[:,2])
+
+            # check for a very crude overlap - we are not doing much averaging here to keep the test short
+            self.assert_(almost_equal(D, T/gamma, 0.1))
+
+            self.assert_(almost_equal(vsq, 3*T, 0.1))
+
+    def test_dscale(self):
+        # Setup an ideal gas with a gamma and T and validate the MSD
+        T=1.8
+        gamma=2;
+        dt=0.01;
+        steps=10000;
+
+        integrate.mode_standard(dt=dt);
+        bd = integrate.langevin(group.all(), T=T, seed=1, dscale=gamma);
+
+        run(steps);
+
+        snap = self.s.take_snapshot();
+        if comm.get_rank() == 0:
+            msd = numpy.mean(snap.particles.position[:,0] * snap.particles.position[:,0] +
+                             snap.particles.position[:,1] * snap.particles.position[:,1] +
+                             snap.particles.position[:,2] * snap.particles.position[:,2])
+
+            vsq = numpy.mean(snap.particles.velocity[:,0] * snap.particles.velocity[:,0] +
+                             snap.particles.velocity[:,1] * snap.particles.velocity[:,1] +
+                             snap.particles.velocity[:,2] * snap.particles.velocity[:,2])
+
+            D = msd / (6*dt*steps);
+
+            # check for a very crude overlap - we are not doing much averaging here to keep the test short
+            self.assert_(almost_equal(D, T/gamma, 0.1))
+
+            self.assert_(almost_equal(vsq, 3*T, 0.1))
+            print(vsq)
+
+    def tearDown(self):
+        init.reset();
 
 if __name__ == '__main__':
     unittest.main(argv = ['test.py', '-v'])
