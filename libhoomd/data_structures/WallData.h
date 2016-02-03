ad02e154e7c29aac00f301c193896dba2488acba
James Proctor <jproc@umich.edu>
2016-02-03 07:42:18 -0500

    formatting nonsense

--- libhoomd/data_structures/WallData.h
+++ libhoomd/data_structures/WallData.h
@@ -139,8 +139,9 @@ struct PlaneWall
     bool            inside;
     };
 
-// ! Point to wall vector for a sphere wall geometry
-// Returns 0 vector when all normal directions are equal
+//! Point to wall vector for a sphere wall geometry
+/* Returns 0 vector when all normal directions are equal
+*/
 DEVICE inline vec3<Scalar> vecPtToWall(const SphereWall& wall, const vec3<Scalar>& position, bool& inside)
     {
     vec3<Scalar> t = position;
@@ -161,8 +162,9 @@ DEVICE inline vec3<Scalar> vecPtToWall(const SphereWall& wall, const vec3<Scalar
         }
     };
 
-// ! Point to wall vector for a cylinder wall geometry
-// Returns 0 vector when all normal directions are equal
+//! Point to wall vector for a cylinder wall geometry
+/* Returns 0 vector when all normal directions are equal
+*/
 DEVICE inline vec3<Scalar> vecPtToWall(const CylinderWall& wall, const vec3<Scalar>& position, bool& inside)
     {
     vec3<Scalar> t = position;
