fdc80b60bf0c7ab729e1e19b64280e64fa0af598
Jens Glaser <jsglaser@umich.edu>
2016-02-19 01:10:54 -0500

    tentatively enable system.replicate() with rigid bodies
    need to test

--- libhoomd/data_structures/ParticleData.cc
+++ libhoomd/data_structures/ParticleData.cc
@@ -2974,7 +2974,7 @@ void SnapshotParticleData<Real>::replicate(unsigned int nx, unsigned int ny, uns
                     mass[k] = mass[i];
                     charge[k] = charge[i];
                     diameter[k] = diameter[i];
-                    body[k] = body[i];
+                    body[k] = j*old_size + body[i];
                     orientation[k] = orientation[i];
                     angmom[k] = angmom[i];
                     inertia[k] = inertia[i];
