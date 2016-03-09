cc267b69ee33d51ab72358a2d8a74caae5d55711
Chengyu Dai <Chengyu@Chengyus-MacBook-Pro.local>
2016-03-09 11:46:10 -0500

    add unittest

--- test/hoomd_script/test_integrate_langevin.py
+++ test/hoomd_script/test_integrate_langevin.py
@@ -32,6 +32,15 @@ class integrate_langevin_tests (unittest.TestCase):
         bd.disable();
         bd = integrate.langevin(all, T=1.2, seed=1, dscale=1.0, tally=True);
         run(100);
+        bd.disable();        
+        bd = integrate.langevin(all, T=1.2, seed=1, dscale=1.0, noiseless_t=True);
+        run(100);
+        bd.disable();
+        bd = integrate.langevin(all, T=1.2, seed=1, dscale=1.0, noiseless_r=True);
+        run(100);
+        bd.disable();        
+        bd = integrate.langevin(all, T=1.2, seed=1, dscale=1.0, noiseless_t=True, noiseless_r=True);
+        run(100);
         bd.disable();
 
     # test set_params
@@ -47,6 +56,13 @@ class integrate_langevin_tests (unittest.TestCase):
         bd = integrate.langevin(all, T=1.2, seed=1);
         bd.set_gamma('A', 0.5);
         bd.set_gamma('B', 1.0);
+    
+    # test set_gamma
+    def test_set_gamma_r(self):
+        all = group.all();
+        bd = integrate.langevin(all, T=1.2, seed=1);
+        bd.set_gamma_r('A', 0.5);
+        bd.set_gamma_r('B', 1.0);
 
     # test w/ empty group
     def test_empty(self):
@@ -79,6 +95,36 @@ class integrate_langevin_diffusion (unittest.TestCase):
         self.s = init.read_snapshot(snap)
 
         sorter.set_params(grid=8)
+        
+    def test_noiseless_t(self):
+        # Setup an ideal gas with a gamma and T and validate the MSD
+        T=1.8
+        gamma=1;
+        dt=0.01;
+        steps=10000;
+
+        integrate.mode_standard(dt=dt);
+        bd = integrate.langevin(group.all(), T=T, seed=1, dscale=False, noiseless_t=True);
+        bd.set_gamma('A', gamma);
+
+        run(steps);
+
+        snap = self.s.take_snapshot();
+        if comm.get_rank() == 0:
+            msd = numpy.mean(snap.particles.position[:,0] * snap.particles.position[:,0] +
+                             snap.particles.position[:,1] * snap.particles.position[:,1] +
+                             snap.particles.position[:,2] * snap.particles.position[:,2])
+
+            D = msd / (6*dt*steps);
+
+            vsq = numpy.mean(snap.particles.velocity[:,0] * snap.particles.velocity[:,0] +
+                             snap.particles.velocity[:,1] * snap.particles.velocity[:,1] +
+                             snap.particles.velocity[:,2] * snap.particles.velocity[:,2])
+
+            # check for a very crude overlap - we are not doing much averaging here to keep the test short
+            self.assert_(almost_equal(D, 0, 0.1))
+
+            self.assert_(almost_equal(vsq, 0, 0.1))
 
     def test_gamma(self):
         # Setup an ideal gas with a gamma and T and validate the MSD
