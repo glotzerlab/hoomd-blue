b3c9ad0b1225ba9c593f35743851327a8afb9b1b
Joshua A. Anderson <joaander@umich.edu>
2016-03-21 11:57:36 -0400

    Remove share/

--- CMake_install_options.cmake
+++ CMake_install_options.cmake
@@ -3,7 +3,6 @@
 option(ENABLE_EMBED_CUDA "Enable embedding of the CUDA libraries into lib/hoomd" OFF)
 mark_as_advanced(ENABLE_EMBED_CUDA)
 
-set(DATA_INSTALL_DIR "share/hoomd")
 set(LIB_INSTALL_DIR "lib${LIB_SUFFIX}/hoomd")
 set(LIB_BASE_INSTALL_DIR "lib${LIB_SUFFIX}")
 set(BIN_INSTALL_DIR "bin")
