1166cefd908ef46af420609727e8f617761fe29e
Joshua A. Anderson <joaander@umich.edu>
2016-04-29 10:54:01 -0400

    Embed dfft into _md
    This simplifies library pathing issues.

--- CMake_src_setup.cmake
+++ CMake_src_setup.cmake
@@ -6,12 +6,6 @@ include_directories(${HOOMD_SOURCE_DIR}
                     ${CUDA_INCLUDE}
                     ${HOOMD_BINARY_DIR}/include)
 
-if (ENABLE_MPI)
-    # include dfftlib headers
-    include_directories(${HOOMD_SOURCE_DIR}/libhoomd/extern/dfftlib/src)
-    include_directories("${PROJECT_BINARY_DIR}")
-endif(ENABLE_MPI)
-
 #######################
 ## Get the compile date
 exec_program("date +%x" OUTPUT_VARIABLE COMPILE_DATE)
