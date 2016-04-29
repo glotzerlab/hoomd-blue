d23b9297ce7f6fbf9550b466a4bdf8537671cc02
Joshua A. Anderson <joaander@umich.edu>
2016-04-29 12:06:45 -0400

    Add setup.py (for readthedocs builds)

---
+++ setup.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import sys
+from subprocess import call
+import os
+
+if len(sys.argv) == 1:
+    print("invalid input");
+    sys.exit(1)
+
+if sys.argv[1] != 'install':
+    print("invalid input");
+    sys.exit(1)
+
+if len(sys.argv) == 3 and sys.argv[2] != '--force':
+    print("invalid input");
+    sys.exit(1)
+
+if len(sys.argv) > 3:
+    print("invalid input");
+    sys.exit(1)
+
+call(["mkdir", "-p", "build"])
+os.chdir("build");
+call(["cmake", "../", "-DINSTALL_SITE=on", "-DBUILD_TESTING=off"])
+call(["make", "install", "-j4"])
