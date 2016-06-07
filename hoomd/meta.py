f61de0c085da6f72cb4fadd8a9f91bd9421c8aea
Carl Simon Adorf <csadorf@umich.edu>
2016-06-07 17:57:57 -0400

    Improve metadata output handling of non-unique entries.
    Fixes issue #117.

--- hoomd/meta.py
+++ hoomd/meta.py
@@ -81,14 +81,13 @@ def dump_metadata(filename=None,user=None,overwrite=False,indent=4):
         hoomd.context.msg.error("Need to initialize system first.\n")
         raise RuntimeError("Error writing out metadata.")
 
-    metadata = []
-    obj = OrderedDict()
+    metadata = dict()
 
     if user is not None:
         if not isinstance(user, collections.Mapping):
             hoomd.context.msg.warning("Extra meta data needs to be a mapping type. Ignoring.\n")
         else:
-            obj['user'] = _metadata_from_dict(user);
+            metadata['user'] = _metadata_from_dict(user);
 
     if not overwrite and filename is not None:
         try:
@@ -101,38 +100,49 @@ def dump_metadata(filename=None,user=None,overwrite=False,indent=4):
     # Generate time stamp
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    obj['timestamp'] = st
-    obj['context'] = hoomd.context.ExecutionContext()
-    obj['hoomd'] = hoomd.context.HOOMDContext()
+    metadata['timestamp'] = st
+    metadata['context'] = hoomd.context.ExecutionContext()
+    metadata['hoomd'] = hoomd.context.HOOMDContext()
+
 
     global_objs = [hoomd.data.system_data(hoomd.context.current.system_definition)];
-    global_objs += hoomd.context.current.forces;
-    global_objs += hoomd.context.current.constraint_forces;
     global_objs += [hoomd.context.current.integrator];
-    global_objs += hoomd.context.current.integration_methods;
-    global_objs += hoomd.context.current.forces
-    global_objs += hoomd.context.current.analyzers;
-    global_objs += hoomd.context.current.updaters;
 
-    # add list of objects to JSON
     for o in global_objs:
         if o is not None:
             name = o.__module__+'.'+o.__class__.__name__;
             if len(name) > 13 and name[:13] == 'hoomd.':
                 name = name[13:];
-            obj[name] = o
+            metadata[name] = o
 
-    metadata.append(obj)
+    global_objs = hoomd.context.current.forces;
+    global_objs += hoomd.context.current.constraint_forces;
+    global_objs += hoomd.context.current.integration_methods;
+    global_objs += hoomd.context.current.forces
+    global_objs += hoomd.context.current.analyzers;
+    global_objs += hoomd.context.current.updaters;
+
+    for o in global_objs:
+        if o is not None:
+            name = o.__module__+'.'+o.__class__.__name__;
+            if len(name) > 13 and name[:13] == 'hoomd.':
+                name = name[13:];
+            metadata.setdefault(name, set())
+            assert isinstance(metadata[name], set)
+            metadata[name].add(o)
 
     # handler for unknown objects
     def default_handler(obj):
+        if isinstance(obj, set) and len(obj) > 0:
+            return list(filter(None, (default_handler(o) for o in obj)))
         try:
             return obj.get_metadata()
         except (AttributeError, NotImplementedError):
             return None
 
     # dump to JSON
-    meta_str = json.dumps(metadata,default=default_handler,indent=indent)
+    meta_str = json.dumps(
+        metadata, default=default_handler,indent=indent, sort_keys=True)
 
     # only write files on rank 0
     if filename is not None and hoomd.comm.get_rank() == 0:
