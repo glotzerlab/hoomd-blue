28dd309bc2b971c39a20cd11a5071bcb77f1d9d4
Joshua A. Anderson <joaander@umich.edu>
2016-07-19 12:21:44 -0400

    Work around compile warnings on Mac OS X.

--- hoomd/test/upp11_config.h
+++ hoomd/test/upp11_config.h
@@ -16,10 +16,10 @@
 #include "hoomd/extern/upp11/upp11.h"
 
 // ******** helper macros
-#define CHECK_CLOSE(a,b,c) UP_ASSERT((fabs((a)-(b)) <= ((c) * fabs(a))) && (fabs((a)-(b)) <= ((c) * fabs(b))))
-#define CHECK_SMALL(a,c) UP_ASSERT(fabs(a) < c)
+#define CHECK_CLOSE(a,b,c) UP_ASSERT((std::abs((a)-(b)) <= ((c) * std::abs(a))) && (std::abs((a)-(b)) <= ((c) * std::abs(b))))
+#define CHECK_SMALL(a,c) UP_ASSERT(std::abs(a) < c)
 //! Helper macro for checking if two numbers are close
-#define MY_CHECK_CLOSE(a,b,c) UP_ASSERT((fabs((a)-(b)) <= ((c) * fabs(a))) && (fabs((a)-(b)) <= ((c) * fabs(b))))
+#define MY_CHECK_CLOSE(a,b,c) UP_ASSERT((std::abs((a)-(b)) <= ((c) * std::abs(a))) && (std::abs((a)-(b)) <= ((c) * std::abs(b))))
 //! Helper macro for checking if a number is small
 #define MY_CHECK_SMALL(a,c) CHECK_SMALL( a, Scalar(c))
 //! Need a simple define for checking two values which are unsigned
