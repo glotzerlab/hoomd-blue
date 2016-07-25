7040c1e15072bfb4de060d6b2b091d4912be6ec7
Jens Glaser <jsglaser@umich.edu>
2016-07-25 17:52:47 -0400

    support non-contiguous tag -> snapshot id mapping
    refs #175

--- hoomd/ParticleData.h
+++ hoomd/ParticleData.h
@@ -40,6 +40,7 @@
 
 #include <stdlib.h>
 #include <vector>
+#include <map>
 #include <string>
 #include <bitset>
 #include <stack>
@@ -854,7 +855,7 @@ class ParticleData : boost::noncopyable
 
         //! Take a snapshot
         template <class Real>
-        void takeSnapshot(SnapshotParticleData<Real> &snapshot);
+        const std::map<unsigned int, unsigned int> takeSnapshot(SnapshotParticleData<Real> &snapshot);
 
         //! Add ghost particles at the end of the local particle data
         void addGhostParticles(const unsigned int nghosts);
