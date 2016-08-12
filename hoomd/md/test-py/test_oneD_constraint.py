8ac6653a74be68132fd580f3d240cd5a8e0bc7dc
Bryan VanSaders <bvansade@umich.edu>
2016-08-12 10:47:41 -0400

    fixing the unittest

--- hoomd/md/test-py/test_oneD_constraint.py
+++ hoomd/md/test-py/test_oneD_constraint.py
@@ -10,9 +10,9 @@ import unittest
 class test_oneD(unittest.TestCase):
     def setUp(self):
         hoomd.context.initialize()
-        system = hoomd.init.create_lattice(hoomd.lattice.fcc(a=4.0), n=[1,1,1])
-        system.box = hoomd.data.boxdim(Lx=system.box.Lx*2, Ly=system.box.Ly*2, Lz=system.box.Lz*2)
-        self.initial = system.take_snapshot(particles=True)
+        self.system = hoomd.init.create_lattice(hoomd.lattice.fcc(a=4.0), n=[1,1,1])
+        self.system.box = hoomd.data.boxdim(Lx=self.system.box.Lx*2, Ly=self.system.box.Ly*2, Lz=self.system.box.Lz*2)
+        self.initial = self.system.take_snapshot(particles=True)
         lj = hoomd.md.pair.lj(r_cut=3.0, nlist=hoomd.md.nlist.cell())
         lj.pair_coeff.set('A','A', epsilon=1.0, sigma=1.0)
         hoomd.md.integrate.mode_standard(dt=5e-3)
@@ -23,21 +23,15 @@ class test_oneD(unittest.TestCase):
         oneD = hoomd.md.constrain.oneD(group=hoomd.group.type('A'), constraint_vector=constraint_vec)
         hoomd.md.force.constant(fx=1, fy=0, fz=0, group=hoomd.group.type('A'))
         hoomd.run(1e2)
-        self.final = system.take_snapshot(particles=True)
+        self.final = self.system.take_snapshot(particles=True)
 
         pos_diff = np.asarray(self.final.particles.position) - np.asarray(self.initial.particles.position)
         dots = np.dot(pos_diff,np.asarray(constraint_vec))/(np.linalg.norm(pos_diff,axis=1)*np.linalg.norm(np.asarray(constraint_vec)))
         tolerance = 1e-15
-        # Intentionally set to fail
-        assert(np.abs(np.abs(dots).sum() - final.particles.N)<-1)
+        assert(np.abs(np.abs(dots).sum() - final.particles.N)<tolerance)
 
     def tearDown(self):
-        del(system)
-        del(lj)
-        nvt.disable()
-        del(nvt)
-        oneD.disable()
-        del(oneD)
+        del(self.system)
 
 if __name__ == '__main__':
     unittest.main(argv = ['test.py', '-v'])
