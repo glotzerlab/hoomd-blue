a6d22e54d423ea7354ebd673bf8aeddb17196915
Jens Glaser <jsglaser@umich.edu>
2016-09-15 20:34:32 -0400

    fix bug where MolecularForceCompute consumed the sort signal
    then ForceComposite wouldn't get the signal and refuse
    to compute on the second call to compute() in the time step
    
    the way things are currently set up is that computeNetForce()
    is called twice during prepRun, with the same time
    step argument
    
    in MPI simulations this would lead to a communication without
    rigid force recomputation, and a wrong energy on the first time step

--- hoomd/md/MolecularForceCompute.cc
+++ hoomd/md/MolecularForceCompute.cc
@@ -21,13 +21,16 @@
 MolecularForceCompute::MolecularForceCompute(boost::shared_ptr<SystemDefinition> sysdef)
     : ForceConstraint(sysdef), m_molecule_tag(m_exec_conf), m_n_molecules_global(0),
       m_molecule_list(m_exec_conf), m_molecule_length(m_exec_conf), m_molecule_order(m_exec_conf),
-      m_molecule_idx(m_exec_conf)
+      m_molecule_idx(m_exec_conf), m_dirty(true)
     {
+    // connect to the ParticleData to recieve notifications when particles change order in memory
+    m_pdata->getParticleSortSignal().connect<MolecularForceCompute, &MolecularForceCompute::setDirty>(this);
     }
 
 //! Destructor
 MolecularForceCompute::~MolecularForceCompute()
     {
+    m_pdata->getParticleSortSignal().disconnect<MolecularForceCompute, &MolecularForceCompute::setDirty>(this);
     }
 
 void MolecularForceCompute::initMolecules()
