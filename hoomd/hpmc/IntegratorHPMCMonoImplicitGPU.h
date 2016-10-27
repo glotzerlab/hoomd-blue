fe2042c228ae353ed0fccbbb4bf05f6255cb1e85
Jens Glaser <jsglaser@umich.edu>
2016-10-27 12:56:05 -0400

    Revert "also fix bus error with implicit depletants"
    This reverts commit 7aa13fb21c55ffb109414efc3dfee9cf37a2b670.

--- hoomd/hpmc/IntegratorHPMCMonoImplicitGPU.h
+++ hoomd/hpmc/IntegratorHPMCMonoImplicitGPU.h
@@ -276,7 +276,9 @@ IntegratorHPMCMonoImplicitGPU< Shape >::~IntegratorHPMCMonoImplicitGPU()
 template< class Shape >
 void IntegratorHPMCMonoImplicitGPU< Shape >::update(unsigned int timestep)
     {
-    // update poisson distributions (needs to come first, before GPU access to m_params)
+    IntegratorHPMC::update(timestep);
+
+    // update poisson distributions
     if (this->m_need_initialize_poisson)
         {
         this->updatePoissonParameters();
@@ -288,8 +290,6 @@ void IntegratorHPMCMonoImplicitGPU< Shape >::update(unsigned int timestep)
         ArrayHandle<hpmc_implicit_counters_t> h_implicit_counters(this->m_implicit_count, access_location::host, access_mode::readwrite);
         this->m_implicit_count_step_start = h_implicit_counters.data[0];
         }
-
-    IntegratorHPMC::update(timestep);
 
     // check if we are below a minimum image convention box size
     BoxDim box = this->m_pdata->getBox();
