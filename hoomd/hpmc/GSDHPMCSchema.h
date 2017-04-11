4f85f0b7a58d18cb9c26fedbe172243a16c3ccf1
Paul Dodd <pdodd@umich.edu>
2017-04-11 12:52:40 -0400

    reworked general writes to be MPI safe

--- hoomd/hpmc/GSDHPMCSchema.h
+++ hoomd/hpmc/GSDHPMCSchema.h
@@ -37,21 +37,39 @@ struct gsd_schema_hpmc_base
 struct gsd_schema_hpmc : public gsd_schema_hpmc_base
     {
     gsd_schema_hpmc(const std::shared_ptr<const ExecutionConfiguration> exec_conf, bool mpi) : gsd_schema_hpmc_base(exec_conf, mpi) {}
-    int write(gsd_handle& handle, const std::string& name, unsigned int Ntypes, const float* const d, const float* const a)
+    template<class T>
+    int write(gsd_handle& handle, const std::string& name, unsigned int Ntypes, const T* const data, gsd_type type)
         {
+        if(!m_exec_conf->isRoot())
+            return 0;
         int retval = 0;
-        std::string path = name + "/d";
-        if(d)
-            retval |= gsd_write_chunk(&handle, path.c_str(), GSD_TYPE_FLOAT, Ntypes, 1, 0, (void *)d);
-        path = name + "/a";
-        if(a)
-            retval |= gsd_write_chunk(&handle, path.c_str(), GSD_TYPE_FLOAT, Ntypes, 1, 0, (void *)a);
+        retval |= gsd_write_chunk(&handle, name.c_str(), type, Ntypes, 1, 0, (void *)data);
         return retval;
         }
-    bool read(std::shared_ptr<GSDReader>, uint64_t, const std::string&, unsigned int, const float* const d, const float* const a)
-        {
 
-        return false;
+    template<class T>
+    bool read(std::shared_ptr<GSDReader> reader, uint64_t frame, const std::string& name, unsigned int Ntypes, T* const data, gsd_type type)
+        {
+        bool success = true;
+        std::vector<T> d;
+        if(m_exec_conf->isRoot())
+            {
+            d.resize(Ntypes);
+            success = reader->readChunk((void *) &d[0], frame, name.c_str(), Ntypes*gsd_sizeof_type(type), Ntypes) && success;
+            }
+    #ifdef ENABLE_MPI
+        if(m_mpi)
+            {
+            bcast(d, 0, m_exec_conf->getMPICommunicator()); // broadcast the data
+            }
+    #endif
+        if(!d.size())
+            throw std::runtime_error("Error occured while attempting to restore from gsd file.");
+        for(unsigned int i = 0; i < Ntypes; i++)
+            {
+            data[i] = d[i];
+            }
+        return success;
         }
     };
 
@@ -63,16 +81,9 @@ struct gsd_shape_schema : public gsd_schema_hpmc_base
     int write(gsd_handle&, const std::string&, unsigned int, const param_array<T>& )
         {
         throw std::runtime_error("This is not implemented");
-        // retval |= gsd_write_chunk(&handle, path.c_str(), GSD_TYPE_FLOAT, Ntypes, 1, 0, (void *)&data[0]);
         return 0;
         }
-    bool read(
-                        std::shared_ptr<GSDReader>,
-                        uint64_t,
-                        const std::string&,
-                        unsigned int,
-                        param_array<T>&
-                )
+    bool read(std::shared_ptr<GSDReader>, uint64_t, const std::string&, unsigned int, param_array<T>&)
         {
         throw std::runtime_error("This is not implemented");
         return false;
@@ -270,7 +281,7 @@ struct gsd_shape_schema< hpmc::detail::poly3d_verts > : public gsd_schema_hpmc_b
         for (unsigned int i = 0; i < Ntypes; i++)
             {
             float dsq = 0.0;
-            hpmc::detail::poly3d_verts result(N[i], m_exec_conf->isCUDAEnabled()); // This
+            hpmc::detail::poly3d_verts result(N[i], m_exec_conf->isCUDAEnabled());
             for (unsigned int v = 0; v < N[i]; v++)
                 {
                 result.x[v] = vertices[v*3+0+count];
@@ -282,7 +293,7 @@ struct gsd_shape_schema< hpmc::detail::poly3d_verts > : public gsd_schema_hpmc_b
             result.N = N[i];
             result.sweep_radius = sweep_radius[i];
             count += N[i];
-            shape[i] = result;
+            shape[i] = result; // Can we avoid a full copy of the data (move semanitcs?)
             }
         return success;
         }
