c55ce17f8270f2abf007daff0def5f9ff2aea06e
Paul Dodd <pdodd@umich.edu>
2017-04-11 12:51:04 -0400

    removing comments we will not support those cases.

--- hoomd/init.py
+++ hoomd/init.py
@@ -308,9 +308,8 @@ def read_gsd(filename, restart = None, frame = 0, time_step = None):
     hoomd.context.current.system = _hoomd.System(hoomd.context.current.system_definition, time_step);
 
     _perform_common_init_tasks();
-    hoomd.context.current.state_reader = reader; # this means that you can only restore state from the same file you initialized from.
-                                                 # Is there a use case where you want to restore from a different file? Do we want to support that?
-    hoomd.context.current.state_reader.clearSnapshot();                                
+    hoomd.context.current.state_reader = reader;
+    hoomd.context.current.state_reader.clearSnapshot();
     return hoomd.data.system_data(hoomd.context.current.system_definition);
 
 def restore_getar(filename, modes={'any': 'any'}):
