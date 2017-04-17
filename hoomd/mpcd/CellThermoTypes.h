3e9d9112317930f0484eb8d3bc88cbcbf40df4e7
Michael Howard <mphoward@princeton.edu>
2017-04-17 13:18:33 -0400

    Clean up pack operators

--- hoomd/mpcd/CellThermoTypes.h
+++ hoomd/mpcd/CellThermoTypes.h
@@ -14,9 +14,9 @@
 #include "hoomd/HOOMDMath.h"
 
 #ifdef NVCC
-#define HOSTDEVICE __device__ __forceinline__
+#define DEVICE __device__ __forceinline__
 #else
-#define HOSTDEVICE
+#define DEVICE
 #endif
 
 namespace mpcd
@@ -41,47 +41,18 @@ struct thermo_index
         };
     };
 
-//! Summed reduction of a Scalar3 which has an int stashed in the last element
-struct SumScalar2Int
-    {
-    //! Functor to sum a Scalar3 having an int as the third element
-    /*!
-     * \param a First Scalar3
-     * \param b Second Scalar3
-     * \returns Sum of a and b, with the x and y components added by float and
-     *          the z component added by integer
-     */
-    HOSTDEVICE Scalar3 operator()(const Scalar3& a, const Scalar3& b) const
-        {
-        return make_scalar3(a.x+b.x,
-                            a.y+b.y,
-                            __int_as_scalar(__scalar_as_int(a.z)+__scalar_as_int(b.z)));
-        }
-    };
-
 struct CellVelocityPackOp
     {
-    typedef struct
-        {
-        Scalar x;
-        Scalar y;
-        Scalar z;
-        float mass;
-        } element;
+    typedef Scalar4 element;
 
-    HOSTDEVICE element pack(const Scalar4& val) const
+    DEVICE element pack(const Scalar4& val) const
         {
-        element e;
-        e.x = val.x;
-        e.y = val.y;
-        e.z = val.z;
-        e.mass = val.w;
-        return e;
+        return val;
         }
 
-    HOSTDEVICE Scalar4 unpack(const element& e, const Scalar4& val) const
+    DEVICE Scalar4 unpack(const element& e, const Scalar4& val) const
         {
-        return make_scalar4(e.x + val.x, e.y + val.y, e.z + val.z, e.mass + val.w);
+        return make_scalar4(e.x + val.x, e.y + val.y, e.z + val.z, e.w + val.w);
         }
     };
 
@@ -93,7 +64,7 @@ struct CellEnergyPackOp
         unsigned int np;
         } element;
 
-    HOSTDEVICE element pack(const Scalar3& val) const
+    DEVICE element pack(const Scalar3& val) const
         {
         element e;
         e.energy = val.x;
@@ -101,7 +72,7 @@ struct CellEnergyPackOp
         return e;
         }
 
-    HOSTDEVICE Scalar3 unpack(const element& e, const Scalar3& val) const
+    DEVICE Scalar3 unpack(const element& e, const Scalar3& val) const
         {
         return make_scalar3(e.energy + val.x,
                             val.y,
@@ -112,6 +83,6 @@ struct CellEnergyPackOp
 } // end namespace detail
 } // end namespace mpcd
 
-#undef HOSTDEVICE
+#undef DEVICE
 
 #endif // MPCD_CELL_THERMO_TYPES_H_
