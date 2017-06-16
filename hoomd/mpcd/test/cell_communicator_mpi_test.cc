a861888cce22c9cf74aa99c54855337f08a075ca
Michael Howard <mphoward@princeton.edu>
2017-06-16 12:41:39 -0400

    Fix typo in failing unit test

--- hoomd/mpcd/test/cell_communicator_mpi_test.cc
+++ hoomd/mpcd/test/cell_communicator_mpi_test.cc
@@ -72,8 +72,8 @@ void cell_communicator_reduce_test(std::shared_ptr<ExecutionConfiguration> exec_
                     int3 global_cell = cl->getGlobalCell(make_int3(i,j,k));
                     global_cell.x += 1; global_cell.y += 1; global_cell.z += 1;
 
-                    h_props.data[ci(i,j,k)] = make_scalar3(global_cell.x, global_cell.y, global_cell.z);
-                    h_ref_props.data[ci(i,j,k)] = make_scalar3(global_cell.x, global_cell.y, global_cell.z);
+                    h_props.data[ci(i,j,k)] = make_scalar3(global_cell.x, global_cell.y, __int_as_scalar(global_cell.z));
+                    h_ref_props.data[ci(i,j,k)] = make_scalar3(global_cell.x, global_cell.y, __int_as_scalar(global_cell.z));
                     }
                 }
             }
