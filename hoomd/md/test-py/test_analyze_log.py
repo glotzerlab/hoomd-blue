a664c18a341fdbc44112912c468f327e72db550e
jproc <jproc@umich.edu>
2017-07-20 18:14:19 -0400

    remove create_random's that snuck back in

--- hoomd/md/test-py/test_analyze_log.py
+++ hoomd/md/test-py/test_analyze_log.py
@@ -142,7 +142,7 @@ if enable_hdf5:
 @unittest.skipIf(not enable_hdf5, "no h5py module available.")
 class analyze_log_hdf5_query_tests (unittest.TestCase):
     def setUp(self):
-        deprecated.init.create_random(N=100, phi_p=0.005);
+        init.create_lattice(lattice.sc(a=1.5),n=[5,5,4]); # must be close enough to interact
         nl = hoomd.md.nlist.cell()
         self.pair = hoomd.md.pair.lj(r_cut=2.5, nlist = nl)
         self.pair.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
@@ -188,7 +188,7 @@ class analyze_log_hdf5_query_tests (unittest.TestCase):
 @unittest.skipIf(not enable_hdf5, "no h5py module available.")
 class analyze_log_hdf5_tests (unittest.TestCase):
     def setUp(self):
-        deprecated.init.create_random(N=100, phi_p=0.05);
+        init.create_lattice(lattice.sc(a=2.1878096788957757),n=[5,5,4]);
 
         hoomd.context.current.sorter.set_params(grid=8)
 
