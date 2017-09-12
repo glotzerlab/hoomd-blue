19b75e388e309cb992d9b15edebeca8a756d2225
Paul Dodd <pdodd@umich.edu>
2017-09-12 16:19:12 -0400

    updating new and removing the old

--- hoomd/hpmc/module_convex_spheropolyhedron.cc
+++ hoomd/hpmc/module_convex_spheropolyhedron.cc
@@ -23,6 +23,10 @@
 #include "UpdaterMuVT.h"
 #include "UpdaterMuVTImplicit.h"
 
+#include "ShapeUtils.h"
+#include "ShapeMoves.h"
+#include "UpdaterShape.h"
+
 #ifdef ENABLE_CUDA
 #include "IntegratorHPMCMonoGPU.h"
 #include "IntegratorHPMCMonoImplicitGPU.h"
@@ -59,6 +63,13 @@ void export_convex_spheropolyhedron(py::module& m)
     // export_ExternalFieldWall<ShapeSpheropolyhedron >(m, "WallSpheropolyhedron");
     // export_UpdaterExternalFieldWall<ShapeSpheropolyhedron >(m, "UpdaterExternalFieldWallSpheropolyhedron");
     export_ExternalCallback<ShapeSpheropolyhedron>(m, "ExternalCallbackSpheropolyhedron");
+
+    export_ShapeMoveInterface< ShapeSpheropolyhedron >(m, "ShapeMoveSpheropolyhedron");
+    export_ShapeLogBoltzmann< ShapeSpheropolyhedron >(m, "LogBoltzmannSpheropolyhedron");
+    export_AlchemyLogBoltzmannFunction< ShapeSpheropolyhedron >(m, "AlchemyLogBoltzmannSpheropolyhedron");
+    export_UpdaterShape< ShapeSpheropolyhedron >(m, "UpdaterShapeSpheropolyhedron");
+    export_PythonShapeMove< ShapeSpheropolyhedron >(m, "PythonShapeMoveSpheropolyhedron");
+    export_ConstantShapeMove< ShapeSpheropolyhedron >(m, "ConstantShapeMoveSpheropolyhedron");
 
     #ifdef ENABLE_CUDA
 
