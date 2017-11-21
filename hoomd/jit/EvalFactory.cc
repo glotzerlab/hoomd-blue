7ce7400d599551f581188d31d0b872d86081c695
Jens Glaser <jsglaser@umich.edu>
2017-11-21 14:03:43 -0500

    refactor JIT & really fix LLVM linking on flux
    we now encapsulate the LLVM-interfacing code into an
    EvaluatorFactory that can be compiled with --no-rtti
    (hence, it does not use exceptions or dynamic_casts,
    and in particular not any pybind11 headers)

---
+++ hoomd/jit/EvalFactory.cc
@@ -0,0 +1,91 @@
+#include <utility>
+#include <memory>
+#include "EvalFactory.h"
+#include "OrcLazyJIT.h"
+
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/ExecutionEngine/Orc/OrcArchitectureSupport.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/Support/DynamicLibrary.h"
+
+#include "llvm/Support/raw_os_ostream.h"
+
+//! C'tor
+EvalFactory::EvalFactory(const std::string& fname)
+    {
+    // set to null pointer
+    m_eval = NULL;
+
+    // initialize LLVM
+    std::ostringstream sstream;
+    llvm::raw_os_ostream llvm_err(sstream);
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+
+    // Add the program's symbols into the JIT's search space.
+    if (llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr))
+        {
+            m_error_msg = "Error loading program symbols.\n";
+            return;
+        }
+
+    llvm::LLVMContext &Context = llvm::getGlobalContext();
+    llvm::SMDiagnostic Err;
+
+    // Read the input file
+    std::unique_ptr<llvm::Module> Mod = llvm::parseIRFile(fname, Err, Context);
+
+    if (!Mod)
+        {
+        // if the module didn't load, report an error
+        Err.print("PatchEnergyJIT", llvm_err);
+        llvm_err.flush();
+        m_error_msg = sstream.str();
+        return;
+        }
+
+    // Build engine with JIT
+    llvm::EngineBuilder EB;
+    auto TM = std::unique_ptr<llvm::TargetMachine>(EB.selectTarget());
+    auto CompileCallbackMgr = llvm::OrcLazyJIT::createCompileCallbackMgr(llvm::Triple(TM->getTargetTriple()));
+
+    // If we couldn't build the factory function then there must not be a callback
+    // manager for this target. Bail out.
+    if (!CompileCallbackMgr)
+        {
+        m_error_msg = "No callback manager available for target '" + TM->getTargetTriple().str() + "'.\n";
+        return;
+        }
+
+    auto IndirectStubsMgrBuilder = llvm::OrcLazyJIT::createIndirectStubsMgrBuilder(llvm::Triple(TM->getTargetTriple()));
+
+    // If we couldn't build a stubs-manager-builder for this target then bail out.
+    if (!IndirectStubsMgrBuilder)
+        {
+        m_error_msg = "No indirect stubs manager available for target '" + TM->getTargetTriple().str() + "'.\n";
+        return;
+        }
+
+    // Everything looks good. Build the JIT.
+    m_jit = std::shared_ptr<llvm::OrcLazyJIT>(new llvm::OrcLazyJIT(std::move(TM),
+                                std::move(CompileCallbackMgr),
+                                std::move(IndirectStubsMgrBuilder),
+                                true));
+
+    // Add the module, look up main and run it.
+    auto MainHandle = m_jit->addModule(std::move(Mod));
+    auto eval = m_jit->findSymbolIn(MainHandle, "eval");
+
+    if (!eval)
+        {
+        m_error_msg = "Could not find eval function in LLVM module.\n";
+        return;
+        }
+
+    llvm_err.flush();
+    m_eval = llvm::fromTargetAddress<EvalFnPtr>(eval.getAddress());
+    }
--- hoomd/jit/PatchEnergyJIT.cc
+++ hoomd/jit/PatchEnergyJIT.cc
@@ -1,14 +1,5 @@
 #include "PatchEnergyJIT.h"
-
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/ExecutionEngine/Orc/OrcArchitectureSupport.h"
-#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/Support/DynamicLibrary.h"
-
-#include "llvm/Support/raw_os_ostream.h"
+#include "EvalFactory.h"
 
 #include <sstream>
 
@@ -26,79 +17,17 @@ PatchEnergyJIT::PatchEnergyJIT(std::shared_ptr<ExecutionConfiguration> exec_conf
     //m_PatchProvidedQuantities.push_back(PATCH_ENERGY_LOG_NAME);
     //m_PatchProvidedQuantities.push_back(PATCH_ENERGY_RCUT);
 
-    // initialize LLVM
-    std::ostringstream sstream;
-    llvm::raw_os_ostream llvm_err(sstream);
-    llvm::InitializeNativeTarget();
-    llvm::InitializeNativeTargetAsmPrinter();
-    llvm::InitializeNativeTargetAsmParser();
+    // build the JIT.
+    m_factory = std::shared_ptr<EvalFactory>(new EvalFactory(fname));
 
-    // Add the program's symbols into the JIT's search space.
-    if (llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr))
+    // get the evaluator
+    m_eval = m_factory->getEval();
+
+    if (!m_eval)
         {
-        exec_conf->msg->error() << "Error loading program symbols.\n";
-        throw std::runtime_error("Error loading program symbols.");
+        exec_conf->msg->error() << m_factory->getError() << std::endl;
+        throw std::runtime_error("Error compiling JIT code.");
         }
-
-    llvm::LLVMContext &Context = llvm::getGlobalContext();
-    llvm::SMDiagnostic Err;
-
-    // Read the input file
-    std::unique_ptr<llvm::Module> Mod = llvm::parseIRFile(fname, Err, Context);
-
-    if (!Mod)
-        {
-        // if the module didn't load, report an error
-        exec_conf->msg->error() << "could not load LLVM IR" << std::endl;
-        Err.print("PatchEnergyJIT", llvm_err);
-        llvm_err.flush();
-        exec_conf->msg->errorStr(sstream.str());
-        throw std::runtime_error("could not load LLVM IR");
-        }
-
-    // Build engine with JIT
-    llvm::EngineBuilder EB;
-    auto TM = std::unique_ptr<llvm::TargetMachine>(EB.selectTarget());
-    auto CompileCallbackMgr = llvm::OrcLazyJIT::createCompileCallbackMgr(llvm::Triple(TM->getTargetTriple()));
-
-    // If we couldn't build the factory function then there must not be a callback
-    // manager for this target. Bail out.
-    if (!CompileCallbackMgr)
-        {
-        exec_conf->msg->error() << "No callback manager available for target '"
-                                << TM->getTargetTriple().str() << "'.\n";
-        throw std::runtime_error("Error compiling LLVM JIT.");
-        }
-
-    auto IndirectStubsMgrBuilder = llvm::OrcLazyJIT::createIndirectStubsMgrBuilder(llvm::Triple(TM->getTargetTriple()));
-
-    // If we couldn't build a stubs-manager-builder for this target then bail out.
-    if (!IndirectStubsMgrBuilder)
-        {
-        exec_conf->msg->error() << "No indirect stubs manager available for target '"
-                                << TM->getTargetTriple().str() << "'.\n";
-        throw std::runtime_error("Error compiling LLVM JIT.");
-        }
-
-    // Everything looks good. Build the JIT.
-    m_JIT =  std::shared_ptr<llvm::OrcLazyJIT>(new llvm::OrcLazyJIT(std::move(TM),
-                                                                    std::move(CompileCallbackMgr),
-                                                                    std::move(IndirectStubsMgrBuilder),
-                                                                    true));
-
-    // Add the module, look up main and run it.
-    auto MainHandle = m_JIT->addModule(std::move(Mod));
-    auto eval = m_JIT->findSymbolIn(MainHandle, "eval");
-
-    if (!eval)
-        {
-        exec_conf->msg->error() << "Could not find eval function in LLVM module.\n";
-        throw std::runtime_error("Error running LLVM JIT.");
-        }
-
-    m_eval = llvm::fromTargetAddress<EvalFnPtr>(eval.getAddress());
-    llvm_err.flush();
-
     }
 
 
