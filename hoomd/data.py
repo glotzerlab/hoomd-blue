80a954cb46032e7082836844f3428c2cbd7617bc
Jens Glaser <jsglaser@umich.edu>
2017-11-29 13:45:28 -0500

    print a status line

--- hoomd/data.py
+++ hoomd/data.py
@@ -2210,6 +2210,7 @@ def set_snapshot_box(snapshot, box):
 ## \internal
 # \brief Broadcast snapshot to all ranks
 def broadcast_snapshot(cpp_snapshot):
+    hoomd.util.print_status_line();
     hoomd.context._verify_init();
     # broadcast from rank 0
     cpp_snapshot._broadcast(0, hoomd.context.exec_conf);
@@ -2217,6 +2218,7 @@ def broadcast_snapshot(cpp_snapshot):
 ## \internal
 # \brief Broadcast snapshot to all ranks
 def broadcast_snapshot_all(cpp_snapshot):
+    hoomd.util.print_status_line();
     hoomd.context._verify_init();
     # broadcast from rank 0
     cpp_snapshot._broadcast_all(0, hoomd.context.exec_conf);
