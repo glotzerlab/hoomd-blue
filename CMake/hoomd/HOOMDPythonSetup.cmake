77c5244c539be01fa16f3de3c2577fccd849a58f
Joshua A. Anderson <joaander@umich.edu>
2018-02-02 13:22:39 -0500

    Trick pybind11 into letting hoomd handle cflags

--- CMake/hoomd/HOOMDPythonSetup.cmake
+++ CMake/hoomd/HOOMDPythonSetup.cmake
@@ -2,4 +2,11 @@
 # TODO: add search path for make installed hoomd as well
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/hoomd/extern/pybind/tools)
 
+# trick pybind11 tools to allow us to manage C++ flags
+# cmake ignores this in 2.8, but when pybind11 sees this
+# it will not override hoomd's cflags
+set(CMAKE_CXX_STANDARD 11)
+
+# hoomd's clfags setup script will take care of proper cxx flags settings
+
 include(pybind11Tools)
