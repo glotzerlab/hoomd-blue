c58da6ca4aa9ba4a7916037d4cc635d693f26071
Joshua A. Anderson <joaander@umich.edu>
2018-02-05 08:54:18 -0500

    Support external plugin builds

--- CMake/hoomd/HOOMDPythonSetup.cmake
+++ CMake/hoomd/HOOMDPythonSetup.cmake
@@ -1,6 +1,10 @@
 # use pybind11's tools to find python and create python modules
-# TODO: add search path for make installed hoomd as well
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/hoomd/extern/pybind/tools)
+
+# for installed hoomd with external plugins
+if (HOOMD_INCLUDE_DIR)
+    list(APPEND CMAKE_MODULE_PATH ${HOOMD_INCLUDE_DIR}/hoomd/extern/pybind/tools)
+endif()
 
 # trick pybind11 tools to allow us to manage C++ flags
 # cmake ignores this in 2.8, but when pybind11 sees this
