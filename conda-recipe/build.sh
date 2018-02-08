6c43d10cf3450674b422d62708122b665c1f006c
Joshua A. Anderson <joaander@umich.edu>
2018-02-08 09:30:02 -0500

    Set the -march in the conda build recpie
    fixes #298

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,13 +2,12 @@ mkdir -p build-conda
 cd build-conda
 rm -rf ./*
 
-export GCC_ARCH=core2
-
 if [ "$(uname)" == "Darwin" ]; then
 
 cmake ../ \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 \
-      -DCMAKE_CXX_FLAGS="-mmacosx-version-min=10.8 -stdlib=libc++" \
+      -DCMAKE_CXX_FLAGS="-mmacosx-version-min=10.8 -stdlib=libc++ -march=core2" \
+      -DCMAKE_C_FLAGS="-march=core2" \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
        \
@@ -27,6 +26,8 @@ else
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
+      -DCMAKE_CXX_FLAGS="-march=core2" \
+      -DCMAKE_C_FLAGS="-march=core2" \
       -DCUDA_HOST_COMPILER=${CC} \
        \
       -DENABLE_MPI=on \
