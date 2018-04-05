ecf56fd64fcbc5fa8cd816ffe92cb3469fd1681c
Bradley Dice <bdice@bradleydice.com>
2018-04-05 17:48:52 -0400

    Replaced MPI_COMM_WORLD in files and tests that should use the new HOOMD world communicator.

--- hoomd/hpmc/UpdaterMuVTImplicit.h
+++ hoomd/hpmc/UpdaterMuVTImplicit.h
@@ -246,8 +246,8 @@ bool UpdaterMuVTImplicit<Shape,Integrator>::tryInsertParticle(unsigned int times
             {
             MPI_Request req[2];
             MPI_Status status[2];
-            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[0]);
-            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[1]);
+            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[0]);
+            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[1]);
             MPI_Waitall(2, req, status);
             }
         if (this->m_comm)
@@ -366,8 +366,8 @@ bool UpdaterMuVTImplicit<Shape,Integrator>::trySwitchType(unsigned int timestep,
             {
             MPI_Request req[2];
             MPI_Status status[2];
-            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[0]);
-            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[1]);
+            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[0]);
+            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[1]);
             MPI_Waitall(2, req, status);
             }
         if (this->m_comm)
@@ -419,8 +419,8 @@ bool UpdaterMuVTImplicit<Shape,Integrator>::tryRemoveParticle(unsigned int times
             {
             MPI_Request req[2];
             MPI_Status status[2];
-            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[0]);
-            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, MPI_COMM_WORLD, &req[1]);
+            MPI_Isend(&n_overlap, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[0]);
+            MPI_Irecv(&n_insert, 1, MPI_UNSIGNED, other, 0, this->m_exec_conf->getHOOMDWorldMPICommunicator(), &req[1]);
             MPI_Waitall(2, req, status);
             }
         if (this->m_comm)
