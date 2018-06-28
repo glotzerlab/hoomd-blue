51ee73c48633584f41e2e32a583b24c5d283f185
Joshua A. Anderson <joaander@umich.edu>
2018-06-28 07:36:58 -0400

    Lower tolerance in dipole test

--- hoomd/md/test-py/test_ai_pair_dipole_compute.py
+++ hoomd/md/test-py/test_ai_pair_dipole_compute.py
@@ -7,6 +7,7 @@ from hoomd import md
 context.initialize()
 import unittest
 import os
+import numpy
 
 class pair_dipole_tests_force (unittest.TestCase):
     def setUp(self):
@@ -34,21 +35,21 @@ class pair_dipole_tests_force (unittest.TestCase):
 
         force_1 = self.system.particles[0].net_force
         torque_1 = self.system.particles[0].net_torque
-        self.assertAlmostEqual(-1.0783202, force_1[0])
-        self.assertAlmostEqual(-1.2620099, force_1[1])
-        self.assertAlmostEqual(-0.8108346, force_1[2])
-        self.assertAlmostEqual(0, torque_1[0])
-        self.assertAlmostEqual(0.1542009, torque_1[1])
-        self.assertAlmostEqual(-0.2560913, torque_1[2])
+        numpy.testing.assert_allclose(-1.0783202, force_1[0], rtol=1e-6)
+        numpy.testing.assert_allclose(-1.2620099, force_1[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.8108346, force_1[2], rtol=1e-6)
+        numpy.testing.assert_allclose(0, torque_1[0], atol=1e-7)
+        numpy.testing.assert_allclose(0.1542009, torque_1[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.2560913, torque_1[2], rtol=1e-6)
 
         force_2 = self.system.particles[1].net_force
         torque_2 = self.system.particles[1].net_torque
-        self.assertAlmostEqual(1.0783202, force_2[0])
-        self.assertAlmostEqual(1.2620099, force_2[1])
-        self.assertAlmostEqual(0.8108346, force_2[2])
-        self.assertAlmostEqual(0.7709333, torque_2[0])
-        self.assertAlmostEqual(-0.4760214, torque_2[1])
-        self.assertAlmostEqual(-0.2682726, torque_2[2])
+        numpy.testing.assert_allclose(1.0783202, force_2[0], rtol=1e-6)
+        numpy.testing.assert_allclose(1.2620099, force_2[1], rtol=1e-6)
+        numpy.testing.assert_allclose(0.8108346, force_2[2], rtol=1e-6)
+        numpy.testing.assert_allclose(0.7709333, torque_2[0], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.4760214, torque_2[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.2682726, torque_2[2], rtol=1e-6)
 
     def test_force_torque_kappa1(self):
 
@@ -61,21 +62,21 @@ class pair_dipole_tests_force (unittest.TestCase):
 
         force_1 = self.system.particles[0].net_force
         torque_1 = self.system.particles[0].net_torque
-        self.assertAlmostEqual(-0.6139756, force_1[0])
-        self.assertAlmostEqual(-0.5266033, force_1[1])
-        self.assertAlmostEqual(-0.5794879, force_1[2])
-        self.assertAlmostEqual(0, torque_1[0])
-        self.assertAlmostEqual(0.0426386, torque_1[1])
-        self.assertAlmostEqual(-0.0708127, torque_1[2])
+        numpy.testing.assert_allclose(-0.6139756, force_1[0], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.5266033, force_1[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.5794879, force_1[2], rtol=1e-6)
+        numpy.testing.assert_allclose(0, torque_1[0], atol=1e-7)
+        numpy.testing.assert_allclose(0.0426386, torque_1[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.0708127, torque_1[2], rtol=1e-6)
 
         force_2 = self.system.particles[1].net_force
         torque_2 = self.system.particles[1].net_torque
-        self.assertAlmostEqual(0.6139756, force_2[0])
-        self.assertAlmostEqual(0.5266033, force_2[1])
-        self.assertAlmostEqual(0.5794879, force_2[2])
-        self.assertAlmostEqual(0.2131734, torque_2[0])
-        self.assertAlmostEqual(-0.1316263, torque_2[1])
-        self.assertAlmostEqual(-0.0741810, torque_2[2])
+        numpy.testing.assert_allclose(0.6139756, force_2[0], rtol=1e-6)
+        numpy.testing.assert_allclose(0.5266033, force_2[1], rtol=1e-6)
+        numpy.testing.assert_allclose(0.5794879, force_2[2], rtol=1e-6)
+        numpy.testing.assert_allclose(0.2131734, torque_2[0], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.1316263, torque_2[1], rtol=1e-6)
+        numpy.testing.assert_allclose(-0.0741810, torque_2[2], rtol=1e-6)
 
     def tearDown(self):
         del self.system, self.nl
