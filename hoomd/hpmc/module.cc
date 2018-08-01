13008e9927eedfaf317c6f77d12731b0070554de
Jens Glaser <jsglaser@umich.edu>
2018-08-01 18:10:03 +0200

    let convex_polyhedron_union take in an optional sweep radius

--- hoomd/hpmc/module.cc
+++ hoomd/hpmc/module.cc
@@ -88,7 +88,7 @@ PYBIND11_MODULE(_hpmc, m)
         .def_readwrite("circumsphereDiameter",&sphinx3d_params::circumsphereDiameter);
     py::class_< ShapeUnion<ShapeSphere>::param_type, std::shared_ptr< ShapeUnion<ShapeSphere>::param_type> >(m, "msph_params");
 
-    py::class_< ShapeUnion<ShapeConvexPolyhedron>::param_type, std::shared_ptr< ShapeUnion<ShapeConvexPolyhedron>::param_type> >(m, "mpoly3d_params");
+    py::class_< ShapeUnion<ShapeSpheropolyhedron>::param_type, std::shared_ptr< ShapeUnion<ShapeSpheropolyhedron>::param_type> >(m, "mpoly3d_params");
 
     m.def("make_poly2d_verts", &make_poly2d_verts);
     m.def("make_poly3d_data", &make_poly3d_data);
@@ -97,7 +97,7 @@ PYBIND11_MODULE(_hpmc, m)
     m.def("make_sph_params", &make_sph_params);
     m.def("make_faceted_sphere", &make_faceted_sphere);
     m.def("make_sphinx3d_params", &make_sphinx3d_params);
-    m.def("make_convex_polyhedron_union_params", &make_union_params<ShapeConvexPolyhedron>);
+    m.def("make_convex_polyhedron_union_params", &make_union_params<ShapeSpheropolyhedron>);
     m.def("make_sphere_union_params", &make_union_params<ShapeSphere>);
     m.def("make_overlapreal3", &make_overlapreal3);
     m.def("make_overlapreal4", &make_overlapreal4);
