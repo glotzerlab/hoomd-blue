946f19d0f1bc6418e55094b3bb5fd39bf44c7a05
Kwanghwi Je <jekw@umich.edu>
2018-08-15 13:22:35 -0400

    I separated different pair potential kernel from AllDriverPotentialPairGPU

---
+++ hoomd/md/PairLJ1208DriverPotentialPairGPU.cu
@@ -0,0 +1,20 @@
+// Copyright (c) 2009-2018 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
+
+
+// Maintainer: joaander / Everyone is free to add additional potentials
+
+/*! \file AllDriverPotentialPairGPU.cu
+    \brief Defines the driver functions for computing all types of pair forces on the GPU
+*/
+
+#include "EvaluatorPairLJ1208.h"
+#include "AllDriverPotentialPairGPU.cuh"
+
+cudaError_t gpu_compute_lj1208_forces(const pair_args_t& pair_args,
+                                      const Scalar2 *d_params)
+    {
+    return gpu_compute_pair_forces<EvaluatorPairLJ1208>(pair_args,
+                                                    d_params);
+    }
+
