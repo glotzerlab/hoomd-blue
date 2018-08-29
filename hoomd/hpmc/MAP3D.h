5cae7e9a8bd078b1fb0d8913544bcd0f498e70b4
Jens Glaser <jsglaser@umich.edu>
2018-08-28 22:39:35 -0400

    found tuning parameters that appear to work
    also implement two-body MAP parallel to three-body one

--- hoomd/hpmc/MAP3D.h
+++ hoomd/hpmc/MAP3D.h
@@ -30,76 +30,16 @@ namespace detail
 
 const unsigned int MAP_3D_MAX_ITERATIONS = 1024;
 
-//! Test for pairwise overlap using the method of alternating projections
-/*! \param a First shape to test for intersection
-    \param b Second shape to test for intersection
-    \param sa first support function
-    \param sb second support function
-    \param pa first projection function
-    \param pb second projection function
-    \param dr Position of second shape relative to first
-    \param err Output variable that is incremented upon non-convergence
-    \param sweep_radius Radius of sphere to sweep the shapes by
- */
-template <class Shape,
-          class SupportFuncA, class SupportFuncB,
-          class ProjectionFuncA, class ProjectionFuncB>
-DEVICE inline bool map_two(const Shape& a, const Shape& b,
-    const SupportFuncA& sa, const SupportFuncB& sb,
-    const ProjectionFuncA& pa, const ProjectionFuncB& pb,
-    const vec3<OverlapReal>& dr, unsigned int &err,
-    Scalar sweep_radius)
-    {
-    quat<OverlapReal> qa(a.orientation);
-    quat<OverlapReal> qb(b.orientation);
-    quat<OverlapReal> q(conj(qa)* qb);
-    vec3<OverlapReal> dr_rot(rotate(conj(qa), dr));
-
-    OverlapReal r = sweep_radius;
-
-    CompositeSupportFunc3D<SupportFuncA, SupportFuncB> S(sa, sb, dr_rot, q);
-    vec3<OverlapReal> p(0,0,0);
-
-    unsigned int it = 0;
-    err = 0;
-
-    const OverlapReal tol(1e-7); // for squares of distances
-    const OverlapReal root_tol(3e-4); // sqrt of tol
-
-    vec3<OverlapReal> v;
-
-    while (it++ <= MAP_3D_MAX_ITERATIONS)
-        {
-        vec3<OverlapReal> proj = rotate(qa,pa(rotate(conj(qa),p)));
-
-        p = dr+rotate(qb,pb(rotate(conj(qb),proj-dr)));
-
-        if (dot(p-proj,p-proj) <= OverlapReal(4.0)*r*r+tol)
-            {
-            // the point p is in the intersection
-            return true;
-            }
-
-        // conversely, check if we found a separating hyperplane
-        v = rotate(conj(qa),proj - p);
-        if (dot(S(v), v) < (-OverlapReal(2.0)*r-root_tol)*fast::sqrt(dot(v,v)))
-            {
-            return false;   // origin is outside v1 support plane
-            }
-        }
-
-    // maximum number of iterations reached, return overlap and indicate error
-    err++;
-    return true;
-    }
-
 //! Test for a common point in the intersection of three shapes using the extrapolated parallel projections method
-/*! \param a First shape to test for intersection
-    \param b Second shape to test for intersection
+/*! \param a First shape to test
+    \param b Second shape to test
+    \param c Third shape to test
     \param sa first support function
     \param sb second support function
+    \param sc third support function
     \param pa first projection function
     \param pb second projection function
+    \param pc third projection function
     \param ab_t Position of second shape relative to first
     \param ac_t Position of third shape relative to first
     \param err Output variable that is incremented upon non-convergence
@@ -131,16 +71,16 @@ DEVICE inline bool map_three(const ShapeA& a, const ShapeB& b, const ShapeC& c,
     vec3<OverlapReal> q_c;
 
     // element of the diagonal space D
-    vec3<OverlapReal> b_diag(0.0,0.0,0.0);
-    vec3<OverlapReal> b_prime_diag(0.0,0.0,0.0);
+    vec3<OverlapReal> b_prime_diag;
     vec3<OverlapReal> u_diag(0.0,0.0,0.0);
 
     unsigned int it = 0;
-    err = 0;
 
     vec3<OverlapReal> v_a, v_b, v_c;
 
-    const OverlapReal tol(1e-7); // for squares of distances
+    // this seems to be a reasonable lower bound on the single-precision error, when
+    // reducing further one should see an increased number of forced loop terminations
+    const OverlapReal tol(4e-7); // for squares of distances
 
     /*
      * Tuning parameters
@@ -149,8 +89,10 @@ DEVICE inline bool map_three(const ShapeA& a, const ShapeB& b, const ShapeC& c,
     // amount of extrapolation to apply (0 <= lambda <=1)
     OverlapReal lambda(1.0);
     const unsigned int k = 10;
+    const OverlapReal f(0.9);
 
     OverlapReal sum;
+    OverlapReal normsq;
 
     while (it++ <= MAP_3D_MAX_ITERATIONS)
         {
@@ -185,7 +127,7 @@ DEVICE inline bool map_three(const ShapeA& a, const ShapeB& b, const ShapeC& c,
             }
 
         // test for common point
-        if (dot(q_a-u_diag,q_a-u_diag) <= tol && dot(q_b-u_diag,q_b-u_diag) <= tol && dot(q_c-u_diag,q_c-u_diag) <= tol)
+        if (dot(q_a-u_diag,q_a-u_diag) + dot(q_b-u_diag,q_b-u_diag) + dot(q_c-u_diag,q_c-u_diag) <= tol)
             return true;
 
         // project back into diagonal space (barycenter)
@@ -221,7 +163,10 @@ DEVICE inline bool map_three(const ShapeA& a, const ShapeB& b, const ShapeC& c,
             sum += dot(b_prime_diag-v_c, b_prime_diag - q_c);
             }
 
-        if (sum > OverlapReal(0.0))
+        normsq = dot(b_prime_diag-q_a,b_prime_diag-q_a)+
+            dot(b_prime_diag-q_b,b_prime_diag-q_b)+
+            dot(b_prime_diag-q_c,b_prime_diag-q_c);
+        if (sum > -fast::sqrt(normsq*tol))
             return false;   // found a separating plane
 
         // second step, extrapolation
@@ -232,7 +177,133 @@ DEVICE inline bool map_three(const ShapeA& a, const ShapeB& b, const ShapeC& c,
 
         //  reduce lambda every k iterations
         if (it % k == 0)
-            lambda *= OverlapReal(0.9);
+            lambda *= f;
+        }
+
+    // maximum number of iterations reached, return overlap and indicate error
+    err++;
+    return true;
+    }
+
+//! Test for pairwise overlap using the extrapolated parallel projections method
+/*! \param a First shape to test for intersection
+    \param b Second shape to test for intersection
+    \param sa first support function
+    \param sb second support function
+    \param pa first projection function
+    \param pb second projection function
+    \param ab_t Position of second shape relative to first
+    \param err Output variable that is incremented upon non-convergence
+    \param sweep_radius Radius of sphere to sweep all shapes by
+ */
+template <class ShapeA, class ShapeB,
+          class SupportFuncA, class SupportFuncB,
+          class ProjectionFuncA, class ProjectionFuncB>
+DEVICE inline bool map_two(const ShapeA& a, const ShapeB& b,
+    const SupportFuncA& sa, const SupportFuncB& sb,
+    const ProjectionFuncA& pa, const ProjectionFuncB& pb,
+    const vec3<OverlapReal>& ab_t, unsigned int &err,
+    Scalar sweep_radius)
+    {
+    quat<OverlapReal> qa(a.orientation);
+    quat<OverlapReal> qb(b.orientation);
+
+    OverlapReal r = sweep_radius;
+
+    // element of the cartesian product C = A x B
+    vec3<OverlapReal> q_a;
+    vec3<OverlapReal> q_b;
+
+    // element of the diagonal space D
+    vec3<OverlapReal> b_prime_diag;
+    vec3<OverlapReal> u_diag(0.0,0.0,0.0);
+
+    unsigned int it = 0;
+
+    vec3<OverlapReal> v_a, v_b;
+
+    // this seems to be a reasonable lower bound on the single-precision error, when
+    // reducing further one should see an increased number of forced loop terminations
+    const OverlapReal tol(4e-7); // for squares of distances
+
+    /*
+     * Tuning parameters
+     */
+
+    // amount of extrapolation to apply (0 <= lambda <=1)
+    OverlapReal lambda(1.0);
+    const unsigned int k = 20;
+    const OverlapReal f(0.999);
+
+    OverlapReal sum;
+    OverlapReal normsq;
+
+    while (it++ <= MAP_3D_MAX_ITERATIONS)
+        {
+        // first step: project b onto product space
+        q_a = rotate(qa,pa(rotate(conj(qa),u_diag)));
+        q_b = ab_t+rotate(qb,pb(rotate(conj(qb),u_diag-ab_t)));
+
+        if (r != OverlapReal(0.0))
+            {
+            // project on extended shape
+            vec3<OverlapReal> del = u_diag - q_a;
+            OverlapReal d = fast::sqrt(dot(del,del));
+            if (d > r)
+                q_a += r/d * del;
+            else
+                q_a = u_diag;
+
+            del = u_diag - q_b;
+            d = fast::sqrt(dot(del,del));
+            if (d > r)
+                q_b += r/d * del;
+            else
+                q_b = u_diag;
+            }
+
+        // test for common point
+        if (dot(q_a-u_diag,q_a-u_diag) + dot(q_b-u_diag,q_b-u_diag) <= tol)
+            return true;
+
+        // project back into diagonal space (barycenter)
+        b_prime_diag = (q_a+q_b)/OverlapReal(2.0);
+
+        //  check if we found a separating hyperplane between C and the linear subspace D
+
+        // get the support vertex in the direction b' - q
+        sum = OverlapReal(0.0);
+
+        vec3<OverlapReal> n;
+        n = b_prime_diag - q_a;
+        if (dot(n,n) > OverlapReal(0.0))
+            {
+            v_a = rotate(qa,sa(rotate(conj(qa),b_prime_diag - q_a)));
+            v_a += (r * fast::rsqrt(dot(n,n))) * n;
+            sum += dot(b_prime_diag-v_a, b_prime_diag - q_a);
+            }
+
+        n = b_prime_diag - q_b;
+        if (dot(n,n) > OverlapReal(0.0))
+            {
+            v_b = ab_t + rotate(qb,sb(rotate(conj(qb),b_prime_diag - q_b)));
+            v_b += (r * fast::rsqrt(dot(n,n))) * n;
+            sum += dot(b_prime_diag-v_b, b_prime_diag - q_b);
+            }
+
+        normsq = dot(b_prime_diag-q_a,b_prime_diag-q_a)+dot(b_prime_diag-q_b,b_prime_diag-q_b);
+        if (sum > -fast::sqrt(normsq*tol))
+            return false;   // found a separating plane
+
+        // second step, extrapolation
+        OverlapReal coeff = dot(q_a-u_diag,q_a-u_diag) + dot(q_b-u_diag,q_b-u_diag);
+        coeff /= OverlapReal(2.0)*dot(b_prime_diag-u_diag,b_prime_diag-u_diag);
+
+        u_diag += (OverlapReal(1.0) + lambda*(coeff-OverlapReal(1.0)))*(b_prime_diag-u_diag);
+
+        //  reduce lambda every k iterations
+        if (it % k == 0)
+            lambda *= f;
         }
 
     // maximum number of iterations reached, return overlap and indicate error
