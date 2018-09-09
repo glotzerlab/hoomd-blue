bc4461086d18c7263939f1b050e6c1b5bdb9cc69
Jens Glaser <jsglaser@umich.edu>
2018-09-09 01:51:49 -0400

    clarify notice message

--- hoomd/MemoryTraceback.cc
+++ hoomd/MemoryTraceback.cc
@@ -86,7 +86,7 @@ void MemoryTraceback::outputTraces(std::shared_ptr<Messenger> msg) const
         nbytes_tot += it_trace->first.second;
         }
 
-    msg->notice(2) << "Total amount of memory allocated in Global[Array,Vector]: " << pretty_bytes(nbytes_tot) << std::endl;
+    msg->notice(2) << "Total amount of managed memory allocated through Global[Array,Vector]: " << pretty_bytes(nbytes_tot) << std::endl;
     msg->notice(2) << "Actual allocation sizes may be larger by up to the OS page size due to alignment." << std::endl;
     msg->notice(2) << "List of memory allocations and last " << MAX_TRACEBACK-1 << " functions called at time of (re-)allocation" << std::endl;
 
