e90acacfa3b58d55ac2a7d5017b98dca7e07fd14
Jens Glaser <jsglaser@umich.edu>
2018-09-24 16:47:02 -0400

    Revert "Revert "don't construct sphere tree""
    This reverts commit d7382ef02f6e6f2beb2229e40aaf64fc5db00178.

--- hoomd/hpmc/ShapeProxy.h
+++ hoomd/hpmc/ShapeProxy.h
@@ -457,7 +457,6 @@ typename ShapeUnion<Shape>::param_type make_union_params(pybind11::list _members
     // extract member parameters, posistions, and orientations and compute the radius along the way
     OverlapReal diameter = OverlapReal(0.0);
 
-    bool all_spheres = true;
     for (unsigned int i = 0; i < result.N; i++)
         {
         typename Shape::param_type param = pybind11::cast<typename Shape::param_type>(_members[i]);
@@ -482,7 +481,6 @@ typename ShapeUnion<Shape>::param_type make_union_params(pybind11::list _members
             {
             // construct OBB
             obbs[i] = dummy.getOBB(pos);
-            all_spheres = false;
             }
         else
             {
@@ -499,7 +497,7 @@ typename ShapeUnion<Shape>::param_type make_union_params(pybind11::list _members
     // build tree and store GPU accessible version in parameter structure
     typedef typename ShapeUnion<Shape>::param_type::gpu_tree_type gpu_tree_type;
     OBBTree tree;
-    tree.buildTree(obbs, result.N, leaf_capacity, all_spheres);
+    tree.buildTree(obbs, result.N, leaf_capacity, false);
     delete [] obbs;
     result.tree = gpu_tree_type(tree,exec_conf->isCUDAEnabled());
 
