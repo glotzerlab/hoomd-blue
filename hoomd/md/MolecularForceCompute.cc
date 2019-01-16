3047a1a5a171c62872bb08bd60376a05e750fd90
Jens Glaser <jsglaser@umich.edu>
2019-01-16 16:53:51 -0500

    set memory hints in MolecularForceCompute

--- hoomd/md/MolecularForceCompute.cc
+++ hoomd/md/MolecularForceCompute.cc
@@ -154,14 +154,38 @@ void MolecularForceCompute::initMoleculesGPU()
         m_tuner_fill->end();
         }
 
+    // distribute molecules evenly over GPUs
+    // NOTE: going forward we could slave the GPU partition of the molecules
+    // to that of the local particles in the ParticleData
+    m_gpu_partition = GPUPartition(m_exec_conf->getGPUIds());
+    m_gpu_partition.setN(n_local_molecules);
+
     if (m_exec_conf->allConcurrentManagedAccess())
         {
         auto gpu_map = m_exec_conf->getGPUIds();
 
         for (unsigned int idev = 0; idev < m_exec_conf->getNumActiveGPUs(); ++idev)
             {
-            cudaMemAdvise(m_molecule_list.get(), sizeof(unsigned int)*m_molecule_list.getNumElements(), cudaMemAdviseSetAccessedBy, gpu_map[idev]);
-            cudaMemAdvise(m_molecule_length.get(), sizeof(unsigned int)*m_molecule_length.getNumElements(), cudaMemAdviseSetAccessedBy, gpu_map[idev]);
+            std::pair<unsigned int, unsigned int> range = m_gpu_partition.getRange(idev);
+            unsigned int nelem = range.second - range.first;
+
+            if (nelem == 0)
+                continue;
+
+            cudaMemAdvise(m_molecule_list.get()+m_molecule_indexer(0,range.first),
+                sizeof(unsigned int)*nelem*m_molecule_indexer.getW(),
+                cudaMemAdviseSetPreferredLocation, gpu_map[idev]);
+            cudaMemAdvise(m_molecule_length.get()+range.first,
+                sizeof(unsigned int)*nelem*m_molecule_indexer.getW(),
+                cudaMemAdviseSetPreferredLocation,
+                gpu_map[idev]);
+
+            cudaMemPrefetchAsync(m_molecule_list.get()+m_molecule_indexer(0,range.first),
+                sizeof(unsigned int)*nelem*m_molecule_indexer.getW(),
+                gpu_map[idev]);
+            cudaMemPrefetchAsync(m_molecule_length.get()+range.first,
+                sizeof(unsigned int)*nelem*m_molecule_indexer.getW(),
+                gpu_map[idev]);
             }
 
         for (unsigned int idev = 0; idev < m_exec_conf->getNumActiveGPUs(); ++idev)
