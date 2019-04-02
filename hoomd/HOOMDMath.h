08bdc4f28ddd6c81dce7aad57f7632b20b34502a
Joshua A. Anderson <joaander@umich.edu>
2019-04-02 13:52:08 -0400

    Test the box-muller embedded in Saru

--- hoomd/HOOMDMath.h
+++ hoomd/HOOMDMath.h
@@ -717,19 +717,7 @@ inline HOSTDEVICE double acos(double x)
 template<class Real, class RNG>
 inline Real DEVICE gaussian_rng(RNG &rng, const Real sigma)
     {
-    // use Box-Muller transformation to get a gaussian random number
-    float x1, x2, w, y1;
-
-    do  {
-        x1 = rng.s(-1.0, 1.0);
-        x2 = rng.s(-1.0, 1.0);
-        w = x1 * x1 + x2 * x2;
-        } while ( w >= Real(1.0) );
-
-    w = fast::sqrt((Scalar(-2.0) * log(w)) / w);
-    y1 = x1 * w;
-
-    return y1 * sigma;
+    return rng.template normal<Real>() * sigma;
     }
 
 // undefine HOSTDEVICE so we don't interfere with other headers
