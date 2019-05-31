95bf8b4c2b3d6ba1109ba35cdd938058082fee67
Joshua A. Anderson <joaander@umich.edu>
2019-05-31 11:19:27 -0400

    Apply workaround only when ENABLE_CUDA is set

--- CMake/hoomd/HOOMDMPISetup.cmake
+++ CMake/hoomd/HOOMDMPISetup.cmake
@@ -54,7 +54,7 @@ if (ENABLE_MPI)
 # backport CMake FindMPI fix from 3.12 to earlier versions
 # https://gitlab.kitware.com/cmake/cmake/merge_requests/2529/diffs
 
-if (CMAKE_VERSION VERSION_LESS 3.12.0)
+if (CMAKE_VERSION VERSION_LESS 3.12.0 AND ENABLE_CUDA)
     string(REPLACE "-pthread" "$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler>;-pthread"
       _MPI_C_COMPILE_OPTIONS "${MPI_C_COMPILE_OPTIONS}")
     set_property(TARGET MPI::MPI_C PROPERTY INTERFACE_COMPILE_OPTIONS "${_MPI_C_COMPILE_OPTIONS}")
