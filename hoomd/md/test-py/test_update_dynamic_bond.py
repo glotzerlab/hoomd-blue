5bcf17a2471863e7109b820fa2083a6e01ecadb6
Alyssa Travitz <atravitz@umich.edu>
2019-09-20 16:22:53 -0400

    adding c++ test framework

--- hoomd/md/test-py/test_update_dynamic_bond.py
+++ hoomd/md/test-py/test_update_dynamic_bond.py
@@ -2,6 +2,7 @@
 
 from hoomd import *
 from hoomd import md
+
 context.initialize()
 import unittest
 import os
@@ -9,48 +10,73 @@ import os
 r_colloid = 1
 
 # tests for md.update.dynamic_bond
-class update_dynamic_bond (unittest.TestCase):
+class update_dynamic_bond(unittest.TestCase):
     def setUp(self):
-        snap = data.make_snapshot(N=2, box=data.boxdim(Lx=80, Ly=80, Lz=80),
-                                  bond_types=['polymer'], particle_types=['colloid'])
+        snap = data.make_snapshot(
+            N=2,
+            box=data.boxdim(Lx=80, Ly=80, Lz=80),
+            bond_types=["polymer"],
+            particle_types=["colloid"],
+        )
 
         if comm.get_rank() == 0:
-            snap.particles.diameter[:] = [r_colloid*2]*2
+            snap.particles.diameter[:] = [r_colloid * 2] * 2
             snap.particles.position[0] = [-1, 0, 0]
             snap.particles.position[1] = [1, 0, 0]
 
-        self.s = init.read_snapshot(snap);
-        self.nl = md.nlist.tree();
+        self.s = init.read_snapshot(snap)
+        self.nl = md.nlist.tree()
         self.integrator = md.integrate.mode_standard(dt=0.001)
         md.integrate.brownian(group=group.all(), kT=1, seed=0)
 
     # tests basic creation of the updater
     def test_create(self):
         # nl = md.nlist.tree();
-        dybond = md.update.dynamic_bond(group=group.all(), nlist=self.nl, seed=1, period=1, integrator=self.integrator, table_width=10)
-        run(10);
+        dybond = md.update.dynamic_bond(
+            group=group.all(),
+            nlist=self.nl,
+            seed=1,
+            period=1,
+            integrator=self.integrator,
+            table_width=10,
+        )
+        run(10)
 
-    # # tests formation of a bond within a cutoff radius
-    # def test_formation(self):
-    #     dybond = md.update.dynamic_bond(group.all(), nlist=self.nl, seed=1, period=1)
-    #     dybond.set_params(r_cut=2.0, bond_type='harmonic', prob_form=1, prob_break=0)
+    # tests formation of a bond within a cutoff radius
+    def test_set_params(self):
+        dybond = md.update.dynamic_bond(
+            group=group.all(),
+            nlist=self.nl,
+            seed=1,
+            period=1,
+            integrator=self.integrator,
+            table_width=10,
+        )
+        dybond.set_params(
+            r_cut=2.0,
+            r_true=1.0,
+            bond_type="harmonic",
+            delta_G=8.0,
+            n_polymer=10,
+            nK=10,
+        )
 
-    # # tests breakage of a bond within a cutoff radius
-    # def test_breakage(self):
-    #     dybond = md.update.dynamic_bond(group.all(), nlist=self.nl, seed=1, period=1)
-    #     dybond.set_params(r_cut=2.0, bond_type='harmonic', prob_form=0, prob_break=1)
+    def test_set_from_file(self):
+        dybond = md.update.dynamic_bond(
+            group=group.all(),
+            nlist=self.nl,
+            seed=1,
+            period=1,
+            integrator=self.integrator,
+            table_width=10,
+        )
+        dybond.set_from_file('test.txt')
 
-    # # test that no bonds are formed outside the rcut
-    # def test_outside_cutoff(self):
-    #     dybond = md.update.dynamic_bond(group.all(), nlist=self.nl, seed=1, period=1)
-    #     dybond.set_params(r_cut=1.0, bond_type='harmonic', prob_form=1, prob_break=0)
-
-    # def tearDown(self):
-    #     del self.s
-    #     context.initialize();
-
-if __name__ == '__main__':
-    unittest.main(argv = ['test.py', '-v'])
+    def tearDown(self):
+        del self.s
+        context.initialize()
 
 
+if __name__ == "__main__":
+    unittest.main(argv=["test.py", "-v"])
 
--- hoomd/md/test/test_dynamic_bond.cc
+++ hoomd/md/test/test_dynamic_bond.cc
@@ -1,28 +1,30 @@
 // Copyright (c) 2009-2019 The Regents of the University of Michigan
 // This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
-#include <iostream>
-#include <memory>
-
 #include "hoomd/ExecutionConfiguration.h"
 #include "hoomd/md/DynamicBond.h"
 
 #include "hoomd/Initializers.h"
 #include "hoomd/SnapshotSystemData.h"
 #include "hoomd/md/NeighborListTree.h"
+
 #include <math.h>
+#include <iostream>
+
+#include <memory>
 #include <functional>
+
 using namespace std;
 using namespace std::placeholders;
-
-#include "hoomd/test/upp11_config.h"
-HOOMD_UP_MAIN();
-
 
 /*! \file test_dynamic_bond.cc
     \brief Unit tests for the DynamicBond class
     \ingroup unit_tests
  */
+
+#include "hoomd/test/upp11_config.h"
+
+HOOMD_UP_MAIN();
 
 
 //! Typedef to make using the std::function factory easier
@@ -30,36 +32,108 @@ typedef std::function<std::shared_ptr<DynamicBond>  (std::shared_ptr<SystemDefin
                                                      std::shared_ptr<ParticleGroup> group,
                                                      std::shared_ptr<NeighborList> nlist,
                                                      int seed,
-                                                     Scalar delta_t,
+                                                     Scalar delta_t
                                                      int period
+                                                     unsigned int table_width
                                                      )> dybond_creator;
 
-void dynamic_bond_initialization_test(dybond_creator db_creator, std::shared_ptr<ExecutionConfiguration> exec_conf)
+//! Test bond creation
+void dynamic_bond_create_destroy_test(dybond_creator db_creator, std::shared_ptr<ExecutionConfiguration> exec_conf)
     {
     // start with the simplest possible test: 2 particles in a box with only one bond type
-
     std::shared_ptr<SystemDefinition> sysdef(new SystemDefinition(2, BoxDim(100.0), 1, 1, 0, 0, 0,  exec_conf));
     std::shared_ptr<ParticleData> pdata = sysdef->getParticleData();
 
-
     pdata->setFlags(~PDataFlags(0));
     pdata->setPosition(0,make_scalar3(0.0,0.0,0.0));
-    pdata->setPosition(1,make_scalar3(0.9,0.0,0.0));
+    pdata->setPosition(1,make_scalar3(1.2,0.0,0.0));
 
-    Scalar maxdiam = pdata->getMaxDiameter();
-    Scalar r_cut = Scalar(1.3);
-    Scalar r_alpha = maxdiam/2 - 0.5;
-    Scalar r_cut_wc = r_cut + 2 * r_alpha;
+    Scalar r_cut = Scalar(3.0);
+    Scalar max_diam = Scalar(1.0);
 
     ArrayHandle<Scalar4> h_pos(pdata->getPositions(), access_location::host, access_mode::readwrite);
     ArrayHandle<Scalar> h_diameter(pdata->getDiameters(), access_location::host, access_mode::readwrite);
 
-    std::shared_ptr<NeighborListTree> nlist(new NeighborListTree(sysdef, r_cut_wc, Scalar(3.0)));
+    std::shared_ptr<NeighborListTree> nlist(new NeighborListTree(sysdef, max_diam, r_cut));
     std::shared_ptr<ParticleSelector> selector_all(new ParticleSelectorTag(sysdef, 0, pdata->getN()-1));
     std::shared_ptr<ParticleGroup> group_all(new ParticleGroup(sysdef, selector_all));
 
-    std::shared_ptr<DynamicBond> dybond = db_creator(sysdef, group_all, nlist, 0, 0.05, 1);
+    std::shared_ptr<DynamicBond> dybond = db_creator(sysdef, group_all, nlist, 0, 1);
+    dybond->setParams(r_cut, "harmonic", 1, 0);
 
+    std::shared_ptr<BondData> bdata(sysdef->getBondData());
+    // Access the GPU bond table for reading
+    ArrayHandle<BondData::members_t> h_gpu_bondlist(bdata->getGPUTable(), access_location::host, access_mode::read);
+    ArrayHandle<unsigned int > h_gpu_n_bonds(bdata->getNGroupsArray(), access_location::host, access_mode::read);
 
-    // dybond->setParams(30.0, 10.0, "fene", 6.0, 10, 60);
+    dybond->update(0);
+
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[0], 1);
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[1], 1);
+
+    dybond->setParams(r_cut, "harmonic", 0, 1);
+    dybond->update(1);
+
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[0], 0);
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[1], 0);
     }
+
+
+void dynamic_bond_rcut_test(dybond_creator db_creator, std::shared_ptr<ExecutionConfiguration> exec_conf)
+    {
+    // start with the simplest possible test: 2 particles in a box with only one bond type
+    std::shared_ptr<SystemDefinition> sysdef(new SystemDefinition(2, BoxDim(100.0), 1, 1, 0, 0, 0,  exec_conf));
+    std::shared_ptr<ParticleData> pdata = sysdef->getParticleData();
+
+    pdata->setFlags(~PDataFlags(0));
+    pdata->setPosition(0,make_scalar3(0.0,0.0,0.0));
+    pdata->setPosition(1,make_scalar3(1.2,0.0,0.0));
+
+    Scalar r_cut = Scalar(3.0);
+    Scalar max_diam = Scalar(1.0);
+
+    ArrayHandle<Scalar4> h_pos(pdata->getPositions(), access_location::host, access_mode::readwrite);
+    ArrayHandle<Scalar> h_diameter(pdata->getDiameters(), access_location::host, access_mode::readwrite);
+
+    std::shared_ptr<NeighborListTree> nlist(new NeighborListTree(sysdef, max_diam, r_cut));
+    std::shared_ptr<ParticleSelector> selector_all(new ParticleSelectorTag(sysdef, 0, pdata->getN()-1));
+    std::shared_ptr<ParticleGroup> group_all(new ParticleGroup(sysdef, selector_all));
+
+    std::shared_ptr<DynamicBond> dybond = db_creator(sysdef, group_all, nlist, 0, 1);
+    dybond->setParams(r_cut, "harmonic", 1, 0);
+
+    std::shared_ptr<BondData> bdata(sysdef->getBondData());
+    // Access the GPU bond table for reading
+    ArrayHandle<BondData::members_t> h_gpu_bondlist(bdata->getGPUTable(), access_location::host, access_mode::read);
+    ArrayHandle<unsigned int > h_gpu_n_bonds(bdata->getNGroupsArray(), access_location::host, access_mode::read);
+
+    dybond->update(0);
+
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[0], 1);
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[1], 1);
+
+    dybond->setParams(r_cut, "harmonic", 0, 1);
+    dybond->update(1);
+
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[0], 0);
+    UP_ASSERT_EQUAL(h_gpu_n_bonds.data[1], 0);
+    }
+
+//! DynamicBond creator for unit tests
+std::shared_ptr<DynamicBond> base_class_db_creator(std::shared_ptr<SystemDefinition> sysdef, std::shared_ptr<ParticleGroup> group, std::shared_ptr<NeighborList> nlist, int seed, int period)
+   {
+    return std::shared_ptr<DynamicBond>(new DynamicBond(sysdef, group, nlist, seed, period));
+   }
+
+
+UP_TEST(dybond_create_destroy)
+{
+dybond_creator db_creator_base = bind(base_class_db_creator, _1, _2, _3, _4, _5);
+dynamic_bond_create_destroy_test(db_creator_base,  std::shared_ptr<ExecutionConfiguration>(new ExecutionConfiguration(ExecutionConfiguration::CPU)));
+}
+
+UP_TEST(dybond_rcut)
+{
+dybond_creator db_creator_base = bind(base_class_db_creator, _1, _2, _3, _4, _5);
+dynamic_bond_rcut_test(db_creator_base,  std::shared_ptr<ExecutionConfiguration>(new ExecutionConfiguration(ExecutionConfiguration::CPU)));
+}
