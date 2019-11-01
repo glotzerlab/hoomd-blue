35b0f7f8b0da3edd655a21e996b06bafc93c72fe
Brandon Butler <butlerbr@umich.edu>
2019-11-01 14:00:39 -0400

    TypeParameterDict working

--- hoomd/typeparameterdict.py
+++ hoomd/typeparameterdict.py
@@ -1,34 +1,154 @@
 from collections import defaultdict
+from itertools import product
+from functools import partial
+from copy import deepcopy
 
+# Psudonym for None that states an argument is required to be supplied by the
+# user
 RequiredArg = None
 
-class TypeParameterDict:
 
-    def __init__(self, len_keys=1, **kwargs):
-        self._dict = defaultdict(kwargs)
+# Checks if a value is iterable and not a string
+def is_iterable(obj):
+    return hasattr(obj, '__iter__') and not isinstance(obj, str)
+
+
+def has_str_elems(obj):
+    return all([isinstance(elem, str) for elem in obj])
+
+
+def is_bad_iterable(obj):
+    return is_iterable(obj) and not has_str_elems(obj)
+
+
+def partial_dict(**kwargs):
+    return partial(dict, **kwargs)
+
+
+def const(val):
+    return lambda: val
+
+
+class _ValidateDict:
+
+    def _validate_and_split_key(self, key):
+        if self._len_keys == 1:
+            return self._validate_and_split_len_one(key)
+        elif self._len_keys == 2:
+            return self._validate_and_split_len_two(key)
+        else:
+            return None
+
+    def _validate_and_split_len_one(self, key):
+        if isinstance(key, str):
+            return [key]
+        elif is_iterable(key) and has_str_elems(key):
+            return list(key)
+        else:
+            raise KeyError("The key {} is not valid.".format(key))
+
+    def _validate_and_split_len_two(self, key):
+        if isinstance(key, tuple) and len(key) == 2:
+            fst, snd = key
+            if is_bad_iterable(fst) or is_bad_iterable(snd):
+                raise KeyError("The key {} is not valid.".format(key))
+            if isinstance(fst, str):
+                fst = [fst]
+            if isinstance(snd, str):
+                snd = [snd]
+            return product(fst, snd)
+        elif is_iterable(key):
+            keys = []
+            for k in key:
+                if isinstance(k, tuple) and len(k) == 2 and has_str_elems(k):
+                    keys.append(k)
+                else:
+                    raise KeyError("The key {} is not valid.".format(k))
+            return keys
+        else:
+            raise KeyError("The key {} is not valid.".format(key))
+
+
+class TypeParameterDict(_ValidateDict):
+
+    def __init__(self, len_keys=1, *args, **kwargs):
+
+        # Validate proper key constraint
+        if len_keys > 2:
+            raise ValueError("TypeParameterDict does not support keys larger "
+                             "than 2 types.")
         self._len_keys = len_keys
+
+        # Create default dictionary
+        if len(kwargs) != 0 and len(args) != 0:
+            raise ValueError("An unnamed argument and keyword arguments "
+                             "cannot both be specified.")
+        if len(kwargs) == 0 and len(args) == 0:
+            raise ValueError("Either an unnamed argument or keyword "
+                             "arguments must be specified.")
+        if len(args) > 1:
+            raise ValueError("Only one unnamed argument allowed.")
+        if len(kwargs) > 0:
+            self._dict = defaultdict(partial_dict(**kwargs))
+            self._is_kw = True
+        else:
+            self._dict = defaultdict(const(args[0]))
+            self._is_kw = False
 
     def __getitem__(self, key):
         keys = self._validate_and_split_key(key)
         vals = dict()
         for key in keys:
+            if self._len_keys > 1:
+                key = tuple(sorted(key))
             vals[key] = self._dict[key]
         return vals
 
     def __setitem__(self, key, val):
         keys = self._validate_and_split_key(key)
-        val = self.validate_values(val)
+        val = self._validate_values(val)
         for key in keys:
+            if self._len_keys > 1:
+                key = tuple(sorted(key))
+            self._setkey(key, val)
+
+    def _setkey(self, key, val):
+        if self._is_kw:
+            if key in self._dict:
+                self._dict[key] = self._dict.default_factory()
+                self._dict[key].update(val)
+            else:
+                self._dict[key].update(val)
+        else:
             self._dict[key] = val
 
-    def _validate_and_split_key(self, key):
-        pass
-
     def _validate_values(self, val):
-        pass
+        curr_dft = self._dict.default_factory()
+        if self._is_kw:
+            if not isinstance(val, dict):
+                raise ValueError("Cannot set type to non-dictionary value.")
+            dft_keys = set(curr_dft.keys())
+            if len(dft_keys.intersection(val.keys())) != len(val.keys()):
+                raise ValueError("Keys must be a subset of available keys ")
+        return val
 
+    @property
+    def default(self):
+        return self._dict.default_factory()
 
-class AttachedTypeParameterDict:
+    @default.setter
+    def default(self, val):
+        curr_dft = self.default
+        if not isinstance(val, type(curr_dft)):
+            raise ValueError("New default expected type {} "
+                             "but received type {}".format(type(curr_dft),
+                                                           type(val)))
+        if isinstance(val, dict):
+            if curr_dft.keys() != val.keys():
+                raise ValueError("New default must contain the same keys.")
+            self._dict.default_factory = partial_dict(**val)
+        else:
+            self._dict.default_factory = const(val)
 
     def __init__(self, types, type_param_dict, cpp_obj, sim):
         # add all types to c++
