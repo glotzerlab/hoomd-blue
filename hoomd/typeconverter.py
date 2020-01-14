175a44d743751f9f78a8f910e2462e71419c7592
Brandon Butler <butlerbr@umich.edu>
2020-01-14 14:26:45 -0500

    Move get_type_converter to from_default classmethod
    Allow explicit defaults to overwrite defaults assumed by type_converter
    input

--- hoomd/typeconverter.py
+++ hoomd/typeconverter.py
@@ -1,3 +1,4 @@
+from numpy import array, ndarray
 from hoomd.util import is_constructor
 from inspect import isfunction
 
@@ -6,21 +7,42 @@ class TypeConversionError(Exception):
     pass
 
 
+def to_list(value):
+    if isinstance(value, str) or isinstance(value, dict):
+        raise ValueError
+    else:
+        return list(value)
+
+
+def to_array(value):
+    if isinstance(value, ndarray):
+        return TypeConverter(array)
+
+
 class TypeConverter:
+    _conversion_func_dict = {list: to_list, ndarray: to_array}
+
     def __init__(self, converter):
         self.converter = converter
 
     def _raise_error(self, value, expected_type):
-        raise TypeConversionError("Value {} of type {} cannot be converted "
-                                  "using {}.".format(value, type(value),
-                                                     expected_type))
+        err = "Value {} of type {} cannot be converted using {}."
+        err = err.format(value, type(value), expected_type)
+        raise TypeConversionError(err)
+
+    def _raise_from_previous_error(self, err, key):
+        if len(err.args) > 1:
+
+            raise TypeConversionError(err.args[0], [key] + err.args[1])
+        else:
+            raise TypeConversionError(err.args[0], [key])
 
     def convert(self, value):
         if isinstance(self.converter, dict):
             new_value = dict()
             try:
                 for key, v in value.items():
-                    temp_value = self._convert_value(key, v)
+                    temp_value = self._convert_value(v, key)
                     if temp_value == {}:
                         continue
                     else:
@@ -31,42 +53,66 @@ class TypeConverter:
         else:
             return self._convert_value(value)
 
-    def _convert_value(self, key, value):
-        try:
-            return self.converter[key](value)
-        except (TypeError, ValueError):
-            self._raise_error(value, self.converter[key])
-        except KeyError:
-            return value
+    def _convert_value(self, value, key=None):
+        if key is None:
+            try:
+                return self.converter(value)
+            except (TypeError, ValueError):
+                self._raise_error(value, self.converter)
+        else:
+            try:
+                return self.converter[key](value)
+            except TypeConversionError as err:
+                self._raise_from_previous_error(err, key)
+            except KeyError:
+                return value
 
     def __call__(self, value):
         return self.convert(value)
 
     def __str__(self):
-        return '{' + ', '.join(["{}: {}".format(key, value)
-                                for key, value in self.converter.items()]
-                               ) + '}'
+        if isinstance(self.converter, dict):
+            return '{' + ', '.join(["{}: {}".format(key, value)
+                                    for key, value in self.converter.items()]
+                                   ) + '}'
+        else:
+            return str(self.converter)
+
+    @classmethod
+    def from_default(cls, default):
+        # if default is a dictionary recursively call from_default on values
+        if isinstance(default, dict):
+            converter = dict()
+            for key, value in default.items():
+                converter[key] = cls.from_default(value)
+            return cls(converter)
+        # if constructor with special default setting logic
+        elif default in cls._conversion_func_dict.keys():
+            return cls(cls._conversion_func_dict[default])
+        # if type with special default setting logic
+        elif type(default) in cls._conversion_func_dict.keys():
+            return cls(cls._conversion_func_dict[type(default)])
+        # if function or object constructor
+        elif is_constructor(default) or isfunction(default):
+            return cls(default)
+        # if other object
+        else:
+            return cls(type(default))
 
 
-def get_type_converter(default):
-    if isinstance(default, dict):
-        converter = dict()
-        for key, value in default.items():
-            converter[key] = get_type_converter(value)
-        return TypeConverter(converter)
-    elif is_constructor(default) or isfunction(default):
-        return default
-    else:
-        return type(default)
-
-
-def from_type_converter_input_to_default(default):
+def from_type_converter_input_to_default(default, overwrite_default=None):
     if isinstance(default, dict):
         new_default = dict()
-        for key, value in default.items():
-            new_default[key] = from_type_converter_input_to_default(value)
+        # if overwrite_default exists use those values over default
+        if overwrite_default is not None:
+            for key, value in default.items():
+                value = overwrite_default.get(key, value)
+                new_default[key] = from_type_converter_input_to_default(value)
+        else:
+            for key, value in default.items():
+                new_default[key] = from_type_converter_input_to_default(value)
         return new_default
     elif is_constructor(default) or isfunction(default):
-        return None
+        return None if overwrite_default is None else overwrite_default
     else:
-        return default
+        return default if overwrite_default is None else overwrite_default
