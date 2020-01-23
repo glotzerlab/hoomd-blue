2a0d823231139d3c4ce531b4967609cbe1ec61f4
Brandon Butler <butlerbr@umich.edu>
2020-01-23 16:45:50 -0500

    Remove TriggeredOps handling of adding to list
    SyncedLists now handle adding themselves to the appropriate list

--- hoomd/meta.py
+++ hoomd/meta.py
@@ -304,19 +304,21 @@ class _TriggeredOperation(_Operation):
             raise ValueError("Trigger of type {} must be a subclass of "
                              "hoomd.triggers.Trigger".format(type(new_trigger))
                              )
-        self._trigger = new_trigger
         if self.is_attached:
             sys = self._simulation._cpp_sys
             triggered_ops = getattr(sys, self._cpp_list_name)
             for index in range(len(triggered_ops)):
-                if triggered_ops[index][0] == self._cpp_obj:
+                op, trigger = triggered_ops[index]
+                # If tuple is the operation and trigger according to memory
+                # location (python's is), replace with new trigger
+                if op is self._cpp_obj and trigger is self._trigger:
                     new_tuple = (self._cpp_obj, new_trigger)
                     triggered_ops[index] = new_tuple
+        # Overwrite python trigger
+        self._trigger = new_trigger
 
     def attach(self, simulation):
         self._simulation = simulation
-        sys = simulation._cpp_sys
-        getattr(sys, self._cpp_list_name).append((self._cpp_obj, self.trigger))
 
 
 class _Updater(_TriggeredOperation):
--- hoomd/pytest/test_triggeredops.py
+++ hoomd/pytest/test_triggeredops.py
@@ -1,5 +1,6 @@
 from hoomd.pytest.dummy import DummyCppObj, DummySimulation, DummyTrigger
 from hoomd.pytest.dummy import DummyOperation, DummyTriggeredOp
+from hoomd.syncedlist import SyncedList
 from hoomd.triggers import PeriodicTrigger
 
 
@@ -26,8 +27,11 @@ def test_trigger_resetting():
 def test_attach():
     triggered_op = DummyTriggeredOp(trigger=1)
     sim = DummySimulation()
+    slist = SyncedList(lambda x: isinstance(x, DummyTriggeredOp),
+                       lambda x: (x._cpp_obj, x.trigger))
+    slist.append(triggered_op)
     triggered_op._cpp_obj = DummyCppObj()
-    triggered_op.attach(sim)
+    slist.attach(sim, sim._cpp_sys.dummy_list)
     assert len(sim._cpp_sys.dummy_list) == 1
     assert len(sim._cpp_sys.dummy_list[0]) == 2
     assert triggered_op._cpp_obj == sim._cpp_sys.dummy_list[0][0]
@@ -37,8 +41,11 @@ def test_attach():
 def test_attach_trigger_resetting():
     triggered_op = DummyTriggeredOp(trigger=1)
     sim = DummySimulation()
+    slist = SyncedList(lambda x: isinstance(x, DummyTriggeredOp),
+                       lambda x: (x._cpp_obj, x.trigger))
+    slist.append(triggered_op)
     triggered_op._cpp_obj = DummyCppObj()
-    triggered_op.attach(sim)
+    slist.attach(sim, sim._cpp_sys.dummy_list)
     triggered_op.trigger = DummyTrigger()
     assert len(sim._cpp_sys.dummy_list) == 1
     assert len(sim._cpp_sys.dummy_list[0]) == 2
