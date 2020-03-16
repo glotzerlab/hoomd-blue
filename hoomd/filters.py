bfc2a992aaa560c7b6560b7837e8ca4aed9391f0
Brandon Butler <butlerbr@umich.edu>
2020-03-16 14:41:57 -0400

    Fix hashing and == for All and Tags filters
    Hashing these objects was not working, but that has been fixed with
    this commit. Now caching filters is relatively straight forward and
    robust.

--- hoomd/filters.py
+++ hoomd/filters.py
@@ -3,11 +3,8 @@ import numpy as np
 
 
 class ParticleFilter:
-    def __init__(self, *args, **kwargs):
-        self._id = self._get_id(*args, **kwargs)
-
     def __hash__(self):
-        return self._id
+        return NotImplementedError("Must implement hash for Filters.")
 
     def __eq__(self, other):
         raise NotImplementedError(
@@ -20,21 +17,12 @@ class ParticleFilter:
         '''Needs to interact with state to get particles across MPI rank.'''
         raise NotImplementedError
 
-    def _compute_id(self, *args, **kwargs):
-        raise NotImplementedError(
-            "_compute_id for {} is not defined.".format(self.__class__))
-
-    @property
-    def id(self):
-        return self._id
-
 
 class All(ParticleFilter, _hoomd.ParticleFilterAll):
     def __init__(self):
-        ParticleFilter.__init__(self)
         _hoomd.ParticleFilterAll.__init__(self)
 
-    def _compute_id(self):
+    def __hash__(self):
         return 0
 
     def __eq__(self, other):
@@ -45,11 +33,20 @@ class Tags(ParticleFilter, _hoomd.ParticleFilterTags):
     def __init__(self, tags):
         if isinstance(tags, np.ndarray):
             tags = tags.astype(np.uint32)
-        ParticleFilter.__init__(self, tags)
+        else:
+            tags = np.array(tags, dtype=np.uint32)
+        tags = np.unique(tags)
+        self._tags = tags
         _hoomd.ParticleFilterTags.__init__(self, tags)
 
-    def _compute_id(self, tags):
-        return hash(tags.tobytes())
+    def __hash__(self):
+        if not hasattr(self, '_id'):
+            self._id = hash(self._tags.tobytes())
+        return self._id
 
     def __eq__(self, other):
-        return self.tags == other.tags and type(self) == type(other)
+        return all(self.tags == other.tags) and type(self) == type(other)
+
+    @property
+    def tags(self):
+        return self._tags
