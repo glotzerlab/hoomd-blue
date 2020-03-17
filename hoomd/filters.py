61527ee3a46058f455de3a50efefd4745bf7774a
Brandon Butler <butlerbr@umich.edu>
2020-03-16 20:02:27 -0400

    Add set operation filters (difference, intersection, union)
    The current implementation computes both internal filters separately
    before using the set methods in std::algorithm. This is due to there not
    being a method to check a single member for passing through a filter. In
    many cases this is desirable since we have to get handles to global
    arrays and iterate through them. Not having to do that every time
    prevents standard filters from being slow.

--- hoomd/filters.py
+++ hoomd/filters.py
@@ -67,3 +67,36 @@ class Types(ParticleFilter, _hoomd.ParticleFilterType):
     @property
     def types(self):
         return self._types
+
+
+class ParticleFilterSetOperations(ParticleFilter):
+    def __init__(self, f, g):
+        if f == g:
+            raise ValueError("Cannot use same filter for {}"
+                             "".format(self.__class__.__name__))
+        else:
+            self._f = f
+            self._g = g
+        getattr(_hoomd, self._cpp_cls_name).__init__(self, f, g)
+
+    def __hash__(self):
+        return hash(hash(self._f) + hash(self._g))
+
+    def __eq__(self, other):
+        return type(self) == type(other) and \
+            self._f == other._f and \
+            self._g == other._g
+
+
+class SetDifference(ParticleFilterSetOperations,
+                    _hoomd.ParticleFilterSetDifference):
+    _cpp_cls_name = 'ParticleFilterSetDifference'
+
+
+class Union(ParticleFilterSetOperations, _hoomd.ParticleFilterUnion):
+    _cpp_cls_name = 'ParticleFilterUnion'
+
+
+class Intersection(ParticleFilterSetOperations,
+                   _hoomd.ParticleFilterIntersection):
+    _cpp_cls_name = 'ParticleFilterIntersection'
