92737e3f95ff94129e52e1fab1f40a0f70550d46
Brandon Butler <butlerbr@umich.edu>
2020-03-20 14:33:46 -0400

    Update the ParticleFilterSetOperations
    Obeys symmetry in __eq__ now and a developer commment

--- hoomd/filter/set_.py
+++ hoomd/filter/set_.py
@@ -10,26 +10,35 @@ class ParticleFilterSetOperations(ParticleFilter):
         else:
             self._f = f
             self._g = g
+        # Grab the C++ class constructor for the set operation using the class
+        # variable _cpp_cls_name
         getattr(_hoomd, self._cpp_cls_name).__init__(self, f, g)
 
     def __hash__(self):
         return hash(hash(self._f) + hash(self._g))
 
     def __eq__(self, other):
-        return type(self) == type(other) and \
-            self._f == other._f and \
-            self._g == other._g
+        if self._symmetric:
+            return type(self) == type(other) and \
+                (self._f == other._f or self._f == other._g) and \
+                (self._g == other._g or self._g == other._f)
+        else:
+            return type(self) == type(other) and \
+                self._f == other._f and self._g == other._g
 
 
-class SetDifference(ParticleFilterSetOperations,
+class SetDifference(_ParticleFilterSetOperations,
                     _hoomd.ParticleFilterSetDifference):
     _cpp_cls_name = 'ParticleFilterSetDifference'
+    _symmetric = False
 
 
-class Union(ParticleFilterSetOperations, _hoomd.ParticleFilterUnion):
+class Union(_ParticleFilterSetOperations, _hoomd.ParticleFilterUnion):
     _cpp_cls_name = 'ParticleFilterUnion'
+    _symmetric = True
 
 
-class Intersection(ParticleFilterSetOperations,
+class Intersection(_ParticleFilterSetOperations,
                    _hoomd.ParticleFilterIntersection):
     _cpp_cls_name = 'ParticleFilterIntersection'
+    _symmetric = True
