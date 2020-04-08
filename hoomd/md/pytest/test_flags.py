1f331d2bfe0f5b8534f052995453ad41acfdb847
Joshua A. Anderson <joaander@umich.edu>
2020-04-08 09:41:38 -0400

    Use lattice_snapshot_factory in test_flags.py

--- hoomd/md/pytest/test_flags.py
+++ hoomd/md/pytest/test_flags.py
@@ -4,51 +4,14 @@ import numpy
 import itertools
 
 
-@pytest.fixture(scope='session')
-def md_simulation_factory(device):
-    def make_simulation(particle_types=['A']):
-        s = hoomd.Snapshot(device.comm)
-
-        # initialize particles on a simple cubic lattice and perturb them
-        # slightly off the lattice positions
-        # TODO: When hpmc_integrator_api is merged, port this functionality
-        # over to the fixture in conftest.py
-        l = 20
-        a = 2**(1.0 / 6.0)
-        r = a * 0.01
-
-        if s.exists:
-            s.configuration.box = [l * a, l * a, l * a, 0, 0, 0]
-
-            N = l**3
-            s.particles.N = N
-            pos = numpy.array(list(itertools.product(range(-l // 2, l // 2),
-                                                        repeat=3)))
-            s.particles.position[:] = pos
-            s.particles.position[:] += [a / 2, a / 2, a / 2]
-            s.particles.position[:] += numpy.random.uniform(-r, r, size=(N, 3))
-            s.particles.types = particle_types
-
-        sim = hoomd.Simulation(device)
-
-        # reduce sorter grid to avoid Hilbert curve overhead in unit tests
-        for tuner in sim.operations.tuners:
-            if isinstance(tuner, hoomd.tuner.ParticleSorter):
-                tuner.grid = 8
-
-        sim.create_state_from_snapshot(s)
-        return sim
-
-    return make_simulation
-
-
-def test_per_particle_virial(md_simulation_factory):
+def test_per_particle_virial(lattice_simulation_factory):
     cell = hoomd.md.nlist.Cell()
     lj = hoomd.md.pair.LJ(nlist=cell)
     lj.params[('A', 'A')] = dict(sigma=1.0, epsilon=1.0)
     lj.r_cut[('A', 'A')] = 2.5
 
-    sim = md_simulation_factory()
+    a = 2**(1.0 / 6.0)
+    sim = lattice_simulation_factory(n=20, a=a, r=a * 0.01)
 
     assert sim.always_compute_pressure == False
 
