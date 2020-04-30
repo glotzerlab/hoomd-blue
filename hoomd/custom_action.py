faa60ba5e33999571378a0fe3551f16dd0586f11
Brandon Butler <butlerbr@umich.edu>
2020-04-30 14:47:19 -0400

    Make _InternalCustomAction inherit from _HOOMDGetSetAttrBase

--- hoomd/custom_action.py
+++ hoomd/custom_action.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from hoomd.parameterdicts import ParameterDict
+from hoomd.operation import _HOOMDGetSetAttrBase
 
 
 class _CustomAction(ABC):
@@ -40,42 +41,10 @@ class _CustomAction(ABC):
         pass
 
 
-class _InternalCustomAction(_CustomAction):
+class _InternalCustomAction(_CustomAction, _HOOMDGetSetAttrBase):
     """An internal class for Python ``Action``s.
 
     Gives additional support in using HOOMD constructs like ``ParameterDict``s
     and ``TypeParameters``.
     """
-
-    _reserved_attrs_with_dft = {'_param_dict': ParameterDict,
-                                '_typeparam_dict': dict}
-
-    def __getattr__(self, attr):
-        if attr in self._reserved_attrs_with_dft.keys():
-            setattr(self, attr, self._reserved_attrs_with_dft[attr]())
-            return self.__dict__[attr]
-        elif attr in self._param_dict.keys():
-            return self._getattr_param(attr)
-        elif attr in self._typeparam_dict.keys():
-            return self._getattr_typeparam(attr)
-        else:
-            raise AttributeError("Object {} has no attribute {}".format(
-                self, attr))
-
-    def __setattr__(self, attr, value):
-        if attr in self._reserved_attrs_with_dft.keys():
-            super().__setattr__(attr, value)
-        elif attr in self._param_dict.keys():
-            self._param_dict[attr] = value
-        elif attr in self._typeparam_dict.keys():
-            self._setattr_typeparam(attr, value)
-        else:
-            super().__setattr__(attr, value)
-
-    def _setattr_typeparam(self, attr, value):
-        try:
-            for k, v in value.items():
-                self._typeparam_dict[attr][k] = v
-        except TypeError:
-            raise ValueError("To set {}, you must use a dictionary "
-                             "with types as keys.".format(attr))
+    pass
