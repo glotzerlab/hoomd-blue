dbc28600e4994324179e74603103caba0ef15559
Brandon Butler <butlerbr@umich.edu>
2020-04-30 14:54:53 -0400

    Add helper classes for custom {analyzer,updater}s

--- hoomd/analyze/custom_analyzer.py
+++ hoomd/analyze/custom_analyzer.py
@@ -2,21 +2,20 @@ from hoomd.custom_operation import _CustomOperation, _InternalCustomOperation
 from hoomd.custom_action import _CustomAction, _InternalCustomAction
 
 
-class _CustomAnalyzerAction(_CustomAction):
+class _AnalyzeMethod:
     def analyze(self, timestep):
         return self.act(timestep)
 
 
-class _InternalCustomAnalyzerAction(_InternalCustomAction):
-    def analyze(self, timestep):
-        return self.act(timestep)
+class _CustomAnalyzerAction(_CustomAction, _AnalyzeMethod):
+    pass
 
 
-class _CustomAnalyzer(_CustomOperation):
-    _cpp_list_name = 'analyzers'
-    _cpp_class_name = 'PythonAnalyzer'
-    _cpp_action = 'analyze'
+class _InternalCustomAnalyzerAction(_InternalCustomAction, _AnalyzeMethod):
+    pass
 
+
+class _AnalyzerProperty:
     @property
     def analyzer(self):
         return self._action
@@ -29,18 +28,11 @@ class _CustomAnalyzer(_CustomOperation):
             raise ValueError("analyzer must be an instance of _CustomAction")
 
 
-class _InternalCustomAnalyzer(_InternalCustomOperation):
+class _CustomAnalyzer(_CustomOperation, _AnalyzerProperty):
     _cpp_list_name = 'analyzers'
     _cpp_class_name = 'PythonAnalyzer'
-    _cpp_action = 'analyze'
 
-    @property
-    def analyzer(self):
-        return self._action
 
-    @analyzer.setter
-    def analyzer(self, analyzer):
-        if isinstance(analyzer, _CustomAction):
-            self._action = analyzer
-        else:
-            raise ValueError("analyzer must be an instance of _CustomAction")
+class _InternalCustomAnalyzer(_InternalCustomOperation, _AnalyzerProperty):
+    _cpp_list_name = 'analyzers'
+    _cpp_class_name = 'PythonAnalyzer'
--- hoomd/update/custom_updater.py
+++ hoomd/update/custom_updater.py
@@ -2,21 +2,20 @@ from hoomd.custom_operation import _CustomOperation, _InternalCustomOperation
 from hoomd.custom_action import _CustomAction, _InternalCustomAction
 
 
-class _CustomUpdaterAction(_CustomAction):
+class _UpdateMethod:
     def update(self, timestep):
         return self.act(timestep)
 
 
-class _InternalCustomUpdaterAction(_InternalCustomAction):
-    def update(self, timestep):
-        return self.act(timestep)
+class _CustomUpdaterAction(_CustomAction, _UpdateMethod):
+    pass
 
 
-class _CustomUpdater(_CustomOperation):
-    _cpp_list_name = 'updaters'
-    _cpp_class_name = 'PythonUpdater'
-    _cpp_action = 'update'
+class _InternalCustomUpdaterAction(_InternalCustomAction, _UpdateMethod):
+    pass
 
+
+class _UpdaterProperty:
     @property
     def updater(self):
         return self._action
@@ -29,18 +28,11 @@ class _CustomUpdater(_CustomOperation):
             raise ValueError("updater must be an instance of _CustomAction")
 
 
-class _InternalCustomUpdater(_InternalCustomOperation):
+class _CustomUpdater(_CustomOperation, _UpdaterProperty):
     _cpp_list_name = 'updaters'
     _cpp_class_name = 'PythonUpdater'
-    _cpp_action = 'update'
 
-    @property
-    def updater(self):
-        return self._action
 
-    @updater.setter
-    def updater(self, updater):
-        if isinstance(updater, _CustomAction):
-            self._action = updater
-        else:
-            raise ValueError("updater must be an instance of _CustomAction")
+class _InternalCustomUpdater(_InternalCustomOperation, _UpdaterProperty):
+    _cpp_list_name = 'updaters'
+    _cpp_class_name = 'PythonUpdater'
