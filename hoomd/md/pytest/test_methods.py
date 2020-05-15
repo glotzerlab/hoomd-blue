04761582e6d574d63ff17b191606f713f7b8df9f
Tommy Waltmann <tomwalt@umich.edu>
2020-05-15 13:25:15 -0400

    reformatted test

--- hoomd/md/pytest/test_methods.py
+++ hoomd/md/pytest/test_methods.py
@@ -3,13 +3,18 @@ import pytest
 import numpy
 import itertools
 
-def test_attach_nvt(simulation_factory, two_particle_snapshot_factory):
+def attach_and_sim(simulation_factory, two_particle_snapshot_factory, method):
     sim = simulation_factory(two_particle_snapshot_factory(dimensions=3, d=.9))
-    f = hoomd.filter.All()
-    nvt = hoomd.md.methods.NVT(f, kT=1, tau=1)
     integrator = hoomd.md.Integrator(.05)
-    integrator.methods.append(nvt)
+    integrator.methods.append(method)
     sim.operations.integrator = integrator
     sim.operations.schedule()
     sim.run(10)
 
+def test_attach_nvt(simulation_factory, two_particle_snapshot_factory):
+    nvt = hoomd.md.methods.NVT(hoomd.filter.All(), kT=1, tau=1)
+    attach_and_sim(simulation_factory, two_particle_snapshot_factory, nvt)
+
+def test_attach_langevin(simulation_factory, two_particle_snapshot_factory):
+    lang = hoomd.md.methods.Langevin(hoomd.filter.All(), kT=1, seed=1)
+    attach_and_sim(simulation_factory, two_particle_snapshot_factory, lang)
