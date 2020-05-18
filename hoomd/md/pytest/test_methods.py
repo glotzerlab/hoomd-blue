d68b344f5023d973c894bcb13958334019ae2954
Tommy Waltmann <tomwalt@umich.edu>
2020-05-18 10:04:06 -0400

    reformatted test_attach

--- hoomd/md/pytest/test_methods.py
+++ hoomd/md/pytest/test_methods.py
@@ -2,19 +2,22 @@ import hoomd
 import pytest
 import numpy
 import itertools
+from copy import deepcopy
 
-def attach_and_sim(simulation_factory, two_particle_snapshot_factory, method):
+_int_methods = [
+    hoomd.md.methods.Langevin(hoomd.filter.All(), kT=1, seed=1),
+    hoomd.md.methods.NVT(hoomd.filter.All(), kT=1, tau=1),
+]
+
+@pytest.fixture(scope='function', params=_int_methods)
+def int_method(request):
+    return deepcopy(request.param)
+
+def test_attach(simulation_factory, two_particle_snapshot_factory, int_method):
     sim = simulation_factory(two_particle_snapshot_factory(dimensions=3, d=.9))
     integrator = hoomd.md.Integrator(.05)
-    integrator.methods.append(method)
+    integrator.methods.append(int_method)
     sim.operations.integrator = integrator
     sim.operations.schedule()
     sim.run(10)
 
-def test_attach_nvt(simulation_factory, two_particle_snapshot_factory):
-    nvt = hoomd.md.methods.NVT(hoomd.filter.All(), kT=1, tau=1)
-    attach_and_sim(simulation_factory, two_particle_snapshot_factory, nvt)
-
-def test_attach_langevin(simulation_factory, two_particle_snapshot_factory):
-    lang = hoomd.md.methods.Langevin(hoomd.filter.All(), kT=1, seed=1)
-    attach_and_sim(simulation_factory, two_particle_snapshot_factory, lang)
