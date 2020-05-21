f567db96fb4eed21ef13b5cf0cede3cba587f6a9
Tommy Waltmann <tomwalt@umich.edu>
2020-05-21 09:28:47 -0400

    forgot import statement

--- hoomd/md/pair.py
+++ hoomd/md/pair.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2009-2019 The Regents of the University of Michigan
 # This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
+import hoomd
 from hoomd import _hoomd
 from hoomd.md import _md
 from hoomd.md import force
