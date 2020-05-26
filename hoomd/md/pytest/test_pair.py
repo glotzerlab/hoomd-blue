81c4cf73bf462da27c0a09f9e6e82174e9c3ee1e
Dan <danevans@umich.edu>
2020-05-26 17:49:17 -0400

    Test running md simulation

--- hoomd/md/pytest/test_pair.py
+++ hoomd/md/pytest/test_pair.py
@@ -94,9 +94,11 @@ def test_attached_params(simulation_factory, lattice_snapshot_factory,
             pot.r_on[pair] = r_on[pair]
 
     snap = lattice_snapshot_factory(particle_types=particle_types,
-                                    n=100, a=0.5, r=0.01)
-    snap.particles.typeid[:] = np.random.randint(0, len(snap.particles.types),
-                                                 snap.particles.N)
+                                    n=10, a=0.5, r=0.01)
+    if snap.exists:
+        snap.particles.typeid[:] = np.random.randint(0,
+                                                     len(snap.particles.types),
+                                                     snap.particles.N)
     sim = simulation_factory(snap)
     sim.operations.integrator = hoomd.md.Integrator(dt=0.005)
     sim.operations.integrator.forces.append(pot)
@@ -111,3 +113,40 @@ def test_attached_params(simulation_factory, lattice_snapshot_factory,
         assert_equivalent_type_params(attached_pot.r_on.to_dict(), r_on)
     assert_equivalent_parameter_dicts(attached_pot.nlist._param_dict,
                                       cell._param_dict)
+
+
+def test_run(simulation_factory, lattice_snapshot_factory,
+             valid_params):
+    pair_potential, pair_potential_dict, r_cut, r_on, mode = valid_params
+    particle_types = list(set(itertools.chain.from_iterable(r_cut.keys())))
+    cell = hoomd.md.nlist.Cell()
+    if mode is not None:
+        pot = pair_potential(nlist=cell, r_cut=2.5, mode=mode)
+    else:
+        pot = pair_potential(nlist=cell, r_cut=2.5)
+
+    for pair in pair_potential_dict:
+        pot.params[pair] = pair_potential_dict[pair]
+        pot.r_cut[pair] = r_cut[pair]
+        if r_on is not None:
+            pot.r_on[pair] = r_on[pair]
+
+    snap = lattice_snapshot_factory(particle_types=particle_types,
+                                    n=10, a=1, r=0.01)
+    if snap.exists:
+        snap.particles.typeid[:] = np.random.randint(0,
+                                                     len(snap.particles.types),
+                                                     snap.particles.N)
+    sim = simulation_factory(snap)
+    integrator = hoomd.md.Integrator(dt=0.005)
+    integrator.forces.append(pot)
+    integrator.methods.append(hoomd.md.methods.Langevin(hoomd.filter.All(),
+                                                        kT=1, seed=1))
+    sim.operations.integrator = integrator
+    sim.operations.schedule()
+    for nsteps in [1, 5, 10]:
+        initial_pos = sim.state.snapshot.particles.position
+        sim.run(nsteps)
+        with pytest.raises(AssertionError):
+            np.testing.assert_allclose(sim.state.snapshot.particles.position,
+                                       initial_pos)
