a07d7d333acf5b2fd824356c3de05079f6ab3102
Dan <danevans@umich.edu>
2020-06-01 11:21:48 -0400

    Fix test force

--- hoomd/md/pytest/test_pair.py
+++ hoomd/md/pytest/test_pair.py
@@ -152,6 +152,34 @@ def test_run(simulation_factory, lattice_snapshot_factory,
                                    initial_pos)
 
 
+# This function calculates the forces in a two particle simulation frame by
+# finding the negative derivative of energy over inter particle distance
+def calculate_force(sim):
+    snap = sim.state.snapshot
+    initial_pos = snap.particles.position
+    snap.particles.position[1] = initial_pos[1] * 0.99999999
+    sim.state.snapshot = snap
+    E0 = sim.operations.integrator.forces[0].energies
+    pos = sim.state.snapshot.particles.position
+    r0 = pos[0] - pos[1]
+    mag_r0 = np.linalg.norm(r0)
+    direction = r0 / mag_r0
+
+    snap = sim.state.snapshot
+    snap.particles.position[1] = initial_pos[1] * 1.00000001
+    sim.state.snapshot = snap
+    E1 = sim.operations.integrator.forces[0].energies
+    pos = sim.state.snapshot.particles.position
+    mag_r1 = np.linalg.norm(pos[0] - pos[1])
+
+    Fa = -1 * ((E1[0] - E0[0]) / (mag_r1 - mag_r0)) * 2 * direction
+    Fb = -1 * ((E1[1] - E0[1]) / (mag_r1 - mag_r0)) * 2 * direction * -1
+    snap = sim.state.snapshot
+    snap.particles.position[1] = initial_pos[1]
+    sim.state.snapshot = snap
+    return Fa, Fb
+
+
 @pytest.mark.parametrize("nsteps", [1, 5, 10])
 def test_compute_energy(simulation_factory, two_particle_snapshot_factory,
                         valid_params, nsteps):
@@ -167,7 +195,7 @@ def test_compute_energy(simulation_factory, two_particle_snapshot_factory,
         pot.r_cut[pair] = r_cut[pair]
         if r_on is not None:
             pot.r_on[pair] = r_on[pair]
-    snap = two_particle_snapshot_factory(particle_types=particle_types, d=5)
+    snap = two_particle_snapshot_factory(particle_types=particle_types, d=1.5)
     sim = simulation_factory(snap)
     integrator = hoomd.md.Integrator(dt=0.005)
     integrator.forces.append(pot)
@@ -180,30 +208,15 @@ def test_compute_energy(simulation_factory, two_particle_snapshot_factory,
         if snap.exists:
             snap.particles.typeid[0] = particle_types.index(pair[0])
             snap.particles.typeid[1] = particle_types.index(pair[1])
-        r0 = snap.particles.position[0] - snap.particles.position[1]
-        E0 = sim.operations.integrator.forces[0].energies
         sim.state.snapshot = snap
-        sim.run(nsteps)
-        snap = sim.state.snapshot
 
-        r1 = snap.particles.position[0] - snap.particles.position[1]
-        E1 = sim.operations.integrator.forces[0].energies
-        dr = r1 - r0
-        magnitude_dr = np.linalg.norm(dr)
-        unit_dr = dr / magnitude_dr
-
-        if magnitude_dr == 0:
-            F = (np.array([0., 0., 0.]), np.array([0., 0., 0.]))
-        else:
-            Fa = -(E1[0] - E0[0] / magnitude_dr) * unit_dr
-            Fb = -(E1[1] - E0[1] / magnitude_dr) * unit_dr
-            F = (Fa, Fb)
-
-        sim_energies = sim.operations.integrator.forces[0].energies
-        sim_forces = sim.operations.integrator.forces[0].forces
         pair_energy = pot.compute_energy(np.array([0], dtype=np.int32),
                                          np.array([1], dtype=np.int32))
+        sim_energies = sim.operations.integrator.forces[0].energies
         assert pair_energy / 2 == sim_energies[0]
         assert pair_energy / 2 == sim_energies[1]
-        np.testing.assert_allclose(F[0], sim_forces[0])
-        np.testing.assert_allclose(F[1], sim_forces[1])
+
+        calculated_forces = calculate_force(sim)
+        sim_forces = sim.operations.integrator.forces[0].forces
+        np.testing.assert_allclose(calculated_forces[0], sim_forces[0])
+        np.testing.assert_allclose(calculated_forces[1], sim_forces[1])
