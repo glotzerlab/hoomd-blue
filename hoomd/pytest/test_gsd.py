4eb7e873bc97a0b508f7dc8128c359c58b8631b9
Dan Evans <danevans@umich.edu>
2020-08-24 11:12:45 -0400

    Add basic dump test to test_gsd.py

--- hoomd/pytest/test_gsd.py
+++ hoomd/pytest/test_gsd.py
@@ -1,84 +1,93 @@
 from hoomd import *
 import hoomd
-import numpy
+import numpy as np
 import pytest
 import os
 import tempfile
+import gsd
+
+
+def _make_gsd_snapshot(hoomd_snapshot):
+    s = gsd.hoomd.Snapshot()
+    for attr in dir(hoomd_snapshot):
+        if attr[0] != '_' and attr not in ['exists', 'replicate']:
+            for prop in dir(getattr(hoomd_snapshot, attr)):
+                if prop[0] != '_':
+                    # s.attr.prop = hoomd_snapshot.attr.prop
+                    setattr(getattr(s, attr), prop,
+                            getattr(getattr(hoomd_snapshot, attr), prop))
+    return s
+
 
 @pytest.fixture(scope='function')
-def gsd_snapshot(self):
-    context.initialize()
-        if hoomd.context.current.device.comm.rank == 0:
-            tmp = tempfile.mkstemp(suffix='.test.gsd');
-            self.tmp_file = tmp[1];
-        else:
-            self.tmp_file = "invalid";
-            
-        self.snapshot = data.make_snapshot(N=4, box=data.boxdim(Lx=10, Ly=20, Lz=30), dtype='float');
-        if context.current.device.comm.rank == 0:
-            # particles
-            self.snapshot.particles.position[0] = [0,1,2];
-            self.snapshot.particles.position[1] = [1,2,3];
-            self.snapshot.particles.position[2] = [0,-1,-2];
-            self.snapshot.particles.position[3] = [-1, -2, -3];
-            self.snapshot.particles.velocity[0] = [10, 11, 12];
-            self.snapshot.particles.velocity[1] = [11, 12, 13];
-            self.snapshot.particles.velocity[2] = [12, 13, 14];
-            self.snapshot.particles.velocity[3] = [13, 14, 15];
-            self.snapshot.particles.acceleration[0] = [20, 21, 22];
-            self.snapshot.particles.acceleration[1] = [21, 22, 23];
-            self.snapshot.particles.acceleration[2] = [22, 23, 24];
-            self.snapshot.particles.acceleration[3] = [23, 24, 25];
-            self.snapshot.particles.typeid[:] = [0,0,1,1];
-            self.snapshot.particles.mass[:] = [33, 34, 35,  36];
-            self.snapshot.particles.charge[:] = [44, 45, 46, 47];
-            self.snapshot.particles.diameter[:] = [55, 56, 57, 58];
-            self.snapshot.particles.image[0] = [60, 61, 62];
-            self.snapshot.particles.image[1] = [61, 62, 63];
-            self.snapshot.particles.image[2] = [62, 63, 64];
-            self.snapshot.particles.image[3] = [63, 64, 65];
-            self.snapshot.particles.types = ['p1', 'p2'];
+def gsd_snapshot(lattice_snapshot_factory):
+    snap = lattice_snapshot_factory(particle_types=particle_types,
+                                    n=10, a=2.0, r=0.01)
+    gsd_snap = _make_gsd_snapshot(snap)
+    max_val, min_val = 1, -1
+    range_size = (max_val - min_val)  # 2
+    positions = np.random.rand(len(gsd_snap.particles), 3) * range_size + min_val
+    positions *= 20
+    velocities = np.random.rand(len(gsd_snap.particles), 3) * range_size + min_val
+    accelerations = np.random.rand(len(gsd_snap.particles), 3) * range_size + min_val
 
-            # bonds
-            self.snapshot.bonds.types = ['b1', 'b2'];
-            self.snapshot.bonds.resize(2);
-            self.snapshot.bonds.typeid[:] = [0, 1];
-            self.snapshot.bonds.group[0] = [0, 1];
-            self.snapshot.bonds.group[1] = [2, 3];
+    gsd_snap.particles.position[:] = positions
+    gsd_snap.particles.velocities[:] = velocities
+    gsd_snap.particles.accelerations[:] = accelerations
+    gsd_snap.particles.mass[:] = np.random.rand(len(gsd_snap.particles))
+    gsd_snap.particles.charge[:] = np.random.rand(len(gsd_snap.particles))
+    gsd_snap.particles.diameter[:] = np.random.rand(len(gsd_snap.particles))
+    gsd_snap.particles.image[:] = np.random.randint(1, 100, (len(gsd_snap.particles), 3))
+    gsd_snap.particles.types = ['p1', 'p2']
+    gsd_snap.particles.typeid = np.random.randint(0, 2, len(gsd_snap.particles))
 
-            # angles
-            self.snapshot.angles.types = ['a1', 'a2'];
-            self.snapshot.angles.resize(2);
-            self.snapshot.angles.typeid[:] = [1, 0];
-            self.snapshot.angles.group[0] = [0, 1, 2];
-            self.snapshot.angles.group[1] = [2, 3, 0];
+    # bonds
+    gsd_snap.bonds.types = ['b1', 'b2']
+    gsd_snap.bonds.resize(2)
+    gsd_snap.bonds.typeid[:] = [0, 1]
+    gsd_snap.bonds.group[0] = [0, 1]
+    gsd_snap.bonds.group[1] = [2, 3]
 
-            # dihedrals
-            self.snapshot.dihedrals.types = ['d1'];
-            self.snapshot.dihedrals.resize(1);
-            self.snapshot.dihedrals.typeid[:] = [0];
-            self.snapshot.dihedrals.group[0] = [0, 1, 2, 3];
+    # angles
+    gsd_snap.angles.types = ['a1', 'a2']
+    gsd_snap.angles.resize(2)
+    gsd_snap.angles.typeid[:] = [1, 0]
+    gsd_snap.angles.group[0] = [0, 1, 2]
+    gsd_snap.angles.group[1] = [2, 3, 0]
 
-            # impropers
-            self.snapshot.impropers.types = ['i1'];
-            self.snapshot.impropers.resize(1);
-            self.snapshot.impropers.typeid[:] = [0];
-            self.snapshot.impropers.group[0] = [3, 2, 1, 0];
+    # dihedrals
+    gsd_snap.dihedrals.types = ['d1']
+    gsd_snap.dihedrals.resize(1)
+    gsd_snap.dihedrals.typeid[:] = [0]
+    gsd_snap.dihedrals.group[0] = [0, 1, 2, 3]
 
-            # constraints
-            self.snapshot.constraints.resize(1)
-            self.snapshot.constraints.group[0] = [0, 1]
-            self.snapshot.constraints.value[0] = 2.5
+    # impropers
+    gsd_snap.impropers.types = ['i1']
+    gsd_snap.impropers.resize(1)
+    gsd_snap.impropers.typeid[:] = [0]
+    gsd_snap.impropers.group[0] = [3, 2, 1, 0]
 
-            # special pairs
-            self.snapshot.pairs.types = ['p1', 'p2'];
-            self.snapshot.pairs.resize(2);
-            self.snapshot.pairs.typeid[:] = [0, 1];
-            self.snapshot.pairs.group[0] = [0, 1];
-            self.snapshot.pairs.group[1] = [2, 3];
+    # constraints
+    gsd_snap.constraints.resize(1)
+    gsd_snap.constraints.group[0] = [0, 1]
+    gsd_snap.constraints.value[0] = 2.5
+
+    # special pairs
+    gsd_snap.pairs.types = ['p1', 'p2']
+    gsd_snap.pairs.resize(2)
+    gsd_snap.pairs.typeid[:] = [0, 1]
+    gsd_snap.pairs.group[0] = [0, 1]
+    gsd_snap.pairs.group[1] = [2, 3]
+    return gsd_snap
 
 
-        self.s = init.read_snapshot(self.snapshot);
-
-        context.current.sorter.set_params(grid=8)
-            
+# tests basic creation of the dump
+def test_dump(simulation_factory, lattice_snapshot_factory, tmp_path):
+    sim = simulation_factory(lattice_snapshot_factory())
+    d = tmp_path / "sub"
+    d.mkdir()
+    filename = d / "temporary_test_file.gsd"
+    gsd_dump = hoomd.dump(filename, hoomd.PeriodicTrigger(1))
+    sim.operations += gsd_dump
+    sim.operations.schedule()
+    sim.run(1)
