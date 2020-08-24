3b25dfae44766e80560419a45510c5923fb38071
Sophie Youjung Lee <syjlee@umich.edu>
2020-08-24 12:55:00 -0600

    edit references for hoomd3 API, add attributes, add links to forces,methods,constrains

--- hoomd/md/integrate.py
+++ hoomd/md/integrate.py
@@ -85,21 +85,18 @@ class Integrator(_DynamicIntegrator):
     R""" Enables a variety of standard integration methods.
 
     Args: 
-        dt (float): Each time step of the simulation `hoomd.Simulation.run`?
-            will advance the real time of the system forward by *dt* (in time units).
-        methods (list of `hoomd.md.methods`?): Integration method `hoomd.md.methods`. It performs a time step integration ?line-breaking
-            technique to move the system forward. At each time step, all of the specified forces are evaluated and used in moving the system forward to the next step.
-            Each integration method can be applied to only a specific group of particles enabling advanced simulation techniques.?how method argument works, multiple component or not
-            ,default None ?what if no methods
-        forces (list?): Forces that be applied to the particles in the system. `hoomd.md.pair`, `hoomd.md.force`, `hoomd.md.dihedral`, `hoomd.md.angle`, `hoomd.md.bond`
-            ,default None ?what if no forces
-        aniso (bool): Whether to integrate rotational degrees of freedom (bool),
-            default None (autodetect).
-        constraints (list?): Constrains a given set of particles to a given surface, to have some relative orientation, or impose some other type of constraint from `hoomd.md.constrain`, default None. 
-    
+        dt (float): Integrator time step size (in time units).
 
-    The following commands can be used to specify the integration methods used
-    by ``methods`` argument.
+        methods: default value to `methods`. 
+
+        forces: default value to `forces`.
+
+        aniso (str): Whether to integrate rotational degrees of freedom (bool),
+            default 'auto' (autodetect).
+
+        constraints: default value to `constraints`.
+
+    The following classes can be used as elements in `methods`
 
     - `hoomd.md.methods.Brownian`
     - `hoomd.md.methods.Langevin`
@@ -108,21 +105,51 @@ class Integrator(_DynamicIntegrator):
     - `hoomd.md.methods.npt`
     - `hoomd.md.methods.nph`
 
-    There can only be one integration method active at a time. 
-    ?If there are more than one ``integrate.mode_*`` commands in a hoomd script, only the most
-    recent before a given ```hoomd.run``` will take effect.
+    The classes of following modules can be used as elements in `forces`
+
+    - `hoomd.md.angle`
+    - `hoomd.md.bond`
+    - `hoomd.md.charge`
+    - `hoomd.md.dihedral`
+    - `hoomd.md.external`
+    - `hoomd.md.force`
+    - `hoomd.md.improper`
+    - `hoomd.md.pair`
+    - `hoomd.md.wall`
+    - `hoomd.md.special_pair`
+    - `hoomd.dem.pair`
+
+    The classes of the following module can be used as elements in `constraints`
+    
+    - `hoomd.md.constrain`
 
     Examples::
-        nlist = hoomd.md.nlist.Cell()
-        lj = hoomd.md.pair.LJ(nlist=nlist)
-        lj.params.default = dict(epsilon=1.0, sigma=1.0)
-        lj.r_cut[('A', 'A')] = 2**(1/6)
-        nve = hoomd.md.methods.NVE(filter=hoomd.filter.All())
+
         integrator = hoomd.md.Integrator(dt=0.001, methods=[nve], forces=[lj])
+        sim.operations.integrator = integrator
+
+
+    Attributes: 
+        dt (float): Integrator time step size (in time units).
+
+        methods (List[hoomd.md.methods._Method]): List of integration methods.
+            Each integration method can be applied to only a specific subset of 
+            particles.
+            
+        forces (List[hoomd.md.force._Force]): List of forces applied to the 
+            particles in the system. All the forces are summed together.
+
+        aniso (str): Whether to integrate rotational degrees of freedom (bool),
+            default 'auto' (autodetect).
+
+        constraints (List[hoomd.md.constrain._ConstraintForce]): List of 
+            constraint forces applied to the particles in the system.
+
+
     """
 
-    def __init__(self, dt, aniso=None, forces=None, constraints=None,
-                 methods=None):
+    def __init__(self, dt, aniso='auto', forces=[], constraints=[],
+                 methods=[]):
 
         super().__init__(forces, constraints, methods)
 
@@ -135,7 +162,7 @@ class Integrator(_DynamicIntegrator):
         if aniso is not None:
             self.aniso = aniso
 
-    def attach(self, simulation):
+    def attach(self, simulation):  # noqa: D102
         # initialize the reflected c++ class
         self._cpp_obj = _md.IntegratorTwoStep(simulation.state._cpp_sys_def,
                                               self.dt)
