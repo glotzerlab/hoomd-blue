26b5dc6d694623039e121ce01f730ae87417db41
Tommy Waltmann <tomwalt@umich.edu>
2020-09-25 15:49:22 -0400

    updates to documentation

--- hoomd/md/many_body.py
+++ hoomd/md/many_body.py
@@ -38,6 +38,11 @@ class Tersoff(_ThreeBody):
     used in this work is exponential in nature as opposed to the sinusoid used by Tersoff. The exponential
     function provides continuity up (I believe) the second derivative.
 
+    Warning:
+        Currently HOOMD does not support reverse force communication between MPI domains on the GPU.
+        Since reverse force communication is required for the calculation of multi-body potentials, attempting to use this
+        potential on the GPU with MPI will result in an error.
+
     """
     _cpp_class_name = "PotentialTersoff"
     def __init__(self, nlist, r_cut=None, r_on=0., mode='none'):
@@ -123,9 +128,9 @@ class RevCross(_ThreeBody):
         The RevCross potential models an asymmetric interaction between two different chemical moieties that can form a reversible bond.
         This requires the definition of (at least) two different types of particles.
         A reversible bond is only possible between two different species, otherwise :math:`v^{\left( 3b \right)}_{ijk}`, would prevent any bond.
-        In our example we then set the interactions for types A and B with ``potRevC.params[(['A','B'],['A','B'])] = dict(sigma=0.0,n=0,epsilon=0,lambda3=0)``
-        and the only non-zero energy only between the different types ``potRevC.params[('A','B')] = dict(sigma=1,n=100,epsilon=100,lambda3=1)``.
-        Notice that the number of the minoritary species corresponds to the maximum number of bonds.
+        In our example we then set the interactions for types A and B with ``rev_c.params[[('A','B'),('A','B')]] = dict(sigma=0.0,n=0,epsilon=0,lambda3=0)``
+        and the only non-zero energy only between the different types ``rev_c.params[('A','B')] = dict(sigma=1,n=100,epsilon=100,lambda3=1)``.
+        Notice that the number of the minority species corresponds to the maximum number of bonds.
 
 
     This three-body term also tunes the energy required for a bond swap through the coefficient:
@@ -142,10 +147,15 @@ class RevCross(_ThreeBody):
     Example::
 
         nl = md.nlist.Cell()
-        potBondSwap = md.many_body.RevCross(r_cut=1.3,nlist=nl)
-        potBondSwap.params[(['A','B'],['A','B'])] = dict(sigma=0,n=0,epsilon=0,lambda3=0)
+        bond_swap = md.many_body.RevCross(r_cut=1.3,nlist=nl)
+        bond_swap.params[(['A','B'],['A','B'])] = dict(sigma=0,n=0,epsilon=0,lambda3=0)
         # a bond can be made only between A-B and not A-A or B-B
-        potBondSwap.params[('A','B')] = dict(sigma=1,n=100,epsilon=10,lambda3=1)
+        bond_swap.params[('A','B')] = dict(sigma=1,n=100,epsilon=10,lambda3=1)
+
+    Warning:
+        Currently HOOMD does not support reverse force communication between MPI domains on the GPU.
+        Since reverse force communication is required for the calculation of multi-body potentials, attempting to use this
+        potential on the GPU with MPI will result in an error.
     """
     _cpp_class_name = "PotentialRevCross"
     def __init__(self, nlist, r_cut=None, r_on=0., mode='none'):
@@ -157,10 +167,10 @@ class RevCross(_ThreeBody):
 
 
 class SquareDensity(_ThreeBody):
-    R""" Soft potential for simulating a van-der-Waals liquid
+    R""" Soft potential for simulating a van der Waals liquid
 
     Args:
-        nlist (:py:mod:`hoomd.md.nlist`): Neighbor list
+        nlist (:py:mod:`hoomd.md._NList`): Neighbor list
         r_cut (float): Default cutoff radius (in distance units).
         r_on (float): Default turn-on radius (in distance units).
         mode (str): Energy shifting mode.
@@ -195,12 +205,12 @@ class SquareDensity(_ThreeBody):
         sqd = md.many_body.SquareDensity(r_cut=3.0, nlist=nl)
         sqd.params[('A', 'A')] = dict(A=0.1, B=1.0)
 
-    For further details regarding this multibody potential, see
-
     Warning:
         Currently HOOMD does not support reverse force communication between MPI domains on the GPU.
-        Since reverse force communication is required for the calculation of multi-body potentials, attempting to use the
-        square_density potential on the GPU with MPI will result in an error.
+        Since reverse force communication is required for the calculation of multi-body potentials, attempting to use this
+        potential on the GPU with MPI will result in an error.
+
+    For further details regarding this multibody potential, see
 
     [1] P. B. Warren, "Vapor-liquid coexistence in many-body dissipative particle dynamics"
     Phys. Rev. E. Stat. Nonlin. Soft Matter Phys., vol. 68, no. 6 Pt 2, p. 066702, 2003.
