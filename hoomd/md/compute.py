64c187477c9646208d7fa17010507b0597b88482
Tommy Waltmann <tomwalt@umich.edu>
2020-09-28 17:04:25 -0400

    cleaned up line breaks in docstrings

--- hoomd/md/compute.py
+++ hoomd/md/compute.py
@@ -22,11 +22,14 @@ class ThermodynamicQuantities(_Thermo):
     """Compute thermodynamic properties of a group of particles.
 
     Args:
-        filter (``hoomd.filter``): Particle filter to compute thermodynamic properties for.
+        filter (``hoomd.filter``): Particle filter to compute thermodynamic
+        properties for.
 
-    :py:class:`ThermodynamicQuantities` acts on a given group of particles and calculates thermodynamic properties of
-    those particles when requested. All specified :py:class:`ThermodynamicQuantities` objects can be added to a logger
-    for logging during a simulation, see :py:class:`hoomd.logging.Logger` for more details.
+    :py:class:`ThermodynamicQuantities` acts on a given group of particles and
+    calculates thermodynamic properties of those particles when requested. All
+    specified :py:class:`ThermodynamicQuantities` objects can be added to a
+    logger for logging during a simulation, see :py:class:`hoomd.logging.Logger`
+    for more details.
 
     Examples::
 
@@ -48,7 +51,8 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def kinetic_temperature(self):
-        """:math:`kT_k`, instantaneous thermal energy of the group (in energy units).
+        """:math:`kT_k`, instantaneous thermal energy of the group (in energy
+        units).
 
         Calculated as:
 
@@ -72,13 +76,20 @@ class ThermodynamicQuantities(_Thermo):
 
             P = \\frac{ 2 \\cdot K_{\\mathrm{trans}} + W }{D \\cdot V},
 
-        where :math:`D` is the dimensionality of the system, :math:`V` is the total volume of the simulation box (or area in 2D), and :math:`W` is calculated as:
+        where :math:`D` is the dimensionality of the system, :math:`V` is the
+        total volume of the simulation box (or area in 2D), and :math:`W` is
+        calculated as:
 
         .. math::
 
-            W = \\frac{1}{2} \\sum_{i \\in \\mathrm{filter}} \\sum_{j} \\vec{F}_{ij} \\cdot \\vec{r_{ij}} + \\sum_{k} \\vec{F}_{k} \\cdot \\vec{r_{k}},
+            W = \\frac{1}{2} \\sum_{i \\in \\mathrm{filter}} \\sum_{j}
+            \\vec{F}_{ij} \\cdot \\vec{r_{ij}} + \\sum_{k} \\vec{F}_{k} \\cdot
+            \\vec{r_{k}},
 
-        where :math:`i` and :math:`j` are particle tags, :math:`\\vec{F}_{ij}` are pairwise forces between particles and :math:`\\vec{F}_k` are forces due to explicit constraints, implicit rigid body constraints, external walls, and fields.
+        where :math:`i` and :math:`j` are particle tags, :math:`\\vec{F}_{ij}`
+        are pairwise forces between particles and :math:`\\vec{F}_k` are forces
+        due to explicit constraints, implicit rigid body constraints, external
+        walls, and fields.
         """
         if self._attached:
             self._cpp_obj.compute(self._simulation.timestep)
@@ -88,14 +99,18 @@ class ThermodynamicQuantities(_Thermo):
 
     @log(flag='sequence')
     def pressure_tensor(self):
-        """(:math:`P_{xx}`, :math:`P_{xy}`, :math:`P_{xz}`, :math:`P_{yy}`, :math:`P_{yz}`, :math:`P_{zz}`).
+        """(:math:`P_{xx}`, :math:`P_{xy}`, :math:`P_{xz}`, :math:`P_{yy}`,
+        :math:`P_{yz}`, :math:`P_{zz}`).
 
-        Instantaneous pressure tensor of the group (in pressure units), calculated as:
+        Instantaneous pressure tensor of the group (in pressure units),
+        calculated as:
 
           .. math::
 
-              P_{ij} = \\left[  \\sum_{k \\in \\mathrm{filter}} m_k v_{k,i} v_{k,j} +
-                               \\sum_{k \\in \\mathrm{filter}} \\sum_{l} \\frac{1}{2} \\left(\\vec{r}_{kl,i} \\vec{F}_{kl,j} + \\vec{r}_{kl,j} \\vec{F}_{kl, i} \\right) \\right]/V
+              P_{ij} = \\left[  \\sum_{k \\in \\mathrm{filter}} m_k v_{k,i}
+              v_{k,j} + \\sum_{k \\in \\mathrm{filter}} \\sum_{l} \\frac{1}{2}
+              \\left(\\vec{r}_{kl,i} \\vec{F}_{kl,j} + \\vec{r}_{kl,j}
+              \\vec{F}_{kl, i} \\right) \\right]/V
 
         where :math:`V` is the total simulation box volume (or area in 2D).
         """
@@ -107,7 +122,8 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def kinetic_energy(self):
-        """:math:`K`, total kinetic energy of all particles in the group (in energy units).
+        """:math:`K`, total kinetic energy of all particles in the group (in
+        energy units).
 
         .. math::
 
@@ -122,11 +138,13 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def translational_kinetic_energy(self):
-        """:math:`K_{\\mathrm{trans}}`, translational kinetic energy of all particles in the group (in energy units).
+        """:math:`K_{\\mathrm{trans}}`, translational kinetic energy of all
+        particles in the group (in energy units).
 
         .. math::
 
-            K_{\\mathrm{trans}} = \\frac{1}{2}\\sum_{i \\in \\mathrm{filter}} m_i|\\vec{v}_i|^2
+            K_{\\mathrm{trans}} = \\frac{1}{2}\\sum_{i \\in \\mathrm{filter}}
+            m_i|\\vec{v}_i|^2
 
         """
         if self._attached:
@@ -137,15 +155,19 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def rotational_kinetic_energy(self):
-        """:math:`K_{\\mathrm{rot}}`, rotational kinetic energy of all particles in the group (in energy units).
+        """:math:`K_{\\mathrm{rot}}`, rotational kinetic energy of all particles
+        in the group (in energy units).
 
         Calculated as:
 
         .. math::
 
-            K_{\\mathrm{rot}} = \\frac{1}{2} \\sum_{i \\in \\mathrm{filter}} \\frac{L_{x,i}^2}{I_{x,i}} + \\frac{L_{y,i}^2}{I_{y,i}} + \\frac{L_{z,i}^2}{I_{z,i}},
+            K_{\\mathrm{rot}} = \\frac{1}{2} \\sum_{i \\in \\mathrm{filter}}
+            \\frac{L_{x,i}^2}{I_{x,i}} + \\frac{L_{y,i}^2}{I_{y,i}} +
+            \\frac{L_{z,i}^2}{I_{z,i}},
 
-        where :math:`I` is the moment of inertia and :math:`L` is the angular momentum in the (diagonal) reference frame of the particle.
+        where :math:`I` is the moment of inertia and :math:`L` is the angular
+        momentum in the (diagonal) reference frame of the particle.
         """
         if self._attached:
             self._cpp_obj.compute(self._simulation.timestep)
@@ -155,9 +177,11 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def potential_energy(self):
-        """:math:`U`, potential energy that the group contributes to the entire system (in energy units).
+        """:math:`U`, potential energy that the group contributes to the entire
+        system (in energy units).
 
-        The potential energy is calculated as a sum of per-particle energy contributions:
+        The potential energy is calculated as a sum of per-particle energy
+        contributions:
 
         .. math::
 
@@ -167,7 +191,10 @@ class ThermodynamicQuantities(_Thermo):
 
         .. math::
 
-            U_i = U_{\\mathrm{pair}, i} + U_{\\mathrm{bond}, i} + U_{\\mathrm{angle}, i} + U_{\\mathrm{dihedral}, i} + U_{\\mathrm{improper}, i} + U_{\\mathrm{external}, i} + U_{\\mathrm{other}, i}
+            U_i = U_{\\mathrm{pair}, i} + U_{\\mathrm{bond}, i} +
+            U_{\\mathrm{angle}, i} + U_{\\mathrm{dihedral}, i} +
+            U_{\\mathrm{improper}, i} + U_{\\mathrm{external}, i} +
+            U_{\\mathrm{other}, i}
 
         and each term on the RHS is calculated as:
 
@@ -175,16 +202,23 @@ class ThermodynamicQuantities(_Thermo):
 
             U_{\\mathrm{pair}, i} &= \\frac{1}{2} \\sum_j V_{\\mathrm{pair}, ij}
 
-            U_{\\mathrm{bond}, i} &= \\frac{1}{2} \\sum_{(j, k) \\in \\mathrm{bonds}} V_{\\mathrm{bond}, jk}
+            U_{\\mathrm{bond}, i} &= \\frac{1}{2} \\sum_{(j, k) \\in
+            \\mathrm{bonds}} V_{\\mathrm{bond}, jk}
 
-            U_{\\mathrm{angle}, i} &= \\frac{1}{3} \\sum_{(j, k, l) \\in \\mathrm{angles}} V_{\\mathrm{angle}, jkl}
+            U_{\\mathrm{angle}, i} &= \\frac{1}{3} \\sum_{(j, k, l) \\in
+            \\mathrm{angles}} V_{\\mathrm{angle}, jkl}
 
-            U_{\\mathrm{dihedral}, i} &= \\frac{1}{4} \\sum_{(j, k, l, m) \\in \\mathrm{dihedrals}} V_{\\mathrm{dihedral}, jklm}
+            U_{\\mathrm{dihedral}, i} &= \\frac{1}{4} \\sum_{(j, k, l, m) \\in
+            \\mathrm{dihedrals}} V_{\\mathrm{dihedral}, jklm}
 
-            U_{\\mathrm{improper}, i} &= \\frac{1}{4} \\sum_{(j, k, l, m) \\in \\mathrm{impropers}} V_{\\mathrm{improper}, jklm}
+            U_{\\mathrm{improper}, i} &= \\frac{1}{4} \\sum_{(j, k, l, m) \\in
+            \\mathrm{impropers}} V_{\\mathrm{improper}, jklm}
 
-        In each summation above, the indices go over all particles and we only use terms where one of the summation indices (:math:`j`, :math:`k`, :math:`l`, or :math:`m`)
-        is equal to :math:`i`. External and other potentials are summed similar to the other terms using per-particle contributions.
+        In each summation above, the indices go over all particles and we only
+        use terms where one of the summation indices (:math:`j`, :math:`k`,
+        :math:`l`, or :math:`m`) is equal to :math:`i`. External and other
+        potentials are summed similar to the other terms using per-particle
+        contributions.
         """
         if self._attached:
             self._cpp_obj.compute(self._simulation.timestep)
@@ -194,7 +228,8 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def degrees_of_freedom(self):
-        """:math:`N_{\\mathrm{dof}}`, number of degrees of freedom given to the group by its integration method.
+        """:math:`N_{\\mathrm{dof}}`, number of degrees of freedom given to the
+        group by its integration method.
 
         Calculated as:
 
@@ -209,12 +244,20 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def translational_degrees_of_freedom(self):
-        """:math:`N_{\\mathrm{dof, trans}}`, number of translational degrees of freedom given to the group by its integration method.
+        """:math:`N_{\\mathrm{dof, trans}}`, number of translational degrees of
+        freedom given to the group by its integration method.
 
-        When using a single integration method that is momentum conserving and operates on all particles, :math:`N_{\\mathrm{dof, trans}} = DN - D - N_{constraints}`, where :math:`D` is the dimensionality of the system.
+        When using a single integration method that is momentum conserving and
+        operates on all particles,
+        :math:`N_{\\mathrm{dof, trans}} = DN - D - N_{constraints}`, where
+        :math:`D` is the dimensionality of the system.
 
         Note:
-            The removal of :math:`D` degrees of freedom accounts for the fixed center of mass in using periodic boundary conditions. When the *filter* in :py:class:`ThermodynamicQuantities` selects a subset of all particles, the removed degrees of freedom are spread proportionately.
+            The removal of :math:`D` degrees of freedom accounts for the fixed
+            center of mass in using periodic boundary conditions. When the
+            *filter* in :py:class:`ThermodynamicQuantities` selects a subset
+            of all particles, the removed degrees of freedom are spread
+            proportionately.
         """
         if self._attached:
             return self._cpp_obj.translational_degrees_of_freedom
@@ -223,7 +266,8 @@ class ThermodynamicQuantities(_Thermo):
 
     @log
     def rotational_degrees_of_freedom(self):
-        """:math:`N_{\\mathrm{dof, rot}}`, number of rotational degrees of freedom given to the group by its integration method. """
+        """:math:`N_{\\mathrm{dof, rot}}`, number of rotational degrees of
+        freedom given to the group by its integration method. """
         if self._attached:
             return self._cpp_obj.rotational_degrees_of_freedom
         else:
