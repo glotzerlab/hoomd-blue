ee589a745db25bcf896157312bfa12f89f2b397f
Dan Evans <danevans@umich.edu>
2020-11-10 20:28:50 -0500

    Use pytest in test_shape_utils.py

--- hoomd/hpmc/pytest/test_shape_utils.py
+++ hoomd/hpmc/pytest/test_shape_utils.py
@@ -1,25 +1,14 @@
 import numpy as np
-import time
 import pytest
 import coxeter
 from plato import geometry
-ConvexHull = None;
-library = None;
-lattice = None;
-trianglehedra = None;
-try:
-    from pyhull.convex_hull import ConvexHull
-except ImportError:
-    ConvexHull = None;
-    library = None;
-    lattice = None;
-    trianglehedra = None;
-
+from pyhull.convex_hull import ConvexHull
 import hoomd
 from hoomd import hpmc
+from copy import deepcopy
 
 
-def getOutwardNormal(verts, face, thresh=0.0001):
+def get_outward_normal(verts, face, thresh=0.0001):
     assert(len(face) == 3)
     (a, b, c) = verts[face]
     n = np.cross((b - a), (c - a))
@@ -35,10 +24,10 @@ def getOutwardNormal(verts, face, thresh=0.0001):
     return n
 
 
-def sortFaces(verts, faces):
+def sort_faces(verts, faces):
     sorted_face = []
     for face in faces:
-        nout = getOutwardNormal(verts, face)
+        nout = get_outward_normal(verts, face)
         (a, b, c) = verts[face]
         n = np.cross((b - a), (c - a))
         if n.dot(nout) > 0:
@@ -48,70 +37,115 @@ def sortFaces(verts, faces):
     return np.array(sorted_face)
 
 
-def test_mass_properties():
-    cpp_time = 0.0
-    py_time = 0.0
-    for _ in range(100):
-        # nverts = np.random.randint(10, 128)
-        nverts =16
-        make_verts = hpmc._hpmc.PolyhedronVertices
-        mass_class = hpmc._hpmc.MassPropertiesConvexPolyhedron
-        verts = 5.0 * np.random.rand(nverts, 3)
-        hull = ConvexHull(verts);
-        faces = np.array(hull.vertices);
-        faces = sortFaces(verts, faces);
-        vol, com, inertia = geometry.massProperties(verts, faces);
+def _vertices():
+    platonic_shapes = ["Cube", "Tetrahedron", "Octahedron", "Icosahedron", "Dodecahedron"]
+    verts = []
+    names = []
+    for shape in platonic_shapes:
+        verts.append(np.asarray(coxeter.families.PlatonicFamily.get_shape(shape).vertices))
+        names.append(shape)
+    for i in range(20):
+        verts.append(np.random.rand(np.random.randint(10, 128), 3))
+        names.append("random" + str(i))
+    return zip(names, verts)
 
-        start = time.time()
-        py_shape = coxeter.shapes.ConvexPolyhedron(verts)
-        py_shape.diagonalize_inertia()
-        end = time.time()
-        py_time += end - start
-        py_verts = [] # the actual points in the convex_hull
-        for i, j, k in hull.vertices:
-            py_verts.append(list(verts[i]))
-            py_verts.append(list(verts[j]))
-            py_verts.append(list(verts[k]))
-        py_verts = np.asarray(sorted(py_verts,
-                                     key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
-        # py_verts = np.asarray(sorted(list(py_shape.vertices),
-        #                              key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
-        start = time.time()
-        mp = mass_class(make_verts({'vertices': verts, 'sweep_radius': 0.0, 'ignore_statistics': 0}))
-        end = time.time()
-        cpp_time += end - start
 
-        cpp_volume = mp.volume()
-        cpp_com = [mp.center_of_mass(i) for i in range(3)]
-        cpp_inertia = [mp.moment_of_inertia(i) for i in range(6)]
+@pytest.fixture(scope="function", params=_vertices(), ids=(lambda x: x[0]))
+def vertices(request):
+    return deepcopy(request.param)
 
-        cpp_verts = []  # the actual points in the convex_hull
-        for f in range(mp.num_faces()):
-            for i in range(3):
-                cpp_verts.append(mp.vertices(f, i))
-            # cpp_ids.update([mp.index(f, i) for i in range(3)]);
+_make_verts = hpmc._hpmc.PolyhedronVertices
+_mass_class = hpmc._hpmc.MassPropertiesConvexPolyhedron
 
-        # faces may be different because triangulation is not unique but there should be
-        # the same number of faces and the same vertices will be in the hull.
-        # Also test the result gives us the same result for the volume, inertia and com
-        np.testing.assert_allclose(py_shape.num_faces, mp.num_faces())
-        # test the vertices are the same.
-        cpp_verts = np.asarray(sorted(cpp_verts,
-                                      key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
-        cpp_verts = np.unique(cpp_verts, axis=0)
-        py_verts = np.unique(py_verts, axis=0)
-        print(py_shape.num_vertices)
-        np.testing.assert_allclose(cpp_verts, py_verts)
-        np.testing.assert_allclose(cpp_volume, py_shape.volume, rtol=0.2)
-        np.testing.assert_allclose(cpp_com, py_shape.center, rtol=0.2)
-        np.testing.assert_allclose(cpp_inertia, py_shape.inertia_tensor, rtol=0.2)
-        np.testing.assert_allclose(cpp_volume, vol)
-        np.testing.assert_allclose(cpp_com, com)
-        tmp = inertia
-        inertia[1] = tmp[3]
-        inertia[2] = tmp[5]
-        inertia[3] = tmp[1]
-        inertia[5] = tmp[2]
-        # np.testing.assert_allclose(cpp_inertia, inertia)
-    print("c++ ran 100 convex hulls in {} ({} per call)".format(cpp_time, cpp_time / 100))
-    print("py ran 100 convex hulls in {} ({} per call)".format(py_time, py_time / 100))
+
+def test_convex_hull_vertices(vertices):
+    _, verts = vertices
+
+    hull = ConvexHull(verts)
+    py_verts = []
+    for i, j, k in hull.vertices:
+        py_verts.append(list(verts[i]))
+        py_verts.append(list(verts[j]))
+        py_verts.append(list(verts[k]))
+    py_verts = np.asarray(sorted(py_verts,
+                                 key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
+    py_verts = np.unique(py_verts, axis=0)
+
+    mp = _mass_class(_make_verts({'vertices': verts,
+                                  'sweep_radius': 0.0,
+                                  'ignore_statistics': 0}))
+    cpp_verts = []
+    for f in range(mp.num_faces()):
+        for i in range(3):
+            cpp_verts.append(mp.vertices(f, i))
+    cpp_verts = np.asarray(sorted(cpp_verts,
+                                  key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
+    cpp_verts = np.unique(cpp_verts, axis=0)
+
+    np.testing.assert_allclose(cpp_verts, py_verts)
+
+
+def test_num_faces(vertices):
+    _, verts = vertices
+
+    hull = ConvexHull(verts)
+    faces = np.array(hull.vertices)
+    faces = sort_faces(verts, faces)
+
+    mp = _mass_class(_make_verts({'vertices': verts,
+                                  'sweep_radius': 0.0,
+                                  'ignore_statistics': 0}))
+
+    np.testing.assert_allclose(mp.num_faces(), len(faces))
+
+
+def test_volume(vertices):
+    _, verts = vertices
+
+    hull = ConvexHull(verts)
+    faces = np.array(hull.vertices)
+    faces = sort_faces(verts, faces)
+    py_vol, _, _ = geometry.massProperties(verts, faces)
+
+    mp = _mass_class(_make_verts({'vertices': verts,
+                                  'sweep_radius': 0.0,
+                                  'ignore_statistics': 0}))
+
+    np.testing.assert_allclose(mp.volume(), py_vol, rtol=1e-5, atol=1e-5)
+
+
+def test_center_of_mass(vertices):
+    _, verts = vertices
+
+    hull = ConvexHull(verts)
+    faces = np.array(hull.vertices)
+    faces = sort_faces(verts, faces)
+    _, py_com, _ = geometry.massProperties(verts, faces)
+
+    mp = _mass_class(_make_verts({'vertices': verts,
+                                  'sweep_radius': 0.0,
+                                  'ignore_statistics': 0}))
+    cpp_com = [mp.center_of_mass(i) for i in range(3)]
+
+    np.testing.assert_allclose(cpp_com, py_com, rtol=1e-5, atol=1e-5)
+
+
+def test_inertia(vertices):
+    _, verts = vertices
+
+    hull = ConvexHull(verts)
+    faces = np.array(hull.vertices)
+    faces = sort_faces(verts, faces)
+    _, _, py_inertia = geometry.massProperties(verts, faces)
+    tmp = py_inertia + 0.0
+    py_inertia[1] = tmp[3]
+    py_inertia[2] = tmp[5]
+    py_inertia[3] = tmp[1]
+    py_inertia[5] = tmp[2]
+
+    mp = _mass_class(_make_verts({'vertices': verts,
+                                  'sweep_radius': 0.0,
+                                  'ignore_statistics': 0}))
+    cpp_inertia = [mp.moment_of_inertia(i) for i in range(6)]
+
+    np.testing.assert_allclose(cpp_inertia, py_inertia, rtol=1e-5, atol=1e-5)
