7700e0ac393553182b0057bf5dab813226d8b728
Dan Evans <danevans@umich.edu>
2021-01-17 15:55:39 -0500

    Update documentation

--- hoomd/write/dcd.py
+++ hoomd/write/dcd.py
@@ -34,8 +34,8 @@ class DCD(Writer):
 
     Examples::
 
-        dump.dcd(filename="trajectory.dcd", trigger=hoomd.trigger.Periodic(1000))
-        dcd = dump.dcd(filename="data/dump.dcd", trigger=hoomd.trigger.Periodic(100, 10))
+        hoomd.write.DCD("trajectory.dcd", hoomd.trigger.Periodic(1000))
+        dcd = hoomd.write.DCD(filename="data/dump.dcd", trigger=hoomd.trigger.Periodic(100, 10))
 
     Warning:
         When you use dump.dcd to append to an existing dcd file:
@@ -43,6 +43,20 @@ class DCD(Writer):
         * The period must be the same or the time data in the file will not be consistent.
         * dump.dcd will not write out data at time steps that already are present in the dcd file to maintain a
           consistent timeline
+
+    Attributes:
+        filename (str): File name to write.
+        trigger (hoomd.trigger.Trigger): Select the timesteps to write.
+        filter (hoomd.filter.ParticleFilter): Select the particles to write.
+        overwrite (bool): When False, an existing DCD file will be appended to. When True, an existing DCD
+                          file *filename* will be overwritten.
+        unwrap_full (bool): When False, particle coordinates are always written inside the simulation box.
+                            When True, particles will be unwrapped into their current box image before writing to the dcd file.
+        unwrap_rigid (bool): When False, individual particles are written inside the simulation box which
+               breaks up rigid bodies near box boundaries. When True, particles belonging to the same rigid body will be
+               unwrapped so that the body is continuous. The center of mass of the body remains in the simulation box, but
+               some particles may be written just outside it. *unwrap_rigid* is ignored when *unwrap_full* is True.
+        angle_z (bool): When True, the particle orientation angle is written to the z component
     """
     def __init__(self,
                  filename,
