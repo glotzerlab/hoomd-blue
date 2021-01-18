aab5953333b5d7a1bbae1060c548ed289bf482cc
Dan Evans <danevans@umich.edu>
2021-01-18 18:08:38 -0500

    Remove unnecessary directory creation

--- hoomd/pytest/test_dcd.py
+++ hoomd/pytest/test_dcd.py
@@ -4,30 +4,22 @@ import numpy as np
 
 
 def test_attach(simulation_factory, two_particle_snapshot_factory, tmp_path):
-    d = tmp_path / "sub"
-    d.mkdir()
-    filename = d / "temporary_test_file.dcd"
+    filename = tmp_path / "temporary_test_file.dcd"
     sim = simulation_factory(two_particle_snapshot_factory())
     dcd_dump = hoomd.write.DCD(filename, hoomd.trigger.Periodic(1))
     sim.operations.add(dcd_dump)
-    sim.operations._schedule()
-    for i in range(10):
-        sim.run(1)
+    sim.run(10)
 
 
 def test_set_period(tmp_path):
-    d = tmp_path / "sub"
-    d.mkdir()
-    filename = d / "temporary_test_file.dcd"
+    filename = tmp_path / "temporary_test_file.dcd"
     dcd_dump = hoomd.write.DCD(filename, hoomd.trigger.Periodic(1))
     with pytest.raises(RuntimeError):
         dcd_dump.set_period(1)
 
 
 def test_enabled(tmp_path):
-    d = tmp_path / "sub"
-    d.mkdir()
-    filename = d / "temporary_test_file.dcd"
+    filename = tmp_path / "temporary_test_file.dcd"
     dcd_dump = hoomd.write.DCD(filename, hoomd.trigger.Periodic(1))
     dcd_dump.enabled = False
     with pytest.raises(RuntimeError):
@@ -37,13 +29,10 @@ def test_enabled(tmp_path):
 def test_write(simulation_factory, two_particle_snapshot_factory, tmp_path):
     garnett = pytest.importorskip("garnett")
     dcd_reader = garnett.reader.DCDFileReader()
-    d = tmp_path / "sub"
-    d.mkdir()
-    filename = d / "temporary_test_file.dcd"
+    filename = tmp_path / "temporary_test_file.dcd"
     sim = simulation_factory(two_particle_snapshot_factory())
     dcd_dump = hoomd.write.DCD(filename, hoomd.trigger.Periodic(1))
     sim.operations.add(dcd_dump)
-    sim.operations._schedule()
     snap = sim.state.snapshot
     positions = []
     for i in range(10):
