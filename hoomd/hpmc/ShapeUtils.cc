eecf06a4ba44bf5a3d4220fe6ba3dda2829a105d
Dan Evans <danevans@umich.edu>
2021-01-29 21:45:31 -0500

    Add license agreement to alchemy files

--- hoomd/hpmc/ShapeUtils.cc
+++ hoomd/hpmc/ShapeUtils.cc
@@ -1,4 +1,5 @@
-
+// Copyright (c) 2009-2021 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 #include "ShapeUtils.h"
 // #include <hoomd/extern/pybind/include/pybind11/pybind11.h>
 #include <pybind11/pybind11.h>
