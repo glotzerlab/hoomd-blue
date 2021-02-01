f968dc5b70119e801c31b062bc46caa385b85b29
Tommy Waltmann <tomwalt@umich.edu>
2021-02-01 14:37:49 -0500

    updated tree neighborlsit

--- hoomd/md/nlist.py
+++ hoomd/md/nlist.py
@@ -12,19 +12,13 @@ neighbor lists are shared, they find neighbors within the the maximum
 :math:`r_{\mathrm{cut},i,j}` over the associated pair potentials.
 """
 
-from hoomd import _hoomd
-from hoomd.md import _md
 import hoomd
-from hoomd.data.typeconverter import OnlyFrom
+from hoomd import _hoomd
 from hoomd.data.parameterdicts import ParameterDict
-from hoomd.operation import _HOOMDBaseObject
+from hoomd.data.typeconverter import OnlyFrom
 from hoomd.logging import log
-
-
-class nlist:
-    """Obsolete parent class for neighbor list objects.
-    """
-    pass
+from hoomd.md import _md
+from hoomd.operation import _HOOMDBaseObject
 
 # To Do: Migrate all hoomdv2 codes still using nlist to NList
 
@@ -120,14 +114,6 @@ class NList(_HOOMDBaseObject):
             return self._cpp_obj.getSmallestRebuild()
 
     # TODO need to add tuning Updater for NList
-
-
-## \internal
-# \brief %nlist r_cut matrix
-# \details
-# Holds the maximum cutoff radius by pair type, and gracefully updates maximum cutoffs as new pairs are added
-class rcut:
-    pass
 
 
 class Cell(NList):
@@ -289,108 +275,36 @@ class Stencil(NList):
         del self._cpp_cell
         super()._detach()
 
-    def tune_cell_width(self, warmup=200000, min_width=None, max_width=None, jumps=20, steps=5000):
-        R""" Make a series of short runs to determine the fastest performing bin width.
 
-        Args:
-            warmup (int): Number of time steps to run() to warm up the benchmark
-            min_width (float): Minimum cell bin width to try
-            max_width (float): Maximum cell bin width to try
-            jumps (int): Number of different bin width to test
-            steps (int): Number of time steps to run() at each point
-
-        :py:class:`tune_cell_width()` executes *warmup* time steps. Then it sets the nlist *cell_width* value to *min_width* and
-        runs for *steps* time steps. The TPS value is recorded, and the benchmark moves on to the next *cell_width*
-        value completing at *max_width* in *jumps* jumps. Status information is printed out to the screen, and the
-        optimal *cell_width* value is left set for further runs() to continue at optimal settings.
-
-        Each benchmark is repeated 3 times and the median value chosen. In total, ``(warmup + 3*jump*steps)`` time steps
-        are run.
-
-        Returns:
-            The optimal cell width.
-        """
-
-        # check if initialization has occurred
-        if not hoomd.init.is_initialized():
-            hoomd.context.current.device.cpp_msg.error("Cannot tune r_buff before initialization\n");
-
-        if self.cpp_nlist is None:
-            hoomd.context.current.device.cpp_msg.error('Bug in hoomd: cpp_nlist not set, please report\n')
-            raise RuntimeError('Error tuning neighbor list')
-
-        min_cell_width = min_width
-        if min_cell_width is None:
-            min_cell_width = 0.5*self.cpp_nlist.getMinRList()
-        max_cell_width = max_width
-        if max_cell_width is None:
-            max_cell_width = self.cpp_nlist.getMaxRList()
-
-        # make the warmup run
-        hoomd.run(warmup);
-
-        # initialize scan variables
-        dr = (max_cell_width - min_cell_width) / (jumps - 1);
-        width_list = [];
-        tps_list = [];
-
-        # loop over all desired cell width points
-        for i in range(0,jumps):
-            # set the current cell width
-            cw = min_cell_width + i * dr;
-            self.set_cell_width(cell_width=cw)
-
-            # run the benchmark 3 times
-            tps = [];
-            hoomd.run(steps);
-            tps.append(hoomd.context.current.system.getLastTPS())
-            hoomd.run(steps);
-            tps.append(hoomd.context.current.system.getLastTPS())
-            hoomd.run(steps);
-            tps.append(hoomd.context.current.system.getLastTPS())
-
-            # record the median tps of the 3
-            tps.sort();
-            tps_list.append(tps[1]);
-            width_list.append(cw);
-
-        # find the fastest cell width
-        fastest = tps_list.index(max(tps_list));
-        fastest_width = width_list[fastest];
-
-        # set the fastest cell width
-        self.set_cell_width(cell_width=fastest_width)
-
-        # notify the user of the benchmark results
-        hoomd.context.current.device.cpp_msg.notice(2, "cell width = " + str(width_list) + '\n');
-        hoomd.context.current.device.cpp_msg.notice(2, "tps = " + str(tps_list) + '\n');
-        hoomd.context.current.device.cpp_msg.notice(2, "Optimal cell width: " + str(fastest_width) + '\n');
-
-        # return the results to the script
-        return fastest_width
-
-
-class tree(nlist):
-    R""" Bounding volume hierarchy based neighbor list.
+class Tree(NList):
+    """Bounding volume hierarchy based neighbor list.
 
     Args:
         r_buff (float):  Buffer width.
         check_period (int): How often to attempt to rebuild the neighbor list.
-        d_max (float): The maximum diameter a particle will achieve, only used in conjunction with slj diameter shifting.
+        d_max (float): The maximum diameter a particle will achieve, only used
+            in conjunction with slj diameter shifting.
         dist_check (bool): Flag to enable / disable distance checking.
         name (str): Optional name for this neighbor list instance.
 
-    :py:class:`tree` creates a neighbor list using bounding volume hierarchy (BVH) tree traversal. Pair potentials are attached
-    for computing non-bonded pairwise interactions. A BVH tree of axis-aligned bounding boxes is constructed per particle
-    type, and each particle queries each tree to determine its neighbors. This method of searching leads to significantly
-    improved performance compared to cell lists in systems with moderate size asymmetry, but has slightly poorer performance
-    (10% slower) for monodisperse systems. :py:class:`tree` can also be slower than :py:class:`Cell` if there are multiple
-    types in the system, but the cutoffs between types are identical. (This is because one BVH is created per type.)
-    The user should carefully benchmark neighbor list build times to select the appropriate neighbor list construction type.
+    :py:class:`tree` creates a neighbor list using bounding volume hierarchy
+    (BVH) tree traversal. Pair potentials are attached for computing non-bonded
+    pairwise interactions. A BVH tree of axis-aligned bounding boxes is
+    constructed per particle type, and each particle queries each tree to
+    determine its neighbors. This method of searching leads to significantly
+    improved performance compared to cell lists in systems with moderate size
+    asymmetry, but has slightly poorer performance (10% slower) for monodisperse
+    systems. :py:class:`tree` can also be slower than :py:class:`Cell` if there
+    are multiple types in the system, but the cutoffs between types are
+    identical. (This is because one BVH is created per type.) The user should
+    carefully benchmark neighbor list build times to select the appropriate
+    neighbor list construction type.
 
-    `M.P. Howard et al. 2016 <http://dx.doi.org/10.1016/j.cpc.2016.02.003>`_ describes the original implementation of this
-    algorithm for HOOMD-blue. `M.P. Howard et al. 2019 <https://doi.org/10.1016/j.commatsci.2019.04.004>`_ describes the
-    improved algorithm that is currently implemented. Cite both if you utilize this neighbor list style in your work.
+    `M.P. Howard et al. 2016 <http://dx.doi.org/10.1016/j.cpc.2016.02.003>`_
+    describes the original implementation of this algorithm for HOOMD-blue.
+    `M.P. Howard et al. 2019 <https://doi.org/10.1016/j.commatsci.2019.04.004>`_
+    describes the improved algorithm that is currently implemented. Cite both
+    if you utilize this neighbor list style in your work.
 
     Examples::
 
@@ -400,34 +314,23 @@ class tree(nlist):
         nl_t.tune()
 
     Note:
-        *d_max* should only be set when slj diameter shifting is required by a pair potential. Currently, slj
-        is the only pair potential requiring this shifting, and setting *d_max* for other potentials may lead to
+        *d_max* should only be set when slj diameter shifting is required by a
+        pair potential. Currently, slj is the only pair potential requiring
+        this shifting, and setting *d_max* for other potentials may lead to
         significantly degraded performance or incorrect results.
-
     """
-    def __init__(self, r_buff=0.4, check_period=1, d_max=None, dist_check=True, name=None):
-        nlist.__init__(self)
+    def __init__(self, buffer=0.4, exclusions=('bond',), rebuild_check_delay=1,
+                 diameter_shift=False, check_dist=True, max_diameter=1.0):
 
-        # create the C++ mirror class
-        if not hoomd.context.current.device.cpp_exec_conf.isCUDAEnabled():
-            self.cpp_nlist = _md.NeighborListTree(hoomd.context.current.system_definition, 0.0, r_buff)
+        super().__init__(buffer, exclusions, rebuild_check_delay, diameter_shift,
+                         check_dist, max_diameter)
+
+    def _attach(self):
+        if isinstance(self._simulatino.device, hoomd.device.CPU):
+            nlist_cls = _md.NeighborListTree
         else:
-            self.cpp_nlist = _md.NeighborListGPUTree(hoomd.context.current.system_definition, 0.0, r_buff)
-
-        self.cpp_nlist.setEvery(check_period, dist_check)
-
-        if name is None:
-            self.name = "tree_nlist_%d" % tree.cur_id
-            tree.cur_id += 1
-        else:
-            self.name = name
-
-        hoomd.context.current.system.addCompute(self.cpp_nlist, self.name)
-
-        # register this neighbor list with the context
-        hoomd.context.current.neighbor_lists += [self]
-
-        # save the user defined parameters
-        self.set_params(r_buff, check_period, d_max, dist_check)
-
-tree.cur_id = 0
+            nlist_cls = _md.NeighborListGPUTree
+        # TODO remove 0.0 (r_cut) from constructor
+        self._cpp_obj = nlist_cls(self._simulation.state._cpp_sys_def, 0.0,
+                                  self.buffer)
+        super()._attach()
