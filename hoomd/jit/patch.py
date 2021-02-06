ce7d795d00af01c4d47da3655ed86160703381df
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2021-02-06 12:32:05 -0400

    fix imports

--- hoomd/jit/patch.py
+++ hoomd/jit/patch.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2009-2021 The Regents of the University of Michigan
 # This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
+import hoomd
+from hoomd.hpmc import integrate
 from hoomd import _hoomd
 from hoomd.jit import _jit
 from hoomd.operation import Compute
 from hoomd.data.parameterdicts import ParameterDict
-import hoomd
 
 import subprocess
 import os
