faa7d67eaf3a3dd4f33a26091f6961b05b4142a3
Dan Evans <danevans@umich.edu>
2021-02-09 22:38:53 -0500

    Test after attaching

--- hoomd/md/pytest/test_thermoHMA.py
+++ hoomd/md/pytest/test_thermoHMA.py
@@ -13,3 +13,20 @@ def test_before_attaching():
     thermoHMA = hoomd.md.compute.ThermoHMA(filt, 2.5, 0.6)
     assert thermoHMA.temperature == 2.5
     assert thermoHMA.harmonicPressure == 0.6
+
+
+def test_after_attaching(simulation_factory, two_particle_snapshot_factory):
+    filt = hoomd.filter.All()
+    thermoHMA = hoomd.md.compute.ThermoHMA(filt, 1.0)
+
+    sim = simulation_factory(two_particle_snapshot_factory())
+    sim.operations.add(thermoHMA)
+    assert len(sim.operations.computes) == 1
+    sim.run(0)
+    assert isinstance(thermoHMA.potential_energyHMA, float)
+    assert isinstance(thermoHMA.pressureHMA, float)
+
+    sim.operations.remove(thermoHMA)
+    assert len(sim.operations.computes) == 0
+    assert thermoHMA.potential_energyHMA is None
+    assert thermoHMA.pressureHMA is None
