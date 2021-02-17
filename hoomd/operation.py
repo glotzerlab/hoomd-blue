c14b7be1b5c5fff512af469560cb7dccdec242b3
Brandon Butler <butlerbr@umich.edu>
2021-02-17 10:01:13 -0500

    Remove _StatefulAttrBase from class hierarchy and remove state property

--- hoomd/operation.py
+++ hoomd/operation.py
@@ -153,154 +153,6 @@ class _HOOMDGetSetAttrBase:
                              "with types as keys.".format(attr))
 
 
-class _StatefulAttrBase(_HOOMDGetSetAttrBase, metaclass=Loggable):
-    """Extends parent class to provide a mechanism for exporting object state.
-
-    Provides a means for getting object state, the ``state`` property and
-    ``_get_state`` method (the method exists as a hook for later subclasses to
-    export their state). Also, provides a means for creating new objects from
-    another object's state, ``from_state``. The ``from_state`` method has a few
-    caveats. One of the largest is that any variable found in the
-    ``self._param_dict`` and is placed in ``__params__`` is expected to be
-    accepted in the constructor. Another is that any parameters that are needed
-    at construction but are not in the objects ``_param_dict`` or
-    must be passed as a keyword argument to the method.
-
-    Currently ``from_state`` supports getting the state from a GSD file and a
-    Python ``dict``.
-    """
-    def _typeparam_states(self):
-        """Converts all typeparameters into a standard Python ``ict`` object."""
-        state = {name: tp.state for name, tp in self._typeparam_dict.items()}
-        return deepcopy(state)
-
-    def _get_state(self):
-        """Hook to allow subclasses to overwrite state property."""
-        state = self._typeparam_states()
-        state['__params__'] = dict(self._param_dict)
-        return dict_filter(dict_map(state, _convert_values_to_log_form),
-                           lambda x: x is not RequiredArg)
-
-    @log(category='state')
-    def state(self):
-        """The state of the object.
-
-        The state counts as anything stored in the ``_param_dict`` and
-        ``_typeparam_dict``.
-        """
-        return self._get_state()
-
-    @classmethod
-    def from_state(cls, state, final_namespace=None, **kwargs):
-        """Creates a new object from another object's state.
-
-        Args:
-            state (str or dict): A state dictionary for an object of this
-                type, a gsd file name, or a dictionary outputted by
-                `hoomd.logging.Logger`.
-            final_namespace (str): The name for the key of the parent dictionary
-                from where the state is stored. This is to allow for users to
-                specify the property state information in the case where
-                multiple of the same object have their state information stored
-                in the same location. As an example if two LJ pair potentials
-                are stored, the final namespaces would be ``LJ`` and ``LJ_1``.
-            frame (int): Only accepted when a gsd file name is passed for
-                ``state``. The frame to access the state information. Is keyword
-                only.
-        """
-        # resolve the namespace
-        namespace = list(cls._export_dict.values())[0].namespace
-        if final_namespace is not None:
-            namespace = namespace[:-1] + (final_namespace,)
-
-        namespace = namespace + ('state',)
-
-        # recover state dictionary
-        state_dict, unused_args = cls._get_state_dict(state,
-                                                      namespace,
-                                                      **kwargs)
-        return cls._from_state_with_state_dict(state_dict, **unused_args)
-
-    @classmethod
-    def _get_state_dict(cls, data, namespace, **kwargs):
-        """Get the state dictionary from the accepted outputs of from_state.
-
-        Deals with GSD files, namespace dicts (the output of hoomd loggers), and
-        state dictionaries.
-        """
-        # Filenames
-        if isinstance(data, str):
-            if data.endswith('gsd'):
-                state, kwargs = cls._state_from_gsd(data, namespace, **kwargs)
-
-        # Dictionaries and like objects
-        elif isinstance(data, NamespaceDict):
-            state = deepcopy(data[namespace])
-        elif isinstance(data, Mapping):
-            try:
-                # try to grab the namespace
-                state = deepcopy(NamespaceDict(data)[namespace])
-            except KeyError:
-                # if namespace can't be found assume that dictionary is the
-                # state dictionary (This assumes that values are of the form
-                # (value, flag)
-                try:
-                    state = dict_map(data, lambda x: x[0])
-                except TypeError:
-                    # if the map fails, we then assume that the dictionary is
-                    # one without the flag information on the data. This could
-                    # be the case if a logger backend stores the data and that
-                    # returned data is fed in.
-                    state = deepcopy(data)
-
-        # Data is of an unusable type
-        else:
-            raise ValueError("Object {} cannot be used to get state."
-                             "".format(data))
-
-        return (state, kwargs)
-
-    @classmethod
-    def _state_from_gsd(cls, filename, namespace, **kwargs):
-        """Get state dictionary from GSD file."""
-        if 'frame' not in kwargs.keys():
-            frame = -1
-        else:
-            frame = kwargs.pop('frame')
-        # Grab state keys from gsd
-        reader = GSDStateReader(filename, frame)
-        namespace_str = 'log/' + '/'.join(namespace)
-        state_chunks = reader.getAvailableChunks(namespace_str)
-        state_dict = NamespaceDict()
-        chunk_slice = slice(len(namespace_str) + 1, None)
-        # Build up state dict
-        for state_chunk in state_chunks:
-            state_dict_key = tuple(state_chunk[chunk_slice].split('/'))
-            state_dict[state_dict_key] = \
-                _handle_gsd_arrays(reader.readChunk(state_chunk))
-        return (state_dict._dict, kwargs)
-
-    @classmethod
-    def _from_state_with_state_dict(cls, state, **kwargs):
-        """Using the state dictionary create a new object."""
-        # Initialize object using params from state and passed arguments
-        params = state.get('__params__', {})
-        params.update(kwargs)
-        obj = cls(**params)
-        state.pop('__params__', None)
-
-        # Add typeparameter information
-        for name, tp_dict in state.items():
-            if '__default__' in tp_dict.keys():
-                obj._typeparam_dict[name].default = tp_dict['__default__']
-                del tp_dict['__default__']
-            # Parse the stringified tuple back into tuple
-            if obj._typeparam_dict[name]._len_keys > 1:
-                tp_dict = str_to_tuple_keys(tp_dict)
-            setattr(obj, name, tp_dict)
-        return obj
-
-
 class _DependencyRelation:
     """Defines a dependency relationship between Python objects.
 
@@ -357,7 +209,8 @@ class _DependencyRelation:
         self._dependencies.remove(obj)
 
 
-class _HOOMDBaseObject(_StatefulAttrBase, _DependencyRelation):
+class _HOOMDBaseObject(_HOOMDGetSetAttrBase, _DependencyRelation,
+                       metaclass=Loggable):
     """Handles attaching/detaching to a simulation.
 
     ``_StatefulAttrBase`` handles getting and setting attributes as well as
@@ -462,19 +315,6 @@ class _HOOMDBaseObject(_StatefulAttrBase, _DependencyRelation):
     def _update_param_dict(self):
         for key in self._param_dict.keys():
             self._param_dict[key] = getattr(self, key)
-
-    @log(category='state')
-    def state(self):
-        """The state of the object.
-
-        Provides a mapping of attributes to their values for use in storing
-        objects state for later object reinitialization. An object's state can
-        be used to create an identical object using the `from_state` method
-        (some object require other parameters to be passed in `from_state`
-        besides the state mapping).
-        """
-        self._update_param_dict()
-        return super()._get_state()
 
     def _unapply_typeparam_dict(self):
         for typeparam in self._typeparam_dict.values():
