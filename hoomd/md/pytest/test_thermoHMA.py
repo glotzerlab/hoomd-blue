79b9264ddeab5f6dfe90b2ad3d3d4ba5fdbf34d7
Dan Evans <danevans@umich.edu>
2021-02-16 23:05:09 -0500

    Use sim.always_compute_pressure in tests

--- hoomd/md/pytest/test_thermoHMA.py
+++ hoomd/md/pytest/test_thermoHMA.py
@@ -21,6 +21,7 @@ def test_after_attaching(simulation_factory, two_particle_snapshot_factory):
 
     sim = simulation_factory(two_particle_snapshot_factory())
     sim.operations.add(thermoHMA)
+    sim.always_compute_pressure = True
     assert len(sim.operations.computes) == 1
     sim.run(0)
     assert isinstance(thermoHMA.potential_energy, float)
@@ -31,12 +32,21 @@ def test_after_attaching(simulation_factory, two_particle_snapshot_factory):
     assert thermoHMA.potential_energy is None
     assert thermoHMA.pressure is None
 
+
 def test_logging(simulation_factory, two_particle_snapshot_factory):
     filt = hoomd.filter.All()
-    thermoHMA = hoomd.md.compute.ThermoHMA(filt, 1.0)
-
-    sim = simulation_factory(two_particle_snapshot_factory())
+    T = 1.0
+    thermoHMA = hoomd.md.compute.ThermoHMA(filt, T)
+    snap = two_particle_snapshot_factory()
+    if snap.exists:
+        snap.particles.velocity[:] = [[-2, 0, 0], [2, 0, 0]]
+    sim = simulation_factory(snap)
+    sim.always_compute_pressure = True
     sim.operations.add(thermoHMA)
+
+    integrator = hoomd.md.Integrator(dt=0.0001)
+    integrator.methods.append(hoomd.md.methods.NVT(filt, tau=1, kT=T))
+    sim.operations.integrator = integrator
 
     log = hoomd.logging.Logger()
     log += thermoHMA
