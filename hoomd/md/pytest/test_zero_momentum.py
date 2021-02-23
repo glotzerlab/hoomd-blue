076bf6719903787be9be5e198bd30795f48f47c8
Dan Evans <danevans@umich.edu>
2021-02-22 19:36:16 -0500

    Test that momentum is actually getting set to 0

--- hoomd/md/pytest/test_zero_momentum.py
+++ hoomd/md/pytest/test_zero_momentum.py
@@ -33,3 +33,33 @@ def test_after_attaching(simulation_factory,
 
     sim.run(100)
 
+
+def test_momentum_is_zero(simulation_factory,
+                          two_particle_snapshot_factory):
+    snap = two_particle_snapshot_factory()
+    if snap.exists:
+        snap.particles.velocity[0] = [0, 0, 0]
+        snap.particles.velocity[1] = [2, 0, 0]
+        snap.particles.mass[0] = 1
+        snap.particles.mass[1] = 1
+    sim = simulation_factory(snap)
+    brownian = hoomd.md.methods.Brownian(filter=hoomd.filter.All(),
+                                         kT=hoomd.variant.Constant(0.2),
+                                         seed=2)
+    sim.operations.integrator = hoomd.md.Integrator(0.005, methods=[brownian])
+
+    zm = hoomd.md.update.ZeroMomentum(hoomd.trigger.Periodic(1, 1))
+    sim.operations.add(zm)
+    sim.run(0)
+    masses = sim.state.snapshot.particles.mass
+    velocities = sim.state.snapshot.particles.velocity
+    system_momentum = sum([np.linalg.norm(masses[i] * velocities[i]) for i in [0, 1]])
+    assert system_momentum == 2
+
+    sim.run(10)
+    masses = sim.state.snapshot.particles.mass
+    velocities = sim.state.snapshot.particles.velocity
+    print(velocities)
+    for j in [0, 1, 2]:
+        j_momentum = sum([masses[i] * velocities[i][j] for i in [0, 1]])
+        assert j_momentum == 0
