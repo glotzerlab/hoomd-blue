7f96797ea5ddacb263c7a6a2baba2f6d3556bda3
Joshua A. Anderson <joaander@umich.edu>
2021-03-03 09:55:48 -0500

    Format test_compute_free_volumen.py
    Also make arguments to the lattice snapshot factory explicit

--- hoomd/hpmc/pytest/test_compute_free_volume.py
+++ hoomd/hpmc/pytest/test_compute_free_volume.py
@@ -38,27 +38,38 @@ def test_after_attaching(simulation_factory, lattice_snapshot_factory):
     sim.run(10)
     assert isinstance(free_volume.free_volume, float)
 
-_radii = [(0.25, 0.05),
-          (0.4, 0.05),
-          (0.7, 0.17)]
+
+_radii = [
+    (0.25, 0.05),
+    (0.4, 0.05),
+    (0.7, 0.17),
+]
+
 
 @pytest.mark.parametrize("radius1, radius2", _radii)
-def test_validation_systems(simulation_factory,
-                            two_particle_snapshot_factory,
-                            lattice_snapshot_factory,
-                            radius1,
-                            radius2):
-    free_volume = (7**3) * (1 - (4 / 3) * np.pi * (radius1 + radius2)**3)
+def test_validation_systems(simulation_factory, two_particle_snapshot_factory,
+                            lattice_snapshot_factory, radius1, radius2):
+    n = 7
+    free_volume = (n**3) * (1 - (4 / 3) * np.pi * (radius1 + radius2)**3)
     free_volume = max([0.0, free_volume])
-    sim = simulation_factory(lattice_snapshot_factory(particle_types=['A', 'B']))
+    sim = simulation_factory(
+        lattice_snapshot_factory(particle_types=['A', 'B'],
+                                 n=n,
+                                 a=1,
+                                 dimensions=3,
+                                 r=0))
+
     mc = hoomd.hpmc.integrate.Sphere()
     mc.shape["A"] = {'diameter': radius1 * 2}
     mc.shape["B"] = {'diameter': radius2 * 2}
     mc.depletant_fugacity["B"] = 1.5
     sim.operations.add(mc)
 
-    sim.operations._schedule()
-    free_volume_compute = hoomd.hpmc.compute.FreeVolume(mc, test_type='B', nsample=10000)
+    free_volume_compute = hoomd.hpmc.compute.FreeVolume(mc,
+                                                        test_type='B',
+                                                        nsample=10000)
     sim.operations.add(free_volume_compute)
     sim.run(0)
-    np.testing.assert_allclose(free_volume, free_volume_compute.free_volume, rtol=5e-2)
+    np.testing.assert_allclose(free_volume,
+                               free_volume_compute.free_volume,
+                               rtol=1e-2)
