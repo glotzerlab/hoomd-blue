ab067a220abd1c280adde28a5b764b1399a0c698
Joshua A. Anderson <joaander@umich.edu>
2021-03-05 08:17:25 -0500

    Fix compile errors

--- hoomd/md/PPPMForceComputeGPU.cc
+++ hoomd/md/PPPMForceComputeGPU.cc
@@ -55,11 +55,11 @@ void PPPMForceComputeGPU::initializeFFT()
     // free plans if they have already been initialized
     if (m_local_fft && m_cufft_initialized)
         {
-        for (int idev = m_exec_conf->getNumActiveGPUs()-1; idev >= 0; --idev)
-            {
-            cudaSetDevice(m_exec_conf->getGPUIds()[idev]);
-            CHECK_CUFFT_ERROR(cufftDestroy(m_cufft_plan[idev]));
-            }
+        #ifdef __HIP_PLATFORM_HCC__
+        CHECK_HIPFFT_ERROR(hipfftDestroy(m_hipfft_plan));
+        #else
+        CHECK_HIPFFT_ERROR(cufftDestroy(m_hipfft_plan));
+        #endif
         }
     #ifdef ENABLE_MPI
     else if (m_cuda_dfft_initialized)
