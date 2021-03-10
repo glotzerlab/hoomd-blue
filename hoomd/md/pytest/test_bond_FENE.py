0347d6d5482abf7bdefb2438396dfc7dbfdb95a3
Dan Evans <danevans@umich.edu>
2021-03-09 22:20:27 -0500

    Test after attaching

--- hoomd/md/pytest/test_bond_FENE.py
+++ hoomd/md/pytest/test_bond_FENE.py
@@ -22,3 +22,35 @@ def test_before_attaching(bond_params_tuple):
         np.testing.assert_allclose(bond_potential.params['bond'][key],
                                    bond_params[key],
                                    rtol=1e-6)
+
+
+@pytest.mark.parametrize("bond_params_tuple", get_bond_params())
+def test_after_attaching(two_particle_snapshot_factory,
+                         simulation_factory,
+                         bond_params_tuple):
+    snap = two_particle_snapshot_factory(d=0.969, L=5)
+    if snap.exists:
+        snap.bonds.N = 1
+        snap.bonds.types = ['bond']
+        snap.bonds.typeid[0] = 0
+        snap.bonds.group[0] = (0, 1)
+    sim = simulation_factory(snap)
+
+    k, r0, epsilon, sigma = bond_params_tuple
+    bond_params = dict(k=k, r0=r0, epsilon=epsilon, sigma=sigma)
+    bond_potential = hoomd.md.bond.FENE()
+    bond_potential.params['bond'] = bond_params
+
+    integrator = hoomd.md.Integrator(dt=0.005)
+
+    integrator.forces.append(bond_potential)
+
+    nvt = hoomd.md.methods.Langevin(kT=1, filter=hoomd.filter.All(), alpha=0.1)
+    integrator.methods.append(nvt)
+    sim.operations.integrator = integrator
+
+    sim.run(0)
+    for key in bond_params.keys():
+        np.testing.assert_allclose(bond_potential.params['bond'][key],
+                                   bond_params[key],
+                                   rtol=1e-6)
