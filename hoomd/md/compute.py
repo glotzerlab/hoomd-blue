58746097c30383d456aedd0b5f4052af19e929a9
Dan Evans <danevans@umich.edu>
2021-04-06 18:46:44 -0400

    Document when HMA saves lattice positions

--- hoomd/md/compute.py
+++ hoomd/md/compute.py
@@ -301,7 +301,8 @@ class HarmonicAveragedThermodynamicQuantities(Compute):
     (with less variance) for atomic crystals in NVT simulations.  The presence
     of diffusion (vacancy hopping, etc.) will prevent HMA from providing
     improvement.  HMA tracks displacements from the lattice positions, which
-    are saved at the start of the first call to `Simulation.run`.
+    are saved either during first call to `Simulation.run` or when the compute
+    is first added to the simulation, whichever occurs last.
 
     Note:
         :py:class:`ThermoHMA` is an implementation of the methods section of
