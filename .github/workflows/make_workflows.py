2afe9e7a19802434434aee5af63c4e310bf0624c
Joshua A. Anderson <joaander@umich.edu>
2021-05-04 20:53:29 -0400

    Use jinja to write workflow files

---
+++ .github/workflows/make_workflows.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import jinja2
+import yaml
+
+if __name__ == '__main__':
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'),
+                             block_start_string = '<%',
+                             block_end_string = '%>',
+                             variable_start_string = '<<',
+                             variable_end_string = '>>',
+                             comment_start_string = '<#',
+                             comment_end_string = '#>',
+                             trim_blocks=True,
+                             lstrip_blocks=True)
+    template = env.get_template('test.yml')
+    with open('templates/configurations.yml', 'r') as f:
+        configurations = yaml.safe_load(f)
+
+    # preprocess configurations and fill out additional fields needed by
+    # `test.yml` to configure the matrix jobs
+    for name,configuration in configurations.items():
+        for entry in configuration:
+            if entry['config'].startswith('[cuda'):
+                entry['runner'] = "[self-hosted,GPU]"
+                entry['docker_options'] = "--gpus=all"
+            else:
+                entry['runner'] = "ubuntu-latest"
+                entry['docker_options'] = ""
+
+    with open('test.yml', 'w') as f:
+        f.write(template.render(configurations))
