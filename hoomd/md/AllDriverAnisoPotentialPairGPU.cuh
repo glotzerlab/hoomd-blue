036249402213a1399674bd3918cb295f6f6e0599
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-05-08 12:49:15 -0700

    Fix old references to cuda rather than hip.

--- hoomd/md/AllDriverAnisoPotentialPairGPU.cuh
+++ hoomd/md/AllDriverAnisoPotentialPairGPU.cuh
@@ -28,12 +28,16 @@ gpu_compute_pair_aniso_forces_dipole(const a_pair_args_t&,
             const EvaluatorPairDipole::param_type*,
             const EvaluatorPairDipole::shape_type*);
 
-cudaError_t gpu_compute_pair_aniso_forces_ALJ_2D(const a_pair_args_t&,
+
+//! Compute anisotropic Lennard-Jones forces and torques on the GPU with EvaluatorPairALJ
+hipError_t __attribute__((visibility("default")))
+gpu_compute_pair_aniso_forces_ALJ_2D(const a_pair_args_t&,
             const EvaluatorPairALJ<2>::param_type*,
             const EvaluatorPairALJ<2>::shape_param_type*);
 
-cudaError_t gpu_compute_pair_aniso_forces_ALJ_3D(const a_pair_args_t&,
+hipError_t __attribute__((visibility("default")))
+gpu_compute_pair_aniso_forces_ALJ_3D(const a_pair_args_t&,
             const EvaluatorPairALJ<3>::param_type*,
-            const EvaluatorPairALJ<2>::shape_param_type*);
+            const EvaluatorPairALJ<3>::shape_param_type*);
 
 #endif
