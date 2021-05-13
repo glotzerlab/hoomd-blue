ad72f44286d07447076f034bdd715a513957d467
Dan Evans <danevans@umich.edu>
2021-05-13 14:16:57 -0400

    Comment out export_ShapeSpringLogBoltzmannFunction

--- hoomd/hpmc/module_convex_polyhedron_alchemy.cc
+++ hoomd/hpmc/module_convex_polyhedron_alchemy.cc
@@ -22,7 +22,7 @@ void export_convex_polyhedron_alchemy(py::module& m)
     export_ShapeMoveInterface< ShapeConvexPolyhedron >(m, "ShapeMoveConvexPolyhedron");
     export_ShapeLogBoltzmann< ShapeConvexPolyhedron >(m, "LogBoltzmannConvexPolyhedron");
     export_ElasticShapeMove< ShapeConvexPolyhedron >(m, "ElasticShapeMoveConvexPolyhedron");
-    export_ShapeSpringLogBoltzmannFunction<ShapeConvexPolyhedron >(m, "ShapeSpringLogBoltzmannConvexPolyhedron");
+    // export_ShapeSpringLogBoltzmannFunction<ShapeConvexPolyhedron >(m, "ShapeSpringLogBoltzmannConvexPolyhedron");
     export_AlchemyLogBoltzmannFunction< ShapeConvexPolyhedron >(m, "AlchemyLogBoltzmannConvexPolyhedron");
     export_ConvexPolyhedronGeneralizedShapeMove< ShapeConvexPolyhedron >(m, "GeneralizedShapeMoveConvexPolyhedron");
     export_UpdaterShape< ShapeConvexPolyhedron >(m, "UpdaterShapeConvexPolyhedron");
