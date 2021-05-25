15e4f2c1bcdaad981aa2a3738fcfb60b1f3b5a09
Corwin Kerr <cbkerr@umich.edu>
2021-05-25 13:46:03 -0400

    add semicolon

--- hoomd/SnapshotSystemData.cc
+++ hoomd/SnapshotSystemData.cc
@@ -41,10 +41,10 @@ void SnapshotSystemData<Real>::replicate(unsigned int nx, unsigned int ny, unsig
 
 
 template<class Real> void SnapshotSystemData<Real>::wrap()
-    {    
+    {
     for (unsigned int i = 0; i < particle_data.size; i++)
         {
-           global_box.wrap(particle_data.pos[i], particle_data.image[i]) 
+		global_box.wrap(particle_data.pos[i], particle_data.image[i]);
         }
     }
 
