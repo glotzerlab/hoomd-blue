28fe671b0956232f1e103fedc0683cf43ae0d947
Dan Evans <danevans@umich.edu>
2021-06-04 22:23:01 -0400

    Test every property of shape updater

--- hoomd/hpmc/pytest/test_shape_updater.py
+++ hoomd/hpmc/pytest/test_shape_updater.py
@@ -13,24 +13,42 @@ def test_before_attaching():
     move_ratio = 1.0
     trigger = hoomd.trigger.Periodic(1)
     nselect = 1
+    nsweeps = 1
+    multi_phase = True
+    num_phase = 2
     shape_updater = hoomd.hpmc.update.Shape(shape_move=vertex_move,
                                             move_ratio=move_ratio,
                                             trigger=trigger,
-                                            nselect=nselect)
+                                            nselect=nselect,
+                                            nsweeps=nsweeps,
+                                            multi_phase=multi_phase,
+                                            num_phase=num_phase)
     assert shape_updater.shape_move is vertex_move
     assert np.allclose(shape_updater.move_ratio, move_ratio, rtol=1e-4)
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
+    assert shape_updater.nsweeps == nsweeps
+    assert shape_updater.multi_phase == multi_phase
+    assert shape_updater.num_phase == num_phase
 
     move_ratio = 0.5
     trigger = hoomd.trigger.Periodic(10)
     nselect = 2
+    nsweeps = 4
+    multi_phase = False
+    num_phase = 1
     shape_updater.move_ratio = move_ratio
     shape_updater.trigger = trigger
     shape_updater.nselect = nselect
+    shape_updater.nsweeps = nsweeps
+    shape_updater.multi_phase = multi_phase
+    shape_updater.num_phase = num_phase
     assert np.allclose(shape_updater.move_ratio, move_ratio, rtol=1e-4)
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
+    assert shape_updater.nsweeps == nsweeps
+    assert shape_updater.multi_phase == multi_phase
+    assert shape_updater.num_phase == num_phase
 
     assert shape_updater.shape_moves is None
     assert shape_updater.shape_move_energy is None
@@ -45,12 +63,16 @@ def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     move_ratio = 0.5
     trigger = hoomd.trigger.Periodic(10)
     nselect = 2
+    nsweeps = 1
+    multi_phase = True
+    num_phase = 2
     shape_updater = hoomd.hpmc.update.Shape(shape_move=vertex_move,
                                             move_ratio=move_ratio,
                                             trigger=trigger,
                                             nselect=nselect,
-                                            nsweeps=1)
-
+                                            nsweeps=nsweeps,
+                                            multi_phase=multi_phase,
+                                            num_phase=num_phase)
     mc = hoomd.hpmc.integrate.ConvexPolyhedron(0)
     mc.shape['A'] = {'vertices': verts}
     sim = simulation_factory(lattice_snapshot_factory(dimensions=3, a=2.0, n=3))
@@ -63,15 +85,24 @@ def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     move_ratio = 1.0
     trigger = hoomd.trigger.Periodic(1)
     nselect = 1
+    nsweeps = 4
+    multi_phase = False
+    num_phase = 1
     shape_updater.move_ratio = move_ratio
     shape_updater.trigger = trigger
     shape_updater.nselect = nselect
+    shape_updater.nsweeps = nsweeps
+    shape_updater.multi_phase = multi_phase
+    shape_updater.num_phase = num_phase
     assert np.allclose(shape_updater.move_ratio, move_ratio, rtol=1e-4)
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
+    assert shape_updater.nsweeps == nsweeps
+    assert shape_updater.multi_phase == multi_phase
+    assert shape_updater.num_phase == num_phase
 
     sim.run(2)
-    assert sum(shape_updater.shape_moves) == 2
+    assert sum(shape_updater.shape_moves) == 8
     assert shape_updater.shape_move_energy == 0.0
     assert shape_updater.particle_volume == N * particle_volume
 
