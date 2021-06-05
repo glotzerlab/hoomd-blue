81645b5ada8d24c46fcce0638a23e66548d7233d
Dan Evans <danevans@umich.edu>
2021-06-04 22:05:50 -0400

    Test shape move counter and shape move energy loggables

--- hoomd/hpmc/pytest/test_shape_updater.py
+++ hoomd/hpmc/pytest/test_shape_updater.py
@@ -6,14 +6,12 @@ import pytest
 
 def test_before_attaching():
     verts = np.asarray([[-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [1, -1, -1],
-                        [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]])
-    constant_move = hoomd.hpmc.shape_move.Constant(shape_params={'A': dict(vertices=verts,
-                                                                           ignore_statistics=0,
-                                                                           sweep_radius=0.0)})
+                        [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]]) / 2
+   vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=1.0)
     move_ratio = 1.0
     trigger = hoomd.trigger.Periodic(1)
     nselect = 1
-    shape_updater = hoomd.hpmc.update.Shape(shape_move=constant_move,
+    shape_updater = hoomd.hpmc.update.Shape(shape_move=vertex_move,
                                             move_ratio=move_ratio,
                                             trigger=trigger,
                                             nselect=nselect)
@@ -32,20 +30,22 @@ def test_before_attaching():
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
 
+    assert shape_updater.shape_moves is None
+    assert shape_updater.shape_move_energy is None
+
 
 def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     verts = np.asarray([[-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [1, -1, -1],
-                        [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]])
-    constant_move = hoomd.hpmc.shape_move.Constant(shape_params={'A': dict(vertices=verts,
-                                                                           ignore_statistics=0,
-                                                                           sweep_radius=0.0)})
-    move_ratio = 1.0
-    trigger = hoomd.trigger.Periodic(1)
-    nselect = 1
-    shape_updater = hoomd.hpmc.update.Shape(shape_move=constant_move,
+                        [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]]) / 2
+    vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=1.0)
+    move_ratio = 0.5
+    trigger = hoomd.trigger.Periodic(10)
+    nselect = 2
+    shape_updater = hoomd.hpmc.update.Shape(shape_move=vertex_move,
                                             move_ratio=move_ratio,
                                             trigger=trigger,
-                                            nselect=nselect)
+                                            nselect=nselect,
+                                            nsweeps=1)
 
     mc = hoomd.hpmc.integrate.ConvexPolyhedron(0)
     mc.shape['A'] = {'vertices': verts}
@@ -55,12 +55,16 @@ def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     sim.operations.add(shape_updater)
     sim.run(0)
 
-    move_ratio = 0.5
-    trigger = hoomd.trigger.Periodic(10)
-    nselect = 2
+    move_ratio = 1.0
+    trigger = hoomd.trigger.Periodic(1)
+    nselect = 1
     shape_updater.move_ratio = move_ratio
     shape_updater.trigger = trigger
     shape_updater.nselect = nselect
     assert np.allclose(shape_updater.move_ratio, move_ratio, rtol=1e-4)
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
+
+    sim.run(2)
+    assert sum(shape_updater.shape_moves) == 2
+    assert shape_updater.shape_move_energy == 0.0
