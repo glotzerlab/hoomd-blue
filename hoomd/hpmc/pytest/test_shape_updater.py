64a371d88a22aca8afbbdd6613e3ff2ce25d6bce
Dan Evans <danevans@umich.edu>
2021-06-04 22:18:08 -0400

    Test particle volume loggable

--- hoomd/hpmc/pytest/test_shape_updater.py
+++ hoomd/hpmc/pytest/test_shape_updater.py
@@ -2,12 +2,14 @@ import hoomd
 import hoomd.hpmc
 import numpy as np
 import pytest
+from hoomd.logging import LoggerCategories
+from hoomd.conftest import logging_check
 
 
 def test_before_attaching():
     verts = np.asarray([[-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [1, -1, -1],
                         [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]]) / 2
-   vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=1.0)
+    vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=1.0)
     move_ratio = 1.0
     trigger = hoomd.trigger.Periodic(1)
     nselect = 1
@@ -15,7 +17,7 @@ def test_before_attaching():
                                             move_ratio=move_ratio,
                                             trigger=trigger,
                                             nselect=nselect)
-    assert shape_updater.shape_move is constant_move
+    assert shape_updater.shape_move is vertex_move
     assert np.allclose(shape_updater.move_ratio, move_ratio, rtol=1e-4)
     assert shape_updater.trigger is trigger
     assert shape_updater.nselect == nselect
@@ -32,12 +34,14 @@ def test_before_attaching():
 
     assert shape_updater.shape_moves is None
     assert shape_updater.shape_move_energy is None
+    assert shape_updater.particle_volume is None
 
 
 def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     verts = np.asarray([[-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [1, -1, -1],
                         [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, 1]]) / 2
-    vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=1.0)
+    particle_volume = 1.0
+    vertex_move = hoomd.hpmc.shape_move.Vertex(stepsize={'A': 0.01}, param_ratio=0.2, volume=particle_volume)
     move_ratio = 0.5
     trigger = hoomd.trigger.Periodic(10)
     nselect = 2
@@ -50,6 +54,7 @@ def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     mc = hoomd.hpmc.integrate.ConvexPolyhedron(0)
     mc.shape['A'] = {'vertices': verts}
     sim = simulation_factory(lattice_snapshot_factory(dimensions=3, a=2.0, n=3))
+    N = sim.state.snapshot.particles.N
     sim.seed = 0
     sim.operations.add(mc)
     sim.operations.add(shape_updater)
@@ -68,3 +73,4 @@ def test_after_attaching(device, simulation_factory, lattice_snapshot_factory):
     sim.run(2)
     assert sum(shape_updater.shape_moves) == 2
     assert shape_updater.shape_move_energy == 0.0
+    assert shape_updater.particle_volume == N * particle_volume
