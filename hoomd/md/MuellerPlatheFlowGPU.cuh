98ffecfedd5738e9bbec8c5bfa2b925580e29e02
Dan Evans <danevans@umich.edu>
2021-06-07 12:15:17 -0400

    Style check fixes

--- hoomd/md/MuellerPlatheFlowGPU.cuh
+++ hoomd/md/MuellerPlatheFlowGPU.cuh
@@ -1,9 +1,9 @@
 // Copyright (c) 2009-2021 The Regents of the University of Michigan
 // This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
+#include "MuellerPlatheFlow.h"
 #include "hoomd/HOOMDMath.h"
 #include "hoomd/ParticleData.cuh"
-#include "MuellerPlatheFlow.h"
 
 /*! \file MuellerPlatheFlowGPU.cuh
     \brief Declares GPU kernel code for calculating MinMax velocities and updates for the flow.
@@ -30,10 +30,11 @@ hipError_t gpu_search_min_max_velocity(const unsigned int group_size,
                                        flow_enum::Direction flow_direction,
                                        flow_enum::Direction slab_direction);
 
-hipError_t gpu_update_min_max_velocity(const unsigned int* const d_rtag,
-                                       Scalar4* const d_vel,
-                                       const unsigned int Ntotal,
-                                       const Scalar3 last_max_vel,
-                                       const Scalar3 last_min_vel,
-                                       const flow_enum::Direction flow_direction);
+hipError_t
+gpu_update_min_max_velocity(const unsigned int* const d_rtag,
+                            Scalar4* const d_vel,
+                            const unsigned int Ntotal,
+                            const Scalar3 last_max_vel,
+                            const Scalar3 last_min_vel,
+                            const flow_enum::Direction flow_direction);
 #endif //__MUELLER_PLATHE_FLOW_GPU_CUH__
