6a81192446cc0f7a0e6a251289340f76b932f8ef
Joshua A. Anderson <joaander@umich.edu>
2021-06-09 15:42:00 -0400

    Fix compiler warning

--- hoomd/hpmc/PatchEnergyJITGPU.cc
+++ hoomd/hpmc/PatchEnergyJITGPU.cc
@@ -39,7 +39,8 @@ void PatchEnergyJITGPU::computePatchEnergyGPU(const gpu_args_t& args, hipStream_
 
     unsigned int max_queue_size = n_groups * tpp;
 
-    const unsigned int min_shared_bytes = args.num_types * sizeof(Scalar);
+    const unsigned int min_shared_bytes
+        = static_cast<unsigne dint>(args.num_types * sizeof(Scalar));
 
     unsigned int shared_bytes = static_cast<unsigned int>(
         n_groups
