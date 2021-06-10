f296c469da7d7a714a2c47f155473e1cd6439c70
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2021-06-10 18:03:22 -0400

    divide rattle and active force GPU functions

--- hoomd/md/all_kernels_sphere_manifold.cu
+++ hoomd/md/all_kernels_sphere_manifold.cu
@@ -1,7 +1,6 @@
 // Copyright (c) 2009-2019 The Regents of the University of Michigan
 // This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
-#include "ActiveForceConstraintComputeGPU.cuh"
 #include "TwoStepRATTLEBDGPU.cuh"
 #include "TwoStepRATTLELangevinGPU.cuh"
 #include "TwoStepRATTLENVEGPU.cuh"
@@ -88,25 +87,3 @@ template hipError_t gpu_include_rattle_force_nve<ManifoldSphere>(const Scalar4* 
                                                                  Scalar deltaT,
                                                                  bool zero_force,
                                                                  unsigned int block_size);
-
-template hipError_t
-gpu_compute_active_force_set_constraints<ManifoldSphere>(const unsigned int group_size,
-                                                         unsigned int* d_index_array,
-                                                         const Scalar4* d_pos,
-                                                         Scalar4* d_orientation,
-                                                         const Scalar4* d_f_act,
-                                                         ManifoldSphere manifold,
-                                                         unsigned int block_size);
-
-template hipError_t gpu_compute_active_force_constraint_rotational_diffusion<ManifoldSphere>(
-    const unsigned int group_size,
-    unsigned int* d_tag,
-    unsigned int* d_index_array,
-    const Scalar4* d_pos,
-    Scalar4* d_orientation,
-    ManifoldSphere manifold,
-    bool is2D,
-    const Scalar rotationDiff,
-    const uint64_t timestep,
-    const uint16_t seed,
-    unsigned int block_size);
