8a85850f6552fd04962e7c296b0998bd03986981
Dan Evans <danevans@umich.edu>
2021-06-10 12:25:11 -0400

    Fix test angle forces and energies arguments

--- hoomd/md/pytest/test_angle.py
+++ hoomd/md/pytest/test_angle.py
@@ -122,7 +122,7 @@ def test_after_attaching(triplet_snapshot_factory, simulation_factory,
 @pytest.mark.parametrize("angle_cls, potential_kwargs, force, energy",
                          get_angle_args_forces_and_energies())
 def test_forces_and_energies(triplet_snapshot_factory, simulation_factory,
-                             angle_args_force_and_energy):
+                             angle_cls, potential_kwargs, force, energy):
     theta_deg = 60
     theta_rad = theta_deg * (np.pi / 180)
     snap = triplet_snapshot_factory(theta_deg=theta_deg)
