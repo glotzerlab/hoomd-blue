4d773487bb63c4a95cc5847d61850545f266efce
Joshua A. Anderson <joaander@umich.edu>
2021-06-18 13:59:20 -0400

    Wrap line at 80 chars

--- hoomd/__init__.py
+++ hoomd/__init__.py
@@ -21,8 +21,8 @@ the package and import from the Python environment.
 
 To run pytest, either:
 (1) compile then execute `python3 -m pytest <build-directory>/hoomd` or
-(2) compile and install. Then, ensuring your current working directory is outside the
-hoomd source directory, execute `python3 -m pytest --pyargs hoomd`.
+(2) compile and install. Then, ensuring your current working directory is
+outside the hoomd source directory, execute `python3 -m pytest --pyargs hoomd`.
 """,
           file=sys.stderr)
 
