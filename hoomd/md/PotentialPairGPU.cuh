bb9a2dc30fb7d7d086d2c5222edd32eec8028aca
Dan Evans <danevans@umich.edu>
2021-07-04 13:34:11 -0400

    Use correct params variable

--- hoomd/md/PotentialPairGPU.cuh
+++ hoomd/md/PotentialPairGPU.cuh
@@ -455,7 +455,7 @@ struct PairForceComputeKernel
                 unsigned int available_bytes = max_extra_bytes;
                 for (unsigned int i = 0; i < typpair_idx.getNumElements(); ++i)
                     {
-                    params[i].load_shared(ptr, available_bytes);
+                    d_params[i].load_shared(ptr, available_bytes);
                     }
                 extra_bytes = max_extra_bytes - available_bytes;
                 }
