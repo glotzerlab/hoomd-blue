a85f6088d66161b74e6ccb96177963f5c269c300
Brandon Butler <butlerbr@umich.edu>
2021-07-07 10:56:06 -0400

    Remove old code for interfacing md walls with Python

--- hoomd/md/module-md.cc
+++ hoomd/md/module-md.cc
@@ -116,75 +116,6 @@ namespace py = pybind11;
     \brief Brings all of the export_* functions together to export the hoomd python module
 */
 
-//! Helper function for converting python wall group structure to wall_type
-wall_type make_wall_field_params(py::object walls,
-                                 std::shared_ptr<const ExecutionConfiguration> m_exec_conf)
-    {
-    wall_type w;
-    py::list walls_spheres = walls.attr("spheres").cast<py::list>();
-    py::list walls_cylinders = walls.attr("cylinders").cast<py::list>();
-    py::list walls_planes = walls.attr("planes").cast<py::list>();
-    w.numSpheres = (unsigned int)py::len(walls_spheres);
-    w.numCylinders = (unsigned int)py::len(walls_cylinders);
-    w.numPlanes = (unsigned int)py::len(walls_planes);
-
-    if (w.numSpheres > MAX_N_SWALLS || w.numCylinders > MAX_N_CWALLS || w.numPlanes > MAX_N_PWALLS)
-        {
-        m_exec_conf->msg->error() << "A number of walls greater than the maximum number allowed "
-                                     "was specified in a wall force."
-                                  << std::endl;
-        throw std::runtime_error("Error loading wall group.");
-        }
-    else
-        {
-        for (unsigned int i = 0; i < w.numSpheres; i++)
-            {
-            Scalar r = py::cast<Scalar>(py::object(walls_spheres[i]).attr("r"));
-            Scalar3 origin = py::cast<Scalar3>(py::object(walls_spheres[i]).attr("_origin"));
-            bool inside = py::cast<bool>(py::object(walls_spheres[i]).attr("inside"));
-            w.Spheres[i] = SphereWall(r, origin, inside);
-            }
-        for (unsigned int i = 0; i < w.numCylinders; i++)
-            {
-            Scalar r = py::cast<Scalar>(py::object(walls_cylinders[i]).attr("r"));
-            Scalar3 origin = py::cast<Scalar3>(py::object(walls_cylinders[i]).attr("_origin"));
-            Scalar3 axis = py::cast<Scalar3>(py::object(walls_cylinders[i]).attr("_axis"));
-            bool inside = py::cast<bool>(py::object(walls_cylinders[i]).attr("inside"));
-            w.Cylinders[i] = CylinderWall(r, origin, axis, inside);
-            }
-        for (unsigned int i = 0; i < w.numPlanes; i++)
-            {
-            Scalar3 origin = py::cast<Scalar3>(py::object(walls_planes[i]).attr("_origin"));
-            Scalar3 normal = py::cast<Scalar3>(py::object(walls_planes[i]).attr("_normal"));
-            bool inside = py::cast<bool>(py::object(walls_planes[i]).attr("inside"));
-            w.Planes[i] = PlaneWall(origin, normal, inside);
-            }
-        return w;
-        }
-    }
-
-//! Exports helper function for parameters based on standard evaluators
-template<class evaluator> void export_wall_params_helpers(py::module& m)
-    {
-    py::class_<typename EvaluatorWalls<evaluator>::param_type,
-               std::shared_ptr<typename EvaluatorWalls<evaluator>::param_type>>(
-        m,
-        (EvaluatorWalls<evaluator>::getName() + "_params").c_str())
-        .def(py::init<>())
-        .def_readwrite("params", &EvaluatorWalls<evaluator>::param_type::params)
-        .def_readwrite("rextrap", &EvaluatorWalls<evaluator>::param_type::rextrap)
-        .def_readwrite("rcutsq", &EvaluatorWalls<evaluator>::param_type::rcutsq);
-    m.def(std::string("make_" + EvaluatorWalls<evaluator>::getName() + "_params").c_str(),
-          &make_wall_params<evaluator>);
-    }
-
-//! Combines exports of evaluators and parameter helper functions
-template<class evaluator> void export_PotentialExternalWall(py::module& m, const std::string& name)
-    {
-    export_PotentialExternal<PotentialExternal<EvaluatorWalls<evaluator>>>(m, name);
-    export_wall_params_helpers<evaluator>(m);
-    }
-
 // Template specification for Dipole anisotropic pair potential. A specific
 // template instance is needed since we expose the shape as just mu in Python
 // when the default behavior exposes setting and getting the shape through
@@ -291,8 +222,6 @@ PYBIND11_MODULE(_md, m)
     export_ForceDistanceConstraint(m);
     export_ForceComposite(m);
     export_PPPMForceCompute(m);
-    py::class_<wall_type, std::shared_ptr<wall_type>>(m, "wall_type").def(py::init<>());
-    m.def("make_wall_field_params", &make_wall_field_params);
     export_PotentialExternal<PotentialExternalPeriodic>(m, "PotentialExternalPeriodic");
     export_PotentialExternal<PotentialExternalElectricField>(m, "PotentialExternalElectricField");
     // TODO: Port walls to HOOMD v3
