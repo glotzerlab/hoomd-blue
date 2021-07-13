192a29ee830bd25f9b410bf27ab3c6166b8852b2
Sophie Youjung Lee <syjlee@umich.edu>
2021-07-13 17:26:15 -0400

    fixes compiling error and initialization of pytest

--- hoomd/md/pytest/test_bond.py
+++ hoomd/md/pytest/test_bond.py
@@ -15,6 +15,16 @@ _FENE_args = {
 _FENE_arg_list = [(hoomd.md.bond.FENE, dict(zip(_FENE_args, val)))
                   for val in zip(*_FENE_args.values())]
 
+_Tether_args = {
+    'k_b':[5.0, 10.0, 20.0],
+    'l_min':[0.8, 0.9, 1.0],
+    'l_c0':[1.1, 1.2, 1.3],
+    'l_c1':[1.4, 1.5, 1.6], 
+    'l_max':[1.7, 1.8, 1.9]
+}
+_Tether_arg_list = [(hoomd.md.bond.Tether, dict(zip(_Tether_args, val))) 
+                    for val in zip(*_Tether_args.values())]
+
 
 def get_bond_and_args():
     return _harmonic_arg_list + _FENE_arg_list
@@ -25,16 +35,21 @@ def get_bond_args_forces_and_energies():
     harmonic_energies = [5.9724, 6.6795, 6.9056]
     FENE_forces = [282.296, 146.288, 88.8238]
     FENE_energies = [70.5638, 49.2476, 35.3135]
+    Tether_forces = [1.0 , 1.0, 1.0]
+    Tether_energies = [1.0, 1.0, 1.0]
 
     harmonic_args_and_vals = []
     FENE_args_and_vals = []
+    Tether_args_and_vals = []
     for i in range(3):
         harmonic_args_and_vals.append(
             (_harmonic_arg_list[i][0], _harmonic_arg_list[i][1],
              harmonic_forces[i], harmonic_energies[i]))
         FENE_args_and_vals.append((_FENE_arg_list[i][0], _FENE_arg_list[i][1],
                                    FENE_forces[i], FENE_energies[i]))
-    return harmonic_args_and_vals + FENE_args_and_vals
+        Tether_args_and_vals.append((_Tether_arg_list[i][0], _Tether_arg_list[i][1],
+                                     Tether_forces[i], Tether_energies[i]))
+    return harmonic_args_and_vals + FENE_args_and_vals + Tether_args_and_vals
 
 
 @pytest.mark.parametrize("bond_cls, potential_kwargs", get_bond_and_args())
