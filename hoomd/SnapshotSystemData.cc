75bb0fe5ae7d318466f68f94118fdc5baea1c531
Joshua A. Anderson <joaander@umich.edu>
2021-07-22 07:52:37 -0400

    Fix compiler warnings

--- hoomd/SnapshotSystemData.cc
+++ hoomd/SnapshotSystemData.cc
@@ -44,11 +44,11 @@ template<class Real> void SnapshotSystemData<Real>::wrap()
     for (unsigned int i = 0; i < particle_data.size; i++)
         {
         auto const frac = global_box.makeFraction(particle_data.pos[i]);
-        auto modulus_positive = [](auto x){ return std::fmod(std::fmod(x, Real(1.0)) + Real(1.0), Real(1.0)); };
+        auto modulus_positive = [](Real x){ return std::fmod(std::fmod(x, Real(1.0)) + Real(1.0), Real(1.0)); };
         auto const wrapped = vec3<Real>(
-            modulus_positive(frac.x),
-            modulus_positive(frac.y),
-            modulus_positive(frac.z)
+            modulus_positive(static_cast<Real>(frac.x)),
+            modulus_positive(static_cast<Real>(frac.y)),
+            modulus_positive(static_cast<Real>(frac.z))
             );
         particle_data.pos[i] = global_box.makeCoordinates(wrapped);
         auto const img = make_int3(
