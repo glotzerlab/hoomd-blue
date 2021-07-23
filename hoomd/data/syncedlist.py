462ceb973bee9257cb6df019df672576f9e13262
Brandon Butler <butlerbr@umich.edu>
2021-07-23 15:57:40 -0400

    Allow SyncedList.insert to handle values larger than the length
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- hoomd/data/syncedlist.py
+++ hoomd/data/syncedlist.py
@@ -151,7 +151,7 @@ class SyncedList(MutableSequence):
         self._attach_value(value)
         # Wrap index like normal but allow for inserting a new element to the
         # end of the list.
-        if index != len(self):
+        if index < len(self):
             index = self._handle_int(index)
         if self._synced:
             self._synced_list.insert(index,
