19e9f21fe4daca820d33a40f7e7cb34e5959f70a
Dan Evans <danevans@umich.edu>
2021-07-29 14:10:36 -0400

    Parametrize alchemostat tests for both NVT and NVE

--- hoomd/md/pytest/test_alchemostat.py
+++ hoomd/md/pytest/test_alchemostat.py
@@ -1,9 +1,24 @@
 import hoomd
 from hoomd.conftest import pickling_check
-from hoomd.md.pair.alch import LJGauss, NVT
+from hoomd.md.pair.alch import LJGauss, NVT, NVE
+import pytest
 
 
-def test_before_attaching(simulation_factory, two_particle_snapshot_factory):
+_NVE_args = (NVE, {}, {})
+
+_NVT_args = (NVT, {'kT': hoomd.variant.Constant(1)}, {'kT': hoomd.variant.Constant(0.5)})
+
+
+def get_alchemostat():
+    return [_NVE_args, _NVT_args]
+
+
+@pytest.mark.parametrize("alchemostat_cls, extra_property_1st_value, extra_property_2nd_value", get_alchemostat())
+def test_before_attaching(simulation_factory,
+                          two_particle_snapshot_factory,
+                          alchemostat_cls,
+                          extra_property_1st_value,
+                          extra_property_2nd_value):
     sim = simulation_factory(two_particle_snapshot_factory(dimensions=3, d=1))
     ljg = LJGauss(hoomd.md.nlist.Cell(), default_r_cut=3.0)
     ljg.params[('A', 'A')] = dict(epsilon=1., sigma2=0.02, r0=1.8)
@@ -15,33 +30,40 @@ def test_before_attaching(simulation_factory, two_particle_snapshot_factory):
     ar0 = ljg.alchemical_particles[('A', 'A'), 'r0']
 
     filt = hoomd.filter.All()
-    kT = hoomd.variant.Constant(1)
     time_factor = 10
 
-    anvt = NVT(filter=filt, kT=kT, time_factor=time_factor, alchemical_particles=[ar0])
+    alchemostat = alchemostat_cls(filter=filt,
+                                  time_factor=time_factor,
+                                  alchemical_particles=[ar0],
+                                  **extra_property_1st_value)
 
-    assert anvt.filter is filt
-    assert anvt.kT == kT
-    assert anvt.time_factor == time_factor
-
-    kT = hoomd.variant.Constant(0.5)
+    assert alchemostat.filter is filt
+    assert alchemostat.time_factor == time_factor
     time_factor = 5
-    anvt.kT = kT
-    anvt.time_factor = time_factor
-    assert anvt.kT == kT
-    assert anvt.time_factor == time_factor
+    alchemostat.time_factor = time_factor
+    assert alchemostat.time_factor == time_factor
 
-    assert len(anvt.alchemical_particles) == 1
-    assert anvt.alchemical_particles[0] == ar0
+    assert len(alchemostat.alchemical_particles) == 1
+    assert alchemostat.alchemical_particles[0] == ar0
 
-    anvt.alchemical_particles.remove(ar0)
-    assert len(anvt.alchemical_particles) == 0
+    alchemostat.alchemical_particles.remove(ar0)
+    assert len(alchemostat.alchemical_particles) == 0
 
-    anvt.alchemical_particles.append(ar0)
-    assert anvt.alchemical_particles[0] == ar0
+    alchemostat.alchemical_particles.append(ar0)
+    assert alchemostat.alchemical_particles[0] == ar0
+
+    for name in extra_property_1st_value.keys():
+        assert getattr(alchemostat, name) == extra_property_1st_value[name]
+        setattr(alchemostat, name, extra_property_2nd_value[name])
+        assert getattr(alchemostat, name) == extra_property_2nd_value[name]
 
 
-def test_after_attaching(simulation_factory, two_particle_snapshot_factory):
+@pytest.mark.parametrize("alchemostat_cls, extra_property_1st_value, extra_property_2nd_value", get_alchemostat())
+def test_after_attaching(simulation_factory,
+                         two_particle_snapshot_factory,
+                         alchemostat_cls,
+                         extra_property_1st_value,
+                         extra_property_2nd_value):
     sim = simulation_factory(two_particle_snapshot_factory(dimensions=3, d=1))
     ljg = LJGauss(hoomd.md.nlist.Cell(), default_r_cut=3.0)
     ljg.params[('A', 'A')] = dict(epsilon=1., sigma2=0.02, r0=1.8)
@@ -53,37 +75,44 @@ def test_after_attaching(simulation_factory, two_particle_snapshot_factory):
     ar0 = ljg.alchemical_particles[('A', 'A'), 'r0']
 
     filt = hoomd.filter.All()
-    kT = hoomd.variant.Constant(1)
     time_factor = 10
 
-    anvt = NVT(filter=filt, kT=kT, time_factor=time_factor, alchemical_particles=[ar0])
-    sim.operations.integrator.methods.insert(0, anvt)
+    alchemostat = alchemostat_cls(filter=filt,
+                                  time_factor=time_factor,
+                                  alchemical_particles=[ar0],
+                                  **extra_property_1st_value)
+    sim.operations.integrator.methods.insert(0, alchemostat)
     sim.run(0)
 
-    assert anvt.filter is filt
-    assert anvt.kT == kT
-    assert anvt.time_factor == time_factor
-
-    kT = hoomd.variant.Constant(0.5)
+    assert alchemostat.filter is filt
+    assert alchemostat.time_factor == time_factor
     time_factor = 5
-    anvt.kT = kT
-    anvt.time_factor = time_factor
-    assert anvt.kT == kT
-    assert anvt.time_factor == time_factor
+    alchemostat.time_factor = time_factor
+    assert alchemostat.time_factor == time_factor
 
-    assert len(anvt.alchemical_particles) == 1
-    assert anvt.alchemical_particles[0] == ar0
+    assert len(alchemostat.alchemical_particles) == 1
+    assert alchemostat.alchemical_particles[0] == ar0
 
-    anvt.alchemical_particles.remove(ar0)
-    assert len(anvt.alchemical_particles) == 0
+    alchemostat.alchemical_particles.remove(ar0)
+    assert len(alchemostat.alchemical_particles) == 0
 
-    anvt.alchemical_particles.append(ar0)
-    assert anvt.alchemical_particles[0] == ar0
+    alchemostat.alchemical_particles.append(ar0)
+    assert alchemostat.alchemical_particles[0] == ar0
+
+    for name in extra_property_1st_value.keys():
+        assert getattr(alchemostat, name) == extra_property_1st_value[name]
+        setattr(alchemostat, name, extra_property_2nd_value[name])
+        assert getattr(alchemostat, name) == extra_property_2nd_value[name]
 
     sim.run(10)
 
 
-def test_pickling(simulation_factory, two_particle_snapshot_factory):
+@pytest.mark.parametrize("alchemostat_cls, extra_property_1st_value, extra_property_2nd_value", get_alchemostat())
+def test_pickling(simulation_factory,
+                  two_particle_snapshot_factory,
+                  alchemostat_cls,
+                  extra_property_1st_value,
+                  extra_property_2nd_value):
     sim = simulation_factory(two_particle_snapshot_factory(dimensions=3, d=1))
     ljg = LJGauss(hoomd.md.nlist.Cell(), default_r_cut=3.0)
     ljg.params[('A', 'A')] = dict(epsilon=1., sigma2=0.02, r0=1.8)
@@ -95,11 +124,13 @@ def test_pickling(simulation_factory, two_particle_snapshot_factory):
     ar0 = ljg.alchemical_particles[('A', 'A'), 'r0']
 
     filt = hoomd.filter.All()
-    kT = hoomd.variant.Constant(1)
     time_factor = 10
 
-    anvt = NVT(filter=filt, kT=kT, time_factor=time_factor, alchemical_particles=[ar0])
-    pickling_check(anvt)
-    sim.operations.integrator.methods.insert(0, anvt)
+    alchemostat = alchemostat_cls(filter=filt,
+                                  time_factor=time_factor,
+                                  alchemical_particles=[ar0],
+                                  **extra_property_1st_value)
+    pickling_check(alchemostat)
+    sim.operations.integrator.methods.insert(0, alchemostat)
     sim.run(0)
-    pickling_check(anvt)
+    pickling_check(alchemostat)
