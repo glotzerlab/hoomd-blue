a0f5eb7127e5d124afe5f0d2eed5245209d6bf15
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2021-08-04 15:11:28 -0400

    more doc tweaks

--- hoomd/update/remove_drift.py
+++ hoomd/update/remove_drift.py
@@ -17,18 +17,18 @@ class RemoveDrift(Updater):
     Args:
         reference_positions ((*N_particles*, 3) `numpy.ndarray` of
             ``numpy.float64``): the reference positions of the
-            lattice :math:`[\\mathrm{length}]`.
+            lattice :math:`[\mathrm{length}]`.
         trigger (`hoomd.trigger.Trigger`): Select the timesteps to remove drift.
 
     During the time steps specified by *trigger*, the average drift
-    :math:`\\Delta\\vec{r}` from the *reference_positions*
-    (:math:`\\vec{r}_{ref}`) is substracted from the current particle positions.
+    :math:`\Delta\vec{r}` from the *reference_positions*
+    (:math:`\vec{r}_{ref}`) is substracted from the current particle positions.
     The drift computed by this updater is given by:
 
     .. math::
 
-        \\Delta\\vec{r} = \\frac{1}{\\mathrm{N_{particles}} \\sum_i
-            \\mathrm{min\_image}(\\vec{r}_i - \\vec{r}_{ref,i})
+        \Delta\vec{r} = \frac{1}{\mathrm{N_{particles}} \sum_i
+            \mathrm{min\_image}(\vec{r}_i - \vec{r}_{ref,i})
     """
 
     def __init__(self, reference_positions, trigger=1):
