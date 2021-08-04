fe01f03687890f67f41632df4e42afd284af390c
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2021-08-04 12:55:19 -0400

    move the remove drift updater from hpmc to the hoomd.update namespace

--- hoomd/module.cc
+++ hoomd/module.cc
@@ -5,6 +5,7 @@
 #include "Analyzer.h"
 #include "BondedGroupData.h"
 #include "BoxResizeUpdater.h"
+#include "UpdaterRemoveDrift.h"
 #include "CallbackAnalyzer.h"
 #include "CellList.h"
 #include "CellListStencil.h"
@@ -275,6 +276,7 @@ PYBIND11_MODULE(_hoomd, m)
     export_PythonUpdater(m);
     export_Integrator(m);
     export_BoxResizeUpdater(m);
+    export_UpdaterRemoveDrift(m);
 
     // tuners
     export_Tuner(m);
