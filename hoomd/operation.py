9ccb68d6396c0f2efa55676c39049a76a3c36d69
Alain Kadar <alaink@umich.edu>
2021-08-11 14:38:35 -0400

    Delete temporary print() statements

--- hoomd/operation.py
+++ hoomd/operation.py
@@ -34,7 +34,6 @@ class _HOOMDGetSetAttrBase:
     _reserved_default_attrs = dict(_param_dict=ParameterDict,
                                    _typeparam_dict=dict)
     _override_setattr = set()
-    print('class')
     def __getattr__(self, attr):
         if attr in self._reserved_default_attrs.keys():
             default = self._reserved_default_attrs[attr]
