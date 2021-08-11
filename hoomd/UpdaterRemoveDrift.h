764b4f9996fdca99b6623fed7881a56811a09743
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2021-08-11 10:21:13 -0400

    cast to Scalar

--- hoomd/UpdaterRemoveDrift.h
+++ hoomd/UpdaterRemoveDrift.h
@@ -71,7 +71,7 @@ class UpdaterRemoveDrift : public Updater
         // original reference positions.
         const auto reference_array = pybind11::array_t<double>(
             dims,
-            reinterpret_cast<const double*>(this->m_ref_positions.data()));
+            reinterpret_cast<const Scalar*>(this->m_ref_positions.data()));
         // This is necessary to expose the array in a read only fashion through C++
         reinterpret_cast<pybind11::detail::PyArray_Proxy*>(reference_array.ptr())->flags
             &= ~pybind11::detail::npy_api::NPY_ARRAY_WRITEABLE_;
