ae3ce04347d97429d938a9c642215ae9728bf97d
Brandon Butler <butlerbr@umich.edu>
2021-08-17 16:35:34 -0400

    Add type_shapes test for ALJ potential

--- hoomd/md/pytest/test_alj.py
+++ hoomd/md/pytest/test_alj.py
@@ -1,7 +1,10 @@
+import copy
+
 import numpy as np
 import pytest
 
 import hoomd
+import hoomd.conftest
 from hoomd import md
 
 
@@ -75,7 +78,7 @@ def test_conservation(simulation_factory, lattice_snapshot_factory):
     alj.shape["A"] = {
         "vertices": hexagon.vertices,
         "faces": [],
-        "rounding_radius": 0
+        "rounding_radii": 0
     }
 
     alpha = 0  # Make it WCA-only (no attraction)
@@ -114,3 +117,102 @@ def test_conservation(simulation_factory, lattice_snapshot_factory):
     # Test momentum conservation.
     velocities = np.asarray(velocities)
     assert np.std(np.linalg.norm(np.sum(velocities, axis=1), axis=-1)) < 1e-6
+
+
+def test_type_shapes(simulation_factory, two_particle_snapshot_factory):
+    pytest.importorskip("coxeter")
+    alj = md.pair.aniso.ALJ(md.nlist.Cell())
+    sim = simulation_factory(two_particle_snapshot_factory())
+    sim.operations.integrator = md.Integrator(0.005, forces=[alj])
+
+    alj.r_cut.default = 2.5
+    octahedron = [(0.5, 0, 0), (-0.5, 0, 0), (0, 0.5, 0), (0, -0.5, 0),
+                  (0, 0, 0.5), (0, 0, -0.5)]
+    faces = md.pair.aniso.ALJ.get_ordered_vertices(octahedron)[1]
+    rounding_radius = 0.1
+    alj.shape["A"] = {
+        "vertices": octahedron,
+        "faces": faces,
+        "rounding_radii": rounding_radius
+    }
+    # We use a non-zero sigma_i to ensure that it is added appropriately to the
+    # rounding radius.
+    alj.params[("A", "A")] = {
+        "epsilon": 1.0,
+        "sigma_i": 0.1,
+        "sigma_j": 0.1,
+        "alpha": 1
+    }
+    with pytest.raises(hoomd.error.DataAccessError):
+        alj.type_shapes
+
+    def get_rounding_radius(base, param_spec):
+        modification = param_spec["sigma_i"] * param_spec["contact_ratio_i"]
+        return rounding_radius + modification / 2
+
+    sim.run(0)
+    shape_spec = alj.type_shapes
+    assert len(shape_spec) == 1
+    shape_spec = shape_spec[0]
+    assert shape_spec["type"] == "ConvexPolyhedron"
+    assert np.allclose(shape_spec["vertices"], octahedron)
+    assert np.isclose(
+        shape_spec["rounding_radius"],
+        get_rounding_radius(rounding_radius, alj.params[("A", "A")]))
+
+    ellipse_axes = (0.1, 0.2, 0.3)
+    alj.shape["A"] = {
+        "vertices": [],
+        "faces": [],
+        "rounding_radii": ellipse_axes
+    }
+    shape_spec = alj.type_shapes
+    assert len(shape_spec) == 1
+    shape_spec = shape_spec[0]
+    assert shape_spec["type"] == "Ellipsoid"
+    assert np.isclose(
+        shape_spec["a"],
+        get_rounding_radius(ellipse_axes[0], alj.params[("A", "A")]))
+    assert np.isclose(
+        shape_spec["a"],
+        get_rounding_radius(ellipse_axes[1], alj.params[("A", "A")]))
+    assert np.isclose(
+        shape_spec["a"],
+        get_rounding_radius(ellipse_axes[2], alj.params[("A", "A")]))
+
+    alj = copy.deepcopy(alj)
+
+    sim = simulation_factory(two_particle_snapshot_factory(dimensions=2))
+    sim.operations.integrator = md.Integrator(0.005, forces=[alj])
+    square = [(0.5, 0, 0), (-0.5, 0, 0), (-0.5, -0.5, 0), (0.5, 0.5, 0)]
+    alj.shape["A"] = {
+        "vertices": square,
+        "faces": [],
+        "rounding_radii": rounding_radius
+    }
+
+    sim.run(0)
+    shape_spec = alj.type_shapes
+    assert len(shape_spec) == 1
+    shape_spec = shape_spec[0]
+    assert shape_spec["type"] == "Polygon"
+    assert np.allclose(shape_spec["vertices"], np.array(square)[:, :2])
+    assert np.isclose(
+        shape_spec["rounding_radius"],
+        get_rounding_radius(rounding_radius, alj.params[("A", "A")]))
+
+    alj.shape["A"] = {
+        "vertices": [],
+        "faces": [],
+        "rounding_radii": ellipse_axes
+    }
+    shape_spec = alj.type_shapes
+    assert len(shape_spec) == 1
+    shape_spec = shape_spec[0]
+    assert shape_spec["type"] == "Ellipsoid"
+    assert np.isclose(
+        shape_spec["a"],
+        get_rounding_radius(ellipse_axes[0], alj.params[("A", "A")]))
+    assert np.isclose(
+        shape_spec["a"],
+        get_rounding_radius(ellipse_axes[1], alj.params[("A", "A")]))
