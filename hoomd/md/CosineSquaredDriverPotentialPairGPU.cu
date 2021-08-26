f13a9aa4cab476149c9422741be0bdef83505640
Youri Ran <youriran@hotmail.com>
2021-08-26 15:38:24 +0100

    Fixed r_cut in pytest

---
+++ hoomd/md/CosineSquaredDriverPotentialPairGPU.cu
@@ -0,0 +1,12 @@
+/*! \file CosineSquaredDriverPotentialPairGPU.cu
+    \brief Defines the driver functions for computing all types of pair forces
+    on the GPU
+*/
+
+#include "AllDriverPotentialPairGPU.cuh"
+#include "EvaluatorPairCosineSquared.h"
+hipError_t gpu_compute_cosinesquared_forces(const pair_args_t& pair_args,
+                                    const EvaluatorPairCosineSquared::param_type *d_params)
+    {
+    return gpu_compute_pair_forces<EvaluatorPairCosineSquared>(pair_args, d_params);
+    }
