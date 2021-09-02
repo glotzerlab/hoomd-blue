47e3448272a3f016c02f7de5fb98a17a5891653f
Joshua A. Anderson <joaander@umich.edu>
2021-09-02 16:26:18 -0400

    Fix failing tests
    OverdampedViscous needed to be updated to work with another PR that
    was merged.

--- hoomd/md/methods/methods.py
+++ hoomd/md/methods/methods.py
@@ -1203,7 +1203,7 @@ class OverdampedViscous(Method):
 
         OverdampedViscous uses RNGs. Warn the user if they did not set the seed.
         """
-        if simulation is not None:
+        if isinstance(simulation, hoomd.Simulation):
             simulation._warn_if_seed_unset()
 
         super()._add(simulation)
