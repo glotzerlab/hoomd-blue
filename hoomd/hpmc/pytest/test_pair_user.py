2636334ac4fcc09b4368bc1ef67ab0caff7ecfd5
Tim Moore <mtimc@umich.edu>
2021-09-08 12:37:12 -0400

    Update hoomd.hpmc.pair.user tests
    The main changes here are updating to match the current API. I also
    added an LLVM IR to use in the tests. It is the IR generatef for a
    "return 0" pair code.
    
    All of the tests in test_pair_user are now passing.

--- hoomd/hpmc/pytest/test_pair_user.py
+++ hoomd/hpmc/pytest/test_pair_user.py
@@ -11,25 +11,54 @@ import numpy as np
 # check if llvm_enabled
 llvm_disabled = not hoomd.version.llvm_enabled
 
+# this is the IR generated for a pair kernel that is just "return 0;"
+return0_ir = """
+; ModuleID = 'patch-code.cc'
+source_filename = "patch-code.cc"
+target datalayout = "e-m:o-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx11.0.0"
+
+%struct.vec3 = type { float, float, float }
+%struct.quat = type { float, %struct.vec3 }
+
+@param_array = local_unnamed_addr global float* null, align 8
+@alpha_union = local_unnamed_addr global float* null, align 8
+
+; Function Attrs: norecurse nounwind readnone ssp uwtable
+define float @eval(%struct.vec3* nocapture nonnull readnone align 4 dereferenceable(12) %0, i32 %1, %struct.quat* nocapture nonnull readnone align 4 dereferenceable(16) %2, float %3, float %4, i32 %5, %struct.quat* nocapture nonnull readnone align 4 dereferenceable(16) %6, float %7, float %8) local_unnamed_addr #0 {
+  ret float 0.000000e+00
+}
+
+attributes #0 = { norecurse nounwind readnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "frame-pointer"="all" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+cx8,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1, !2}
+!llvm.ident = !{!3}
+
+!0 = !{i32 2, !"SDK Version", [2 x i32] [i32 11, i32 3]}
+!1 = !{i32 1, !"wchar_size", i32 4}
+!2 = !{i32 7, !"PIC Level", i32 2}
+!3 = !{!"clang version 11.1.0"}
+"""
+
 valid_constructor_args = [
     dict(r_cut=3,
          param_array=[0, 1],
          code='return -1;',
-         llvm_ir="code.ll",
+         llvm_ir=return0_ir,
          clang_exec='/usr/bin/clang'),
     dict(r_cut=2, param_array=[1, 2, 3, 4], code='return -1;'),
 ]
 
 # setable attributes before attach for CPPPotential objects
 valid_attrs = [('r_cut', 1.4), ('code', 'return -1;'),
-               ('llvm_ir', 'code.ll'), ('clang_exec', 'clang')]
+               ('llvm_ir', return0_ir), ('clang_exec', 'clang')]
 
 # setable attributes after attach for CPPPotential objects
 valid_attrs_after_attach = [('r_cut', 1.3)]
 
 # attributes that cannot be set after object is attached
-attr_error = [('array_size', 1.1), ('code', 'return -1.0;'),
-              ('llvm_ir', 'test.ll'), ('clang_exec', '/usr/bin/clang')]
+attr_error = [('code', 'return -1.0;'),
+              ('clang_exec', '/usr/bin/clang')]
 
 positions_orientations_result = [
     ([(0, 0, 0), (1, 0, 0)], [(1, 0, 0, 0), (1, 0, 0, 0)], -1),
@@ -38,6 +67,7 @@ positions_orientations_result = [
     ([(0, 0, 0), (0, 0, 1)], [(1, 0, 0, 0), (0, 0, 1, 0)], -1 / 2),
 ]
 
+attr_translator = {'code': '_code', 'llvm_ir': '_cpu_llvm_ir'}
 
 @pytest.mark.serial
 @pytest.mark.parametrize("constructor_args", valid_constructor_args)
@@ -47,11 +77,8 @@ def test_valid_construction_cpp_potential(device, constructor_args):
 
     # validate the params were set properly
     for attr, value in constructor_args.items():
-        if attr == 'code':
-            attr = '_code'
-        elif attr == 'llvm_ir':
-            attr = '_cpu_llvm_ir'
-        elif attr == 'clang_exec':
+        attr = attr_translator.get(attr, attr)
+        if attr == 'clang_exec':  # not saved as a class variable
             continue
         try:
             assert getattr(patch, attr) == value
@@ -69,26 +96,32 @@ def test_valid_construction_and_attach_cpp_potential(
     # create objects
     patch = hoomd.hpmc.pair.user.CPPPotential(**constructor_args)
     mc = hoomd.hpmc.integrate.Sphere()
-    mc.shape['A'] = dict(diameter=0)
+    mc.shape['A'] = dict(diameter=1)
+    mc.potential = patch
 
     # create simulation & attach objects
     sim = simulation_factory(two_particle_snapshot_factory())
     sim.operations.integrator = mc
-    sim.operations += patch
 
     # create C++ mirror classes and set parameters
     sim.run(0)
 
     # validate the params were set properly
     for attr, value in constructor_args.items():
-        assert getattr(patch, attr) == value
+        attr = attr_translator.get(attr, attr)
+        if attr == 'clang_exec':
+            continue
+        try:
+            assert getattr(patch, attr) == value
+        except ValueError:  # array-like
+            assert all(getattr(patch, attr) == value)
 
 
 @pytest.mark.serial
 @pytest.mark.parametrize("attr,value", valid_attrs)
 def test_valid_setattr_cpp_potential(device, attr, value):
     """Test that CPPPotential can get and set attributes before attached."""
-    patch = hoomd.hpmc.pair.user.CPPPotential(r_cut=2)
+    patch = hoomd.hpmc.pair.user.CPPPotential(r_cut=2, code='return 0;')
 
     setattr(patch, attr, value)
     assert getattr(patch, attr) == value
@@ -105,11 +138,11 @@ def test_valid_setattr_attached_cpp_potential(device, attr, value,
     patch = hoomd.hpmc.pair.user.CPPPotential(r_cut=2, code='return -1;')
     mc = hoomd.hpmc.integrate.Sphere()
     mc.shape['A'] = dict(diameter=0)
+    mc.potential = patch
 
     # create simulation & attach objects
     sim = simulation_factory(two_particle_snapshot_factory())
     sim.operations.integrator = mc
-    sim.operations += patch
 
     # create C++ mirror classes and set parameters
     sim.run(0)
@@ -130,15 +163,16 @@ def test_raise_attr_error_cpp_potential(device, attr, val, simulation_factory,
 
     patch = hoomd.hpmc.pair.user.CPPPotential(r_cut=2,
                                               code='return 0;',
-                                              llvm_ir='code.ll')
-
+                                              llvm_ir=return0_ir)
     mc = hoomd.hpmc.integrate.Sphere()
     mc.shape['A'] = dict(diameter=0)
+    mc.potential = patch
+
     # create simulation & attach objects
     sim = simulation_factory(two_particle_snapshot_factory())
     sim.operations.integrator = mc
-    sim.operations += patch
     sim.run(0)
+
     # try to reset when attached
     with pytest.raises(AttributeError):
         setattr(patch, attr, val)
@@ -182,9 +216,9 @@ def test_cpp_potential(device, positions, orientations, result,
                                               code=dipole_dipole.format(r_cut))
     mc = hoomd.hpmc.integrate.Sphere()
     mc.shape['A'] = dict(diameter=0)
+    mc.potential = patch
 
     sim.operations.integrator = mc
-    sim.operations += patch
     with sim.state.cpu_local_snapshot as data:
         data.particles.position[0, :] = positions[0]
         data.particles.position[1, :] = positions[1]
@@ -198,20 +232,20 @@ def test_cpp_potential(device, positions, orientations, result,
 @pytest.mark.serial
 @pytest.mark.parametrize("cls", [hoomd.hpmc.pair.user.CPPPotential])
 @pytest.mark.skipif(llvm_disabled, reason='LLVM not enabled')
-def test_alpha_iso(device, cls, simulation_factory,
+def test_param_array(device, cls, simulation_factory,
                    two_particle_snapshot_factory):
-    """Test that: i) changes to the alpha_iso array reflect on the energy
+    """Test that: i) changes to the param_array reflect on the energy
        caltulation, ii) that it can be accessed from CPPPotential and CPPUnionPotential
        objects and iii) that the energy computed from both classes agree.
     """
 
     lennard_jones = """
                      float rsq = dot(r_ij, r_ij);
-                     float rcut  = alpha_iso[0];
+                     float rcut  = param_array[0];
                      if (rsq <= rcut*rcut)
                          {{
-                         float sigma = alpha_iso[1];
-                         float eps   = alpha_iso[2];
+                         float sigma = param_array[1];
+                         float eps   = param_array[2];
                          float sigmasq = sigma*sigma;
                          float rsqinv = sigmasq / rsq;
                          float r6inv = rsqinv*rsqinv*rsqinv;
@@ -226,7 +260,7 @@ def test_alpha_iso(device, cls, simulation_factory,
     sim = simulation_factory(two_particle_snapshot_factory())
 
     r_cut = 5
-    params = dict(code=lennard_jones, array_size=3, r_cut=r_cut)
+    params = dict(code=lennard_jones, param_array=[2.5, 1.2, 1.0], r_cut=r_cut)
     if "union" in cls.__name__.lower():
         params.update({"r_cut_union": 0})
         params.update({"code_union": "return 0;"})
@@ -239,9 +273,9 @@ def test_alpha_iso(device, cls, simulation_factory,
         patch.charges['A'] = [0]
     mc = hoomd.hpmc.integrate.Sphere()
     mc.shape['A'] = dict(diameter=0)
+    mc.potential = patch
 
     sim.operations.integrator = mc
-    sim.operations += patch
 
     dist = 1
     with sim.state.cpu_local_snapshot as data:
@@ -250,22 +284,22 @@ def test_alpha_iso(device, cls, simulation_factory,
 
     sim.run(0)
     # set alpha to sensible LJ values: [rcut, sigma, epsilon]
-    patch.alpha_iso[0] = 2.5
-    patch.alpha_iso[1] = 1.2
-    patch.alpha_iso[2] = 1
+    patch.param_array[0] = 2.5
+    patch.param_array[1] = 1.2
+    patch.param_array[2] = 1
     energy_old = patch.energy
 
     # make sure energies are calculated properly when using alpha
-    sigma_r_6 = (patch.alpha_iso[1] / dist)**6
-    energy_actual = 4.0 * patch.alpha_iso[2] * sigma_r_6 * (sigma_r_6 - 1.0)
+    sigma_r_6 = (patch.param_array[1] / dist)**6
+    energy_actual = 4.0 * patch.param_array[2] * sigma_r_6 * (sigma_r_6 - 1.0)
     assert np.isclose(energy_old, energy_actual)
 
     # double epsilon and check that energy is doubled
-    patch.alpha_iso[2] = 2
+    patch.param_array[2] = 2
     sim.run(0)
     assert np.isclose(patch.energy, 2.0 * energy_old)
 
     # set r_cut to zero and check energy is zero
-    patch.alpha_iso[0] = 0
+    patch.param_array[0] = 0
     sim.run(0)
     assert np.isclose(patch.energy, 0.0)
