ad686d44b5117398cb92fa678a02eb82e2d951f0
Tim Moore <mtimc@umich.edu>
2021-10-04 14:52:56 -0400

    Mark longer-running test as validate

--- hoomd/hpmc/pytest/test_pair_union_user.py
+++ hoomd/hpmc/pytest/test_pair_union_user.py
@@ -305,6 +305,7 @@ def test_param_array_union_cpu(device, simulation_factory,
 
 
 @pytest.mark.gpu
+@pytest.mark.validate
 @pytest.mark.skipif(llvm_disabled, reason='LLVM not enabled')
 def test_param_array_union_gpu(device, simulation_factory,
                            two_particle_snapshot_factory):
