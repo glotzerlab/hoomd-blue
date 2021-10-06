2976fd7e0ea3f035b65d54e750630e816e8653ec
Tim Moore <mtimc@umich.edu>
2021-10-06 12:08:20 -0400

    Fix formatting

--- hoomd/hpmc/tune/move_size.py
+++ hoomd/hpmc/tune/move_size.py
@@ -320,10 +320,10 @@ class MoveSize(_InternalCustomTuner):
         acceptance ratio. Otherwise, the *global* acceptance ratio, a weighted
         average of the acceptance ratios for each individual particle type, will
         approach the target value, while the per-type acceptance ratios may not
-        be close to the target value. This requires setting the ``types`` attribute
-        to be one type at a time while setting the ``ignore_statistics`` flag of
-        the shape property of the HPMC integrator for all other types to
-        ``True``.
+        be close to the target value. This requires setting the ``types``
+        attribute to be one type at a time while setting the
+        ``ignore_statistics`` flag of the shape property of the HPMC integrator
+        for all other types to ``True``.
 
     """
     _internal_class = _InternalMoveSize
