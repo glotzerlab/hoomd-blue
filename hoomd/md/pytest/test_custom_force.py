1cd305ccff751d4ba249414d41cb0fdf83e12cf0
Tommy Waltmann <tomwalt@umich.edu>
2021-10-06 18:07:15 -0400

    fix virial calc in example class

--- hoomd/md/pytest/test_custom_force.py
+++ hoomd/md/pytest/test_custom_force.py
@@ -10,12 +10,21 @@ class MyConstantForce(md.force.Custom):
         super().__init__()
         self._mag = magnitude
         self._direction = np.array([1, 0, 0])
+        self._force = self._direction * self._mag
 
     def set_forces(self, timestep):
         with self._state.cpu_local_snapshot as snap, self.cpu_local_force_arrays as arrays:
             rtags = snap.particles.rtag
             position = snap.particles.position[rtags]
-            arrays.force[rtags] = self._direction * self._mag
+            arrays.force[rtags] = self._force[None, :]
             arrays.potential_energy[rtags] = -self._mag * position[rtags][0]
-            arrays.torque[rtags] = np.cross(position, self.forces[rtags])
-            arrays.virial[rtags] = ...
+            arrays.torque[rtags] = np.cross(position, arrays.force[rtags])
+
+            # set the virial stress coefficients
+            arrays.virial[rtags][:, 0] = force[0] * position[:, 0]
+            arrays.virial[rtags][:, 1] = force[0] * position[:, 1]
+            arrays.virial[rtags][:, 2] = force[0] * position[:, 2]
+            arrays.virial[rtags][:, 3] = force[1] * position[:, 0]
+            arrays.virial[rtags][:, 4] = force[1] * position[:, 1]
+            arrays.virial[rtags][:, 5] = force[2] * position[:, 2]
+
