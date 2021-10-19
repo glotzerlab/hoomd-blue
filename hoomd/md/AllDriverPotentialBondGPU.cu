940cb50b7cb30824c463f5ebe230ba4a39d71dde
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2021-10-19 18:39:04 -0400

    Mesh Bond Potentials

--- hoomd/md/AllDriverPotentialBondGPU.cu
+++ hoomd/md/AllDriverPotentialBondGPU.cu
@@ -32,3 +32,24 @@ hipError_t gpu_compute_tether_forces(const bond_args_t& bond_args,
     {
     return gpu_compute_bond_forces<EvaluatorBondTether>(bond_args, d_params, d_flags);
     }
+
+hipError_t gpu_compute_harmonic_forces(const mesh_bond_args_t& mesh_bond_args,
+                                       const harmonic_params* d_params,
+                                       unsigned int* d_flags)
+    {
+    return gpu_compute_mesh_bond_forces<EvaluatorBondHarmonic>(mesh_bond_args, d_params, d_flags);
+    }
+
+hipError_t gpu_compute_fene_forces(const mesh_bond_args_t& mesh_bond_args,
+                                   const fene_params* d_params,
+                                   unsigned int* d_flags)
+    {
+    return gpu_compute_mesh_bond_forces<EvaluatorBondFENE>(mesh_bond_args, d_params, d_flags);
+    }
+
+hipError_t gpu_compute_tether_forces(const mesh_bond_args_t& mesh_bond_args,
+                                     const tether_params* d_params,
+                                     unsigned int* d_flags)
+    {
+    return gpu_compute_mesh_bond_forces<EvaluatorBondTether>(mesh_bond_args, d_params, d_flags);
+    }
---
+++ hoomd/md/PotentialMeshBondGPU.h
@@ -0,0 +1,202 @@
+// Copyright (c) 2009-2021 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
+
+// Maintainer: joaander
+
+#ifndef __POTENTIAL_MESH_BOND_GPU_H__
+#define __POTENTIAL_MESH_BOND_GPU_H__
+
+#ifdef ENABLE_HIP
+
+#include "PotentialMeshBond.h"
+#include "PotentialMeshBondGPU.cuh"
+#include "hoomd/Autotuner.h"
+
+/*! \file PotentialMeshBondGPU.h
+    \brief Defines the template class for standard mesh_bond potentials on the GPU
+    \note This header cannot be compiled by nvcc
+*/
+
+#ifdef __HIPCC__
+#error This header cannot be compiled by nvcc
+#endif
+
+//! Template class for computing mesh_bond potentials on the GPU
+
+/*!
+    \tparam evaluator EvaluatorBond class used to evaluate V(r) and F(r)/r
+    \tparam gpu_cgbf Driver function that calls gpu_compute_mesh_bond_forces<evaluator>()
+
+    \sa export_PotentialMeshBondGPU()
+*/
+template<class evaluator,
+         hipError_t gpu_cgbf(const mesh_bond_args_t& mesh_bond_args,
+                             const typename evaluator::param_type* d_params,
+                             unsigned int* d_flags)>
+class PotentialMeshBondGPU : public PotentialMeshBond<evaluator>
+    {
+    public:
+    //! Construct the mesh_bond potential
+    PotentialMeshBondGPU(std::shared_ptr<SystemDefinition> sysdef, std::shared_ptr<MeshDefinition> meshdef);
+    //! Destructor
+    virtual ~PotentialMeshBondGPU() { }
+
+    //! Set autotuner parameters
+    /*! \param enable Enable/disable autotuning
+        \param period period (approximate) in time steps when returning occurs
+    */
+    virtual void setAutotunerParams(bool enable, unsigned int period)
+        {
+        PotentialMeshBond<evaluator>::setAutotunerParams(enable, period);
+        m_tuner->setPeriod(period);
+        m_tuner->setEnabled(enable);
+        }
+
+    protected:
+    std::unique_ptr<Autotuner> m_tuner; //!< Autotuner for block size
+    GPUArray<unsigned int> m_flags;     //!< Flags set during the kernel execution
+
+    //! Actually compute the forces
+    virtual void computeForces(uint64_t timestep);
+    };
+
+template<class evaluator,
+         hipError_t gpu_cgbf(const mesh_bond_args_t& mesh_bond_args,
+                             const typename evaluator::param_type* d_params,
+                             unsigned int* d_flags)>
+PotentialMeshBondGPU<evaluator, gpu_cgbf>::PotentialMeshBondGPU(std::shared_ptr<SystemDefinition> sysdef, std::shared_ptr<MeshDefinition> meshdef)
+    : PotentialMeshBond<evaluator>(sysdef, meshdef)
+    {
+    // can't run on the GPU if there aren't any GPUs in the execution configuration
+    if (!this->m_exec_conf->isCUDAEnabled())
+        {
+        this->m_exec_conf->msg->error()
+            << "Creating a PotentialMeshBondGPU with no GPU in the execution configuration"
+            << std::endl;
+        throw std::runtime_error("Error initializing PotentialMeshBondGPU");
+        }
+
+    // allocate and zero device memory
+    GPUArray<typename evaluator::param_type> params(this->m_mesh_bond_data->getNTypes(),
+                                                    this->m_exec_conf);
+    this->m_params.swap(params);
+
+    // allocate flags storage on the GPU
+    GPUArray<unsigned int> flags(1, this->m_exec_conf);
+    m_flags.swap(flags);
+
+    // reset flags
+    ArrayHandle<unsigned int> h_flags(m_flags, access_location::host, access_mode::overwrite);
+    h_flags.data[0] = 0;
+
+    unsigned int warp_size = this->m_exec_conf->dev_prop.warpSize;
+    m_tuner.reset(
+        new Autotuner(warp_size, 1024, warp_size, 5, 100000, "harmonic_mesh_bond", this->m_exec_conf));
+    }
+
+template<class evaluator,
+         hipError_t gpu_cgbf(const mesh_bond_args_t& mesh_bond_args,
+                             const typename evaluator::param_type* d_params,
+                             unsigned int* d_flags)>
+void PotentialMeshBondGPU<evaluator, gpu_cgbf>::computeForces(uint64_t timestep)
+    {
+    // start the profile
+    if (this->m_prof)
+        this->m_prof->push(this->m_exec_conf, this->m_prof_name);
+
+    // access the particle data
+    ArrayHandle<Scalar4> d_pos(this->m_pdata->getPositions(),
+                               access_location::device,
+                               access_mode::read);
+    ArrayHandle<Scalar> d_diameter(this->m_pdata->getDiameters(),
+                                   access_location::device,
+                                   access_mode::read);
+    ArrayHandle<Scalar> d_charge(this->m_pdata->getCharges(),
+                                 access_location::device,
+                                 access_mode::read);
+
+    // we are using the minimum image of the global box here
+    // to ensure that ghosts are always correctly wrapped (even if a mesh_bond exceeds half the domain
+    // length)
+    BoxDim box = this->m_pdata->getGlobalBox();
+
+    // access parameters
+    ArrayHandle<typename evaluator::param_type> d_params(this->m_params,
+                                                         access_location::device,
+                                                         access_mode::read);
+
+    // access net force & virial
+    ArrayHandle<Scalar4> d_force(this->m_force, access_location::device, access_mode::readwrite);
+    ArrayHandle<Scalar> d_virial(this->m_virial, access_location::device, access_mode::readwrite);
+
+        {
+        const GPUArray<typename MeshBondData::members_t>& gpu_mesh_bond_list
+            = this->m_mesh_bond_data->getGPUTable();
+        const Index2D& gpu_table_indexer = this->m_mesh_bond_data->getGPUTableIndexer();
+
+        ArrayHandle<typename MeshBondData::members_t> d_gpu_mesh_bondlist(gpu_mesh_bond_list,
+                                                                 access_location::device,
+                                                                 access_mode::read);
+        ArrayHandle<unsigned int> d_gpu_n_mesh_bonds(this->m_mesh_bond_data->getNGroupsArray(),
+                                                access_location::device,
+                                                access_mode::read);
+
+        // access the flags array for overwriting
+        ArrayHandle<unsigned int> d_flags(m_flags, access_location::device, access_mode::readwrite);
+
+        this->m_tuner->begin();
+        gpu_cgbf(mesh_bond_args_t(d_force.data,
+                             d_virial.data,
+                             this->m_virial.getPitch(),
+                             this->m_pdata->getN(),
+                             this->m_pdata->getMaxN(),
+                             d_pos.data,
+                             d_charge.data,
+                             d_diameter.data,
+                             box,
+                             d_gpu_mesh_bondlist.data,
+                             gpu_table_indexer,
+                             d_gpu_n_mesh_bonds.data,
+                             this->m_mesh_bond_data->getNTypes(),
+                             this->m_tuner->getParam()),
+                 d_params.data,
+                 d_flags.data);
+        }
+
+    if (this->m_exec_conf->isCUDAErrorCheckingEnabled())
+        {
+        CHECK_CUDA_ERROR();
+
+        // check the flags for any errors
+        ArrayHandle<unsigned int> h_flags(m_flags, access_location::host, access_mode::read);
+
+        if (h_flags.data[0] & 1)
+            {
+            this->m_exec_conf->msg->error()
+                << "mesh_bond." << evaluator::getName() << ": mesh_bond out of bounds (" << h_flags.data[0]
+                << ")" << std::endl
+                << std::endl;
+            throw std::runtime_error("Error in mesh_bond calculation");
+            }
+        }
+    this->m_tuner->end();
+
+    if (this->m_prof)
+        this->m_prof->pop(this->m_exec_conf);
+    }
+
+//! Export this mesh_bond potential to python
+/*! \param name Name of the class in the exported python module
+    \tparam T Class type to export. \b Must be an instantiated PotentialPairGPU class template.
+    \tparam Base Base class of \a T. \b Must be PotentialPair<evaluator> with the same evaluator as
+   used in \a T.
+*/
+template<class T, class Base>
+void export_PotentialMeshBondGPU(pybind11::module& m, const std::string& name)
+    {
+    pybind11::class_<T, Base, std::shared_ptr<T>>(m, name.c_str())
+        .def(pybind11::init<std::shared_ptr<SystemDefinition>,std::shared_ptr<MeshDefinition> >());
+    }
+
+#endif // ENABLE_HIP
+#endif // __POTENTIAL_MESH_PAIR_GPU_H__
