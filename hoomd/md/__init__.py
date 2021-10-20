076e319fcc1badf72044306863fe44afcc79026b
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2021-10-20 18:57:10 -0400

    add mesh.bond python class

--- hoomd/md/__init__.py
+++ hoomd/md/__init__.py
@@ -25,4 +25,5 @@ from hoomd.md import update
 from hoomd.md import wall
 from hoomd.md import special_pair
 from hoomd.md import methods
+from hoomd.md import mesh
 from hoomd.md import many_body
---
+++ hoomd/md/mesh/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2009-2021 The Regents of the University of Michigan
+# This file is part of the HOOMD-blue project, released under the BSD 3-Clause
+# License.
+
+"""Mesh potentials for molecular dynamics."""
+
+from .bond import (Harmonic, FENE, Tether)
