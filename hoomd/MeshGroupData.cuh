fcf057275aed41e50eedb30a19b4b1f5ff8d331a
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2021-10-24 00:17:58 -0400

    add mesh gpu table functiones

---
+++ hoomd/MeshGroupData.cuh
@@ -0,0 +1,69 @@
+// Copyright (c) 2009-2021 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
+
+// Maintainer: jglaser
+
+/*! \file BondedGroupData.cuh
+    \brief Defines the helper functions (GPU version) for updating the GPU bonded group tables
+ */
+
+#include "CachedAllocator.h"
+#include "HOOMDMath.h"
+
+#ifndef __MESH_GROUP_DATA_CUH__
+#define __MESH_GROUP_DATA_CUH__
+
+#ifdef __HIPCC__
+//! Sentinel value
+const unsigned int GROUP_NOT_LOCAL = 0xffffffff;
+
+//! Storage for group members (GPU declaration)
+template<unsigned int group_size> union group_storage {
+    unsigned int tag[group_size]; // access 'tags'
+    unsigned int idx[group_size]; // access 'indices'
+    };
+
+//! A union to allow storing a Scalar constraint value or a type integer (GPU declaration)
+union typeval_union {
+    unsigned int type;
+    Scalar val;
+    };
+
+//! Packed group entry for communication (GPU declaration)
+template<unsigned int group_size> struct packed_storage
+    {
+    group_storage<group_size> tags;  //!< Member tags
+    typeval_union typeval;           //!< Type or constraint value of bonded group
+    unsigned int group_tag;          //!< Tag of this group
+    group_storage<group_size> ranks; //!< Current list of member ranks
+    };
+
+#else
+//! Forward declaration of group_storage
+template<unsigned int group_size> union group_storage;
+
+//! Forward declaration of packed_storage
+template<unsigned int group_size> struct packed_storage;
+
+//! Forward declaration of typeval union
+union typeval_union;
+#endif
+
+template<unsigned int group_size, typename group_t>
+void gpu_update_mesh_table(const unsigned int n_groups,
+                            const unsigned int N,
+                            const group_t* d_group_table,
+                            const typeval_union* d_group_typeval,
+                            const unsigned int* d_rtag,
+                            unsigned int* d_n_groups,
+                            unsigned int max_n_groups,
+                            unsigned int* d_condition,
+                            unsigned int next_flag,
+                            unsigned int& flag,
+                            group_t* d_pidx_group_table,
+                            const unsigned int pidx_group_table_pitch,
+                            unsigned int* d_scratch_g,
+                            unsigned int* d_scratch_idx,
+                            unsigned int* d_offsets,
+                            CachedAllocator& alloc);
+#endif // __BONDED_GROUP_DATA_CUH__
