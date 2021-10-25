783d850b1ba8f242f91d138bc3adf2e4d7a0251c
Brandon Butler <butlerbr@umich.edu>
2021-10-25 16:33:18 -0400

    Rework potential params for wall potential tests
    Fixes errors and removes module variable for test specific ones

--- hoomd/md/pytest/test_wall_potential.py
+++ hoomd/md/pytest/test_wall_potential.py
@@ -85,50 +85,57 @@ def test_wall_setting(cls):
     assert len(wall_pot.walls) == 5
 
 
-_params = (
-    {
-        "sigma": 1.0,
-        "epsilon": 1.0
-    },
-    {
-        "sigma": 1.0,
-        "epsilon": 5.5
-    },
-    {
-        "sigma": 1.0,
-        "epsilon": 1.0,
-        "delta": 0.1
-    },
-    {
-        "kappa": 1.0,
-        "epsilon": 1.5
-    },
-    {
-        "r0": 1.0,
-        "D0": 1.0,
-        "alpha": 1.0
-    },
-    {
-        "sigma": 1.0,
-        "epsilon": 1.0
-    },
-    {
-        "sigma": 1.0,
-        "epsilon": 1.0,
-        "m": 10,
-        "n": 20
-    },
-)
+def _params(r_cut=None, r_extrap=None):
+    base = (
+        {
+            "sigma": 1.0,
+            "epsilon": 1.0
+        },
+        {
+            "sigma": 1.0,
+            "epsilon": 5.5
+        },
+        {
+            "sigma": 1.0,
+            "epsilon": 1.0,
+            "delta": 0.1
+        },
+        {
+            "kappa": 1.0,
+            "epsilon": 1.5
+        },
+        {
+            "r0": 1.0,
+            "D0": 1.0,
+            "alpha": 1.0
+        },
+        {
+            "sigma": 1.0,
+            "epsilon": 1.0
+        },
+        {
+            "sigma": 1.0,
+            "epsilon": 1.0,
+            "m": 10,
+            "n": 20
+        },
+    )
+    for p in base:
+        if r_cut is None:
+            p["r_cut"] = _params.rng.random() * 5
+        else:
+            p["r_cut"] = r_cut
+        if r_extrap is None:
+            p["r_extrap"] = _params.rng.random() * 5
+        else:
+            p["r_extrap"] = r_extrap
+    return base
 
-rng = np.random.default_rng(26456)
-for p in _params:
-    p.update({
-        "r_cut": rng.random() * 5.0,
-        "r_extrap": rng.choice([0.0, rng.random() * 5.0]),
-    })
+
+_params.rng = np.random.default_rng(26456)
 
 
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params()))
 def test_params(cls, params):
     wall_pot = cls(WallGenerator.generate_n(2))
     wall_pot.params["A"] = params
@@ -140,7 +147,7 @@ def test_params(cls, params):
         assert wall_pot.params["foo"][attr] == params[attr]
 
 
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params()))
 def test_attaching(simulation, cls, params):
     wall_pot = cls(WallGenerator.generate_n(2))
     simulation.operations.integrator.forces.append(wall_pot)
@@ -152,11 +159,7 @@ def test_attaching(simulation, cls, params):
         assert np.isclose(wall_pot.params["A"][attr], params[attr])
 
 
-for param in _params:
-    param["r_cut"] = 2.5
-
-
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params(2.5, 0.0)))
 def test_plane(simulation, cls, params):
     wall_pot = cls([
         hoomd.wall.Plane(normal=(0, 0, -1), origin=(0, 0, 1), inside=True),
@@ -171,7 +174,7 @@ def test_plane(simulation, cls, params):
             assert np.all(snap.particles.position[:, 2] > -1)
 
 
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params(2.5, 0.0)))
 def test_sphere(simulation, cls, params):
     radius = 5
     wall_pot = cls(
@@ -185,7 +188,7 @@ def test_sphere(simulation, cls, params):
                 np.linalg.norm(snap.particles.position, axis=1) < radius)
 
 
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params(2.5, 0.0)))
 def test_cylinder(simulation, cls, params):
     radius = 5
     wall_pot = cls([
@@ -203,28 +206,7 @@ def test_cylinder(simulation, cls, params):
                 np.linalg.norm(snap.particles.position[:, :2], axis=1) < radius)
 
 
-for param in _params:
-    param["r_extrap"] = 2.5
-
-
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
-def test_r_extrap(simulation, cls, params):
-    radius = 5.0
-    wall_pot = cls(
-        [hoomd.wall.Sphere(radius=radius, origin=(0, 0, 0), inside=False)])
-    simulation.operations.integrator.forces.append(wall_pot)
-    wall_pot.params["A"] = params
-    snap = simulation.state.get_snapshot()
-    if simulation.device.communicator.rank == 0:
-        snap.particles.position[:] = [[0, 0, 4.8], [0, 0, -4.8]]
-    simulation.state.set_snapshot(snap)
-
-    simulation.run(0)
-    assert np.all(wall_pot.energy != 0)
-    assert np.all(np.any(wall_pot.forces != 0, axis=1))
-
-
-@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params))
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params(2.5, 0.0)))
 def test_outside(simulation, cls, params):
     radius = 5.0
     wall_pot = cls(
@@ -241,3 +223,20 @@ def test_outside(simulation, cls, params):
         with simulation.state.cpu_local_snapshot as snap:
             assert np.all(
                 np.linalg.norm(snap.particles.position, axis=1) > radius)
+
+
+@pytest.mark.parametrize("cls, params", zip(_potential_cls, _params(2.5, 1.1)))
+def test_r_extrap(simulation, cls, params):
+    radius = 5.0
+    wall_pot = cls(
+        [hoomd.wall.Sphere(radius=radius, origin=(0, 0, 0), inside=False)])
+    simulation.operations.integrator.forces.append(wall_pot)
+    wall_pot.params["A"] = params
+    snap = simulation.state.get_snapshot()
+    if simulation.device.communicator.rank == 0:
+        snap.particles.position[:] = [[0, 0, 4.8], [0, 0, -4.8]]
+    simulation.state.set_snapshot(snap)
+
+    simulation.run(0)
+    assert np.all(wall_pot.energy != 0)
+    assert np.all(np.any(wall_pot.forces != 0, axis=1))
