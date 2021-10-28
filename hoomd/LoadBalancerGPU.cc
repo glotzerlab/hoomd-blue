0a88102ea19c30f15df031d7a04c80ed709f3b4c
Brandon Butler <butlerbr@umich.edu>
2021-10-28 13:51:33 -0400

    Add guard to ambiguous conditional.

--- hoomd/LoadBalancerGPU.cc
+++ hoomd/LoadBalancerGPU.cc
@@ -48,7 +48,9 @@ void LoadBalancerGPU::countParticlesOffRank(std::map<unsigned int, unsigned int>
     {
     // do nothing if rank doesn't own any particles
     if (m_pdata->getN() == 0)
+        {
         return;
+        }
 
         // mark the current ranks of each particle (hijack the comm flags array)
         {
