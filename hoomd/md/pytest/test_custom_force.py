657b784b99439968e94bf696739740242749167e
Tommy Waltmann <tomwalt@umich.edu>
2021-11-10 17:34:34 -0500

    make test on gpu work with cupy

--- hoomd/md/pytest/test_custom_force.py
+++ hoomd/md/pytest/test_custom_force.py
@@ -2,6 +2,12 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 
+try:
+    import cupy
+    CUPY_IMPORTED = True
+except:
+    CUPY_IMPORTED = False
+
 import hoomd
 from hoomd import md
 
@@ -41,6 +47,12 @@ def _skip_if_gpu_with_cpu_device(force_cls, sim):
         pytest.skip("Cannot access gpu force arrays with CPU device")
 
 
+def _skip_if_cupy_not_imported_and_gpu_device(sim):
+    gpu_device = isinstance(sim.device, hoomd.device.GPU)
+    if gpu_device and not CUPY_IMPORTED:
+        pytest.skip("Cannot run this test on GPU without cupy")
+
+
 @pytest.mark.parametrize("force_cls", [MyForceCPU, MyForceGPU], ids=lambda x: x.__name__)
 def test_simulation(force_cls, simulation_factory, two_particle_snapshot_factory):
     """Make sure custom force can plug into simulation without crashing."""
@@ -70,10 +82,17 @@ class MyPeriodicFieldCPU(md.force.Custom):
         self._p = p
         self._w = w
 
+    def _numpy_array(self, arr):
+        """If arr is hoomd array change it to numpy."""
+        if arr.__class__.__name__ == 'HOOMDGPUArray':
+            return arr.get()
+        else:
+            return arr
+
     def _evaluate_periodic(self, snapshot):
         """Evaluate force and energy in python."""
         box = snapshot.global_box
-        positions = snapshot.particles.position
+        positions = self._numpy_array(snapshot.particles.position)
         a1, a2, a3 = box.lattice_vectors
         V = np.dot(a1, np.cross(a2, a3))
         b1 = 2 * np.pi / V * np.cross(a2, a3)
@@ -105,8 +124,8 @@ class MyPeriodicFieldGPU(MyPeriodicFieldCPU):
         with self._state.gpu_local_snapshot as snap, \
                 self.gpu_local_force_arrays as arrays:
             forces, potential = self._evaluate_periodic(snap)
-            arrays.force[:] = forces
-            arrays.potential_energy[:] = potential
+            arrays.force[:] = cupy.array(forces)
+            arrays.potential_energy[:] = cupy.array(potential)
 
 
 
@@ -126,6 +145,7 @@ def test_compare_to_periodic(force_cls, simulation_factory, two_particle_snapsho
     snap2 = two_particle_snapshot_factory()
     sim2 = simulation_factory(snap)
     _skip_if_gpu_with_cpu_device(force_cls, sim2)
+    _skip_if_cupy_not_imported_and_gpu_device(sim2)
     periodic2 = force_cls(A=1, i=0, p=1, w=1)
     nvt2 = md.methods.NVT(hoomd.filter.All(), kT=1, tau=1)
     integrator2 = md.Integrator(dt=0.005, forces=[periodic2], methods=[nvt2])
