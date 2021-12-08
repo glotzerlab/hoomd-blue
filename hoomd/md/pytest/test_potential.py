bc692c4ab1e4115cfbe74bad25f4abb6dad99139
Corwin Kerr <cbkerr@umich.edu>
2021-12-08 11:34:55 -0500

    add copy of slj arg dict

--- hoomd/md/pytest/test_potential.py
+++ hoomd/md/pytest/test_potential.py
@@ -506,6 +506,14 @@ def _valid_params(particle_types=['A', 'B']):
     valid_params_list.append(
         paramtuple(md.pair.SLJ, dict(zip(combos, slj_valid_param_dicts)), {}))
 
+    slj_arg_dict = {
+        'sigma': [0.5, 1.0, 1.5],
+        'epsilon': [0.0005, 0.001, 0.0015]
+    }
+    slj_valid_param_dicts = _make_valid_param_dicts(slj_arg_dict)
+    valid_params_list.append(
+        paramtuple(md.pair.SLJ, dict(zip(combos, slj_valid_param_dicts)), {}))
+
     dpd_arg_dict = {'A': [0.5, 1.0, 1.5], 'gamma': [0.0005, 0.001, 0.0015]}
     dpd_valid_param_dicts = _make_valid_param_dicts(dpd_arg_dict)
     valid_params_list.append(
