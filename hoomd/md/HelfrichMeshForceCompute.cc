397519b5dc72a46bf02341a587fb2688475bc86d
Philipp Schönhöfer <philipp@philipps-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-12-14 13:02:06 +0100

    add sigmas to force loop

--- hoomd/md/HelfrichMeshForceCompute.cc
+++ hoomd/md/HelfrichMeshForceCompute.cc
@@ -113,6 +113,8 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
     if (m_prof)
         m_prof->push("Harmonic Angle");
 
+    computeSigma();// precompute sigmas
+
     assert(m_pdata);
     // access the particle data arrays
     ArrayHandle<Scalar4> h_pos(m_pdata->getPositions(), access_location::host, access_mode::read);
@@ -130,6 +132,9 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
                                                    access_location::host,
                                                    access_mode::read);
 
+    ArrayHandle<Scalar> h_sigma(m_sigma, access_location::host, access_mode::read);
+    ArrayHandle<Scalar3> h_sigma_hat(m_sigma_hat, access_location::host, access_mode::read);
+
     // there are enough other checks on the input data: but it doesn't hurt to be safe
     assert(h_force.data);
     assert(h_virial.data);
@@ -137,6 +142,8 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
     assert(h_rtag.data);
     assert(h_bonds.data);
     assert(h_triangles.data);
+    assert(h_sigma.data);
+    assert(h_sigma_hat.data);
 
     // Zero data for force calculation.
     memset((void*)h_force.data, 0, sizeof(Scalar4) * m_force.getNumElements());
@@ -145,8 +152,12 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
     // get a local copy of the simulation box too
     const BoxDim& box = m_pdata->getGlobalBox();
 
+    PDataFlags flags = m_pdata->getFlags();
+    bool compute_virial = flags[pdata_flag::pressure_tensor];
 
-    computeSigma(h_pos, h_rtag, h_bonds, h_triangles);
+    Scalar helfrich_virial[6];
+    for (unsigned int i = 0; i < 6; i++)
+        helfrich_virial[i] = Scalar(0.0);
 
     // for each of the angles
     const unsigned int size = (unsigned int)m_mesh_data->getMeshBondData()->getN();
@@ -330,15 +341,15 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
 
 	Scalar sigma_hat_ab = (cot_accb + cot_addb)/2;
 
-	Scalar sigma_hat_a; //precomputed
-	Scalar sigma_hat_b; //precomputed
-	Scalar sigma_hat_c; //precomputed
-	Scalar sigma_hat_d; //precomputed
+	Scalar3 sigma_hat_a = h_sigma_hat[idx_a]; //precomputed
+	Scalar3 sigma_hat_b = h_sigma_hat[idx_b]; //precomputed
+	Scalar3 sigma_hat_c = h_sigma_hat[idx_c]; //precomputed
+	Scalar3 sigma_hat_d = h_sigma_hat[idx_d]; //precomputed
 
-	Scalar sigma_a; //precomputed
-	Scalar sigma_b; //precomputed
-	Scalar sigma_c; //precomputed
-	Scalar sigma_d; //precomputed
+	Scalar sigma_a = h_sigma[idx_a]; //precomputed
+	Scalar sigma_b = h_sigma[idx_b]; //precomputed
+	Scalar sigma_c = h_sigma[idx_c]; //precomputed
+	Scalar sigma_d = h_sigma[idx_d]; //precomputed
 
 	Scalar3 dc_abbc, dc_abbd, dc_baac, dc_baad;
 	dc_abbc = -nbc/rab + c_abbc/rab*nab;
@@ -364,78 +375,45 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
 	Scalar dsigma_hat_d = -dot(dsigma_hat_ad,dad) - dot(dsigma_hat_bd,dbd);
 
 	Scalar3 Fa;
-	Fa = kappa*(dsigma_hat_a/sigma_a*sigma_hat_a -dot(sigma_hat_a,sigma_hat_a)/(2*sigma_a*sigma_a)*dsigma_a);
-	Fa += kappa*(dsigma_hat_b/sigma_b*sigma_hat_b -dot(sigma_hat_b,sigma_hat_b)/(2*sigma_b*sigma_b)*dsigma_b);
-	Fa += kappa*(dsigma_hat_c/sigma_c*sigma_hat_c -dot(sigma_hat_c,sigma_hat_c)/(2*sigma_c*sigma_c)*dsigma_c);
-	Fa += kappa*(dsigma_hat_d/sigma_d*sigma_hat_d -dot(sigma_hat_d,sigma_hat_d)/(2*sigma_d*sigma_d)*dsigma_d);
+	Fa = m_K[0]*(dsigma_hat_a/sigma_a*sigma_hat_a -dot(sigma_hat_a,sigma_hat_a)/(2*sigma_a*sigma_a)*dsigma_a);
+	Fa += m_K[0]*(dsigma_hat_b/sigma_b*sigma_hat_b -dot(sigma_hat_b,sigma_hat_b)/(2*sigma_b*sigma_b)*dsigma_b);
+	Fa += m_K[0]*(dsigma_hat_c/sigma_c*sigma_hat_c -dot(sigma_hat_c,sigma_hat_c)/(2*sigma_c*sigma_c)*dsigma_c);
+	Fa += m_K[0]*(dsigma_hat_d/sigma_d*sigma_hat_d -dot(sigma_hat_d,sigma_hat_d)/(2*sigma_d*sigma_d)*dsigma_d);
 
-	Fb = -Fa;
+        Scalar angle_eng = (tk * dth) * Scalar(1.0 / 4.0);
+
+        if (compute_virial)
+            {
+            helfrich_virial[0] = Scalar(1. / 2.) * dab.x * Fa.x;// xx
+            helfrich_virial[1] = Scalar(1. / 2.) * dab.y * Fa.x;// xy
+            helfrich_virial[2] = Scalar(1. / 2.) * dab.z * Fa.x;// xz
+            helfrich_virial[3] = Scalar(1. / 2.) * dab.y * Fa.y;// yy
+            helfrich_virial[4] = Scalar(1. / 2.) * dab.z * Fa.y;// yz
+            helfrich_virial[5] = Scalar(1. / 2.) * dab.z * Fa.z;// zz
+            }
 
 
-
-        // actually calculate the force
-        unsigned int angle_type = m_angle_data->getTypeByIndex(i);
-        Scalar dth = acos(c_abbc) - m_t_0[angle_type];
-        Scalar tk = m_K[angle_type] * dth;
-
-        Scalar a = -1.0 * tk * s_abbc;
-        Scalar a11 = a * c_abbc / rsqab;
-        Scalar a12 = -a / (rab * rcb);
-        Scalar a22 = a * c_abbc / rsqcb;
-
-        Scalar fab[3], fcb[3];
-
-        fab[0] = a11 * dab.x + a12 * dcb.x;
-        fab[1] = a11 * dab.y + a12 * dcb.y;
-        fab[2] = a11 * dab.z + a12 * dcb.z;
-
-        fcb[0] = a22 * dcb.x + a12 * dab.x;
-        fcb[1] = a22 * dcb.y + a12 * dab.y;
-        fcb[2] = a22 * dcb.z + a12 * dab.z;
-
-        // compute 1/3 of the energy, 1/3 for each atom in the angle
-        Scalar angle_eng = (tk * dth) * Scalar(1.0 / 6.0);
-
-        // compute 1/3 of the virial, 1/3 for each atom in the angle
-        // upper triangular version of virial tensor
-        Scalar angle_virial[6];
-        angle_virial[0] = Scalar(1. / 3.) * (dab.x * fab[0] + dcb.x * fcb[0]);
-        angle_virial[1] = Scalar(1. / 3.) * (dab.y * fab[0] + dcb.y * fcb[0]);
-        angle_virial[2] = Scalar(1. / 3.) * (dab.z * fab[0] + dcb.z * fcb[0]);
-        angle_virial[3] = Scalar(1. / 3.) * (dab.y * fab[1] + dcb.y * fcb[1]);
-        angle_virial[4] = Scalar(1. / 3.) * (dab.z * fab[1] + dcb.z * fcb[1]);
-        angle_virial[5] = Scalar(1. / 3.) * (dab.z * fab[2] + dcb.z * fcb[2]);
 
         // Now, apply the force to each individual atom a,b,c, and accumulate the energy/virial
         // do not update ghost particles
         if (idx_a < m_pdata->getN())
             {
-            h_force.data[idx_a].x += fab[0];
-            h_force.data[idx_a].y += fab[1];
-            h_force.data[idx_a].z += fab[2];
-            h_force.data[idx_a].w += angle_eng;
+            h_force.data[idx_a].x += Fa.x;
+            h_force.data[idx_a].y += Fa.y;
+            h_force.data[idx_a].z += Fa.z;
+            h_force.data[idx_a].w = m_K[0]/2.0*dot(sigma_hat_a,sigma_hat_a)/simga_a;
             for (int j = 0; j < 6; j++)
-                h_virial.data[j * virial_pitch + idx_a] += angle_virial[j];
+                h_virial.data[j * virial_pitch + idx_a] += helfrich_virial[j];
             }
 
         if (idx_b < m_pdata->getN())
             {
-            h_force.data[idx_b].x -= fab[0] + fcb[0];
-            h_force.data[idx_b].y -= fab[1] + fcb[1];
-            h_force.data[idx_b].z -= fab[2] + fcb[2];
-            h_force.data[idx_b].w += angle_eng;
+            h_force.data[idx_b].x -= Fa.x;
+            h_force.data[idx_b].y -= Fa.y;
+            h_force.data[idx_b].z -= Fa.z;
+            h_force.data[idx_b].w = m_K[0]/2.0*dot(sigma_hat_a,sigma_hat_a)/simga_a;
             for (int j = 0; j < 6; j++)
-                h_virial.data[j * virial_pitch + idx_b] += angle_virial[j];
-            }
-
-        if (idx_c < m_pdata->getN())
-            {
-            h_force.data[idx_c].x += fcb[0];
-            h_force.data[idx_c].y += fcb[1];
-            h_force.data[idx_c].z += fcb[2];
-            h_force.data[idx_c].w += angle_eng;
-            for (int j = 0; j < 6; j++)
-                h_virial.data[j * virial_pitch + idx_c] += angle_virial[j];
+                h_virial.data[j * virial_pitch + idx_b] += helfrich_virial[j];
             }
         }
 
@@ -443,14 +421,25 @@ void HelfrichMeshForceCompute::computeForces(uint64_t timestep)
         m_prof->pop();
     }
 
-void HelfrichMeshForceCompute::computeSigma(ArrayHandle<Scalar4> h_pos, ArrayHandle<unsigned int> h_rtag, ArrayHandle<typename MeshBond::members_t> h_bonds, ArrayHandle<typename MeshTriangle::members_t> h_triangles)
+void HelfrichMeshForceCompute::computeSigma()
     {
+
+    ArrayHandle<Scalar4> h_pos(m_pdata->getPositions(), access_location::host, access_mode::read);
+
+    ArrayHandle<unsigned int> h_rtag(m_pdata->getRTags(), access_location::host, access_mode::read);
+
+    ArrayHandle<typename MeshBond::members_t> h_bonds(m_mesh_data->getMeshBondData()->getMembersArray(),
+                                                   access_location::host,
+                                                   access_mode::read);
+    ArrayHandle<typename MeshTriangle::members_t> h_triangles(m_mesh_data->getMeshTriangleData()->getMembersArray(),
+                                                   access_location::host,
+                                                   access_mode::read);
 
     // get a local copy of the simulation box too
     const BoxDim& box = m_pdata->getGlobalBox();
 
-    ArrayHandle<Scalar> h_sigma(m_sigma, access_location::host, access_mode::redwrite);
-    ArrayHandle<Scalar3> h_sigma_hat(m_sigma_hat, access_location::host, access_mode::redwrite);
+    ArrayHandle<Scalar> h_sigma(m_sigma, access_location::host, access_mode::readwrite);
+    ArrayHandle<Scalar3> h_sigma_hat(m_sigma_hat, access_location::host, access_mode::readwrite);
 
     memset((void*)h_sigma.data, 0, sizeof(Scalar) * m_sigma.getNumElements());
     memset((void*)h_sigma_hat.data, 0, sizeof(Scalar3) * m_sigma_hat.getNumElements());
