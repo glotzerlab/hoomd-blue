a99eea107de2290fc4ec2a4f1eeacd95fc5f7370
Philipp Schönhöfer <philipp@philipps-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-12-14 16:28:26 +0100

    adding Helfrich API to mesh.bond module

--- hoomd/md/mesh/bond.py
+++ hoomd/md/mesh/bond.py
@@ -246,3 +246,36 @@ class Tether(MeshBond):
         self._add_typeparam(params)
 
         super().__init__(mesh)
+
+
+class Helfrich(MeshBond):
+    r"""Helfrich bending potential.
+
+    :py:class:`Helfrich` specifies a Helfrich bending energy applied to
+    all particles within the mesh.
+
+    Args:
+        mesh (:py:mod:`hoomd.mesh.Mesh`): Mesh data structure constraint.
+
+    Attributes:
+        parameter (TypeParameter[dict]):
+            The parameter of the Helfrich energy for the defined mesh.
+            As the mesh can only have one type a type name does not have
+            to be stated. The dictionary has the following keys:
+
+            * ``k`` (`float`, **required**) - bending stiffness
+              :math:`[\mathrm{energy}]`
+
+    Examples::
+
+        helfrich_potential = mesh.bond.Helfrich(mesh)
+        helfrich_potential.parameter = dict(k=10.0)
+    """
+    _cpp_class_name = "HelfrichMeshForceCompute"
+
+    def __init__(self, mesh):
+        params = TypeParameter("params", "types",
+                               TypeParameterDict(k=float, len_keys=1))
+        self._add_typeparam(params)
+
+        super().__init__(mesh)
