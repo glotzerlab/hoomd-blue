445ab3e78039e2caf90073e0f059d422ef9f5822
Tim Moore <mtimc@umich.edu>
2021-12-14 15:08:58 -0500

    Add test to make sure particles don't move too far

--- hoomd/hpmc/pytest/test_external.py
+++ hoomd/hpmc/pytest/test_external.py
@@ -127,7 +127,45 @@ def test_lattice_displacement_energy(device, simulation_factory,
     disp = np.array([dx, 0, 0])
     lattice.reference_positions = lattice.reference_positions + disp
 
-    # create C++ mirror classes and set parameters
+    # run and check energy
     sim.run(0)
     assert np.allclose(lattice.energy,
                        0.5 * dx**2 * k_trans * sim.state.N_particles)
+
+
+@pytest.mark.cpu
+def test_lattice_displacement(device, simulation_factory,
+                                     two_particle_snapshot_factory):
+    """Ensure particles remain close to lattice sites."""
+    mc = hoomd.hpmc.integrate.Sphere()
+    particle_diameter = 0.5
+    mc.shape['A'] = dict(diameter=particle_diameter)
+
+    # create simulation & attach objects
+    sim = simulation_factory(two_particle_snapshot_factory())
+    sim.operations.integrator = mc
+
+    # create lattice field
+    k_trans = 100.0
+    snapshot = sim.state.get_snapshot()
+    if device.communicator.rank == 0:
+        reference_positions = snapshot.particles.position
+        reference_orientations = snapshot.particles.orientation
+    else:
+        reference_positions = [[0, 0, 0], [0, 0, 0]]
+        reference_orientations = [[1, 0, 0, 0], [1, 0, 0, 0]]
+    lattice = hoomd.hpmc.field.LatticeField(
+        reference_positions=reference_positions,
+        reference_orientations=reference_orientations,
+        k_translational=k_trans,
+        k_rotational=1.0,
+        symmetries=[[1, 0, 0, 0]])
+    mc.external_potential = lattice
+
+    # run and check that particles haven't moved farther than half a diameter
+    sim.run(100)
+    snapshot = sim.state.get_snapshot()
+    if device.communicator.rank == 0:
+        new_positions = snapshot.particles.position
+        dx = np.linalg.norm(new_positions - reference_positions, axis=1)
+        assert np.all(np.less(dx, particle_diameter/2))
