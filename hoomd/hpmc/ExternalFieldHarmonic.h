d9fbc5434b2d1e0c39043cb92ea7efa3a3c9cb64
Tim Moore <mtimc@umich.edu>
2022-01-17 16:03:16 -0500

    Refactor loop
    Based on review comment.

--- hoomd/hpmc/ExternalFieldHarmonic.h
+++ hoomd/hpmc/ExternalFieldHarmonic.h
@@ -78,7 +78,7 @@ template<class Shape> class ExternalFieldHarmonic : public ExternalFieldMono<Sha
                 throw std::runtime_error("The array must be of shape (N_particles, 3).");
                 }
             const double* rawdata = static_cast<const double*>(ref_pos.data());
-            for (size_t i = 0; i < std::min<unsigned long>(N_particles, m_pdata->getNGlobal()); i++)
+            for (size_t i = 0; i < N_particles; i++)
                 {
                 const size_t array_index = i * 3;
                 this->m_reference_positions[i] = vec3<Scalar>(rawdata[array_index],
