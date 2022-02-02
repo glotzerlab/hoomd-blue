0310e30d73f788ed9ec0485ef289a2ff9ff28020
Joshua A. Anderson <joaander@umich.edu>
2022-02-02 18:25:11 -0500

    Add unit test for special pairs.

--- hoomd/md/pytest/test_special_pair.py
+++ hoomd/md/pytest/test_special_pair.py
@@ -6,18 +6,30 @@ import pytest
 import numpy
 
 R = 0.9
+CHARGE = [-2.5, 2.5]
 
-# test parameters include the class, special pair params, r_cut, force, and energy
-special_pair_test_parameters = [(
-    hoomd.md.special_pair.LJ,
-    dict(epsilon=1.5, sigma=0.5),
-    2.5,
-    0.0,
-    4 * 1.5 * ((0.5 / R)**12 - (0.5 - R)**6),
-)]
+# test parameters include the class, special pair params, r_cut, force, and
+# energy
+special_pair_test_parameters = [
+    (
+        hoomd.md.special_pair.LJ,
+        dict(epsilon=1.5, sigma=0.5),
+        2.5,
+        24 * 0.5**6 * 1.5 * (R**6 - 2 * 0.5**6) / R**13,
+        4 * 1.5 * ((0.5 / R)**12 - (0.5 / R)**6),
+    ),
+    (
+        hoomd.md.special_pair.Coulomb,
+        dict(alpha=1.5),
+        2.5,
+        -1.5 * CHARGE[0] * CHARGE[1] / R**2,
+        1.5 * CHARGE[0] * CHARGE[1] / R,
+    ),
+]
 
 
-@pytest.mark.parametrize("special_pair_cls, params, r_cut, force, energy", special_pair_test_parameters)
+@pytest.mark.parametrize("special_pair_cls, params, r_cut, force, energy",
+                         special_pair_test_parameters)
 def test_before_attaching(special_pair_cls, params, r_cut, force, energy):
     potential = special_pair_cls()
     potential.params['A-A'] = params
@@ -32,6 +44,7 @@ def snapshot_factory(two_particle_snapshot_factory):
     def make_snapshot():
         snapshot = two_particle_snapshot_factory(d=R, L=R * 10)
         if snapshot.communicator.rank == 0:
+            snapshot.particles.charge[:] = CHARGE
             snapshot.pairs.N = 1
             snapshot.pairs.types = ['A-A']
             snapshot.pairs.typeid[0] = 0
@@ -42,7 +55,8 @@ def snapshot_factory(two_particle_snapshot_factory):
     return make_snapshot
 
 
-@pytest.mark.parametrize("special_pair_cls, params, r_cut, force, energy", special_pair_test_parameters)
+@pytest.mark.parametrize("special_pair_cls, params, r_cut, force, energy",
+                         special_pair_test_parameters)
 def test_after_attaching(snapshot_factory, simulation_factory, special_pair_cls,
                          params, r_cut, force, energy):
     snapshot = snapshot_factory()
@@ -64,45 +78,33 @@ def test_after_attaching(snapshot_factory, simulation_factory, special_pair_cls,
         assert potential.params['A-A'][key] == pytest.approx(params[key])
 
 
-# @pytest.mark.parametrize("bond_cls, potential_kwargs, force, energy",
-#                          get_bond_args_forces_and_energies())
-# def test_forces_and_energies(two_particle_snapshot_factory, simulation_factory,
-#                              bond_cls, potential_kwargs, force, energy):
-#     snap = two_particle_snapshot_factory(d=0.969, L=5)
-#     if snap.communicator.rank == 0:
-#         snap.bonds.N = 1
-#         snap.bonds.types = ['bond']
-#         snap.bonds.typeid[0] = 0
-#         snap.bonds.group[0] = (0, 1)
-#         snap.particles.diameter[0] = 0.5
-#         snap.particles.diameter[1] = 0.5
-#     sim = simulation_factory(snap)
+@pytest.mark.parametrize("special_pair_cls, params, r_cut, force, energy",
+                         special_pair_test_parameters)
+def test_forces_and_energies(snapshot_factory, simulation_factory,
+                             special_pair_cls, params, r_cut, force, energy):
+    snapshot = snapshot_factory()
+    sim = simulation_factory(snapshot)
 
-#     bond_potential = bond_cls()
-#     bond_potential.params['bond'] = potential_kwargs
+    potential = special_pair_cls()
+    potential.params['A-A'] = params
+    potential.r_cut['A-A'] = r_cut
 
-#     integrator = hoomd.md.Integrator(dt=0.005)
+    integrator = hoomd.md.Integrator(dt=0.005)
+    integrator.forces.append(potential)
 
-#     integrator.forces.append(bond_potential)
+    langevin = hoomd.md.methods.Langevin(kT=1, filter=hoomd.filter.All())
+    integrator.methods.append(langevin)
+    sim.operations.integrator = integrator
 
-#     langevin = hoomd.md.methods.Langevin(kT=1,
-#                                          filter=hoomd.filter.All(),
-#                                          alpha=0.1)
-#     integrator.methods.append(langevin)
-#     sim.operations.integrator = integrator
+    sim.run(0)
 
-#     sim.run(0)
-
-#     sim_energies = sim.operations.integrator.forces[0].energies
-#     sim_forces = sim.operations.integrator.forces[0].forces
-#     if sim.device.communicator.rank == 0:
-#         np.testing.assert_allclose(sum(sim_energies),
-#                                    energy,
-#                                    rtol=1e-2,
-#                                    atol=1e-5)
-#         np.testing.assert_allclose(sim_forces[0], [force, 0.0, 0.0],
-#                                    rtol=1e-2,
-#                                    atol=1e-5)
-#         np.testing.assert_allclose(sim_forces[1], [-1 * force, 0.0, 0.0],
-#                                    rtol=1e-2,
-#                                    atol=1e-5)
+    sim_energies = potential.energies
+    sim_forces = potential.forces
+    if sim.device.communicator.rank == 0:
+        assert sum(sim_energies) == pytest.approx(energy)
+        numpy.testing.assert_allclose(sim_forces[0], [force, 0.0, 0.0],
+                                      rtol=1e-6,
+                                      atol=1e-5)
+        numpy.testing.assert_allclose(sim_forces[1], [-1 * force, 0.0, 0.0],
+                                      rtol=1e-6,
+                                      atol=1e-5)
