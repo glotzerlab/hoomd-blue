ada6bea56f32c1f95ffe63f5900d95e737ad8297
Joshua A. Anderson <joaander@umich.edu>
2022-02-04 07:16:26 -0500

    Add Table bond test.

--- hoomd/md/pytest/test_bond.py
+++ hoomd/md/pytest/test_bond.py
@@ -5,68 +5,86 @@ import hoomd
 import pytest
 import numpy as np
 
-# Test parameters include the class, bond params, force, and energy.
+# Test parameters include the class, class keyword arguments, bond params,
+# force, and energy.
 bond_test_parameters = [
     (
         hoomd.md.bond.Harmonic,
+        dict(),
         dict(k=30.0, r0=1.6),
         -18.9300,
         5.9724,
     ),
     (
         hoomd.md.bond.Harmonic,
+        dict(),
         dict(k=25.0, r0=1.7),
         -18.2750,
         6.6795,
     ),
     (
         hoomd.md.bond.Harmonic,
+        dict(),
         dict(k=20.0, r0=1.8),
         -16.6200,
         6.9056,
     ),
     (
         hoomd.md.bond.FENEWCA,
+        dict(),
         dict(k=30.0, r0=1.6, epsilon=0.9, sigma=1.1, delta=-0.5),
         282.296,
         70.5638,
     ),
     (
         hoomd.md.bond.FENEWCA,
+        dict(),
         dict(k=25.0, r0=1.7, epsilon=1.0, sigma=1.0, delta=-0.5),
         146.288,
         49.2476,
     ),
     (
         hoomd.md.bond.FENEWCA,
+        dict(),
         dict(k=20.0, r0=1.8, epsilon=1.1, sigma=0.9, delta=-0.5),
         88.8238,
         35.3135,
     ),
     (
         hoomd.md.bond.Tether,
+        dict(),
         dict(k_b=5.0, l_min=0.7, l_c1=0.9, l_c0=1.1, l_max=1.3),
         0,
         0,
     ),
     (
         hoomd.md.bond.Tether,
+        dict(),
         dict(k_b=6.0, l_min=0.8, l_c1=1.05, l_c0=1.1, l_max=1.3),
         -0.0244441,
         0.000154384,
     ),
     (
         hoomd.md.bond.Tether,
+        dict(),
         dict(k_b=7.0, l_min=0.9, l_c1=1.1, l_c0=1.3, l_max=1.5),
         -3.57225,
         0.0490934,
     ),
+    (
+        hoomd.md.bond.Table,
+        dict(width=2),
+        dict(r_min=0, r_max=1.0, V=[0, 10], F=[0, -20]),
+        19.38,
+        9.69,
+    ),
 ]
 
 
-@pytest.mark.parametrize('bond_cls, params, force, energy', bond_test_parameters)
-def test_before_attaching(bond_cls, params, force, energy):
-    potential = bond_cls()
+@pytest.mark.parametrize('bond_cls, bond_args, params, force, energy',
+                         bond_test_parameters)
+def test_before_attaching(bond_cls, bond_args, params, force, energy):
+    potential = bond_cls(**bond_args)
     potential.params['A-A'] = params
     for key in params:
         assert potential.params['A-A'][key] == pytest.approx(params[key])
@@ -88,28 +106,34 @@ def snapshot_factory(two_particle_snapshot_factory):
     return make_snapshot
 
 
-@pytest.mark.parametrize('bond_cls, params, force, energy', bond_test_parameters)
-def test_after_attaching(snapshot_factory, simulation_factory, bond_cls, params, force, energy):
+@pytest.mark.parametrize('bond_cls, bond_args, params, force, energy',
+                         bond_test_parameters)
+def test_after_attaching(snapshot_factory, simulation_factory, bond_cls,
+                         bond_args, params, force, energy):
     sim = simulation_factory(snapshot_factory())
 
-    potential = bond_cls()
+    potential = bond_cls(**bond_args)
     potential.params['A-A'] = params
 
-    sim.operations.integrator = hoomd.md.Integrator(dt=0.005, forces=[potential])
+    sim.operations.integrator = hoomd.md.Integrator(dt=0.005,
+                                                    forces=[potential])
     sim.run(0)
 
     for key in params:
         assert potential.params['A-A'][key] == pytest.approx(params[key])
 
 
-@pytest.mark.parametrize('bond_cls, params, force, energy', bond_test_parameters)
-def test_forces_and_energies(snapshot_factory, simulation_factory, bond_cls, params, force, energy):
+@pytest.mark.parametrize('bond_cls, bond_args, params, force, energy',
+                         bond_test_parameters)
+def test_forces_and_energies(snapshot_factory, simulation_factory, bond_cls,
+                             bond_args, params, force, energy):
     sim = simulation_factory(snapshot_factory())
 
-    potential = bond_cls()
+    potential = bond_cls(**bond_args)
     potential.params['A-A'] = params
 
-    sim.operations.integrator = hoomd.md.Integrator(dt=0.005, forces=[potential])
+    sim.operations.integrator = hoomd.md.Integrator(dt=0.005,
+                                                    forces=[potential])
 
     sim.run(0)
 
