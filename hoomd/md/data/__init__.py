a04a9ae3bd665664a142fcd521f1534db9676c23
Joshua A. Anderson <joaander@umich.edu>
2022-02-25 10:26:44 -0500

    Update md/data

--- hoomd/md/data/__init__.py
+++ hoomd/md/data/__init__.py
@@ -1,7 +1,16 @@
 # Copyright (c) 2009-2022 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
-"""Force data local access."""
+"""Force data local access.
+
+`ForceLocalAccess`, `ForceLocalAccessGPU`, and related classes provide direct
+access to the data buffers managed by `hoomd.md.force.Force`.
+
+See Also:
+    `hoomd.md.force.Force`
+
+    `hoomd.md.force.Custom`
+"""
 
 from .local_access import _ForceLocalAccessBase
 from .local_access_cpu import ForceLocalAccess
