test shape utils and fix ellipsoid moi calculation

hoomd/hpmc/pytest/test_shape_utils.py
@@ -3,172 +3,93 @@
 
 import numpy as np
 import pytest
-import coxeter
-from plato import geometry
-from pyhull.convex_hull import ConvexHull
 import hoomd
-from hoomd import hpmc
-from copy import deepcopy
+import hoomd.hpmc
+from hoomd.hpmc._hpmc import PolyhedronVertices, EllipsoidParams
+from hoomd.hpmc._hpmc import MassPropertiesConvexPolyhedron, MassPropertiesEllipsoid
+
+shape_list = [
+    # cube
+    ("polyhedron", {"vertices": [[-1,-1,-1],
+                                 [-1,-1,1],
+                                 [-1,1,1],
+                                 [-1,1,-1],
+                                 [1,-1,-1],
+                                 [1,-1,1],
+                                 [1,1,1],
+                                 [1,1,-1]],
+                                 "sweep_radius": 0, "ignore_statistics": True}),
+    # deformed cubed
+    ("polyhedron", {"vertices": [[-1.00010558, -1.05498028, -1.02785711],
+                                 [-0.60306277, -0.94357813,  1.15216173],
+                                 [-0.93980368,  1.10805945,  1.07469946],
+                                 [-0.92443732,  0.98195736, -1.46118865],
+                                 [ 1.228552  , -1.30713086, -1.1727504 ],
+                                 [ 1.20330899, -0.79320722,  0.83510155],
+                                 [ 1.00378097,  0.92333129,  0.93916291],
+                                 [ 1.1994583 ,  0.97454523, -1.29517718]],
+                                 "sweep_radius": 0, "ignore_statistics": True}),
+    # truncated tetrahedron with truncation  = 0.4
+    ("polyhedron", {"vertices": [[-1.,  -0.6,  0.6],
+                                 [-1.,   0.6, -0.6],
+                                 [-0.6, -1.,   0.6],
+                                 [-0.6, -0.6,  1. ],
+                                 [-0.6,  0.6, -1. ],
+                                 [-0.6,  1.,  -0.6],
+                                 [ 0.6, -1.,  -0.6],
+                                 [ 0.6, -0.6, -1. ],
+                                 [ 0.6,  0.6,  1. ],
+                                 [ 0.6,  1.,   0.6],
+                                 [ 1.,  -0.6, -0.6],
+                                 [ 1.,   0.6,  0.6]],
+                                 "sweep_radius": 0, "ignore_statistics": True}),
+    # deformed truncated tetrahedron
+    ("polyhedron", {"vertices":[[-1.38818127, -0.43327022,  0.48655642],
+                                [-0.76510261,  0.66381377, -0.56182591],
+                                [-0.52614596, -1.02022957,  0.4116381 ],
+                                [-0.88082834, -0.1838706,   0.97593675],
+                                [-0.44804162,  0.96548643, -1.13214542],
+                                [-0.76156125,  1.17756002, -0.64348949],
+                                [ 0.4120951,  -0.88009234, -0.15537746],
+                                [ 0.80000109, -0.37006509, -1.03111526],
+                                [ 0.26988483,  0.30897717,  1.06415232],
+                                [ 0.76226993,  0.95183908,  0.63302422],
+                                [ 0.99329989, -0.58242708, -0.39317069],
+                                [ 0.78783469,  0.39728311,  0.51595356]],
+                                "sweep_radius": 0, "ignore_statistics": True}),
+    ("ellipsoid", {"a": 1, "b": 1, "c": 1, "ignore_statistics": True}),
+    ("ellipsoid", {"a": 1, "b": 1, "c": 2, "ignore_statistics": True}),
+    ("ellipsoid", {"a": 1.5, "b": 1, "c": 1, "ignore_statistics": True}),
+    ("ellipsoid", {"a": 1, "b": 0.8, "c": 1, "ignore_statistics": True}),
+    ("ellipsoid", {"a": 1.3, "b": 2.7, "c": 0.7, "ignore_statistics": True}),
+]
+
+volume_list = [8.0, 9.906613237811571, 2.5813333333333337, 3.406051603090999,
+               4.1887902047863905, 8.377580409572781, 6.283185307179586,
+               3.3510321638291125, 10.291857533160162]
 
 
-def get_outward_normal(verts, face, thresh=0.0001):
-    assert (len(face) == 3)
-    (a, b, c) = verts[face]
-    n = np.cross((b - a), (c - a))
-    flip = False
-    for k in range(len(verts)):
-        if (not k in face):
-            d = n.dot(verts[k] - a)
-            if abs(d) > thresh and d > 0:  # by convexity
-                flip = True
-                break
-    if flip:
-        n = -n
-    return n
+det_moi_list = [151.70370370370372, 433.6939933469258, 0.873927553653835,
+                2.754115599932528, 4.70376701046326, 235.18835052316288,
+                41.920486071765346, 1.6193602241717742, 1328.2618881466828]
 
 
-def sort_faces(verts, faces):
-    sorted_face = []
-    for face in faces:
-        nout = get_outward_normal(verts, face)
-        (a, b, c) = verts[face]
-        n = np.cross((b - a), (c - a))
-        if n.dot(nout) > 0:
-            sorted_face.append(face)
-        else:
-            sorted_face.append(face[::-1])
-    return np.array(sorted_face)
+def _get_cpp_cls(shape_type):
+    if shape_type == "polyhedron":
+        return (PolyhedronVertices, MassPropertiesConvexPolyhedron)
+    elif shape_type == "ellipsoid":
+        return (EllipsoidParams, MassPropertiesEllipsoid)
 
 
-def _vertices():
-    platonic_shapes = [
-        "Cube", "Tetrahedron", "Octahedron", "Icosahedron", "Dodecahedron"
-    ]
-    verts = []
-    names = []
-    for shape in platonic_shapes:
-        verts.append(
-            np.asarray(
-                coxeter.families.PlatonicFamily.get_shape(shape).vertices))
-        names.append(shape)
-    for i in range(20):
-        verts.append(np.random.rand(np.random.randint(10, 128), 3))
-        names.append("random" + str(i))
-    return zip(names, verts)
+@pytest.mark.parametrize("shape,volume", zip(shape_list, volume_list))
+def test_volume(shape, volume):
+    param_cpp_cls, mass_props_cpp_cls = _get_cpp_cls(shape[0])
+    mass_props_obj = mass_props_cpp_cls(param_cpp_cls(shape[1]))
+    assert np.allclose(mass_props_obj.getVolume(), volume)
 
 
-@pytest.fixture(scope="function", params=_vertices(), ids=(lambda x: x[0]))
-def vertices(request):
-    return deepcopy(request.param)
-
-
-_make_verts = hpmc._hpmc.PolyhedronVertices
-_mass_class = hpmc._hpmc.MassPropertiesConvexPolyhedron
-
-
-def test_convex_hull_vertices(vertices):
-    _, verts = vertices
-
-    hull = ConvexHull(verts)
-    py_verts = []
-    for i, j, k in hull.vertices:
-        py_verts.append(list(verts[i]))
-        py_verts.append(list(verts[j]))
-        py_verts.append(list(verts[k]))
-    py_verts = np.asarray(
-        sorted(py_verts, key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
-    py_verts = np.unique(py_verts, axis=0)
-
-    mp = _mass_class(
-        _make_verts({
-            'vertices': verts,
-            'sweep_radius': 0.0,
-            'ignore_statistics': 0
-        }))
-    cpp_verts = []
-    for f in range(mp.num_faces()):
-        for i in range(3):
-            cpp_verts.append(mp.vertices(f, i))
-    cpp_verts = np.asarray(
-        sorted(cpp_verts, key=lambda x: x[0]**2 + x[1]**2 + x[2]**2))
-    cpp_verts = np.unique(cpp_verts, axis=0)
-
-    np.testing.assert_allclose(cpp_verts, py_verts)
-
-
-def test_num_faces(vertices):
-    _, verts = vertices
-
-    hull = ConvexHull(verts)
-    faces = np.array(hull.vertices)
-    faces = sort_faces(verts, faces)
-
-    mp = _mass_class(
-        _make_verts({
-            'vertices': verts,
-            'sweep_radius': 0.0,
-            'ignore_statistics': 0
-        }))
-
-    np.testing.assert_allclose(mp.num_faces(), len(faces))
-
-
-def test_volume(vertices):
-    _, verts = vertices
-
-    hull = ConvexHull(verts)
-    faces = np.array(hull.vertices)
-    faces = sort_faces(verts, faces)
-    py_vol, _, _ = geometry.massProperties(verts, faces)
-
-    mp = _mass_class(
-        _make_verts({
-            'vertices': verts,
-            'sweep_radius': 0.0,
-            'ignore_statistics': 0
-        }))
-
-    np.testing.assert_allclose(mp.volume(), py_vol, rtol=1e-5, atol=1e-5)
-
-
-def test_center_of_mass(vertices):
-    _, verts = vertices
-
-    hull = ConvexHull(verts)
-    faces = np.array(hull.vertices)
-    faces = sort_faces(verts, faces)
-    _, py_com, _ = geometry.massProperties(verts, faces)
-
-    mp = _mass_class(
-        _make_verts({
-            'vertices': verts,
-            'sweep_radius': 0.0,
-            'ignore_statistics': 0
-        }))
-    cpp_com = [mp.center_of_mass(i) for i in range(3)]
-
-    np.testing.assert_allclose(cpp_com, py_com, rtol=1e-5, atol=1e-5)
-
-
-def test_inertia(vertices):
-    _, verts = vertices
-
-    hull = ConvexHull(verts)
-    faces = np.array(hull.vertices)
-    faces = sort_faces(verts, faces)
-    _, _, py_inertia = geometry.massProperties(verts, faces)
-    tmp = py_inertia + 0.0
-    py_inertia[1] = tmp[3]
-    py_inertia[2] = tmp[5]
-    py_inertia[3] = tmp[1]
-    py_inertia[5] = tmp[2]
-
-    mp = _mass_class(
-        _make_verts({
-            'vertices': verts,
-            'sweep_radius': 0.0,
-            'ignore_statistics': 0
-        }))
-    cpp_inertia = [mp.moment_of_inertia(i) for i in range(6)]
-
-    np.testing.assert_allclose(cpp_inertia, py_inertia, rtol=1e-5, atol=1e-5)
+@pytest.mark.parametrize("shape,det_moi", zip(shape_list, det_moi_list))
+def test_det_moi(shape, det_moi):
+    param_cpp_cls, mass_props_cpp_cls = _get_cpp_cls(shape[0])
+    mass_props_obj = mass_props_cpp_cls(param_cpp_cls(shape[1]))
+    assert np.allclose(mass_props_obj.getDetInertiaTensor(), det_moi)