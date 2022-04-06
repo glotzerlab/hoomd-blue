f2ca2f3a07d9c6bc57c08e287c9593744a4e85e1
Joshua A. Anderson <joaander@umich.edu>
2022-04-06 11:19:20 -0400

    Fix pre-commit failures.

--- hoomd/md/PotentialPairAlchemical.h
+++ hoomd/md/PotentialPairAlchemical.h
@@ -1,8 +1,6 @@
 // Copyright (c) 2009-2022 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
-// Maintainer: jproc
-
 #ifndef POTENTIAL_PAIR_ALCHEMICAL_H
 #define POTENTIAL_PAIR_ALCHEMICAL_H
 
--- hoomd/md/TwoStepNVTAlchemy.h
+++ hoomd/md/TwoStepNVTAlchemy.h
@@ -4,8 +4,6 @@
 // Copyright (c) 2009-2016 The Regents of the University of Michigan
 // This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
 
-// Maintainer: jproc
-
 #ifndef __TWOSTEP_NVT_ALCHEMY_H__
 #define __TWOSTEP_NVT_ALCHEMY_H__
 
