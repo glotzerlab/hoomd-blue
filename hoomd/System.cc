58d87d3f3451ade279db89fea57a1beb4c4489c7
Brandon Butler <butlerbr@umich.edu>
2022-05-11 13:20:46 -0400

    refactor: Broadcast walltime/tps each timestep

--- hoomd/System.cc
+++ hoomd/System.cc
@@ -169,7 +169,14 @@ void System::run(uint64_t nsteps, bool write_at_start)
             }
 
         updateTPS();
-
+#ifdef ENABLE_MPI
+        // make sure all ranks return the same TPS after the run completes
+        if (m_sysdef->isDomainDecomposed())
+            {
+            bcast(m_last_TPS, 0, m_exec_conf->getMPICommunicator());
+            bcast(m_last_walltime, 0, m_exec_conf->getMPICommunicator());
+            }
+#endif
         // propagate Python exceptions related to signals
         if (PyErr_CheckSignals() != 0)
             {
@@ -177,14 +184,6 @@ void System::run(uint64_t nsteps, bool write_at_start)
             }
         }
 
-#ifdef ENABLE_MPI
-    // make sure all ranks return the same TPS after the run completes
-    if (m_sysdef->isDomainDecomposed())
-        {
-        bcast(m_last_TPS, 0, m_exec_conf->getMPICommunicator());
-        bcast(m_last_walltime, 0, m_exec_conf->getMPICommunicator());
-        }
-#endif
     }
 
 void System::updateTPS()
