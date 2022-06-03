108e73551d4573d002b760f5af8ba8b905492a15
Brandon Butler <butlerbr@umich.edu>
2022-06-03 14:21:50 -0400

    doc: Explicitly state conversion for *_like objects

--- hoomd/trigger.py
+++ hoomd/trigger.py
@@ -382,9 +382,9 @@ trigger_like = typing.Union[Trigger, int]
 """
 An object that can serve as a trigger for an operation.
 
-Any instance of a `Trigger` subclass is allowed, as well as an int instance. The
-integer is converted to a `Periodic` trigger via ``Periodic(period=a)`` where
-``a`` is the passed integer.
+Any instance of a `Trigger` subclass is allowed, as well as an int instance or
+any object convertible to an int. The integer is converted to a `Periodic`
+trigger via ``Periodic(period=int(a))`` where ``a`` is the passed integer.
 
 Note:
     Attributes that are `Trigger` objects can be set via a `trigger_like`
