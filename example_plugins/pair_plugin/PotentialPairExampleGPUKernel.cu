56291a7d8e75378bba4d5269858f3ab92e7c7f94
Ian Graham <irgraham1@gmail.com>
2022-08-16 13:26:20 -0400

    passed pre-commit one final time

--- example_plugins/pair_plugin/PotentialPairExampleGPUKernel.cu
+++ example_plugins/pair_plugin/PotentialPairExampleGPUKernel.cu
@@ -1,12 +1,8 @@
 // Copyright (c) 2009-2022 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
-// See md/CMakeLists.txt for the source of these variables to be processed by CMake's
-// configure_file().
-
-// clang-format off
-#include "hoomd/md/PotentialPairGPU.cuh"
 #include "EvaluatorPairExample.h"
+#include "hoomd/md/PotentialPairGPU.cuh"
 
 namespace hoomd
     {
@@ -16,7 +12,7 @@ namespace kernel
     {
 template __attribute__((visibility("default"))) hipError_t
 gpu_compute_pair_forces<EvaluatorPairExample>(const pair_args_t& pair_args,
-                                         const EvaluatorPairExample::param_type* d_params);
+                                              const EvaluatorPairExample::param_type* d_params);
     } // end namespace kernel
     } // end namespace md
     } // end namespace hoomd
