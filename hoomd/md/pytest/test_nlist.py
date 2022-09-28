48be6974b1b8551369d919f4c1d113639c3120dd
Ian Graham <irgraham1@gmail.com>
2022-09-28 13:48:05 -0400

    first set of pytests written

--- hoomd/md/pytest/test_nlist.py
+++ hoomd/md/pytest/test_nlist.py
@@ -203,25 +203,74 @@ def test_logging():
         })
 
 
-def test_rcut():
-    pass
+TRUE_PAIR_LIST = [[0, 3], [0, 6], [0, 9], [0, 18], [0, 1], [0, 2],
+                  [1, 2], [1, 4], [1, 7], [1, 10], [1, 19], [2, 5], [2, 8],
+                  [2, 11], [2, 20], [3, 6], [3, 12], [3, 21], [3, 4], [3, 5],
+                  [4, 5], [4, 7], [4, 13], [4, 22], [5, 8], [5, 14], [5, 23],
+                  [6, 15], [6, 24], [6, 7], [6, 8], [7, 8], [7, 16], [7, 25],
+                  [8, 17], [8, 26], [9, 18], [9, 12], [9, 15], [9, 10], [9, 11],
+                  [10, 11], [10, 19], [10, 13], [10, 16], [11, 20], [11, 14],
+                  [11, 17], [12, 15], [12, 21], [12, 13], [12, 14], [13, 14],
+                  [13, 16], [13, 22], [14, 17], [14, 23], [15, 24], [15, 16],
+                  [15, 17], [16, 17], [16, 25], [17, 26], [18, 21], [18, 24],
+                  [18, 19], [18, 20], [19, 20], [19, 22], [19, 25], [20, 23],
+                  [20, 26], [21, 24], [21, 22], [21, 23], [22, 23], [22, 25],
+                  [23, 26], [24, 25], [24, 26], [25, 26]]
+
+TRUE_PAIR_LIST = set([frozenset(pair) for pair in TRUE_PAIR_LIST])
 
 
-def test_global_pair_list():
-    pass
+def test_global_pair_list(simulation_factory, lattice_snapshot_factory):
+
+    nlist = hoomd.md.nlist.Cell(buffer=0, default_r_cut=1.1)
+    print(nlist)
+
+    sim = simulation_factory(lattice_snapshot_factory(n=3))
+    sim.operations.computes.append(nlist)
+
+    sim.run(0)
+
+    pair_list = nlist.pair_list
+    if sim.device.communicator.rank == 0:
+        pair_list = set([frozenset(pair) for pair in pair_list])
+        assert pair_list == TRUE_PAIR_LIST
 
 
-def test_rank_local_pair_list():
-    pass
+def test_rank_local_pair_list(simulation_factory, lattice_snapshot_factory):
+
+    nlist = hoomd.md.nlist.Cell(buffer=0, default_r_cut=1.1)
+
+    sim = simulation_factory(lattice_snapshot_factory(n=3))
+    sim.operations.computes.append(nlist)
+
+    sim.run(0)
+
+    local_pair_list = nlist.local_pair_list
+    pair_list = set()
+    with sim.state.cpu_local_snapshot as data:
+        rtags = data.particles.rtag
+        for pair in local_pair_list:
+            pair_list.add(frozenset([rtags[pair[0]], rtags[pair[1]]]))
+        assert pair_list == TRUE_PAIR_LIST
 
 
-def test_rank_local_nlist_arrays():
-    pass
+def test_rank_local_nlist_arrays(simulation_factory, lattice_snapshot_factory):
 
+    nlist = hoomd.md.nlist.Cell(buffer=0, default_r_cut=1.1)
 
-def test_pair_list_access():
-    pass
+    sim = simulation_factory(lattice_snapshot_factory(n=3))
+    sim.operations.computes.append(nlist)
 
+    sim.run(0)
 
-def test_pair_list_deletion():
-    pass
+    local_pair_list = nlist.local_pair_list
+
+    with nlist.cpu_local_nlist_arrays as data:
+        k = 0
+        for i, (head, nn) in enumerate(zip(data.head_list, data.n_neigh)):
+            for j_idx in range(head, head + nn):
+                j = data.nlist[j_idx]
+                pair = local_pair_list[k]
+                assert i == pair[0]
+                assert j == pair[1]
+                k += 1
