eb8c6b18869ab224a36ec7a4e5372636784b6200
Corwin Kerr <cbkerr@umich.edu>
2022-09-30 19:22:00 -0400

    Add bones of PairModulator.h
    Adapted from EvaluatorPairIsoModulated.h

---
+++ hoomd/md/PairModulator.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2009-2022 The Regents of the University of Michigan.
+// Part of HOOMD-blue, released under the BSD 3-Clause License.
+
+
+
+// Used to be called EvaluatorPairIsoModulated
+
+
+/*
+  This class  aplies the directionalEnvelope to the pairEvaluator, turning the isotropic pair potential into an anisotropic potential.
+*/
+
+
+//template <typename isoEval, typename dirEval>
+template <typename pairEvaluator, typename directionalEnvelope>
+class PairModulator
+{
+
+}
