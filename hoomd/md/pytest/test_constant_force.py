563466e4c7fa2f01cdbdcd0fcc844e4755b951d0
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2022-10-26 18:40:47 -0400

    adjust constant force test

--- hoomd/md/pytest/test_constant_force.py
+++ hoomd/md/pytest/test_constant_force.py
@@ -8,7 +8,7 @@ from hoomd.conftest import pickling_check, autotuned_kernel_parameter_check
 def test_attributes():
     constant = hoomd.md.force.Constant(filter=hoomd.filter.All())
 
-    assert constant.constant_force['A'] == (1.0, 0.0, 0.0)
+    assert constant.constant_force['A'] == (0.0, 0.0, 0.0)
     assert constant.constant_torque['A'] == (0.0, 0.0, 0.0)
 
     constant.constant_force['A'] = (0.5, 0.0, 0.0)
@@ -28,7 +28,7 @@ def test_attach(simulation_factory, two_particle_snapshot_factory):
     sim.operations.integrator = integrator
     sim.run(0)
 
-    assert constant.constant_force['A'] == (1.0, 0.0, 0.0)
+    assert constant.constant_force['A'] == (0.0, 0.0, 0.0)
     assert constant.constant_torque['A'] == (0.0, 0.0, 0.0)
 
     constant.constant_force['A'] = (0.5, 0.0, 0.0)
