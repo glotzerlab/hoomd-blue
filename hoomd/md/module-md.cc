2391666f79e18298a23742e9beb119455f447a15
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2022-10-26 18:07:43 -0400

    link constant force

--- hoomd/md/module-md.cc
+++ hoomd/md/module-md.cc
@@ -21,6 +21,7 @@ void export_ActiveForceConstraintComputeSphere(pybind11::module& m);
 void export_ActiveRotationalDiffusionUpdater(pybind11::module& m);
 void export_ComputeThermo(pybind11::module& m);
 void export_ComputeThermoHMA(pybind11::module& m);
+void export_ConstantForceCompute(pybind11::module& m);
 void export_HarmonicAngleForceCompute(pybind11::module& m);
 void export_CosineSqAngleForceCompute(pybind11::module& m);
 void export_TableAngleForceCompute(pybind11::module& m);
@@ -159,6 +160,7 @@ void export_ActiveForceConstraintComputeSphereGPU(pybind11::module& m);
 void export_ActiveForceComputeGPU(pybind11::module& m);
 void export_ComputeThermoGPU(pybind11::module& m);
 void export_ComputeThermoHMAGPU(pybind11::module& m);
+void export_ConstantForceComputeGPU(pybind11::module& m);
 void export_HarmonicAngleForceComputeGPU(pybind11::module& m);
 void export_CosineSqAngleForceComputeGPU(pybind11::module& m);
 void export_TableAngleForceComputeGPU(pybind11::module& m);
@@ -289,6 +291,7 @@ PYBIND11_MODULE(_md, m)
     export_ActiveRotationalDiffusionUpdater(m);
     export_ComputeThermo(m);
     export_ComputeThermoHMA(m);
+    export_ConstantForceCompute(m);
     export_HarmonicAngleForceCompute(m);
     export_CosineSqAngleForceCompute(m);
     export_TableAngleForceCompute(m);
@@ -441,6 +444,7 @@ PYBIND11_MODULE(_md, m)
     export_ActiveForceConstraintComputePlaneGPU(m);
     export_ActiveForceConstraintComputePrimitiveGPU(m);
     export_ActiveForceConstraintComputeSphereGPU(m);
+    export_ConstantForceComputeGPU(m);
     export_PotentialExternalPeriodicGPU(m);
     export_PotentialExternalElectricFieldGPU(m);
 
