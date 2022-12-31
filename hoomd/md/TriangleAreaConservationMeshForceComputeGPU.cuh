9d3ce91acf9019a6f15f101b9af18e70b7648775
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2022-12-31 12:37:50 -0500

    update headers

--- hoomd/md/TriangleAreaConservationMeshForceComputeGPU.cuh
+++ hoomd/md/TriangleAreaConservationMeshForceComputeGPU.cuh
@@ -1,9 +1,9 @@
 // Copyright (c) 2009-2022 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
+#include "hoomd/BondedGroupData.cuh"
 #include "hoomd/HOOMDMath.h"
 #include "hoomd/Index1D.h"
-#include "hoomd/MeshGroupData.cuh"
 #include "hoomd/ParticleData.cuh"
 
 /*! \file TriangleAreaConservationMeshForceComputeGPU.cuh
