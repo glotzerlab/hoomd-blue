a69e8f8670b1a7a5ed45d069fc202fdbcc9f4fe1
Corwin Kerr <cbkerr@umich.edu>
2023-01-12 14:15:25 -0500

    Add other vector types

--- hoomd/md/JanusFactor.h
+++ hoomd/md/JanusFactor.h
@@ -55,6 +55,10 @@ public:
         __attribute__((aligned(16)));
 #endif
 
+    //!
+    /*! \param
+
+     */
     DEVICE JanusFactor(const Scalar3& _dr,
                        const Scalar4& _qi,
                        const Scalar4& _qj,
@@ -66,24 +70,24 @@ public:
             vec3<Scalar> ex { make_scalar3(1, 0, 0) };
             vec3<Scalar> ey { make_scalar3(0, 1, 0) };
             vec3<Scalar> ez { make_scalar3(0, 0, 1) };
-            vec3<Scalar> ei;
-            vec3<Scalar> a1;
-            vec3<Scalar> a2;
-            vec3<Scalar> a3;
-            vec3<Scalar> ej;
-            vec3<Scalar> b1;
-            vec3<Scalar> b2;
-            vec3<Scalar> b3;
+
+            vec3<Scalar> ei, ej;
+            vec3<Scalar> a1, a2, a3, b1, b2, b3;
 
             ei = rotate(quat<Scalar>(qi), ex);
             a1 = rotate(quat<Scalar>(qi), ex);
             a2 = rotate(quat<Scalar>(qi), ey);
             a3 = rotate(quat<Scalar>(qi), ez);
+            // patch points relative to x (a1) direction of particle
+            ni = rotate(quat<Scalar>(a1), oi);
 
             ej = rotate(quat<Scalar>(qj), ex);
             b1 = rotate(quat<Scalar>(qj), ex);
             b2 = rotate(quat<Scalar>(qj), ey);
             b3 = rotate(quat<Scalar>(qj), ez);
+
+            // patch points relative to x (b1) direction of particle
+            ni = rotate(quat<Scalar>(b1), oj);
 
             // compute distance
             drsq = dot(dr, dr);
@@ -93,42 +97,46 @@ public:
             // which as first implemented is the same as the angle between the patch and pointing director
             doti = -dot(vec3<Scalar>(dr), ei) / magdr; // negative because dr = dx = pi - pj
             dotj = dot(vec3<Scalar>(dr), ej) / magdr;
+
+            costhetai = -dot(vec3<Scalar>(dr), ni) / magdr;
+            costhetaj = dot(vec3<Scalar>(dr), nj) / magdr;
         }
 
     DEVICE inline Scalar Modulatori()
         {
-            return Scalar(1.0) / ( Scalar(1.0) + fast::exp(-params.omega*(doti-params.cosalpha)) );
+            return Scalar(1.0) / ( Scalar(1.0) + fast::exp(-params.omega*(costhetai-params.cosalpha)) );
         }
 
     DEVICE inline Scalar Modulatorj()
         {
-            return Scalar(1.0) / ( Scalar(1.0) + fast::exp(-params.omega*(dotj-params.cosalpha)) );
+            return Scalar(1.0) / ( Scalar(1.0) + fast::exp(-params.omega*(costhetaj-params.cosalpha)) );
         }
 
     DEVICE Scalar ModulatorPrimei() // D[f[\[Theta], \[Alpha], \[Omega]], Cos[\[Theta]]]
         {
             Scalar fact = Modulatori();
-            // weird way of writing out the derivative of f with respect to doti = Cos[theta] = 
-            return params.omega * fast::exp(-params.omega*(doti-params.cosalpha)) * fact * fact;
+            // weird way of writing out the derivative of f with respect to doti = Cos[theta] =
+            return params.omega * fast::exp(-params.omega*(costhetai-params.cosalpha)) * fact * fact;
         }
 
     DEVICE Scalar ModulatorPrimej()
         {
             Scalar fact = Modulatorj();
-            return params.omega * fast::exp(-params.omega*(dotj-params.cosalpha)) * fact * fact;
+            return params.omega * fast::exp(-params.omega*(costhetaj-params.cosalpha)) * fact * fact;
         }
 
 
-    // things that get passed in to constructor
     Scalar3 dr;
     Scalar4 qi;
     Scalar4 qj;
-    Scalar4 oi;
-    Scalar4 oj;
+
+    Scalar4 oi; // quaternion representing orientation of patch with respect to particle x direction (a1)
+    Scalar4 oj; // quaternion representing orientation of patch with respect to particle x direction (b1)
     param_type params;
-    // things that get calculated when constructor is called
-    Scalar3 ei;
-    Scalar3 ej;
+
+    Scalar3 ni; // pointing vector for patch on particle i
+    Scalar3 nj; // pointing vector for patch on particle j
+
     Scalar3 a1;
     Scalar3 a2;
     Scalar3 a3;
