47b376f116dba234779cbe013e51aa468f60f866
Joshua A. Anderson <joaander@umich.edu>
2023-01-13 11:54:41 -0500

    Revise thermostat documentation.

--- hoomd/md/methods/thermostats.py
+++ hoomd/md/methods/thermostats.py
@@ -5,6 +5,13 @@
 
 Classes are for use with `hoomd.md.methods.ConstantVolume` and
 `hoomd.md.methods.ConstantPressure`.
+
+Important:
+    Ensure that your initial condition includes non-zero particle velocities and
+    angular momenta (when appropriate). The coupling between the thermostat and
+    the velocities / angular momenta occurs via multiplication, so the
+    thermostat cannot convert a zero velocity into a non-zero one except
+    through particle collisions.
 """
 
 from hoomd.md import _md
@@ -39,7 +46,7 @@ class Thermostat(_HOOMDBaseObject):
 class MTTK(Thermostat):
     r"""The Nosé-Hoover thermostat.
 
-    Produces themperature control through a Nosé-Hoover thermostat.
+    Produces temperature control through a Nosé-Hoover thermostat.
 
     Args:
         kT (hoomd.variant.variant_like): Temperature set point for the
@@ -54,12 +61,10 @@ class MTTK(Thermostat):
     these quantities using `translational_thermostat_dof` and
     `rotational_thermostat_dof`.
 
-    Important:
-        Ensure that your initial condition includes non-zero particle velocities
-        and angular momenta (when appropriate). The coupling between the
-        thermostat and the velocities and angular momenta occurs via
-        multiplication, so `MTTK` cannot convert a zero velocity into a non-zero
-        one except through particle collisions.
+    See Also:
+        `G. J. Martyna, D. J. Tobias, M. L. Klein 1994
+        <http://dx.doi.org/10.1063/1.467468>`_ and `J. Cao, G. J. Martyna 1996
+        <http://dx.doi.org/10.1063/1.470959>`_.
 
     Attributes:
         kT (hoomd.variant.variant_like): Temperature set point for the
@@ -133,12 +138,17 @@ class Bussi(Thermostat):
     Provides temperature control by rescaling the velocity by a factor taken
     from the canonical velocity distribution. On each timestep, velocities are
     rescaled by a factor :math:`\alpha=\sqrt{K_t / K}`, where :math:`K` is the
-    current kinetic energy, and :math:`K_t` is taken from
+    current kinetic energy, and :math:`K_t` is chosen randomly from the
+    distribution
 
     .. math::
         P(K_t) \propto K_t^{N_f/2 - 1} \exp(-K_t / kT)
 
     where :math:`N_f` is the number of degrees of freedom thermalized.
+
+    See Also:
+        `Bussi et. al. 2007 <https://doi.org/10.1063/1.2408420>`.
+
 
     Attributes:
         kT (hoomd.variant.variant_like): Temperature set point
@@ -163,12 +173,11 @@ class Berendsen(Thermostat):
         kT (hoomd.variant.variant_like): Temperature of the simulation.
             :math:`[\mathrm{energy}]`
 
-        tau (float): Time constant of thermostat. :math:`[\mathrm{time}]`
+        tau (float): Thermostat time constant. :math:`[\mathrm{time}]`
 
     `Berendsen` rescales the velocities of all particles on each time step. The
     rescaling is performed so that the difference in the current temperature
     from the set point decays exponentially:
-    `Berendsen et. al. 1984 <http://dx.doi.org/10.1063/1.448118>`_.
 
     .. math::
 
@@ -176,6 +185,9 @@ class Berendsen(Thermostat):
 
     .. attention::
         `Berendsen` does not yield a proper canonical ensemble
+
+    See Also:
+        `Berendsen et. al. 1984 <http://dx.doi.org/10.1063/1.448118>`_.
 
     Attributes:
         kT (hoomd.variant.variant_like): Temperature of the simulation.
