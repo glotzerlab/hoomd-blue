54fa3b3eb68a5fd2ae718a5f25bef11088319203
Martin Girard <martin.girard@mpip-mainz.mpg.de>
2023-01-25 10:39:06 +0100

    initial work

--- hoomd/md/MoleculeEnsemble.h
+++ hoomd/md/MoleculeEnsemble.h
@@ -17,7 +17,10 @@ namespace hoomd::md {
         MoleculeEnsemble(std::shared_ptr<SystemDefinition> sysdef,
                          std::shared_ptr<ParticleGroup> group,
                          bool include_all_bonded = true) :
-        MolecularForceCompute(sysdef), m_group(group){}
+        MolecularForceCompute(sysdef), m_group(group){
+            if(sysdef->isDomainDecomposed())
+                throw std::runtime_error("Molecular ensembles does not work on domain decomposed simulations");
+        }
 
         void initMolecules() override;
 
@@ -31,12 +34,28 @@ namespace hoomd::md {
 
         void computeHashes(std::size_t);
 
+        auto get_molecule_sizes(){
+            return m_molecule_size;
+        }
+
+        auto get_molecule_indexer(){
+            return m_molecule_indexer;
+        }
+
+        auto get_molecules(){
+            return m_molecules;
+        }
+
     protected:
         std::shared_ptr<ParticleGroup> m_group;
 
         unsigned int m_hash_size = 0; //! the number of bits currently associated with the hashes of this set of molecules
         GlobalArray<unsigned int> m_hashes; //! the hashes of all molecules within this set
         GlobalArray<Scalar> m_chemical_potentials; //! chemical potentials associated with a given hash, if any
+
+        GlobalArray<unsigned int> m_molecule_size; //! number of beads in each molecule
+        GlobalArray<unsigned int> m_molecules; //! indexes of beads of each molecule
+        Index2D m_molecule_indexer; //! yields index (molecule, bead_index) into m_molecules
 
         std::vector<std::weak_ptr<MolecularHashAction>> m_registered_actions;
         std::vector<std::weak_ptr<MolecularHashCompute>> m_registered_computes;
