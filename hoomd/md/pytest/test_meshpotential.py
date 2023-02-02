4dfcdcfaf8093c909a031c8c4127621bcdb57d11
Philipp Schönhöfer <philipp@Philipps-MacBook-Pro.local>
2023-02-02 10:32:17 -0500

    fix failing pytest

--- hoomd/md/pytest/test_meshpotential.py
+++ hoomd/md/pytest/test_meshpotential.py
@@ -188,7 +188,7 @@ def test_multiple_types(tetrahedron_snapshot_factory, simulation_factory,
     mesh.triangles = [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]]
 
     mesh_potential = mesh_potential_cls(mesh)
-    mesh_potential.all_params = potential_kwargs
+    mesh_potential.params.default = potential_kwargs
 
     integrator = hoomd.md.Integrator(dt=0.005)
 
