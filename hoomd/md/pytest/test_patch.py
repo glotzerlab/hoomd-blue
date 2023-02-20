cdb869bb14f063f70beb7d4e8cd14476bcff5723
Corwin Kerr <cbkerr@umich.edu>
2023-02-20 17:39:04 -0500

    Check that we get normalized ni, nj back

--- hoomd/md/pytest/test_patch.py
+++ hoomd/md/pytest/test_patch.py
@@ -87,9 +87,17 @@ def test_before_attaching(patch_cls, patch_args, params, positions, orientations
     potential = patch_cls(nlist = hoomd.md.nlist.Cell(buffer=0.4), default_r_cut=4, **patch_args)
     potential.params[('A','A')] = params
     for key in params:
-        assert potential.params[('A','A')][key] == pytest.approx(params[key])
+        # ni and nj are returned normalized, so replace them in the params we check
+        if key == "envelope_params":
+            for nkey in ("ni", "nj"):
+                nn = numpy.array(params[key][nkey])
+                params[key][nkey] = tuple(nn / numpy.linalg.norm(nn))
+            assert potential.params[('A','A')][key] == pytest.approx(params[key])
+        else:
+            assert potential.params[('A','A')][key] == pytest.approx(params[key])
 
-        
+
+# TODO: exclude the one that is not normalized
 @pytest.mark.parametrize('patch_cls, patch_args, params, positions, orientations, force, energy, torques',
                          patch_test_parameters)
 def test_after_attaching(patchy_snapshot_factory, simulation_factory,
@@ -103,7 +111,14 @@ def test_after_attaching(patchy_snapshot_factory, simulation_factory,
                                                     integrate_rotational_dof = True)
     sim.run(0)
     for key in params:
-        assert potential.params[('A','A')][key] == pytest.approx(params[key])
+        # ni and nj are returned normalized, so replace them in the params we check
+        if key == "envelope_params":
+            for nkey in ("ni", "nj"):
+                nn = numpy.array(params[key][nkey])
+                params[key][nkey] = tuple(nn / numpy.linalg.norm(nn))
+            assert potential.params[('A','A')][key] == pytest.approx(params[key])
+        else:
+            assert potential.params[('A','A')][key] == pytest.approx(params[key])
 
 
 @pytest.mark.parametrize('patch_cls, patch_args, params, positions, orientations, force, energy, torques',
