9ee6b5f20ca7b57666943ad67f95181f98d914ba
Joshua A. Anderson <joaander@umich.edu>
2023-03-07 11:36:31 -0500

    Fix typos in documentation.
    Co-authored-by: Tommy Waltmann <53307607+tommy-waltmann@users.noreply.github.com>

--- hoomd/md/pair/pair.py
+++ hoomd/md/pair/pair.py
@@ -326,7 +326,7 @@ class ExpandedGaussian(Pair):
           :math:`\varepsilon` :math:`[\mathrm{energy}]`
         * ``sigma`` (`float`, **required**) - particle size
           :math:`\sigma` :math:`[\mathrm{length}]`
-        * ``Delta`` (`float`, **required**) - shift distabce
+        * ``delta`` (`float`, **required**) - shift distance
           :math:`\delta` :math:`[\mathrm{length}]`
 
         Type: `TypeParameter` [`tuple` [``particle_type``, ``particle_type``],
