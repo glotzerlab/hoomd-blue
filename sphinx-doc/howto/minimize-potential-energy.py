de795566815bcce80b52b53a736a93d4c616ec5b
Brandon Butler <butlerbr@umich.edu>
2023-08-29 11:28:41 -0400

    style: Fix minimize-potential-energy howto style

--- sphinx-doc/howto/minimize-potential-energy.py
+++ sphinx-doc/howto/minimize-potential-energy.py
@@ -6,10 +6,10 @@ simulation = hoomd.util.make_example_simulation()
 # Step 1: Use hoomd.md.minize.FIRE as the integrator.
 constant_volume = hoomd.md.methods.ConstantVolume(filter=hoomd.filter.All())
 fire = hoomd.md.minimize.FIRE(dt=0.001,
-                        force_tol=1e-3,
-                        angmom_tol=1e-3,
-                        energy_tol=1e-6,
-                        methods=[constant_volume])
+                              force_tol=1e-3,
+                              angmom_tol=1e-3,
+                              energy_tol=1e-6,
+                              methods=[constant_volume])
 simulation.operations.integrator = fire
 
 # Step 2: Apply forces to the particles.
@@ -20,5 +20,5 @@ lj.r_cut[('A', 'A')] = 2.5
 simulation.operations.integrator.forces = [lj]
 
 # Step 3: Run simulation steps until the minimization converges.
-while not(fire.converged):
-   simulation.run(100)
+while not (fire.converged):
+    simulation.run(100)
