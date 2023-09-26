881ad4cc1aad2e6be3d5dc4c712b8d51b68edd08
Joshua A. Anderson <joaander@umich.edu>
2023-09-26 10:52:43 -0400

    Update hoomd/write/hdf5.py

--- hoomd/write/hdf5.py
+++ hoomd/write/hdf5.py
@@ -209,7 +209,7 @@ class _HDF5LogInternal(custom._InternalAction):
             chunk_size = None
             if category == "scalar":
                 data_shape = (1,)
-                if isinstance(value, (np.integer, np.floating, np.bool_)):
+                if isinstance(value, (np.number, np.bool_)):
                     dtype = value.dtype
                 elif isinstance(value, int):
                     dtype = np.dtype(bool) if isinstance(value, bool) else "i8"
