72238eadfb8a5a6bdd8a32db954412ef2ff202dc
Tim Moore <mtimc@umich.edu>
2024-01-11 17:44:20 -0500

    Add docstring to variant/scalar.py

--- hoomd/variant/scalar.py
+++ hoomd/variant/scalar.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2009-2023 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
+"""Implement variants that return scalar values."""
+
 import typing
 
 from hoomd import _hoomd
