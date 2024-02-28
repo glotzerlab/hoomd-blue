00d009046a057124d127b54e52aa2d66260b7bd5
Joshua A. Anderson <joaander@umich.edu>
2024-02-28 08:12:20 -0500

    Apply changes from code review.

--- hoomd/pytest/test_box_variant.py
+++ hoomd/pytest/test_box_variant.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2023 The Regents of the University of Michigan.
+# Copyright (c) 2009-2024 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 import numpy as np
@@ -12,22 +12,22 @@ def box_to_array(box):
     return np.array([box.Lx, box.Ly, box.Lz, box.xy, box.xz, box.yz])
 
 
-_test_box1 = hoomd.Box(10, 50, 20, 0.2, 0.4, 0.6)
-_test_box2 = hoomd.Box(16, 25, 36, 0.1, 0.2, 0.3)
-_test_scalar_variant1 = hoomd.variant.Ramp(0, 1, 100, 200)
-_test_scalar_variant2 = hoomd.variant.Ramp(0, 1, 10, 30)
+test_box1 = hoomd.Box(10, 50, 20, 0.2, 0.4, 0.6)
+test_box2 = hoomd.Box(16, 25, 36, 0.1, 0.2, 0.3)
+scalar_variant1 = hoomd.variant.Ramp(0, 1, 100, 200)
+scalar_variant2 = hoomd.variant.Ramp(0, 1, 10, 30)
 
 valid_constructors = [
     (hoomd.variant.box.Constant, {
-        'box': _test_box1
+        'box': test_box1
     }),
     (hoomd.variant.box.Interpolate, {
-        'initial_box': _test_box1,
-        'final_box': _test_box2,
-        'variant': _test_scalar_variant1
+        'initial_box': test_box1,
+        'final_box': test_box2,
+        'variant': scalar_variant1
     }),
     (hoomd.variant.box.InverseVolumeRamp, {
-        'initial_box': _test_box2,
+        'initial_box': test_box2,
         'final_volume': 1000,
         't_start': 10,
         't_ramp': 50
@@ -37,15 +37,15 @@ valid_constructors = [
 # variant: dict(attr: [val1, val2,...])
 valid_attrs = [
     (hoomd.variant.box.Constant, {
-        'box': [_test_box1, _test_box2]
+        'box': [test_box1, test_box2]
     }),
     (hoomd.variant.box.Interpolate, {
-        'initial_box': [_test_box1, _test_box2],
-        'final_box': [_test_box2, _test_box1],
-        'variant': [_test_scalar_variant1, _test_scalar_variant2]
+        'initial_box': [test_box1, test_box2],
+        'final_box': [test_box2, test_box1],
+        'variant': [scalar_variant1, scalar_variant2]
     }),
     (hoomd.variant.box.InverseVolumeRamp, {
-        'initial_box': [_test_box1, _test_box2],
+        'initial_box': [test_box1, test_box2],
         'final_volume': [1000, 300],
         't_start': [0, 10],
         't_ramp': [10, 50, 100]
@@ -92,19 +92,19 @@ def test_custom():
     # test that the custom variant can be called from c++ and that it returns
     # the expected values
 
-    final_volume = _test_box1.volume * 2
-    test_box = hoomd.Box(_test_box1.Lx, _test_box1.Ly, _test_box1.Lz,
-                         _test_box1.xy, _test_box1.xz, _test_box1.yz)
-    custom_variant = VolumeRampBoxVariant(_test_box1, final_volume, 100, 100)
+    final_volume = test_box1.volume * 2
+    test_box = hoomd.Box(test_box1.Lx, test_box1.Ly, test_box1.Lz, test_box1.xy,
+                         test_box1.xz, test_box1.yz)
+    custom_variant = VolumeRampBoxVariant(test_box1, final_volume, 100, 100)
 
     def box_t(custom_variant, timestep):
         return hoomd._hoomd._test_vector_variant_box_call(
             custom_variant, timestep)
 
     for t, f in ((0, 0), (42, 0), (100, 0), (101, 0.01), (150, 0.5),
-                   (175, 0.75), (199, 0.99), (200, 1.0), (250, 1.0), (123456789,
-                                                                      1.0)):
-        test_box.volume = (1 - f) * _test_box1.volume + f * final_volume
+                 (175, 0.75), (199, 0.99), (200, 1.0), (250, 1.0), (123456789,
+                                                                    1.0)):
+        test_box.volume = (1 - f) * test_box1.volume + f * final_volume
         npt.assert_allclose(box_t(custom_variant, t), box_to_array(test_box))
 
 
@@ -112,33 +112,33 @@ def test_interpolate_evaluation():
     t_start = 50
     t_ramp = 100
     scalar_variant = hoomd.variant.Ramp(0, 1, t_start, t_ramp)
-    box_variant = hoomd.variant.box.Interpolate(_test_box1, _test_box2,
+    box_variant = hoomd.variant.box.Interpolate(test_box1, test_box2,
                                                 scalar_variant)
-    npt.assert_allclose(box_variant(0), box_to_array(_test_box1))
-    npt.assert_allclose(box_variant(25), box_to_array(_test_box1))
-    npt.assert_allclose(box_variant(t_start), box_to_array(_test_box1))
+    npt.assert_allclose(box_variant(0), box_to_array(test_box1))
+    npt.assert_allclose(box_variant(25), box_to_array(test_box1))
+    npt.assert_allclose(box_variant(t_start), box_to_array(test_box1))
 
     npt.assert_allclose(
         box_variant(51),
-        0.99 * box_to_array(_test_box1) + 0.01 * box_to_array(_test_box2))
+        0.99 * box_to_array(test_box1) + 0.01 * box_to_array(test_box2))
     npt.assert_allclose(
         box_variant(75),
-        0.75 * box_to_array(_test_box1) + 0.25 * box_to_array(_test_box2))
+        0.75 * box_to_array(test_box1) + 0.25 * box_to_array(test_box2))
     npt.assert_allclose(
         box_variant(100),
-        0.5 * box_to_array(_test_box1) + 0.5 * box_to_array(_test_box2))
+        0.5 * box_to_array(test_box1) + 0.5 * box_to_array(test_box2))
     npt.assert_allclose(
         box_variant(125),
-        0.25 * box_to_array(_test_box1) + 0.75 * box_to_array(_test_box2))
+        0.25 * box_to_array(test_box1) + 0.75 * box_to_array(test_box2))
     npt.assert_allclose(
         box_variant(149),
-        0.01 * box_to_array(_test_box1) + 0.99 * box_to_array(_test_box2))
+        0.01 * box_to_array(test_box1) + 0.99 * box_to_array(test_box2))
 
-    npt.assert_allclose(box_variant(t_start + t_ramp), box_to_array(_test_box2))
+    npt.assert_allclose(box_variant(t_start + t_ramp), box_to_array(test_box2))
     npt.assert_allclose(box_variant(t_start + t_ramp + 100),
-                        box_to_array(_test_box2))
+                        box_to_array(test_box2))
     npt.assert_allclose(box_variant(t_start + t_ramp + 1000000),
-                        box_to_array(_test_box2))
+                        box_to_array(test_box2))
 
 
 def test_inverse_volume_ramp_evaluation():
@@ -165,5 +165,5 @@ def test_inverse_volume_ramp_evaluation():
     npt.assert_allclose(get_volume(variant, 110), final_volume)
     npt.assert_allclose(get_volume(variant, 1010), final_volume)
     # make sure tilts don't change
-    for step in (0, 5, 252 125, 625):
+    for step in (0, 5, 252, 125, 625):
         npt.assert_allclose(box1.tilts, variant(step)[3:])
