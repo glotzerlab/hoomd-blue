681045b217cadcaf5aa1efa6578dabcef0a56273
Melody Zhang <yiyuanmz@umich.edu>
2024-03-01 20:02:41 -0500

    complete unit test

--- hoomd/hpmc/pytest/test_pair_angular_step.py
+++ hoomd/hpmc/pytest/test_pair_angular_step.py
@@ -6,7 +6,8 @@
 import copy
 import pytest
 import numpy as np
-from numpy import npt
+import rowan 
+import numpy.testing as npt
 
 from hoomd import hpmc
 from hoomd.error import TypeConversionError, IncompleteSpecificationError
@@ -50,19 +51,15 @@ def pair_angular_step_simulation_factory(simulation_factory,
                                   two_particle_snapshot_factory):
     """Make two particle sphere simulations with an angular step potential."""
 
-    def make_angular_step_sim(angular_step_potential, 
-                              particle_types=['A'], d=1, L=20, theta_0=0, theta_1=0):
-        snapshot = two_particle_snapshot_factory(particle_types, d=d, L=L)
+    def make_angular_step_sim(d=1, theta_0=[1,0,0,0], theta_1=[1,0,0,0]):
+        snapshot = two_particle_snapshot_factory(d=d)
         if snapshot.communicator.rank == 0:
-            snapshot.particle.orientation[0] = [//TO DO]
-            snapshot.particle.orientation[1] = [//TO DO]
-        
+            snapshot.particle.orientation[0] = theta_0
+            snapshot.particle.orientation[1] = theta_1
+        sim = simulation_factory(snapshot)
 
-        sim = simulation_factory(
-            two_particle_snapshot_factory(particle_types, d=d, L=L))
         sphere = hpmc.integrate.Sphere()
-        sphere.shape["A"] = dict(diameter=2.0)
-        sphere.pair_potentials = [angular_step_potential]
+        sphere.shape["A"] = dict(diameter=0.0)
         sim.operations.integrator = sphere
         return sim
 
@@ -122,7 +119,7 @@ def test_get_set_patch_params(pair_angular_step_simulation_factory,
                               angular_step_potential):
     """Testing getting/setting in multiple ways, before and after attaching."""
     # before attaching, setting as dict
-    particle_dict = dict(directors=[(0, 0, 1)], deltas=[0.1])
+    particle_dict = dict(directors=[(1.0, 0, 0)], deltas=[0.1])
     angular_step_potential.patch["A"] = particle_dict
     assert angular_step_potential.patch["A"]["directors"] 
            == particle_dict["directors"]
@@ -145,9 +142,9 @@ def test_get_set_patch_params(pair_angular_step_simulation_factory,
     assert angular_step_potential.patch["A"]["deltas"] == None
 
     # after attaching, setting deltas only
-    angular_step_potential.patch["A"]["deltas"] = [0.1, 0.2]
+    angular_step_potential.patch["A"]["deltas"] = [0.2]
     assert angular_step_potential.patch["A"]["directors"] == None
-    assert angular_step_potential.patch["A"]["deltas"] == [0.1, 0.2]
+    assert angular_step_potential.patch["A"]["deltas"] == [0.2]
 
 @pytest.mark.cpu
 def test_detach(pair_angular_step_simulation_factory, angular_step_potential):
@@ -163,33 +160,89 @@ def test_detach(pair_angular_step_simulation_factory, angular_step_potential):
     assert not angular_step_potential._attached
     assert not angular_step_potential.isotropic_potential._attached
 
+
+def lj(r, r_cut, epsilon, sigma):
+    """Compute the lj energy."""
+    return 4 * epsilon * ((sigma / r)**12 - (sigma / r)**6)
+
+# (patch dict,
+#  theta_0, # orientation of the first particle
+#  theta_1,
+#  distance between particles,
+#  expected energy)
+angular_step_test_parameters = [
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        [0,0,0,1],
+        5.0, # >rcut
+        0.0,
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        [1,0,0,0],
+        5.0, # > rcut
+        0.0,
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        [0,0,0,1],
+        2.0, # < rcut
+        lj(2.0, 4.0, 1, 1),
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        [1,0,0,0],
+        2.0, # < rcut
+        0.0,
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        rowan.vector_vector_rotation(np.array([1,0,0]), 
+                                     np.array([-np.cos(0.1), np.sin(0.1), 0]))
+        5.0, # < rcut
+        0.0,
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        rowan.vector_vector_rotation(np.array([1,0,0]), 
+                                     np.array([-np.cos(0.1), np.sin(0.1), 0]))
+        2.0, # < rcut
+        0.0,
+    ),
+    (
+        dict(directors=[(1.0, 0, 0)], deltas=[0.1]),
+        [1,0,0,0],
+        rowan.vector_vector_rotation(np.array([1,0,0]), 
+                                     np.array([-cos(0.05), sin(0.05), 0]))
+        2.0, # < rcut
+        lj(2.0, 4.0, 1, 1),
+    ),
+]
+
+
+@pytest.mark.parametrize('params, theta_0, theta_1, d, expected_energy',
+                         angular_step_test_parameters)
 @pytest.mark.cpu
-def test_energy(pair_angular_step_simulation_factory, 
-                angular_step_potential):
-    """Test the energy is being calculated right in a small system."""
-    # set up system
+def test_energy(pair_angular_step_simulation_factory, params, theta_0, theta_1,
+                d, expected_energy):
+    """Test that LennardJones computes the correct energies for 1 pair."""
+    lennard_jones = hoomd.hpmc.pair.LennardJones(mode='none')
+    lennard_jones.params[('A', 'A')] = dict(epsilon=1.0, sigma=1.0, r_cut=4.0)
+    angular_step = hoomd.hpmc.pair.angular_step(
+        isotropic_potential=lennard_jones)
+    angular_step.patch['A'] = params
 
-    lj = angular_step_potential.isotropic_potential
-    lj.params[("B", "B")] = dict(epsilon=3.0, sigma=1.0, r_cut=4.0)
-    lj.params[("A", "B")] = dict(epsilon=2.0, sigma=1.0, r_cut=4.0)
-    lj.params[("A", "A")] = dict(epsilon=1.0, sigma=1.0, r_cut=4.0)
-    angular_step_potential.patch["A"] = dict(
-                                directors=[(1.0, 0, 0), (0, 1.0, 0)], 
-                                deltas=[0.1, 0.2])
-    angular_step_potential.patch["B"] = dict(directors=[(1.0, 0, 0), 
-                                                  (0, 1.0, 0), (0, 0, 1.0)], 
-                                     deltas=[0.1, 0.2, 0.3])
-    sim = pair_angular_step_simulation_factory(angular_step_potential,
-                                        particle_types=['A', 'B'],
-                                        d=3,
-                                        L=30)
-    sim.operations.integrator.shape["B"] = dict(diameter=0, orientable=True)
-    sim.run(0)
+    simulation = pair_angular_step_simulation_factory(d=d, theta_0=theta_0, 
+                                       theta_1=theta_1)
+    simulation.operations.integrator.pair_potentials = [angular_step]
+    simulation.run(0)
 
-    def lj_energy(epsilon, sigma, distance):
-        sdivd = sigma / distance
-        return 4 * epsilon * (sdivd**12 - sdivd**6)
+    assert angular_step.energy == pytest.approx(expected=expected_energy,
+                                                 rel=1e-5)
 
-    system_energy = lj_energy(1.0, 1.0, 3.0) + lj_energy(
-        3.0, 1.0, 3.0) + lj_energy(2.0, 1.0, 1.0)
-    npt.assert_allclose(system_energy, angular_step_potential.energy)
