749345db95aaea102daeaa1ef13a188b1160e658
Tim Moore <mtimc@umich.edu>
2024-03-23 20:41:54 -0400

    Remove unused type

--- sphinx-doc/howto/tune-mc-move-sizes-binary-system.py
+++ sphinx-doc/howto/tune-mc-move-sizes-binary-system.py
@@ -6,7 +6,6 @@ simulation.state.replicate(nx=3, ny=3, nz=3)
 mc = hoomd.hpmc.integrate.Ellipsoid()
 mc.shape['A'] = dict(a=1.0, b=1.0, c=0.25)
 mc.shape['B'] = dict(a=1.0, b=1.0, c=0.5)
-mc.shape['C'] = dict(a=1.0, b=1.0, c=1.0)
 simulation.operations.integrator = mc
 
 # loop over particle types and set ignore_statistics = True
