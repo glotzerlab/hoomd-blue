960b595229921a91bf813914f714b871803ff5f5
Tim Moore <mtimc@umich.edu>
2024-09-10 00:10:19 -0400

    Add random number identifier

--- hoomd/RNGIdentifiers.h
+++ hoomd/RNGIdentifiers.h
@@ -70,6 +70,7 @@ struct RNGIdentifier
     static const uint8_t BussiThermostat = 45;
     static const uint8_t ConstantPressure = 46;
     static const uint8_t MPCDCellList = 47;
+    static const uint8_t UpdaterVMMC=48;
     };
 
     } // namespace hoomd
