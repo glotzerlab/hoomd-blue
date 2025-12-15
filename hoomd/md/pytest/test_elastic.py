a962f32c8222f612d643f2399703d9ee9309554a
Kate Jensen <kmjens@umich.edu>
2025-12-15 13:00:05 -0500

    add pytests for elastic PBCs and  mroe

--- hoomd/md/pytest/test_elastic.py
+++ hoomd/md/pytest/test_elastic.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2025 The Regents of the University of Michigan.
+#Copyright (c) 2009-2025 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 import hoomd
@@ -7,6 +7,8 @@ import pytest
 import numpy
 from hoomd.snapshot import Snapshot
 from pathlib import Path
+import gsd.hoomd
+import gsd
 
 
 def test_elastic(device, simulation_factory):
@@ -31,8 +33,8 @@ def test_elastic(device, simulation_factory):
     sim = simulation_factory(snapshot)
 
     elastic = hoomd.md.mesh.Elastic(mesh, reference_tags)
-    elastic.params["A"] = {"C_xxxx": 1.0, "C_xxyy": 1.0, "C_xyxy": 1.0}
-    elastic.params["B"] = {"C_xxxx": 2.0, "C_xxyy": 2.0, "C_xyxy": 2.0}
+    elastic.params["A"] = {"C_xxxx": 1.0, "C_xxyy": 0.5, "C_xyxy": 0.25}
+    elastic.params["B"] = {"C_xxxx": 2.0, "C_xxyy": 1.0, "C_xyxy": 0.5}
 
     integrator = hoomd.md.Integrator(dt=0.005)
     integrator.forces.append(elastic)
@@ -115,3 +117,105 @@ def test_sphere_stretch(device, simulation_factory):
     sim.run(0)
 
     elastic.energy > 0
+
+# test across periodic boundaries
+def test_periodic_boundaries(device, simulation_factory):
+    reference_positions = numpy.array(
+        [[0., 0, 0.5/(numpy.sqrt(3))], [1, 1/2, 0], [1, -1/2, 0], [1, 0, 1]]
+    )
+    box_L = 10
+    stretch = 1
+    positions0 = reference_positions.copy()
+    positions0[0] -= numpy.array([stretch,0.0,0.0])
+
+    positions1 = positions0.copy() - numpy.array([1.1*box_L/2,0,0])
+    positions1[0] += numpy.array([box_L,0.0,0.0])
+
+    positions2 = positions1.copy() + numpy.array([0,0,box_L/2])
+    positions2[3] -= numpy.array([0.0, 0.0, box_L])
+    positions2[0] -= numpy.array([0.0, 0.0, box_L])
+
+    tetrahedra = numpy.array([[0, 1, 2, 3],[4, 5, 6, 7],[8,9,10,11]]) 
+    types = ["A"]
+    type_ids = [0]
+    reference_tags = [[0, 1, 2, 3], [4, 5, 6, 7], [8,9,10,11]]
+
+    # For comparison
+    mesh0 = Mesh3D(reference_positions, tetrahedra[0], types, type_ids)
+    # Across one boundary:
+    mesh1 = Mesh3D(reference_positions, tetrahedra[1], types, type_ids)
+    # Across edge boundary:
+    mesh2 = Mesh3D(reference_positions, tetrahedra[2], types, type_ids)
+
+    snapshot = Snapshot(device.communicator)
+    if snapshot.communicator.rank == 0:
+        snapshot.particles.N = len(positions0)*3
+        snapshot.particles.position[:] = list(positions0) + list(positions1) + list(positions2)
+        snapshot.particles.types = types
+        snapshot.configuration.box = [box_L, box_L, box_L, 0, 0, 0]
+    sim = simulation_factory(snapshot)
+
+    elastic0 = hoomd.md.mesh.Elastic(mesh0, reference_tags[0])
+    elastic1 = hoomd.md.mesh.Elastic(mesh1, reference_tags[1])
+    elastic2 = hoomd.md.mesh.Elastic(mesh2, reference_tags[2])
+    elastic0.params["A"] = {"C_xxxx": 0.1, "C_xxyy": 0.05, "C_xyxy": 0.025}
+    elastic1.params["A"] = {"C_xxxx": 0.1, "C_xxyy": 0.05, "C_xyxy": 0.025}
+    elastic2.params["A"] = {"C_xxxx": 0.1, "C_xxyy": 0.05, "C_xyxy": 0.025}
+    
+    integrator = hoomd.md.Integrator(dt=0.005)
+    integrator.forces.extend([elastic0, elastic1, elastic2])
+
+    filter_all = hoomd.filter.All() 
+    nve = hoomd.md.methods.ConstantVolume(filter=filter_all)
+    integrator.methods.append(nve)
+    sim.operations.integrator = integrator
+    
+    sim.run(200)
+    
+    assert elastic0.energy == pytest.approx(elastic1.energy, abs=1e-4) and elastic0.energy == pytest.approx(elastic2.energy, abs=1e-4)
+
+# particles not in mesh
+def test_other_particles_and_potentials(device, simulation_factory):
+    reference_positions = numpy.array(
+        [[0., 0, 0.5/(numpy.sqrt(3))], [1, 1/2, 0], [1, -1/2, 0], [1, 0, 1]]
+    )
+    p_pos = [-1, 0, 0.5/(numpy.sqrt(3))]
+    positions = reference_positions.copy()
+    tetrahedra = numpy.array([0, 1, 2, 3])
+    mesh_reference_tags = [0, 1, 2, 3]
+
+    mesh = Mesh3D(reference_positions, tetrahedra, ["mesh"], [0])
+
+    snapshot = Snapshot(device.communicator)
+    if snapshot.communicator.rank == 0:
+        snapshot.particles.N = len(positions) + 1
+        snapshot.particles.position[:] =numpy.vstack((positions,p_pos))
+        snapshot.particles.types = ["mesh","particle"]
+        snapshot.particles.typeid[:] = [0]*4 +[1]
+        snapshot.configuration.box = [10, 10, 10, 0, 0, 0]
+    sim = simulation_factory(snapshot)
+
+    elastic = hoomd.md.mesh.Elastic(mesh, mesh_reference_tags)
+    elastic.params["mesh"] = {"C_xxxx": 1.0, "C_xxyy": 0.5, "C_xyxy": 0.25}
+
+    cell = hoomd.md.nlist.Cell(buffer=0.5)
+    LJ = hoomd.md.pair.LJ(nlist=cell,default_r_cut=0)
+    LJ.params.default = dict(epsilon=0, sigma=1)
+    LJ.params[('mesh','particle')] = dict(epsilon=1, sigma=1)
+    LJ.r_cut[('mesh','particle')] = 3
+
+    integrator = hoomd.md.Integrator(dt=0.005)
+    integrator.forces.extend([elastic,LJ])
+
+    sim.operations.integrator = integrator
+    filter_all = hoomd.filter.All() 
+    nve = hoomd.md.methods.ConstantVolume(filter=filter_all)
+    integrator.methods.append(nve)
+    sim.operations.integrator = integrator
+    
+
+    sim.run(500)
+
+    assert elastic.energy > 0 #pytest.approx(0, abs=1e-4)
+
+
